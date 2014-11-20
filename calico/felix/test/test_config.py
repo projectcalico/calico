b7d06e4362949b786ad06c606d95ce648de91e9b
Peter White <peter.white@metaswitch.com>
2014-11-20 13:17:08 +0000

    Initial test infrastructure - no real tests yet.

---
+++ calico/felix/test/test_config.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014 Metaswitch Networks
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+felix.test.test_felix
+~~~~~~~~~~~
+
+Top level tests for Felix.
+"""
+import unittest
+# TODO: Importing config breaks things, so for now do nothing.
+#import calico.felix.config
+
+class TestConfig(unittest.TestCase):
+    def test_basic1(self):
+        self.assertEquals(True, True)
+
+def suite():
+    suite_config = unittest.TestLoader().loadTestsFromTestCase(TestConfig)
+    return unittest.TestSuite([suite_config])
+
+def run_tests():
+    result = unittest.TextTestRunner(verbosity=2).run(suite())
+    if result.errors or result.failures:
+        return 1
+    return 0
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ calico/felix/test/test_felix.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014 Metaswitch Networks
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+felix.test.test_felix
+~~~~~~~~~~~
+
+Top level tests for Felix.
+"""
+import unittest
+
+class TestBasic(unittest.TestCase):
+    def test_basic1(self):
+        self.assertEquals(True, True)
+
+def suite():
+    suite_basic = unittest.TestLoader().loadTestsFromTestCase(TestBasic)
+    return unittest.TestSuite([suite_basic])
+
+def run_tests():
+    result = unittest.TextTestRunner(verbosity=2).run(suite())
+    if result.errors or result.failures:
+        return 1
+    return 0
+
+if __name__ == "__main__":
+    unittest.main()
