fd24718439cbce615db4caf609279ea21e248c12
Cory Benfield <cory.benfield@metaswitch.com>
2014-12-05 13:10:31 -0800

    Add common logging setup functions.

--- calico/common.py
+++ calico/common.py
@@ -21,7 +21,9 @@ calico.common
 
 Calico common utilities.
 """
+import logging
 import os
+import sys
 import errno
 
 AGENT_TYPE_CALICO = 'Calico agent'
@@ -37,3 +39,77 @@ def mkdir_p(path):
             if exc.errno == errno.EEXIST and os.path.isdir(path):
                 pass
             else: raise
+
+
+def default_logging():
+    """
+    Sets up the Calico default logging, with default severities.
+
+    Our default logging consists of:
+
+    - setting the log level of the root logger to DEBUG (a safe initial value)
+    - attaching a SysLog handler with no formatter (log to syslog), ERROR level
+      only
+    - attaching a StreamHandler with the Calico formatter, to log to stdout,
+      with DEBUG level
+
+    This default explicitly excludes adding a log to file. This is because
+    working out what file to log to requires reading the configuration file,
+    and doing that may cause errors that we want to log! To add a file logger,
+    call :meth:`complete_logging() <calico.common.complete_logging>` after
+    this function has been called.
+    """
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+
+    syslog_handler = logging.handlers.SysLogHandler()
+    syslog_handler.setLevel(logging.ERROR)
+    root_logger.addHandler(syslog_handler)
+
+    formatter = logging.Formatter(
+        '%(asctime)s [%(levelname)s] %(name)s %(lineno)d: %(message)s'
+    )
+
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setFormatter(formatter)
+    root_logger.addHandler(stream_handler)
+
+
+def complete_logging(logfile=None,
+                     file_level=logging.DEBUG,
+                     syslog_level=logging.ERROR,
+                     stream_level=logging.DEBUG):
+    """
+    Updates the logging configuration based on learned configuration.
+
+    The purpose of this function is to update the previously set logging
+    configuration such that we can start logging to file. This is done in a
+    separate step to the initial logging configuration in order to ensure that
+    logging is available as early in execution as possible, i.e. before the
+    config file has been parsed.
+    """
+    root_logger = logging.getLogger()
+
+    # If default_logging got called already, we'll have some loggers in place.
+    # Update their levels.
+    for handler in root_logger.handlers:
+        if isinstance(handler, logging.handlers.SysLogHandler):
+            handler.setLevel(syslog_level)
+        elif isinstance(handler, logging.StreamHandler):
+            handler.setLevel(stream_level)
+
+    # If we've been given a log file, log to file as well.
+    if logfile is not None:
+        mkdir_p(os.path.dirname(logfile))
+
+        formatter = logging.Formatter(
+            '%(asctime)s [%(levelname)s] %(name)s %(lineno)d: %(message)s'
+        )
+
+        file_handler = logging.handlers.TimedRotatingFileHandler(
+            logfile, when="D", backupCount=10
+        )
+        file_handler.setLevel(file_level)
+        file_handler.setFormatter(formatter)
+        root_logger.addHandler(file_handler)
