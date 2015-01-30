d155e91433cc09ab416612719fd63d251dcfa3c0
Neil Jerram <neil@ossau.homelinux.net>
2015-01-30 13:33:13 +0000

    Discard the stub version of zmq imported by previous unit tests (fix)

--- calico/openstack/test/test_plugin.py
+++ calico/openstack/test/test_plugin.py
@@ -30,7 +30,7 @@ import traceback
 import json
 import inspect
 
-del sys.modules['zmq']
+if 'zmq' in sys.modules: del sys.modules['zmq']
 sys.modules['neutron'] = m_neutron = mock.Mock()
 sys.modules['neutron.common'] = m_neutron.common
 sys.modules['neutron.openstack'] = m_neutron.openstack
