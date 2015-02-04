a0830adf5f9571fd79221eba0e8fa06079dc8cb3
Neil Jerram <Neil.Jerram@metaswitch.com>
2015-02-04 12:33:30 +0000

    mech_calico: Bind as directed by Neutron server's bind_host config

--- calico/openstack/mech_calico.py
+++ calico/openstack/mech_calico.py
@@ -27,6 +27,7 @@
 #* It is implemented as a Neutron/ML2 mechanism driver.                      *#
 #*****************************************************************************#
 
+from oslo.config import cfg
 from neutron.common import constants
 from neutron.openstack.common import log
 from neutron.plugins.ml2 import driver_api as api
@@ -169,23 +170,28 @@ class CalicoMechanismDriver(mech_agent.SimpleAgentMechanismDriverBase):
         self.zmq_context = zmq.Context()
         LOG.warn("pyzmq version is %s" % zmq.pyzmq_version())
 
+        bind_address = cfg.CONF.bind_host or '*'
+
         #*********************************************************************#
         #* Create ROUTER socket for Felix instances to connect to.           *#
         #*********************************************************************#
         self.felix_router_socket = self.zmq_context.socket(zmq.ROUTER)
-        self.felix_router_socket.bind("tcp://*:%s" % PLUGIN_ENDPOINT_PORT)
+        self.felix_router_socket.bind("tcp://%s:%s" % (bind_address,
+                                                       PLUGIN_ENDPOINT_PORT))
 
         #*********************************************************************#
         #* Create ROUTER socket for ACL Manager(s) to connect to.            *#
         #*********************************************************************#
         self.acl_get_socket = self.zmq_context.socket(zmq.ROUTER)
-        self.acl_get_socket.bind("tcp://*:%s" % PLUGIN_ACLGET_PORT)
+        self.acl_get_socket.bind("tcp://%s:%s" % (bind_address,
+                                                  PLUGIN_ACLGET_PORT))
 
         #*********************************************************************#
         #* Create PUB socket for sending ACL updates to ACL Manager(s).      *#
         #*********************************************************************#
         self.acl_pub_socket = self.zmq_context.socket(zmq.PUB)
-        self.acl_pub_socket.bind("tcp://*:%s" % PLUGIN_ACLPUB_PORT)
+        self.acl_pub_socket.bind("tcp://%s:%s" % (bind_address,
+                                                  PLUGIN_ACLPUB_PORT))
         eventlet.spawn_n(self.acl_heartbeat_thread)
 
         #*********************************************************************#
