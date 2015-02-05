cd294d9f61235b5579f652b88405cd4df6678446
Tom Denham <tom.denham@metaswitch.com>
2015-02-04 17:08:33 -0800

    Get basic calico.py commands working
    node/master/status should work now

--- calico.py
+++ calico.py
@@ -3,91 +3,82 @@
 
 Usage:
   calico master --ip=<IP>
-  calico node
+  calico node --ip=<IP>
   calico assignacl <CONTAINER_ID>
   calico status
-  calico reset [--delete-images]
+  calico reset
   calico version
 
 
 Options:
- --ip=<IP>    The address that other nodes can use to contact the master.
+ --ip=<IP>    The local management address to use.
 """
 #Useful docker aliases
 # alias docker_kill_all='sudo docker kill $(docker ps -q)'
 # alias docker_rm_all='sudo docker rm -v `docker ps -a -q -f status=exited`'
 
-from subprocess import call, check_output, check_call, CalledProcessError
-from string import Template
-import socket
+from subprocess import call, check_output, CalledProcessError
 import os
 from docopt import docopt
-import shlex
-import sh
 from sh import mkdir
 from sh import docker
 from sh import modprobe
-from sh import rm
-import requests
+from sh import grep
 import etcd
 import sys
 
-docker_run = docker.bake("run", "-d", "--net=none")
 mkdir_p = mkdir.bake('-p')
 
-HOSTNAME = socket.gethostname()
 client = etcd.Client()
-
-PLUGIN_TEMPLATE = Template("""[felix $name]
-ip=$ip
-host=$name
-
-$peers
-""")
-
 
 def validate_arguments(arguments):
     # print(arguments)
     return True
 
-def configure_master_components(peers):
-    peer_config = ""
-
-    for peer in peers:
-        peer_config += "[felix {name}]\nip={address}\nhost={name}\n\n".format(name=peer,
-                                                                         address=peer)
-
-    # We need our name and address and name and address for all peers
-    plugin_config = PLUGIN_TEMPLATE.substitute(ip=HOSTNAME, name=HOSTNAME, peers=peer_config)
-    with open('config/data/felix.txt', 'w') as f:
-        f.write(plugin_config)
-
-    # ACL manager just need address of plugin manager
-    aclmanager_config = ACL_TEMPLATE.substitute(ip=HOSTNAME)
-    with open('config/acl_manager.cfg', 'w') as f:
-        f.write(aclmanager_config)
-
 def create_dirs():
-    mkdir_p("config/data")
     mkdir_p("/var/log/calico")
-
 
 def process_output(line):
     sys.stdout.write(line)
 
-def launch(master, peers):
+def node(ip):
     create_dirs()
     modprobe("ip6_tables")
     modprobe("xt_set")
 
-    # Assume that the image is already built - called calico_node
-    docker("run", "-d", "calico/node")
+    cid = docker("run", "-e",  "IP=%s" % ip, "--name=calico-node", "--privileged",
+                                                                "--net=host", "-d", "calico/node")
+    print "Calico node is running with id: %s" % cid
 
+def master(ip):
+    create_dirs()
+
+    # update the master IP
+    client.write('/calico/master/ip', ip)
+
+    # Start the container
+    cid = docker("run", "--name=calico-master", "--privileged", "--net=host", "-d",
+           "calico/master")
+    print "Calico master is running with id: %s" % cid
 
 def status():
-    print(docker("ps"))
+    try:
+        print(grep(docker("ps"), "-i", "calico"))
+    except Exception:
+        print "No calico containers appear to be running"
 
-def reset(delete_images):
+    #If bird is running, then print bird.
+    try:
+        pass
+    except Exception:
+        print "Couldn't collect BGP Peer information"
+
+    print(docker("exec", "calico-node", "/bin/bash",  "-c", "echo show protocols | birdc -s "
+                                                                "/etc/service/bird/bird.ctl"))
+
+
+
+def reset():
     try:
         interfaces_raw = check_output("ip link show | grep -Eo ' (tap(.*?)):' |grep -Eo '[^ :]+'", shell=True)
         print "Removing interfaces:\n%s" % interfaces_raw
@@ -104,12 +95,6 @@ def version():
     print "Unknown"
 
 
-def master(peers):
-    create_dirs()
-    configure_master_components(peers)
-    p = fig_master(l"up", "-d", _err=process_output, _out=process_output)
-    p.wait()
-
 if __name__ == '__main__':
     if os.geteuid() != 0:
         print "Calico must be run as root"
@@ -119,7 +104,7 @@ if __name__ == '__main__':
             if arguments["master"]:
                 master(arguments["--ip"])
             if arguments["node"]:
-                launch()
+                node(arguments["--ip"])
             if arguments["status"]:
                 status()
             if arguments["reset"]:
