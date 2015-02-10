9eb24da45cca8a94f4e158a8c668af985db3b6dd
Spike Curtis <spike.curtis@metaswitch.com>
2015-02-10 13:17:48 -0800

    Fixup copyright statements.

--- node/adapter/powerstrip-calico.py
+++ node/adapter/powerstrip-calico.py
@@ -1,6 +1,17 @@
-# Inspired by https://github.com/ClusterHQ/powerstrip-slowreq
-# Copyright Metaswitch Networks 2015  See LICENSE file for details.
-
+# Copyright (c) 2015 Metaswitch Networks
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
 from twisted.internet import reactor
 from twisted.web import server, resource
@@ -19,6 +30,7 @@ ENV_IP = "CALICO_IP"
 ENV_GROUP = "CALICO_GROUP"
 
 hostname = socket.gethostname()
+
 
 def setup_logging(logfile):
     _log.setLevel(logging.DEBUG)
@@ -128,6 +140,7 @@ class AdapterResource(resource.Resource):
             _log.warning("Key error %s, request: %s", e, client_request)
 
         return
+
 
 def _client_request_net_none(client_request):
     """
