d321e57120569b63c073ea383da453c7cf95c2fc
Tom Denham <tom@tomdee.co.uk>
2015-02-10 18:07:51 -0800

    Fix version of demo used by this vagrant file

--- provision.sh
+++ provision.sh
@@ -4,6 +4,9 @@ set -e
 set -x
 
 git clone https://github.com/metaswitch/calico-docker.git
+cd  calico-docker
+git checkout ad6ec3bdb23d08faf15050b3e3467a494208c8db
+cd ..
 mv calico-docker/* -t .
 
 echo "172.17.8.101 core-01" >> /etc/hosts
