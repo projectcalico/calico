625965b0e158f54df739738a1a875d0a462d8d30
Tom Denham <tom.denham@metaswitch.com>
2015-03-12 09:59:05 -0700

    Don't remove containers during build as circleci doesn't support it

--- create_binary.sh
+++ create_binary.sh
@@ -5,8 +5,8 @@ set -e
 mkdir -p `pwd`/dist
 chmod 777 `pwd`/dist
 docker build -t calico-build .
-docker run -u user -v `pwd`/dist:/code/dist --rm calico-build pyinstaller calicoctl.py -a -F -s --clean
-docker run -u user -v `pwd`/dist:/code/dist --rm calico-build bash -c 'docopt-completion --manual-bash dist/calicoctl && mv calicoctl.sh dist'
+docker run -u user -v `pwd`/dist:/code/dist calico-build pyinstaller calicoctl.py -a -F -s --clean
+docker run -u user -v `pwd`/dist:/code/dist calico-build bash -c 'docopt-completion --manual-bash dist/calicoctl && mv calicoctl.sh dist'
 
 echo "Build output is in dist/"
 echo "Copy dist/calicoctl.sh to /etc/bash_completion.d/ to get bash completion"
