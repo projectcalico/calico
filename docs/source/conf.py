943b0d312c4f12a5fb38d1b66616ac64756b0f8f
Neil Jerram <Neil.Jerram@metaswitch.com>
2015-03-16 16:31:01 +0000

    Docs: correct code to build using sphinx_rtd_theme, both locally and on the RTD website
    (as advised by
    http://docs.readthedocs.org/en/latest/theme.html#how-do-i-use-this-locally-and-on-read-the-docs)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -258,3 +258,13 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
