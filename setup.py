cdd8c60e43215f05f6a17334547c2a31b5d8c245
Matt Dupre <matthew.dupre@metaswitch.com>
2015-04-20 18:13:12 +0100

    Version 0.16

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ def collect_requirements():
 
 setuptools.setup(
     name = "calico",
-    version = "0.15",
+    version = "0.16",
     packages = setuptools.find_packages(),
     entry_points = {
         'console_scripts': [
