e4e93fd702231da768e9b2f2abe3481fc108c1f2
Robert Brockbank <rob.brockbank@metswitch.com>
2015-05-01 15:50:59 -0700

    Add versioning to calico naming

--- node/adapter/ipam.py
+++ node/adapter/ipam.py
@@ -1,8 +1,9 @@
 from etcd import EtcdKeyNotFound, EtcdAlreadyExist
 from netaddr import IPAddress
 from datastore import DatastoreClient
+from datastore import CALICO_V_PATH
 
-IP_ASSIGNMENT_PATH = "/calico/ipam/%(version)s/assignment/%(pool)s"
+IP_ASSIGNMENT_PATH = CALICO_V_PATH + "/ipam/%(version)s/assignment/%(pool)s"
 IP_ASSIGNMENT_KEY = IP_ASSIGNMENT_PATH + "/%(address)s"
 
 
