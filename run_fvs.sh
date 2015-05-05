4ebdb464af14e63892a1317e31ba3a0d7c774b70
Alex Altair <Alex.Altair@metaswitch.com>
2015-05-04 21:03:16 -0700

    Move jenkins config into script.

---
+++ run_fvs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+set -e
+date
+pwd
+git status
+
+./build_node.sh
+pushd ./build_calicoctl
+./create_binary.sh
+popd
+
+# Run the FVs
+sudo ./tests/fv/mainline.sh
+sudo ./tests/fv/add_container.sh
+sudo ./tests/fv/unix_socket.sh
+sudo ./tests/fv/add_ip.sh
+
+echo "All tests have passed."
