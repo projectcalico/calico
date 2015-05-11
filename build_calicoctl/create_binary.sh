aa62f5eb26ff6642140703ff0c032e98739dce8e
Alex Altair <Alex.Altair@metaswitch.com>
2015-05-11 16:07:08 -0700

    Run bash as root but command as user.

--- build_calicoctl/create_binary.sh
+++ build_calicoctl/create_binary.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 set -e
+set -x
 
 docker build -t calico-build .
 
 cd ..
 mkdir -p `pwd`/dist
 chmod 777 `pwd`/dist
-docker run -u user -v `pwd`/:/code/calico -v `pwd`/dist:/code/dist calico-build pyinstaller calico/calicoctl.py -a -F -s --clean
-docker run -u user -v `pwd`/dist:/code/dist calico-build bash -c 'docopt-completion --manual-bash dist/calicoctl && mv calicoctl.sh dist'
+docker run -v `pwd`/:/code/calico -v `pwd`/dist:/code/dist calico-build \
+    bash -c 'su - user -c \
+        "cd /code && pyinstaller calico/calicoctl.py -a -F -s --clean"'
+docker run -v `pwd`/dist:/code/dist calico-build \
+    bash -c 'su - user -c \
+        "cd /code && dist/calicoctl --help && docopt-completion --manual-bash dist/calicoctl && mv calicoctl.sh dist"'
 
 echo "Build output is in dist/"
 echo "Copy dist/calicoctl.sh to /etc/bash_completion.d/ to get bash completion"
