ff23bae072241d579642cb34e05a47cd2c55b428
Tom Denham <tom.denham@metaswitch.com>
2015-05-12 12:44:17 -0700

    Gets tests and builds running in a container

--- build_calicoctl/create_binary.sh
+++ build_calicoctl/create_binary.sh
@@ -9,15 +9,15 @@ cd ..
 mkdir -p `pwd`/dist
 chmod 777 `pwd`/dist
 
-docker run --rm -v `pwd`/:/code/calico calico-build bash -c 'su - user -c \
-    "cd /code/calico && nosetests -c nose.cfg"'
+docker run --rm -v `pwd`/:/code calico-build bash -c '/tmp/etcd & nosetests -c nose.cfg'
 
-docker run --rm -v `pwd`/:/code/calico -v `pwd`/dist:/code/dist calico-build \
-    bash -c 'su - user -c \
-        "cd /code && pyinstaller calico/calicoctl.py -a -F -s --clean"'
-docker run --rm -v `pwd`/dist:/code/dist calico-build \
-    bash -c 'su - user -c \
-        "cd /code && dist/calicoctl --help && docopt-completion --manual-bash dist/calicoctl && mv calicoctl.sh dist"'
+docker run --rm -v `pwd`/:/code calico-build \
+ pyinstaller calicoctl.py -a -F -s --clean
+
+docker run --rm -v `pwd`/:/code calico-build \
+ docopt-completion --manual-bash dist/calicoctl
+
+mv calicoctl.sh dist
 
 echo "Build output is in dist/"
 echo "Copy dist/calicoctl.sh to /etc/bash_completion.d/ to get bash completion"
