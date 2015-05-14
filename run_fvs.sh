962401f0ed0c563796f9211ed1aa34b61866387f
Alex Altair <Alex.Altair@metaswitch.com>
2015-05-14 10:43:51 -0700

    Use jpetazzo/dind image to instantiate multiple hosts for FVs.

--- run_fvs.sh
+++ run_fvs.sh
@@ -6,17 +6,31 @@ date
 pwd
 git status
 
-./build_node.sh
-./create_binary.sh
+docker run --privileged -v `pwd`:/code --name host1 -tid jpetazzo/dind
+
+docker exec -ti host1 bash -c \
+ 'docker -d & \
+ cd /code && \
+ ./build_node.sh && \
+ ./create_binary.sh'
+# docker run --privileged -v `pwd`:/code --name host2 -tid jpetazzo/dind bash -c \
+#  'docker -d -D && \
+#  cd /code && \
+#  ./build_node.sh && \
+#  ./create_binary.sh'
+# docker run --privileged -v `pwd`:/code --name host3 -tid jpetazzo/dind bash -c \
+#  'docker -d -D && \
+#  cd /code && \
+#  ./build_node.sh && \
+#  ./create_binary.sh'
 
 # Run the FVs
-sudo ./tests/fv/mainline.sh
-sudo ./tests/fv/no_powerstrip.sh
-sudo ./tests/fv/arg_parsing.sh
-sudo ./tests/fv/add_container.sh
-sudo ./tests/fv/unix_socket.sh
-sudo ./tests/fv/add_ip.sh
-sudo ./tests/fv/profile_commands.sh
-sudo ./tests/fv/diags.sh
+docker exec -ti host1 bash -c 'cd /code && sudo ./tests/fv/mainline.sh'
+docker exec -ti host1 bash -c 'cd /code && sudo ./tests/fv/add_container.sh'
+docker exec -ti host1 bash -c 'cd /code && sudo ./tests/fv/add_ip.sh'
+docker exec -ti host1 bash -c 'cd /code && sudo ./tests/fv/arg_parsing.sh'
+docker exec -ti host1 bash -c 'cd /code && sudo ./tests/fv/profile_commands.sh'
+docker exec -ti host1 bash -c 'cd /code && sudo ./tests/fv/no_powerstrip.sh'
+docker exec -ti host1 bash -c 'cd /code && sudo ./tests/fv/diags.sh'
 
 echo "All tests have passed."
