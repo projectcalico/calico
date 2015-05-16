c87cb01bb09807e3606049a7a274f1062aca17f2
Alex Altair <Alex.Altair@metaswitch.com>
2015-05-15 17:30:02 -0700

    Use same cleanup afterwards.

--- run_fvs.sh
+++ run_fvs.sh
@@ -6,6 +6,9 @@ date
 pwd
 git status
 
+# We *must* remove all inner containers and images before removing the outer
+# container. Otherwise the inner images will stick around and fill disk.
+# https://github.com/jpetazzo/dind#important-warning-about-disk-usage
 docker exec -t host1 bash -c 'docker rm -f $(docker ps -qa) ; \
                               docker rmi $(docker images -qa)' || true
 docker rm -f host1 || true
@@ -27,7 +30,7 @@ docker exec -t host1 bash -c 'cd /code && sudo ./tests/fv/no_powerstrip.sh'
 docker exec -t host1 bash -c 'cd /code && sudo ./tests/fv/diags.sh'
 
 docker exec -t host1 bash -c 'docker rm -f $(docker ps -qa) ; \
- docker rmi $(docker images -qa)' ; \
- docker rm -f host1 || true
+                              docker rmi $(docker images -qa)' || true
+docker rm -f host1 || true
 
 echo "All tests have passed."
