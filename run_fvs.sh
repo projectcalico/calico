1924142197b4d96b112728298e97a979552db51b
Alex Altair <Alex.Altair@metaswitch.com>
2015-05-18 13:53:59 -0700

    Pull in calico/node image.

--- run_fvs.sh
+++ run_fvs.sh
@@ -13,11 +13,12 @@ docker exec -t host1 bash -c 'docker rm -f $(docker ps -qa) ; \
                               docker rmi $(docker images -qa)' || true
 docker rm -f host1 || true
 
+./build_node.sh
+docker save --output calico-node.tar calico/node
 docker pull busybox:latest
 docker save --output busybox.tar busybox:latest
-./build_node.sh
-docker pull jpetazzo/nsenter
-docker save --output nsenter.tar jpetazzo/nsenter
+docker pull jpetazzo/nsenter:latest
+docker save --output nsenter.tar jpetazzo/nsenter:latest
 docker images | grep quay.io/coreos/etcd || docker pull quay.io/coreos/etcd:v2.0.10
 docker save --output etcd.tar quay.io/coreos/etcd:v2.0.10
 
@@ -26,6 +27,7 @@ docker run --privileged -v `pwd`:/code --name host1 -tid jpetazzo/dind
 
 docker exec -t host1 bash -c \
  'while ! docker ps; do sleep 1; done && \
+ docker load --input /code/calico-node.tar && \
  docker load --input /code/busybox.tar && \
  docker load --input /code/nsenter.tar && \
  docker load --input /code/etcd.tar'
