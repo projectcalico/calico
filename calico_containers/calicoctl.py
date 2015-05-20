ca5fd1f1901f073966021bb2fd3791d9a8d3aa78
Paul Tiplady <paul.tiplady@gmail.com>
2015-05-20 14:39:22 -0700

    Fix allignment.

--- calico_containers/calicoctl.py
+++ calico_containers/calicoctl.py
@@ -61,9 +61,9 @@ from netaddr.core import AddrFormatError
 from prettytable import PrettyTable
 
 from calico_containers.adapter.datastore import (ETCD_AUTHORITY_ENV,
-                                             ETCD_AUTHORITY_DEFAULT,
-                                             Rules,
-                                             DataStoreError)
+                                                 ETCD_AUTHORITY_DEFAULT,
+                                                 Rules,
+                                                 DataStoreError)
 from calico_containers.adapter.docker_restart import REAL_SOCK, POWERSTRIP_SOCK
 from calico_containers.adapter.ipam import IPAMClient
 from calico_containers.adapter import netns, docker_restart
