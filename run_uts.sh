275287b986a327627b5449413f22fff4512cfb0a
Alex Altair <Alex.Altair@metaswitch.com>
2015-05-19 17:34:16 -0700

    Specify nose to run only the UTs.

--- run_uts.sh
+++ run_uts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -rm -v `pwd`/:/code calico-build bash -c '/tmp/etcd -data-dir=/tmp/default.etcd/ & nosetests -c nose.cfg'
+docker run -rm -v `pwd`/:/code calico-build bash -c '/tmp/etcd -data-dir=/tmp/default.etcd/ & nosetests tests/unit -c nose.cfg'
