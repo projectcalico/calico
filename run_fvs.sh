a2239f4d30d2dddbea8416cde2f17c779c319967
Alex Altair <Alex.Altair@metaswitch.com>
2015-05-21 10:22:11 -0700

    Reinstantiate host for old shell tests.

--- run_fvs.sh
+++ run_fvs.sh
@@ -10,6 +10,14 @@ nosetests calico_containers/tests/fv
 
 # Run the FVs. Need to run from the /code directory since the tests expect
 # to be run from the root of the codebase.
+docker run --privileged -v `pwd`:/code --name host1 -tid jpetazzo/dind
+docker exec -t host1 bash -c \
+ 'while ! docker ps; do sleep 1; done && \
+ docker load --input /code/calico-node.tar && \
+ docker load --input /code/busybox.tar && \
+ docker load --input /code/nsenter.tar && \
+ docker load --input /code/etcd.tar'
+
 docker exec -t host1 bash -c 'cd /code && sudo ./calico_containers/tests/fv/mainline.sh'
 docker exec -t host1 bash -c 'cd /code && sudo ./calico_containers/tests/fv/add_container.sh'
 docker exec -t host1 bash -c 'cd /code && sudo ./calico_containers/tests/fv/add_ip.sh'
