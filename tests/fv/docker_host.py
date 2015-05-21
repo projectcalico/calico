421f282f47463b56dad7a1930101f7f63a08df59
Alex Altair <Alex.Altair@metaswitch.com>
2015-05-21 09:15:26 -0700

    Add docstrings.

--- tests/fv/docker_host.py
+++ tests/fv/docker_host.py
@@ -6,43 +6,43 @@ class DockerHost(object):
     """
     A host container which will hold workload containers to be networked by calico.
     """
-    @classmethod
-    def delete_container(cls, name):
-        # We *must* remove all inner containers and images before removing the outer
-        # container. Otherwise the inner images will stick around and fill disk.
-        # https://github.com/jpetazzo/dind#important-warning-about-disk-usage
-        cls.cleanup_inside(name)
-        sh.docker.rm("-f", name, _ok_code=[0, 1])
-
-    @classmethod
-    def cleanup_inside(cls, name):
-        docker("exec", "-t", name, "bash", "-c",
-               "docker rm -f $(docker ps -qa) ; docker rmi $(docker images -qa)", _ok_code=[0, 1])
-
     def __init__(self, name):
+        """
+        Create a container using an image made for docker-in-docker. Load saved images into it.
+        """
         self.name = name
 
         pwd = sh.pwd().stdout.rstrip()
         docker.run("--privileged", "-v", pwd+":/code", "--name", self.name, "-tid", "jpetazzo/dind")
-        print "Host container created"
 
-        # Load the saved images into the host containers.
         self.execute("while ! docker ps; do sleep 1; done && "
                    "docker load --input /code/calico-node.tar && "
                    "docker load --input /code/busybox.tar && "
                    "docker load --input /code/nsenter.tar")
 
     def execute(self, command, **kwargs):
+        """
+        Pass a command into a host container.
+        """
         return docker("exec", "-t", self.name, "bash", c=command, **kwargs)
 
     def listen(self, stdin, **kwargs):
+        """
+        Feed a command to a container via stdin. Used when `bash -c` in
+        DockerHost.execute has bad parsing behavior.
+        """
         return docker("exec", "-i", self.name, "bash", s=True, _in=stdin, **kwargs)
 
     def delete(self):
+        """
+        Have a container delete itself.
+        """
         self.__class__.delete_container(self.name)
 
     def start_etcd(self):
-        # Set up the single-node etcd cluster inside host1.
+        """
+        Start etcd on this host. Not tested for multiple etcd nodes.
+        """
         self.execute("docker load --input /code/etcd.tar")
 
         host_ip = docker.inspect("--format", "'{{ .NetworkSettings.IPAddress }}'", self.name).stdout.rstrip()
@@ -55,4 +55,22 @@ class DockerHost(object):
                "--initial-cluster calico=http://%s:2380 "
                "--initial-cluster-state new" % (host_ip, host_ip, host_ip))
         self.execute("docker run -d -p 2379:2379 quay.io/coreos/etcd:v2.0.10 %s" % cmd)
-        print "Etcd container started"
+
+    @classmethod
+    def delete_container(cls, name):
+        """
+        Cleanly delete a container.
+        """
+        # We *must* remove all inner containers and images before removing the outer
+        # container. Otherwise the inner images will stick around and fill disk.
+        # https://github.com/jpetazzo/dind#important-warning-about-disk-usage
+        cls.cleanup_inside(name)
+        sh.docker.rm("-f", name, _ok_code=[0, 1])
+
+    @classmethod
+    def cleanup_inside(cls, name):
+        """
+        Clean the inside of a container by deleting the containers and images within it.
+        """
+        docker("exec", "-t", name, "bash", "-c",
+               "docker rm -f $(docker ps -qa) ; docker rmi $(docker images -qa)", _ok_code=[0, 1])
--- tests/fv/test_base.py
+++ tests/fv/test_base.py
@@ -6,11 +6,17 @@ from sh import docker
 class TestBase(TestCase):
     """Base class for test-wide methods."""
     def setUp(self):
+        """
+        Clean up host containers before every test.
+        """
         containers = docker.ps("-qa").split()
         for container in containers:
             DockerHost.delete_container(container)
 
     def tearDown(self):
+        """
+        Clean up host containers after every test.
+        """
         containers = docker.ps("-qa").split()
         for container in containers:
             DockerHost.delete_container(container)
