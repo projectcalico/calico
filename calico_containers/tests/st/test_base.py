a22649069248059e5364d9a9c0488ededca44ecd
Alex Altair <Alex.Altair@metaswitch.com>
2015-05-27 15:27:22 -0700

    Move self.ip assignment into setUp.

--- calico_containers/tests/st/test_base.py
+++ calico_containers/tests/st/test_base.py
@@ -14,6 +14,7 @@ class TestBase(TestCase):
         for container in containers:
             DockerHost.delete_container(container)
 
+        self.ip = self.get_ip()
         self.start_etcd()
 
     def tearDown(self):
@@ -29,18 +30,28 @@ class TestBase(TestCase):
         Starts a separate etcd container.
         """
 
-        self.ip = getattr(self, 'ip', self.get_ip())
         docker.run("-d",
                    "-p", "2379:2379",
+                   "-p", "2380:2380",
                    "--name", "etcd", "quay.io/coreos/etcd:v2.0.10",
-                   name="calico",
-                   advertise_client_urls="http://%s:2379" % self.ip,
-                   listen_client_urls="http://0.0.0.0:2379",
-                   initial_advertise_peer_urls="http://%s:2380" % self.ip,
-                   listen_peer_urls="http://0.0.0.0:2380",
-                   initial_cluster_token="etcd-cluster-2",
-                   initial_cluster="calico=http://%s:2380" % self.ip,
-                   initial_cluster_state="new")
+                   # Comment out just in case double-dashes don't work...
+                   # name="calico",
+                   # advertise_client_urls="http://%s:2379" % self.ip,
+                   # listen_client_urls="http://0.0.0.0:2379",
+                   # initial_advertise_peer_urls="http://%s:2380" % self.ip,
+                   # listen_peer_urls="http://0.0.0.0:2380",
+                   # initial_cluster_token="etcd-cluster-2",
+                   # initial_cluster="calico=http://%s:2380" % self.ip,
+                   # initial_cluster_state="new",
+                   "-name", "calico",
+                   "-advertise-client-urls", "http://%s:2379" % self.ip,
+                   "-listen-client-urls", "http://0.0.0.0:2379",
+                   "-initial-advertise-peer-urls", "http://%s:2380" % self.ip,
+                   "-listen-peer-urls", "http://0.0.0.0:2380",
+                   "-initial-cluster-token", "etcd-cluster-2",
+                   "-initial-cluster", "calico=http://%s:2380" % self.ip,
+                   "-initial-cluster-state", "new",
+                  )
 
     def get_ip(self):
         intf = sh.ifconfig.eth0()
