35faf0c8beece87587bdc41bdd73fc31d907ad0d
Spike Curtis <spike.curtis@metaswitch.com>
2015-05-28 18:03:15 -0700

    Fix typo in create_binary comment

--- create_binary.sh
+++ create_binary.sh
@@ -22,7 +22,8 @@ docker rm -f pyinstaller || true
 docker rm -f docopt || true
 # mount calico_containers and dist under /code work directory.  Don't use /code
 # as the mountpoint directly since the host permissions may not allow the
-# `user` account in the container to write to it.docker run -v `pwd`/calico_containers:/code/calico_containers \
+# `user` account in the container to write to it.
+docker run -v `pwd`/calico_containers:/code/calico_containers \
  -v `pwd`/dist:/code/dist --name docopt calico-build \
  docopt-completion --manual-bash dist/calicoctl
 docker rm -f docopt || true
