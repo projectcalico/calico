a9d6e44b59dd7b220da5c9ea9401b8f982dbdd40
Alex Altair <Alex.Altair@metaswitch.com>
2015-05-29 17:11:20 -0700

    Also mount the dist dir.

--- run_sts.sh
+++ run_sts.sh
@@ -6,18 +6,19 @@ date
 pwd
 git status
 
-nosetests calico_containers/tests/st
+# nosetests calico_containers/tests/st
 
 # Run the STs. Need to run from the /code directory since the tests expect
 # to be run from the root of the codebase.
-docker run --privileged -v `pwd`/calico_containers:/code/calico_containers --name host1 -tid jpetazzo/dind
+docker run --privileged -v `pwd`/calico_containers:/code/calico_containers \
+ -v `pwd`/dist:/code/dist \
+ --name host1 -tid jpetazzo/dind
 docker exec -t host1 bash -c \
  'while ! docker ps; do sleep 1; done && \
  docker load --input /code/calico_containers/calico-node.tar && \
  docker load --input /code/calico_containers/busybox.tar && \
  docker load --input /code/calico_containers/nsenter.tar'
 
-# docker exec -t host1 bash -c 'cd /code && sudo ./calico_containers/tests/st/mainline.sh'
 docker exec -t host1 bash -c 'cd /code && sudo ./calico_containers/tests/st/add_container.sh'
 docker exec -t host1 bash -c 'cd /code && sudo ./calico_containers/tests/st/add_ip.sh'
 docker exec -t host1 bash -c 'cd /code && sudo ./calico_containers/tests/st/arg_parsing.sh'
