b0ebdd07a061c819d71cacd0a97596fbb5f0c49c
Alex Altair <Alex.Altair@metaswitch.com>
2015-06-01 18:23:02 -0700

    Improve style for test_duplicate_ips.

--- calico_containers/tests/st/test_duplicate_ips.py
+++ calico_containers/tests/st/test_duplicate_ips.py
@@ -1,5 +1,5 @@
-from sh import ErrorReturnCode
-from time import sleep
+from sh import ErrorReturnCode_1
+from functools import partial
 
 from test_base import TestBase
 from docker_host import DockerHost
@@ -16,10 +16,6 @@ class TestDuplicateIps(TestBase):
         host3 = DockerHost('host3')
 
         calicoctl = "/code/dist/calicoctl %s"
-        try:
-            host1.execute(calicoctl % "reset")
-        except ErrorReturnCode:
-            pass
 
         host1.execute(calicoctl % ("node --ip=%s" % host1.ip))
         host2.execute(calicoctl % ("node --ip=%s" % host2.ip))
@@ -27,15 +23,20 @@ class TestDuplicateIps(TestBase):
 
         # Set up three workloads on three hosts
         self.assert_powerstrip_up(host1)
-        host1.execute("docker run -e CALICO_IP=192.168.1.1 --name workload1 -tid busybox", docker_host=True)
+        host1.execute("docker run -e CALICO_IP=192.168.1.1 --name workload1 -tid busybox",
+                      use_powerstrip=True)
         self.assert_powerstrip_up(host2)
-        host2.execute("docker run -e CALICO_IP=192.168.1.2 --name workload2 -tid busybox", docker_host=True)
+        host2.execute("docker run -e CALICO_IP=192.168.1.2 --name workload2 -tid busybox",
+                      use_powerstrip=True)
         self.assert_powerstrip_up(host3)
-        host3.execute("docker run -e CALICO_IP=192.168.1.3 --name workload3 -tid busybox", docker_host=True)
+        host3.execute("docker run -e CALICO_IP=192.168.1.3 --name workload3 -tid busybox",
+                      use_powerstrip=True)
 
         # Set up the workloads with duplicate IPs
-        host1.execute("docker run -e CALICO_IP=192.168.1.4 --name dup1 -tid busybox", docker_host=True)
-        host2.execute("docker run -e CALICO_IP=192.168.1.4 --name dup2 -tid busybox", docker_host=True)
+        host1.execute("docker run -e CALICO_IP=192.168.1.4 --name dup1 -tid busybox",
+                      use_powerstrip=True)
+        host2.execute("docker run -e CALICO_IP=192.168.1.4 --name dup2 -tid busybox",
+                      use_powerstrip=True)
 
         host1.execute(calicoctl % "profile add TEST_PROFILE")
 
@@ -47,20 +48,21 @@ class TestDuplicateIps(TestBase):
         host3.execute(calicoctl % "profile TEST_PROFILE member add workload3")
 
         # Wait for the workload networking to converge.
-        sleep(1)
+        ping = partial(host1.execute, "docker exec workload1 ping -c 4 192.168.1.4")
+        self.retry_until_success(ping, ex_class=ErrorReturnCode_1)
 
         # Check for standard connectivity
-        assert host1.execute("docker exec workload1 ping -c 4 192.168.1.4")
-        assert host2.execute("docker exec workload2 ping -c 4 192.168.1.4")
-        assert host3.execute("docker exec workload3 ping -c 4 192.168.1.4")
+        host1.execute("docker exec workload1 ping -c 4 192.168.1.4")
+        host2.execute("docker exec workload2 ping -c 4 192.168.1.4")
+        host3.execute("docker exec workload3 ping -c 4 192.168.1.4")
 
         # Delete one of the duplciates.
         host2.execute("docker rm -f dup2")
 
         # Wait for the workload networking to converge.
-        sleep(1)
+        self.retry_until_success(ping, ex_class=ErrorReturnCode_1)
 
         # Check standard connectivity still works.
-        assert host1.execute("docker exec workload1 ping -c 4 192.168.1.4")
-        assert host2.execute("docker exec workload2 ping -c 4 192.168.1.4")
-        assert host3.execute("docker exec workload3 ping -c 4 192.168.1.4")
+        host1.execute("docker exec workload1 ping -c 4 192.168.1.4")
+        host2.execute("docker exec workload2 ping -c 4 192.168.1.4")
+        host3.execute("docker exec workload3 ping -c 4 192.168.1.4")
