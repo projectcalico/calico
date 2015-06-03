1938af22738ed6ad5e296db5224a47d529b9ad58
Tom Denham <tom.denham@metaswitch.com>
2015-06-03 14:07:39 -0700

    Use correct parameter for --rm

--- run_uts.sh
+++ run_uts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -rm -v `pwd`/calico_containers:/code/calico_containers calico-build bash -c '/tmp/etcd -data-dir=/tmp/default.etcd/ & nosetests calico_containers/tests/unit -c nose.cfg'
+docker run --rm -v `pwd`/calico_containers:/code/calico_containers calico-build bash -c '/tmp/etcd -data-dir=/tmp/default.etcd/ & nosetests calico_containers/tests/unit -c nose.cfg'
