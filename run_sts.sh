4192961a34f33e58ce8bbca9ab25d19ddb9f6122
Tom Denham <tom.denham@metaswitch.com>
2015-06-05 15:22:33 -0700

    First pass at add a Makefile to replace bash scripts
    Hasn't had extensive testing. STs are know to fail on this branch

--- run_sts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Ensure the host image is up to date
-./build_host.sh
-
-# Create the calicoctl binary here so it will be in the volume mounted on the hosts.
-./create_binary.sh
-nosetests calico_containers/tests/st --nocapture
