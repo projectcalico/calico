ba8ee64343fc4988be8ed1cd468ed1d793c6071f
Spike Curtis <spike.curtis@metaswitch.com>
2015-06-09 15:50:57 -0700

    Make DockerHost context manager that cleans up.
    DockerHost becomes a context manager so you can use it like
    
    with DockerHost('host') as host:
    	# Do the stuff.
    
    Fixex up existing STs to use this form.

--- calico_containers/tests/st/test_add_ip.py
+++ calico_containers/tests/st/test_add_ip.py
@@ -12,74 +12,76 @@ class TestAddIp(TestBase):
         """
         Test adding multiple IPs per workload.
         """
-        host = DockerHost('host')
+        with DockerHost('host') as host:
 
-        # TODO get this test working with libnetwork.  Right now we don't have
-        # any way to assign multiple IPs with libnetwork, nor can we use
-        # calicoctl commands to manipulate libnetwork-based containers or
-        # profiles since these names are not presented to the driver.
-        # host.execute("docker run --net=calico:test -tid --name=node1 busybox")
-        # ip11 = host.execute("docker inspect --format "
-        #                     "'{{ .NetworkSettings.IPAddress }}' "
-        #                     "node1").rstrip()
-        ip11 = "192.168.1.1"
-        host.execute("docker run -tid --name=node1 --net=none busybox")
-        host.calicoctl("container add node1 %s" % ip11)
+            # TODO get this test working with libnetwork.  Right now we don't
+            # have any way to assign multiple IPs with libnetwork, nor can we
+            # use calicoctl commands to manipulate libnetwork-based containers
+            # or profiles since these names are not presented to the driver.
+            # host.execute("docker run --net=calico:test -tid"
+            #              " --name=node1 busybox")
+            # ip11 = host.execute("docker inspect --format "
+            #                     "'{{ .NetworkSettings.IPAddress }}' "
+            #                     "node1").rstrip()
+            ip11 = "192.168.1.1"
+            host.execute("docker run -tid --name=node1 --net=none busybox")
+            host.calicoctl("container add node1 %s" % ip11)
 
-        ip21 = "192.168.1.2"
-        host.execute("docker run -tid --name=node2 busybox")
-        host.calicoctl("container add node2 %s --interface=hello" % ip21)
+            ip21 = "192.168.1.2"
+            host.execute("docker run -tid --name=node2 busybox")
+            host.calicoctl("container add node2 %s --interface=hello" % ip21)
 
-        host.calicoctl("profile add TEST_GROUP")
-        host.calicoctl("profile TEST_GROUP member add node1")
-        host.calicoctl("profile TEST_GROUP member add node2")
+            host.calicoctl("profile add TEST_GROUP")
+            host.calicoctl("profile TEST_GROUP member add node1")
+            host.calicoctl("profile TEST_GROUP member add node2")
 
-        test_ping = partial(host.execute,
-                            "docker exec node1 ping %s -c 1 -W 1" % ip21)
-        retry_until_success(test_ping, ex_class=CalledProcessError)
+            test_ping = partial(host.execute,
+                                "docker exec node1 ping %s -c 1 -W 1" % ip21)
+            retry_until_success(test_ping, ex_class=CalledProcessError)
 
-        # Add two more addresses to node1 and one more to node2
-        ip12 = "192.168.2.1"
-        ip13 = "192.168.3.1"
-        host.calicoctl("container node1 ip add %s" % ip12)
-        host.calicoctl("container node1 ip add %s" % ip13)
+            # Add two more addresses to node1 and one more to node2
+            ip12 = "192.168.2.1"
+            ip13 = "192.168.3.1"
+            host.calicoctl("container node1 ip add %s" % ip12)
+            host.calicoctl("container node1 ip add %s" % ip13)
 
-        ip22 = "192.168.2.2"
-        host.calicoctl("container node2 ip add %s --interface=hello" % ip22)
+            ip22 = "192.168.2.2"
+            host.calicoctl("container node2 ip add %s --interface=hello" %
+                           ip22)
 
-        host.execute("docker exec node1 ping %s -c 1" % ip22)
-        host.execute("docker exec node2 ping %s -c 1" % ip11)
-        host.execute("docker exec node2 ping %s -c 1" % ip12)
-        host.execute("docker exec node2 ping %s -c 1" % ip13)
+            host.execute("docker exec node1 ping %s -c 1" % ip22)
+            host.execute("docker exec node2 ping %s -c 1" % ip11)
+            host.execute("docker exec node2 ping %s -c 1" % ip12)
+            host.execute("docker exec node2 ping %s -c 1" % ip13)
 
-        # Now stop and restart node 1 and node 2.
-        host.execute("docker stop node1")
-        host.execute("docker stop node2")
-        host.execute("docker start node1")
-        host.execute("docker start node2")
+            # Now stop and restart node 1 and node 2.
+            host.execute("docker stop node1")
+            host.execute("docker stop node2")
+            host.execute("docker start node1")
+            host.execute("docker start node2")
 
-        retry_until_success(test_ping, ex_class=CalledProcessError)
+            retry_until_success(test_ping, ex_class=CalledProcessError)
 
-        # Test pings between the IPs.
-        host.execute("docker exec node1 ping %s -c 1" % ip21)
-        host.execute("docker exec node1 ping %s -c 1" % ip22)
-        host.execute("docker exec node2 ping %s -c 1" % ip11)
-        host.execute("docker exec node2 ping %s -c 1" % ip12)
-        host.execute("docker exec node2 ping %s -c 1" % ip13)
+            # Test pings between the IPs.
+            host.execute("docker exec node1 ping %s -c 1" % ip21)
+            host.execute("docker exec node1 ping %s -c 1" % ip22)
+            host.execute("docker exec node2 ping %s -c 1" % ip11)
+            host.execute("docker exec node2 ping %s -c 1" % ip12)
+            host.execute("docker exec node2 ping %s -c 1" % ip13)
 
-        # Now remove and check pings to the removed addresses no longer work.
-        # host.calicoctl("container node1 ip remove %s" % ip12)
-        host.calicoctl("container node2 ip remove %s "
-                       "--interface=hello" % ip22)
-        host.execute("docker exec node1 ping %s -c 1" % ip21)
-        host.execute("docker exec node2 ping %s -c 1" % ip11)
-        with self.assertRaises(CalledProcessError):
-            host.execute("docker exec node1 ping %s -c 1 -W 1" % ip22)
-        with self.assertRaises(CalledProcessError):
-            host.execute("docker exec node2 ping %s -c 1 -W 1" % ip12)
-        host.execute("docker exec node2 ping %s -c 1" % ip13)
-
-        # Check that we can't remove addresses twice
-        with self.assertRaises(CalledProcessError):
+            # Now remove and check can't ping the removed addresses.
+            # host.calicoctl("container node1 ip remove %s" % ip12)
             host.calicoctl("container node2 ip remove %s "
                            "--interface=hello" % ip22)
+            host.execute("docker exec node1 ping %s -c 1" % ip21)
+            host.execute("docker exec node2 ping %s -c 1" % ip11)
+            with self.assertRaises(CalledProcessError):
+                host.execute("docker exec node1 ping %s -c 1 -W 1" % ip22)
+            with self.assertRaises(CalledProcessError):
+                host.execute("docker exec node2 ping %s -c 1 -W 1" % ip12)
+            host.execute("docker exec node2 ping %s -c 1" % ip13)
+
+            # Check that we can't remove addresses twice
+            with self.assertRaises(CalledProcessError):
+                host.calicoctl("container node2 ip remove %s "
+                               "--interface=hello" % ip22)
--- calico_containers/tests/st/test_ipv6.py
+++ calico_containers/tests/st/test_ipv6.py
@@ -17,30 +17,34 @@ class TestIpv6(TestBase):
         # but Docker does not, since libnetwork will only create the network
         # if it doesn't exist.
         net_name = uuid.uuid4()
-        host = DockerHost('host', start_calico=False)
-        host.start_calico_node(ip=host.ip, ip6=host.ip6)
-        host.assert_driver_up()
+        with DockerHost('host', start_calico=False) as host:
+            host.start_calico_node(ip=host.ip, ip6=host.ip6)
+            host.assert_driver_up()
 
-        host.execute("docker run --net=calico:%s"
-                     " -tid --name=node1 phusion/baseimage:0.9.16" % net_name)
-        host.execute("docker run --net=calico:%s"
-                     " -tid --name=node2 phusion/baseimage:0.9.16" % net_name)
+            host.execute("docker run --net=calico:%s"
+                         " -tid --name=node1 phusion/baseimage:0.9.16" %
+                         net_name)
+            host.execute("docker run --net=calico:%s"
+                         " -tid --name=node2 phusion/baseimage:0.9.16" %
+                         net_name)
 
-        # Perform a docker inspect to extract the configured IP addresses.
-        node1_ip = host.execute("docker inspect --format "
-                                "'{{ .NetworkSettings.GlobalIPv6Address }}' "
-                                "node1").rstrip()
-        node2_ip = host.execute("docker inspect --format "
-                                "'{{ .NetworkSettings.GlobalIPv6Address }}' "
-                                "node2").rstrip()
+            # Perform a docker inspect to extract the configured IP addresses.
+            node1_ip = host.execute("docker inspect --format "
+                                    "'{{ .NetworkSettings."
+                                    "GlobalIPv6Address }}' "
+                                    "node1").rstrip()
+            node2_ip = host.execute("docker inspect --format "
+                                    "'{{ .NetworkSettings."
+                                    "GlobalIPv6Address }}' "
+                                    "node2").rstrip()
 
-        ping = partial(host.execute,
-                       "docker exec %s ping6 %s -c 1 -W 1" % ("node1",
-                                                              node2_ip))
-        retry_until_success(ping, ex_class=CalledProcessError)
+            ping = partial(host.execute,
+                           "docker exec %s ping6 %s -c 1 -W 1" % ("node1",
+                                                                  node2_ip))
+            retry_until_success(ping, ex_class=CalledProcessError)
 
-        # Check connectivity.
-        host.execute("docker exec %s ping6 %s -c 1" % ("node1", node1_ip))
-        host.execute("docker exec %s ping6 %s -c 1" % ("node1", node2_ip))
-        host.execute("docker exec %s ping6 %s -c 1" % ("node2", node1_ip))
-        host.execute("docker exec %s ping6 %s -c 1" % ("node2", node2_ip))
+            # Check connectivity.
+            host.execute("docker exec %s ping6 %s -c 1" % ("node1", node1_ip))
+            host.execute("docker exec %s ping6 %s -c 1" % ("node1", node2_ip))
+            host.execute("docker exec %s ping6 %s -c 1" % ("node2", node1_ip))
+            host.execute("docker exec %s ping6 %s -c 1" % ("node2", node2_ip))
