b5d2674f00fa63c1d326bcd0bc7f5d883fa0dd66
Tom Denham <tom.denham@metaswitch.com>
2015-06-11 17:31:46 -0700

    Fix up locales so bird repo will work on circle CI
    locale-gen is broken on Circle-CI, so...
    Use C.UTF-8 which is installed by default.

--- image/base.sh
+++ image/base.sh
@@ -17,11 +17,8 @@ $minimal_apt_get_install software-properties-common
 grep -Fxvf  /tmp/base.txt <(dpkg -l | grep ^ii | sed 's_  _\t_g' | cut \
 -f 2) >/tmp/add-apt.txt
 
-# Ensure UTF-8, required for add-apt-repository call.
-locale-gen en_US.UTF-8
-
 # Add new repos and update again
-LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8 add-apt-repository -y ppa:cz.nic-labs/bird
+LC_ALL=C.UTF-8 LANG=C.UTF-8 add-apt-repository -y ppa:cz.nic-labs/bird
 add-apt-repository -y ppa:project-calico/icehouse
 apt-get update
 
