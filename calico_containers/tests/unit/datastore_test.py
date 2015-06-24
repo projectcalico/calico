67023a92c8b5bf9ee112256363fea9e9837854c1
Robert Brockbank <rob.brockbank@metswitch.com>
2015-06-24 10:00:07 -0700

    Fix copyright statements

--- calico_containers/tests/unit/datastore_test.py
+++ calico_containers/tests/unit/datastore_test.py
@@ -1,5 +1,4 @@
 # Copyright 2015 Metaswitch Networks
-# Copyright 2015 Metaswitch Networks
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
