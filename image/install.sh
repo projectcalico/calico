0dc4168f47e28d5b44cbdc3334daf12ea0015a0c
Robert Brockbank <rob.brockbank@metswitch.com>
2015-07-01 20:28:52 -0700

    Tidy up comments, remove extra install of curl

--- image/install.sh
+++ image/install.sh
@@ -2,10 +2,12 @@
 set -e
 set -x
 
+# Get the current list of required packages (this is used in the clean up
+# code to remove the temporary packages that are about to be installed).
 dpkg -l | grep ^ii | sed 's_  _\t_g' | cut -f 2 >/tmp/required.txt
 
+# Install temporary packages required for installing Felix and etcd.
 apt-get install -qy \
-        curl \
         git \
         python-dev \
         libffi-dev \
