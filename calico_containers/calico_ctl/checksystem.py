d96667bcce61b7d9fe058b21a5f9f6411973a1ee
Spike Curtis <spike.curtis@metaswitch.com>
2015-07-24 13:46:14 -0700

    Fixes #346 Warns if ipip kernel module is not loaded.

--- calico_containers/calico_ctl/checksystem.py
+++ calico_containers/calico_ctl/checksystem.py
@@ -31,7 +31,6 @@ from utils import enforce_root
 from utils import sysctl
 from utils import docker_client
 
-
 def checksystem(arguments):
     """
     Main dispatcher for checksystem commands. Calls the corresponding helper
@@ -43,6 +42,7 @@ def checksystem(arguments):
     :return: None
     """
     check_system(fix=arguments["--fix"], quit_if_error=True)
+
 
 def check_system(fix=False, quit_if_error=False):
     """
@@ -62,72 +62,9 @@ def check_system(fix=False, quit_if_error=False):
     # modprobe and sysctl require root privileges.
     enforce_root()
 
-    system_ok = True
-    modprobe = sh.Command._create('modprobe')
-    ip6tables = sh.Command._create('ip6tables')
-    try:
-        ip6tables("-L")
-    except:
-        if fix:
-            try:
-                modprobe('ip6_tables')
-            except sh.ErrorReturnCode:
-                print >> sys.stderr, "ERROR: Could not enable ip6_tables."
-                system_ok = False
-        else:
-            print >> sys.stderr, "WARNING: Unable to detect the ip6_tables " \
-                                 "module. Load with `modprobe ip6_tables`"
-            system_ok = False
-
-    if not module_loaded("xt_set"):
-        if fix:
-            try:
-                modprobe('xt_set')
-            except sh.ErrorReturnCode:
-                print >> sys.stderr, "ERROR: Could not enable xt_set."
-                system_ok = False
-        else:
-            print >> sys.stderr, "WARNING: Unable to detect the xt_set " \
-                                 "module. Load with `modprobe xt_set`"
-            system_ok = False
-
-    # Enable IP forwarding since all compute hosts are vRouters.
-    # IPv4 forwarding should be enabled already by docker.
-    if "1" not in sysctl("net.ipv4.ip_forward"):
-        if fix:
-            if "1" not in sysctl("-w", "net.ipv4.ip_forward=1"):
-                print >> sys.stderr, "ERROR: Could not enable ipv4 forwarding."
-                system_ok = False
-        else:
-            print >> sys.stderr, "WARNING: ipv4 forwarding is not enabled."
-            system_ok = False
-
-    if "1" not in sysctl("net.ipv6.conf.all.forwarding"):
-        if fix:
-            if "1" not in sysctl("-w", "net.ipv6.conf.all.forwarding=1"):
-                print >> sys.stderr, "ERROR: Could not enable ipv6 forwarding."
-                system_ok = False
-        else:
-            print >> sys.stderr, "WARNING: ipv6 forwarding is not enabled."
-            system_ok = False
-
-    # Check docker version compatability
-    try:
-        info = docker_client.version()
-    except ConnectionError:
-        print >> sys.stderr, "ERROR: Docker daemon not running."
-        system_ok = False
-    except docker.errors.APIError:
-        print >> sys.stderr, "ERROR: Docker server must support Docker " \
-                             "Remote API v%s or greater." % DOCKER_VERSION
-        system_ok = False
-    else:
-        api_version = normalize_version(info['ApiVersion'])
-        # Check that API Version is above the minimum supported version
-        if cmp(api_version, normalize_version(DOCKER_VERSION)) < 0:
-            print >> sys.stderr, "ERROR: Docker server must support Docker " \
-                                 "Remote API v%s or greater." % DOCKER_VERSION
-            system_ok = False
+    system_ok = (_check_kernel_modules(fix) and
+                 _check_ip_forwarding(fix) and
+                 _check_docker_version())
 
     if quit_if_error and not system_ok:
         sys.exit(1)
@@ -152,3 +89,101 @@ def normalize_version(version):
     http://stackoverflow.com/questions/1714027/version-number-comparison
     """
     return [int(x) for x in re.sub(r'(\.0+)*$', '', version).split(".")]
+
+
+def _check_kernel_modules(fix):
+    """
+    Check system kernel modules
+    :param fix: if True, try to fix any system dependency issues that are
+    detected.
+    :return: True if kernel modules are ok.
+    """
+    modprobe = sh.Command._create('modprobe')
+    ip6tables = sh.Command._create('ip6tables')
+    system_ok = True
+    try:
+        ip6tables("-L")
+    except:
+        if fix:
+            try:
+                modprobe('ip6_tables')
+            except sh.ErrorReturnCode:
+                print >> sys.stderr, "ERROR: Could not enable ip6_tables."
+                system_ok = False
+        else:
+            print >> sys.stderr, "WARNING: Unable to detect the ip6_tables " \
+                                 "module. Load with `modprobe ip6_tables`"
+            system_ok = False
+
+    for module in ["xt_set", "ipip"]:
+        if not module_loaded(module):
+            if fix:
+                try:
+                    modprobe(module)
+                except sh.ErrorReturnCode:
+                    print >> sys.stderr, "ERROR: Could not enable %s." % module
+                    system_ok = False
+            else:
+                print >> sys.stderr, "WARNING: Unable to detect the %s " \
+                                     "module. Load with `modprobe %s`" % \
+                                     (module, module)
+                system_ok = False
+    return system_ok
+
+
+def _check_ip_forwarding(fix):
+    """
+    Check IP forwarding is enabled.
+    :param fix: if True, try to fix any system dependency issues that are
+    detected.
+    :return: True if IP forwarding is ok.
+    """
+    system_ok = True
+    # Enable IP forwarding since all compute hosts are vRouters.
+    # IPv4 forwarding should be enabled already by docker.
+    if "1" not in sysctl("net.ipv4.ip_forward"):
+        if fix:
+            if "1" not in sysctl("-w", "net.ipv4.ip_forward=1"):
+                print >> sys.stderr, "ERROR: Could not enable ipv4 forwarding."
+                system_ok = False
+        else:
+            print >> sys.stderr, "WARNING: ipv4 forwarding is not enabled."
+            system_ok = False
+
+    if "1" not in sysctl("net.ipv6.conf.all.forwarding"):
+        if fix:
+            if "1" not in sysctl("-w", "net.ipv6.conf.all.forwarding=1"):
+                print >> sys.stderr, "ERROR: Could not enable ipv6 forwarding."
+                system_ok = False
+        else:
+            print >> sys.stderr, "WARNING: ipv6 forwarding is not enabled."
+            system_ok = False
+    return system_ok
+
+
+def _check_docker_version():
+    """
+    Check the Docker version is supported.
+
+    :return: True if Docker version is OK.
+    """
+    system_ok = True
+    # Check docker version compatability
+    try:
+        info = docker_client.version()
+    except ConnectionError:
+        print >> sys.stderr, "ERROR: Docker daemon not running."
+        system_ok = False
+    except docker.errors.APIError:
+        print >> sys.stderr, "ERROR: Docker server must support Docker " \
+                             "Remote API v%s or greater." % DOCKER_VERSION
+        system_ok = False
+    else:
+        api_version = normalize_version(info['ApiVersion'])
+        # Check that API Version is above the minimum supported version
+        if cmp(api_version, normalize_version(DOCKER_VERSION)) < 0:
+            print >> sys.stderr, "ERROR: Docker server must support Docker " \
+                                 "Remote API v%s or greater." % DOCKER_VERSION
+            system_ok = False
+
+    return system_ok
