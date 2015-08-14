81744f16a655fd387c5d6d304f29416d7abead46
Shaun Crampton <shaun.crampton@metaswitch.com>
2015-08-14 19:03:41 +0100

    First stab at a posix_spawnp variant of check_call.

--- calico/felix/futils.py
+++ calico/felix/futils.py
@@ -27,10 +27,14 @@ import hashlib
 import logging
 import os
 from types import StringTypes
+import types
+import gc
 import gevent.lock
 from gevent import subprocess
+from gevent.subprocess import Popen
 import tempfile
 import time
+from posix_spawn import posix_spawnp, FileActions
 
 try:
     import resource
@@ -95,6 +99,122 @@ def call_silent(args):
         return e.retcode
 
 
+class SpawnedProcess(Popen):
+    def _execute_child(self, args, executable, preexec_fn, close_fds,
+                       cwd, env, universal_newlines,
+                       startupinfo, creationflags, shell,
+                       p2cread, p2cwrite,
+                       c2pread, c2pwrite,
+                       errread, errwrite):
+        """Execute program (POSIX version)"""
+
+        if preexec_fn is not None:
+            raise NotImplementedError("preexec_fn not supported")
+        if close_fds:
+            raise NotImplementedError("close_fds not implemented")
+        if cwd:
+            raise NotImplementedError("cwd not implemented")
+        if universal_newlines:
+            raise NotImplementedError()
+        assert startupinfo is None and creationflags == 0
+
+        log.debug("Pipes: p2c %s, %s; c2p %s, %s; err %s, %s",
+                  p2cread, p2cwrite,
+                  c2pread, c2pwrite,
+                  errread, errwrite)
+
+        if isinstance(args, types.StringTypes):
+            args = [args]
+        else:
+            args = list(args)
+
+        if shell:
+            args = ["/bin/sh", "-c"] + args
+            if executable:
+                args[0] = executable
+
+        if executable is None:
+            executable = args[0]
+
+        self._loop.install_sigchld()
+
+        file_actions = FileActions()
+        # Close parent's pipe ends
+        if p2cwrite is not None:
+            file_actions.add_close(p2cwrite)
+        if c2pread is not None:
+            file_actions.add_close(c2pread)
+        if errread is not None:
+            file_actions.add_close(errread)
+
+        # When duping fds, if there arises a situation
+        # where one of the fds is either 0, 1 or 2, it
+        # is possible that it is overwritten (#12607).
+        fds_to_close_in_parent = []
+        if c2pwrite == 0:
+            c2pwrite = os.dup(c2pwrite)
+            fds_to_close_in_parent.append(c2pwrite)
+        if errwrite == 0 or errwrite == 1:
+            errwrite = os.dup(errwrite)
+            fds_to_close_in_parent.append(errwrite)
+
+        # Dup fds for child
+        def _dup2(a, b):
+            if a is None:
+                a = b
+            # Need to take a dup so we can remove the non-blocking flag
+            a_dup = os.dup(a)
+            log.debug("Duped %s as %s", a, a_dup)
+            fds_to_close_in_parent.append(a_dup)
+            self._remove_nonblock_flag(a_dup)
+            file_actions.add_dup2(a_dup, b)
+        _dup2(p2cread, 0)
+        _dup2(c2pwrite, 1)
+        _dup2(errwrite, 2)
+
+        # Close pipe fds.  Make sure we don't close the
+        # same fd more than once, or standard fds.
+        closed = set([None])
+        for fd in [p2cread, c2pwrite, errwrite]:
+            if fd not in closed and fd > 2:
+                file_actions.add_close(fd)
+                closed.add(fd)
+
+        gc_was_enabled = gc.isenabled()
+        # FIXME Does this bug apply to posix_spawn version?
+        try:
+            # Disable gc to avoid bug where gc -> file_dealloc ->
+            # write to stderr -> hang.  http://bugs.python.org/issue1336
+            gc.disable()
+            self.pid = posix_spawnp(
+                executable,
+                args,
+                file_actions=file_actions,
+                env=env,
+            )
+        except:
+            if gc_was_enabled:
+                gc.enable()
+            raise
+        finally:
+            for fd in fds_to_close_in_parent:
+                os.close(fd)
+
+        # Parent
+        self._watcher = self._loop.child(self.pid)
+        self._watcher.start(self._on_child, self._watcher)
+
+        if gc_was_enabled:
+            gc.enable()
+
+        if p2cread is not None and p2cwrite is not None:
+            os.close(p2cread)
+        if c2pwrite is not None and c2pread is not None:
+            os.close(c2pwrite)
+        if errwrite is not None and errread is not None:
+            os.close(errwrite)
+
+
 def check_call(args, input_str=None):
     """
     Substitute for the subprocess.check_call function. It has the following
@@ -116,10 +236,10 @@ def check_call(args, input_str=None):
     stdin = subprocess.PIPE if input_str is not None else None
 
     with _call_semaphore:
-        proc = subprocess.Popen(args,
-                                stdin=stdin,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
+        proc = SpawnedProcess(args,
+                              stdin=stdin,
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE)
         stdout, stderr = proc.communicate(input=input_str)
 
     retcode = proc.returncode
