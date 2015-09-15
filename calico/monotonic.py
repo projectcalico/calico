a9f2e0b53232de7c30b842926d91af13253cc3b8
Shaun Crampton <shaun.crampton@metaswitch.com>
2015-09-15 13:59:38 +0100

    Add monotonic_time() function, which returns a monotonic timestamp.

--- calico/monotonic.py
+++ calico/monotonic.py
@@ -35,7 +35,7 @@ import os
 CLOCK_MONOTONIC_RAW = 4 # see <linux/time.h>
 
 
-class timespec(ctypes.Structure):
+class Timespec(ctypes.Structure):
     _fields_ = [
         ('tv_sec', ctypes.c_long),
         ('tv_nsec', ctypes.c_long)
@@ -44,7 +44,7 @@ class timespec(ctypes.Structure):
 
 librt = ctypes.CDLL('librt.so.1', use_errno=True)
 clock_gettime = librt.clock_gettime
-clock_gettime.argtypes = [ctypes.c_int, ctypes.POINTER(timespec)]
+clock_gettime.argtypes = [ctypes.c_int, ctypes.POINTER(Timespec)]
 
 
 def monotonic_time():
@@ -53,7 +53,7 @@ def monotonic_time():
         between processes).  Guaranteed to be monotonic within the life of
         a process.
     """
-    t = timespec()
+    t = Timespec()
     if clock_gettime(CLOCK_MONOTONIC_RAW , ctypes.pointer(t)) != 0:
         errno_ = ctypes.get_errno()
         raise OSError(errno_, os.strerror(errno_))
