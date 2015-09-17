6f1f3002021412423413d42670005f23dae91f46
Shaun Crampton <shaun.crampton@metaswitch.com>
2015-09-17 16:17:35 +0100

    Code review markups.

--- CHANGES.md
+++ CHANGES.md
@@ -2,8 +2,8 @@
 
 ## 1.2.0
 
-- Add liveness reporting to felix.  Felix now reports its liveness into
-  etcd and the neutron driver copies that information to the neutron DB.
+- Add liveness reporting to Felix.  Felix now reports its liveness into
+  etcd and the neutron driver copies that information to the Neutron DB.
 - Performance enhancements to ipset manipulation.
 
 ## 1.1.0
--- calico/felix/test/base.py
+++ calico/felix/test/base.py
@@ -1,4 +1,17 @@
-# Copyright (c) Metaswitch Networks 2015. All rights reserved.
+# -*- coding: utf-8 -*-
+# Copyright 2015 Metaswitch Networks
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import json
 
 import logging
@@ -57,6 +70,7 @@ class JSONString(object):
         else:
             _log.error("JSON didn't match %s != %s",
                        self.json_obj, other_as_obj)
+            return False
 
     def __repr__(self):
         return '%s(%r)' % (self.__class__.__name__, self.json_obj)
