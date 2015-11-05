078dd9474a9a477050f94a84245cb1ed05d29357
Shaun Crampton <shaun.crampton@metaswitch.com>
2015-11-05 10:38:12 +0000

    Full coverage for EndpointManager.

--- calico/felix/test/test_endpoint.py
+++ calico/felix/test/test_endpoint.py
@@ -57,6 +57,7 @@ class TestEndpointManager(BaseTestCase):
                                    self.m_dispatch, self.m_rules_mgr,
                                    self.m_status_reporter)
         self.mgr.get_and_incref = Mock()
+        self.mgr.decref = Mock()
 
     def test_create(self):
         obj = self.mgr._create(ENDPOINT_ID)
@@ -83,13 +84,87 @@ class TestEndpointManager(BaseTestCase):
         self.step_actor(self.mgr)
         self.mgr.on_datamodel_in_sync(async=True)
         self.step_actor(self.mgr)
-        # Second call should have no effect.
-        self.mgr.on_datamodel_in_sync(async=True)
-        self.step_actor(self.mgr)
         self.assertEqual(
             self.m_dispatch.apply_snapshot.mock_calls,
             [mock.call(frozenset(["tap1234"]), async=True)]
         )
+        # Second call should have no effect.
+        self.m_dispatch.apply_snapshot.reset_mock()
+        self.mgr.on_datamodel_in_sync(async=True)
+        self.step_actor(self.mgr)
+        self.assertEqual(self.m_dispatch.apply_snapshot.mock_calls, [])
+
+    def test_endpoint_update_not_our_host(self):
+        ep = {"name": "tap1234"}
+        with mock.patch.object(self.mgr, "_is_starting_or_live") as m_sol:
+            self.mgr.on_endpoint_update(EndpointId("notus", "b", "c", "d"),
+                                        ep,
+                                        async=True)
+            self.step_actor(self.mgr)
+        self.assertFalse(m_sol.called)
+
+    def test_endpoint_live_obj(self):
+        ep = {"name": "tap1234"}
+        # First send in an update to trigger creation.
+        self.mgr.on_endpoint_update(ENDPOINT_ID, ep, async=True)
+        self.step_actor(self.mgr)
+        self.assertEqual(self.mgr.get_and_incref.mock_calls,
+                         [mock.call(ENDPOINT_ID)])
+        m_endpoint = Mock(spec=LocalEndpoint)
+        self.mgr.objects_by_id[ENDPOINT_ID] = m_endpoint
+        # Then send a second update to check that it gets passed on to the
+        # LocalEndpoint.
+        with mock.patch.object(self.mgr, "_is_starting_or_live") as m_sol:
+            m_sol.return_value = True
+            self.mgr.on_endpoint_update(ENDPOINT_ID, ep, async=True)
+            self.step_actor(self.mgr)
+        self.assertEqual(m_sol.mock_calls, [mock.call(ENDPOINT_ID)])
+        self.assertEqual(m_endpoint.on_endpoint_update.mock_calls,
+                         [mock.call(ep, force_reprogram=False,
+                                    async=True)])
+        self.assertTrue(ENDPOINT_ID in self.mgr.local_endpoint_ids)
+        # Finally, send in a deletion.
+        m_endpoint.on_endpoint_update.reset_mock()
+        with mock.patch.object(self.mgr, "_is_starting_or_live") as m_sol:
+            m_sol.return_value = True
+            self.mgr.on_endpoint_update(ENDPOINT_ID, None, async=True)
+            self.step_actor(self.mgr)
+        self.assertEqual(m_endpoint.on_endpoint_update.mock_calls,
+                         [mock.call(None, force_reprogram=False,
+                                    async=True)])
+        self.assertEqual(self.mgr.decref.mock_calls, [mock.call(ENDPOINT_ID)])
+        self.assertFalse(ENDPOINT_ID in self.mgr.local_endpoint_ids)
+
+    def test_on_interface_update_unknown(self):
+        with mock.patch.object(self.mgr, "_is_starting_or_live") as m_sol:
+            self.mgr.on_interface_update("foo", True, async=True)
+            self.step_actor(self.mgr)
+        self.assertFalse(m_sol.called)
+
+    def test_on_interface_update_known(self):
+        ep = {"name": "tap1234"}
+        m_endpoint = Mock(spec=LocalEndpoint)
+        self.mgr.objects_by_id[ENDPOINT_ID] = m_endpoint
+        with mock.patch.object(self.mgr, "_is_starting_or_live") as m_sol:
+            m_sol.return_value = True
+            self.mgr.on_endpoint_update(ENDPOINT_ID, ep, async=True)
+            self.mgr.on_interface_update("tap1234", True, async=True)
+            self.step_actor(self.mgr)
+        self.assertEqual(
+            m_endpoint.on_interface_update.mock_calls,
+            [mock.call(True, async=True)]
+        )
+
+    def test_on_interface_update_known_but_not_live(self):
+        ep = {"name": "tap1234"}
+        m_endpoint = Mock(spec=LocalEndpoint)
+        self.mgr.objects_by_id[ENDPOINT_ID] = m_endpoint
+        with mock.patch.object(self.mgr, "_is_starting_or_live") as m_sol:
+            m_sol.return_value = False
+            self.mgr.on_endpoint_update(ENDPOINT_ID, ep, async=True)
+            self.mgr.on_interface_update("tap1234", True, async=True)
+            self.step_actor(self.mgr)
+        self.assertEqual(m_endpoint.on_interface_update.mock_calls, [])
 
 
 class TestLocalEndpoint(BaseTestCase):
