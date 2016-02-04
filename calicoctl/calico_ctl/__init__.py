7c9648107a4b3e9c06954b794b76675e32f458f4
Tom Denham <tom.denham@metaswitch.com>
2016-02-03 16:17:14 -0800

    Update docs to version v0.16.0

--- calicoctl/calico_ctl/__init__.py
+++ calicoctl/calico_ctl/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.15.0-dev"
+__version__ = "0.16.0-dev"
 __libnetwork_plugin_version__ = "v0.7.0-dev"
-__libcalico_version__ = "v0.10.0-dev"
+__libcalico_version__ = "v0.11.0-dev"
 __felix_version__ = "1.3.0a6-dev"
