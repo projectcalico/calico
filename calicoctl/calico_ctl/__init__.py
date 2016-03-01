0d65c7a3afdbc610cd7ada4e5604eda54f5305b6
Rob Brockbank <rob.brockbank@metaswitch.com>
2016-02-29 21:50:33 -0800

    Update docs to version v0.17.0

--- calicoctl/calico_ctl/__init__.py
+++ calicoctl/calico_ctl/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.16.1-dev"
-__libnetwork_plugin_version__ = "v0.7.0-dev"
-__libcalico_version__ = "v0.11.0-dev"
-__felix_version__ = "1.3.0a6-dev"
+__version__ = "0.17.0-dev"
+__libnetwork_plugin_version__ = "v0.8.0-dev"
+__libcalico_version__ = "v0.12.0-dev"
+__felix_version__ = "1.3.0-dev"
