e249061188ed35d04b7d1ccd839ed920c7e81c4a
Matt Dupre <matt@projectcalico.org>
2016-06-08 01:08:16 +0000

    Update docs to version v0.20.0

--- calicoctl/calico_ctl/__init__.py
+++ calicoctl/calico_ctl/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.19.0-dev"
+__version__ = "0.20.0-dev"
 __libnetwork_plugin_version__ = "v0.8.0-dev"
-__libcalico_version__ = "v0.14.0-dev"
-__felix_version__ = "1.4.0b1-dev"
+__libcalico_version__ = "v0.15.0-dev"
+__felix_version__ = "1.4.0b2-dev"
