8f350c5f241cf425651b6b07ed042490af76d454
Neil Jerram <neil@tigera.io>
2016-09-22 15:39:43 +0100

    Rev setup.py version to distinguish development from last release

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ def collect_requirements():
 
 setuptools.setup(
     name="calico",
-    version="1.4.2",
+    version="1.4.3.dev1",
     packages=setuptools.find_packages(),
     entry_points={
         'console_scripts': [
