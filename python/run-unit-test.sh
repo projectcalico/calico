744365aaa14be43d44235f001996b99b7550ae68
Neil Jerram <neil@tigera.io>
2016-11-03 12:48:32 +0000

    run-unit-test.sh: Remove obsolete comment

--- python/run-unit-test.sh
+++ python/run-unit-test.sh
@@ -15,9 +15,7 @@
 
 # Shell script for running the Calico unit test suite.
 #
-# Invoke as './run-unit-test.sh'. Arguments to this script are passed directly
-# to tox: e.g., to force a rebuild of tox's virtual environments, invoke this
-# script as './run-unit-test.sh -r'.
+# Invoke as './run-unit-test.sh'.
 set -e
 
 coverage erase
