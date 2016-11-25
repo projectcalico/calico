37084f2e4b84537ee5e6017454e2b25cd05c5e53
Neil Jerram <neil@tigera.io>
2016-11-25 16:40:27 +0000

    Felix 1.4.5

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ def collect_requirements():
 
 setuptools.setup(
     name="felix",
-    version="1.4.5.dev1",
+    version="1.4.5",
     packages=setuptools.find_packages(),
     entry_points={
         'console_scripts': [
