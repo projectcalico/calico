70a570213344fc74959ec2fa3875c3c2191401d0
Neil Jerram <neil@tigera.io>
2017-01-04 22:54:18 +0000

    Rev setup.py version to distinguish development from last release

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ def collect_requirements():
 
 setuptools.setup(
     name="felix",
-    version="1.4.6",
+    version="1.4.7.dev1",
     packages=setuptools.find_packages(),
     entry_points={
         'console_scripts': [
