50d60eacd760797919d18760e62eb93661b97d7c
Shaun Crampton <shaun@tigera.io>
2017-01-06 16:12:07 +0000

    Add more UT for set package.

--- go/felix/set/set_test.go
+++ go/felix/set/set_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2016 Tigera, Inc. All rights reserved.
+// Copyright (c) 2016-2017 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -22,14 +22,107 @@ import (
 )
 
 var _ = Describe("Set", func() {
-	It("can copy a Set", func() {
-		var s set.Set = set.New()
-		s.Add(1)
-		s.Add(2)
-		c := s.Copy()
-		Expect(c.Len()).To(Equal(s.Len()))
-		c.Iter(func(item interface{}) error {
-			Expect(s.Contains(item)).To(BeTrue())
+	var s set.Set
+	BeforeEach(func() {
+		s = set.New()
+	})
+
+	Describe("after adding 1 and 2", func() {
+		BeforeEach(func() {
+			s.Add(1)
+			s.Add(2)
+			s.Add(2) // Duplicate should have no effect
+		})
+		It("should contain 1", func() {
+			Expect(s.Contains(1)).To(BeTrue())
+		})
+		It("should contain 2", func() {
+			Expect(s.Contains(2)).To(BeTrue())
+		})
+		It("should not contain 3", func() {
+			Expect(s.Contains(3)).To(BeFalse())
+		})
+		It("should iterate over 1 and 2 in some order", func() {
+			seen1 := false
+			seen2 := false
+			s.Iter(func(item interface{}) error {
+				if item.(int) == 1 {
+					Expect(seen1).To(BeFalse())
+					seen1 = true
+				} else if item.(int) == 2 {
+					Expect(seen2).To(BeFalse())
+					seen2 = true
+				} else {
+					Fail("Unexpected item")
+				}
+				return nil
+			})
+			Expect(seen1).To(BeTrue())
+			Expect(seen2).To(BeTrue())
+		})
+		It("should allow remove during iteration", func() {
+			s.Iter(func(item interface{}) error {
+				if item.(int) == 1 {
+					return set.RemoveItem
+				}
+				return nil
+			})
+			Expect(s.Contains(1)).To(BeFalse())
+			Expect(s.Contains(2)).To(BeTrue())
+		})
+		It("should support stopping iteration", func() {
+			iterationStarted := false
+			s.Iter(func(item interface{}) error {
+				if iterationStarted {
+					Fail("Iteration continued after stop")
+				}
+				iterationStarted = true
+				return set.StopIteration
+			})
+			Expect(s.Contains(1)).To(BeTrue())
+			Expect(s.Contains(2)).To(BeTrue())
+		})
+		It("can copy a Set", func() {
+			c := s.Copy()
+			Expect(c.Len()).To(Equal(s.Len()))
+			Expect(c).NotTo(BeIdenticalTo(s)) // Check they're not the same object.
+			Expect(c).To(Equal(s))            // DeepEquals, will check the contents.
+		})
+
+		Describe("after removing 2", func() {
+			BeforeEach(func() {
+				s.Discard(2)
+			})
+			It("should contain 1", func() {
+				Expect(s.Contains(1)).To(BeTrue())
+			})
+			It("should not contain 2", func() {
+				Expect(s.Contains(2)).To(BeFalse())
+			})
+			It("should not contain 3", func() {
+				Expect(s.Contains(3)).To(BeFalse())
+			})
+		})
+	})
+})
+
+var _ = Describe("EmptySet", func() {
+	var empty set.Set
+	BeforeEach(func() {
+		empty = set.Empty()
+	})
+	It("has length 0", func() {
+		Expect(empty.Len()).To(Equal(0))
+	})
+	It("should panic on add", func() {
+		Expect(func() { empty.Add("foo") }).To(Panic())
+	})
+	It("should ignore discard", func() {
+		Expect(func() { empty.Discard("foo") }).NotTo(Panic())
+	})
+	It("should iterate 0 times", func() {
+		empty.Iter(func(item interface{}) error {
+			Fail("Iterated > 0 times")
 			return nil
 		})
 	})
