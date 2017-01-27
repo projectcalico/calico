bc65ea2c24bd4f954f0072cd1c89a696c43689a5
Shaun Crampton <shaun@tigera.io>
2017-01-27 15:08:46 +0000

    Improve dataplane driver API doc.

--- proto/doc.go
+++ proto/doc.go
@@ -12,48 +12,87 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// The proto package defines the protocol between the main, calico-felix,
-// process (written in Golang) and the dataplane driver plugin.
+// The proto package defines the protocol between Felix's policy
+// "calculation engine", which calculates the policy that should be active
+// on a given host, and the "dataplane driver", which renders that policy
+// into the dataplane.
+//
+//	              +-----------+
+//	              | Datastore |
+//	              +-----+-----+
+//	                    |
+//	                    | <etcd/k8s/etc API>
+//	                    |
+//	         +----------+---------------+
+//	         | libcalico-go "client"    |
+//	         +---------<go API>---------+
+//	         | Felix calculation engine |
+//	         +----------+---------------+
+//	                    |
+//	                    | <this API>
+//	                    |
+//	         +----------+---------------+
+//	         | Dataplane driver         |
+//	         +----------+---------------+
+//	                    |
+//	                    | <dataplane-specific API>
+//	                    |
+//	         ======= Dataplane ==========
 //
 // Overview
 //
-// The main process creates a pair of communication pipes, before forking
-// to start the dataplane driver.  The dataplane driver receives messages on
-// file descriptor 3 and sends on descriptor 4.
+// The protocol is defined as a series of protobuf messages.  This allows for
+// a dataplane driver to run either in-process or in another process.
 //
-// The protocol (described in more detail below) starts with a handshake to
-// exchange configuration.  Then the main process begins its resync with the
-// datastore, emitting updates as it scans through the current state.
-// Once complete, the main process enters the "in-sync" state and starts
-// sending only updates.  The driver may send  process/endpoint status
-// updates at any time after the handshake.
+// An in-process dataplane driver (such as the default one in the
+// "intdataplane" package) receives the protobuf messages as Go structs
+// directly.
 //
-// The wire-format for the protocol uses protobuf.  It is described in
-// more detail below.
+// When running an external dataplane driver, the main process creates a
+// pair of communication pipes, before forking to start the configured
+// dataplane driver. The dataplane driver receives messages on file
+// descriptor 3 and sends on descriptor 4.  The wire format is described
+// in a section below.  The dataplane driver has the same lifetime as the
+// main process.
+//
+// In either case, the protocol (described in more detail below) starts
+// with a handshake to exchange configuration.  Then the calculation engine
+// begins its resync with the datastore, emitting updates as it scans
+// through the current state. Once complete, the calculation engine enters
+// the "in-sync" state and starts sending only updates.  Updates are
+// asynchronous, with no explicit acknowledgement for each message.
+//
+// The dataplane driver should send status updates for itself and
+// the endpoints that it is controlling.
 //
 // Handshake
 //
-// Before sending its stream of updates, the main process loads and resolves
+// Before sending its stream of updates, the calculation engine loads and resolves
 // the configuration (from file, environment variables and the datastore) and
 // then sends a ConfigUpdate message with the resolved configuration.  This
 // ensures that the driver has the configuration before it receives any
 // updates.
 //
-// Note: the main process doesn't currently support any subsequent config
+// Note: the calculation engine doesn't currently support any subsequent config
 // updates.  If the config is updated after the process is running, it will
-// exit, so that the init system can restart it.
+// trigger a process exit, so that the init system can restart it.
 //
 // Resync and updates
 //
 // After the initial ConfigUpdate message, the protocol is in resync state.
-// The main process will send a stream of updates that merges the current state
+// The calculation engine will send a stream of updates that merges the current state
 // of the datastore along with any updates that occur later.  The stream is
 // guaranteed to be eventually consistent.  I.e. if a resource is updated (or
-// deleted) during the resync then the main process is free to skip the
+// deleted) during the resync then the calculation engine is free to skip the
 // intermediate value and send only one update with the most up-to-date value
 // (or none if the object was deleted).
 //
-// Once the main process has finished its initial datastore scan, it sends the
+// Updates are sent to the dataplane driver in dependency order.  I.e.
+// dependencies are sent before the objects that depend on them.  Removes are
+// sent in reverse dependency order so the dataplane driver will not receive
+// a remove for an object that is still required.
+//
+// Once the calculation engine has finished its initial datastore scan, it sends the
 // InSync message.
 //
 // For simplicity and robustness, most <Type>Update messages contain the
@@ -70,22 +109,28 @@
 // the dataplane driver should delay programming of potentially incorrect
 // state until after it receives the InSync message.
 //
-// Note: there are many safe updates that the dataplane driver can make during
-// the resync.  For example, if it receives a profile update, it is free to
-// overwrite the exsting profile state because the profile update contains a
-// complete and consistent snapshot of the profile.
+// Driver status updates
+//
+// The driver should send a ProcessStatusUpdate message every 10s to verify its
+// liveness.  That message flows through to the datastore and some orchestrators
+// (such as OpenStack) rely on the status messages to make scheduling
+// decisions.
+//
+// Endpoint status updates
+//
+// The driver should report the status for each endpoint that it is managing
+// and update the status when it changes.  The driver does not need to periodically
+// refresh the endpoint statuses, the "main" process caches the values and
+// keeps the datastore in sync.
+//
+// Once an endpoint is removed, the dataplane driver should send an
+// XXXEndpointStatusRemove message so the calculation engine can clear up its cache entry.
 //
 // Special cases
 //
-// If the main process fails to parse an update from the datastore, it
-// simulates a deletion for the relevant resource.  As such the driver must be
-// robust to duplicate Remove messages as well as receiving a Remove message
-// for a resource that it hadn't previously been told about.
-//
-// The above also implies that the driver needs to be robust against
-// receiving partial information.  For example,. if it receives an endpoint
-// that refers to profile X but profile X is never sent or is deleted then it
-// should handle that by dropping packets that would go to profile X.
+// Due to coalescing of updates in the calculation engine, the dataplane driver
+// may receive Remove messages for resources that it didn't previously receive an
+// Update for; it should ignore such Remove messages.
 //
 // Illustration
 //
@@ -142,27 +187,136 @@
 //	      |                                                       |
 //	      |                (Workload|Host)Endpoint(Update|Remove) |
 //	      |<------------------------------------------------------|
+//	      |                                                       |
+//	      |                           HostMetadata(Update|Remove) |
+//	      |<------------------------------------------------------|
+//	      |                                                       |
+//	      |                               IPAMPool(Update|Remove) |
+//	      |<------------------------------------------------------|
 //	      | ------------------------------------\                 |
 //	      |-| Status updates (sent at any time) |                 |
 //	      | |-----------------------------------|                 |
 //	      |                                                       |
-//	      | FelixStatusUpdate                                     |
+//	      | ProcessStatusUpdate                                   |
 //	      |------------------------------------------------------>|
 //	      |                                                       |
 //	      | (Workload|Host)EndpointStatus                         |
 //	      |------------------------------------------------------>|
 //	      |                                                       |
 //
-// Wire format
+//
+// Example
+//
+// The sequence diagram below illustrates a scenario where the initial resync finds
+// one local endpoint, then a new endpoint is added, an IP set updated and the
+// endpoints removed.
+//
+//	+-----------+                                                             +-------+
+//	| dp_driver |                                                             | main  |
+//	+-----------+                                                             +-------+
+//	      |                                                                       |
+//	      |                                                            **Create** |
+//	      |<----------------------------------------------------------------------|
+//	      |                                                 --------------------\ |
+//	      |                                                 | Initial handshake |-|
+//	      |                                                 |-------------------| |
+//	      |                                                                       |
+//	      |                                         ConfigUpdate(resolved config) |
+//	      |<----------------------------------------------------------------------|
+//	      |                                                                       |
+//	      |                                     DatastoreStatus("wait-for-ready") |
+//	      |<----------------------------------------------------------------------|
+//	      |                                                                       |
+//	      |                                             DatastoreStatus("resync") |
+//	      |<----------------------------------------------------------------------|
+//	      |       --------------------------------------------------------------\ |
+//	      |       | Loads state: finds one active endpoint on host, one policy, |-|
+//	      |       | using one IP set. Sends updates in dependency order:        | |
+//	      |       |-------------------------------------------------------------| |
+//	      |                             IPSetUpdate("setABCD", ["10.0.0.1", ...]) |
+//	      |<----------------------------------------------------------------------|
+//	      |                                                                       |
+//	      |     ActivePolicyUpdate({inbound_rules: [...], outbound_rules: [...]}) |
+//	      |<----------------------------------------------------------------------|
+//	      |                                                                       |
+//	      |                          WorkloadEndpointUpdate("endpoint1", { ... }) |
+//	      |<----------------------------------------------------------------------|
+//	      |                                      -------------------------------\ |
+//	      |                                      | Finishes sync with datastore |-|
+//	      |                                      |------------------------------| |
+//	      |                                                                       |
+//	      |                                            DatastoreStatus("in-sync") |
+//	      |<----------------------------------------------------------------------|
+//	      | ---------------------------------------------------\                  |
+//	      |-| Now in sync, program dataplane for first time.   |                  |
+//	      | | Do any cleanup of old state, send status updates |                  |
+//	      | |--------------------------------------------------|                  |
+//	      | WorkloadEndpointStatusUpdate("endpoint1", "up")                       |
+//	      |---------------------------------------------------------------------->|
+//	      | ---------------------------------------------\                        |
+//	      |-| Every 10s, send a process status update... |                        |
+//	      | |--------------------------------------------|                        |
+//	      |                                                                       |
+//	      | ProcessStatusUpdate({uptime=...})                                     |
+//	      |---------------------------------------------------------------------->|
+//	      |                        ---------------------------------------------\ |
+//	      |                        | Told about new endpoint, using same policy |-|
+//	      |                        |--------------------------------------------| |
+//	      |                                                                       |
+//	      |                          WorkloadEndpointUpdate("endpoint2", { ... }) |
+//	      |<----------------------------------------------------------------------|
+//	      | ---------------------\                                                |
+//	      |-| Programs dataplane |                                                |
+//	      | |--------------------|                                                |
+//	      |                                                                       |
+//	      | WorkloadEndpointStatusUpdate("endpoint2", "up")                       |
+//	      |---------------------------------------------------------------------->|
+//	      |                                              -----------------------\ |
+//	      |                                              | New IP in the IP set |-|
+//	      |                                              |----------------------| |
+//	      |                                                                       |
+//	      |                 IPSetDeltaUpdate("setABCD", {added_ips=["10.0.0.2"]}) |
+//	      |<----------------------------------------------------------------------|
+//	      | ---------------------\                                                |
+//	      |-| Programs dataplane |                                                |
+//	      | |--------------------|                                                |
+//	      |                        ---------------------------------------------\ |
+//	      |                        | Endpoints deleted, policy no longer active |-|
+//	      |                        | Removes in reverse dependency order:       | |
+//	      |                        |--------------------------------------------| |
+//	      |                                   WorkloadEndpointRemove("endpoint1") |
+//	      |<----------------------------------------------------------------------|
+//	      |                                                                       |
+//	      |                                   WorkloadEndpointRemove("endpoint2") |
+//	      |<----------------------------------------------------------------------|
+//	      |                                                                       |
+//	      |                                            ActivePolicyRemove("polA") |
+//	      |<----------------------------------------------------------------------|
+//	      |                                                                       |
+//	      |                                                IPSetRemove("setABCD") |
+//	      |<----------------------------------------------------------------------|
+//	      |                                                                       |
+//	      | WorkloadEndpointStatusRemove("endpoint1")                             |
+//	      |---------------------------------------------------------------------->|
+//	      |                                                                       |
+//	      | WorkloadEndpointStatusRemove("endpoint2")                             |
+//	      |---------------------------------------------------------------------->|
+//	      |                                                                       |
+//
+// Wire format for external dataplane driver
 //
 // The protocol between the driver and main process is protobuf based.
 // On the wire, each message consists of an 8-byte, little-endian length,
 // followed by a ToDataplane or FromDataplane protobuf envelope message.
 // The length refers to the length of the protobuf data only, it doesn't
 // include the 8-byte length header.
+//
+//	+---------------+--------------------------------------------+
+//	| 8-byte length | Protobuf ToDataplane/FromDataplane message |
+//	+---------------+--------------------------------------------+
 package proto
 
-// http://textart.io/sequence Source code for sequence diagram above:
+// http://textart.io/sequence Source code for sequence diagrams above:
 
 var _ = `
 object dp_driver main
@@ -186,8 +340,54 @@ note right of dp_driver: Finish graceful restart, do DP cleanup
 main->dp_driver: IPSet(Update|DeltaUpdate|Remove)
 main->dp_driver: Active(Profile|Policy)(Update|Remove)
 main->dp_driver: (Workload|Host)Endpoint(Update|Remove)
+main->dp_driver: HostMetadata(Update|Remove)
+main->dp_driver: IPAMPool(Update|Remove)
 
 note right of dp_driver: Status updates (sent at any time)
-dp_driver->main: FelixStatusUpdate
+dp_driver->main: ProcessStatusUpdate
 dp_driver->main: (Workload|Host)EndpointStatus
+`
+
+var _ = `
+object dp_driver main
+main->dp_driver: **Create**
+
+note left of main: Initial handshake
+
+main->dp_driver: ConfigUpdate(resolved config)
+main->dp_driver: DatastoreStatus("wait-for-ready")
+main->dp_driver: DatastoreStatus("resync")
+
+note left of main: Loads state: finds one active endpoint on host, one policy,\n using one IP set. Sends updates in dependency order:
+
+main->dp_driver: IPSetUpdate("setABCD", ["10.0.0.1", ...])
+main->dp_driver: ActivePolicyUpdate({inbound_rules: [...], outbound_rules: [...]})
+main->dp_driver: WorkloadEndpointUpdate("endpoint1", { ... })
+
+note left of main: Finishes sync with datastore
+main->dp_driver: DatastoreStatus("in-sync")
+
+note right of dp_driver: Now in sync, program dataplane for first time.\nDo any cleanup of old state, send status updates
+dp_driver->main: WorkloadEndpointStatusUpdate("endpoint1", "up")
+
+note right of dp_driver: Every 10s, send a process status update...
+dp_driver->main: ProcessStatusUpdate({uptime=...})
+
+note left of main: Told about new endpoint, using same policy
+main->dp_driver: WorkloadEndpointUpdate("endpoint2", { ... })
+note right of dp_driver: Programs dataplane
+dp_driver->main: WorkloadEndpointStatusUpdate("endpoint2", "up")
+
+note left of main: New IP in the IP set
+main->dp_driver: IPSetDeltaUpdate("setABCD", {added_ips=["10.0.0.2"]})
+note right of dp_driver: Programs dataplane
+
+note left of main: Endpoints deleted, policy no longer active\nRemoves in reverse dependency order:
+main->dp_driver: WorkloadEndpointRemove("endpoint1")
+main->dp_driver: WorkloadEndpointRemove("endpoint2")
+main->dp_driver: ActivePolicyRemove("polA")
+main->dp_driver: IPSetRemove("setABCD")
+
+dp_driver->main: WorkloadEndpointStatusRemove("endpoint1")
+dp_driver->main: WorkloadEndpointStatusRemove("endpoint2")
 `
