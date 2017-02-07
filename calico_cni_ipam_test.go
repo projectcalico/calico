projectcalico/cni-plugin

Add support for the new IPv4Pools and IPv6Pools properties on AutoAssign
Now that https://github.com/projectcalico/libcalico-go/pull/271 is merged.

Bump refs to libcalico-go too

Signed-off-by: Doug Davis <dug@us.ibm.com>

calico_cni_ipam_test.go
@@ -83,6 +83,87 @@ var _ = Describe("Calico IPAM Tests", func() {
 		})
 	})
 
+	Describe("Run IPAM plugin - Verify IP Pools", func() {
+		Context("Pass valid pools", func() {
+			It("successfully networks the namespace", func() {
+				netconf := fmt.Sprintf(`
+			        {
+			          "name": "net1",
+			          "type": "calico",
+			          "etcd_endpoints": "http://%s:2379",
+			          "ipam": {
+			            "type": "%s",
+			            "assign_ipv4": "true",
+						"ipv4_pools": [ "192.168.0.0/16" ]
+			          }
+			        }`, os.Getenv("ETCD_IP"), plugin)
+				_, _, _, _, _, _, err := CreateContainer(netconf)
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+		})
+
+		Context("Pass more than one pool", func() {
+			It("successfully networks the namespace", func() {
+				PreCreatePool("192.169.1.0/24")
+				netconf := fmt.Sprintf(`
+			        {
+			          "name": "net1",
+			          "type": "calico",
+			          "etcd_endpoints": "http://%s:2379",
+			          "ipam": {
+			            "type": "%s",
+			            "assign_ipv4": "true",
+						"ipv4_pools": [ "192.169.1.0/24", "192.168.0.0/16" ]
+			          }
+			        }`, os.Getenv("ETCD_IP"), plugin)
+				_, _, session, _, _, _, err := CreateContainer(netconf)
+				if err != nil {
+					fmt.Printf("Session Err: %v\n", string(session.Err.Contents()))
+				}
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+		})
+
+		Context("Pass an invalid pool", func() {
+			It("fails to network the namespace", func() {
+				// Put the bogus pool last in the array
+				netconf := fmt.Sprintf(`
+			        {
+			          "name": "net1",
+			          "type": "calico",
+			          "etcd_endpoints": "http://%s:2379",
+			          "ipam": {
+			            "type": "%s",
+			            "assign_ipv4": "true",
+						"ipv4_pools": [ "192.168.0.0/16", "192.169.1.0/24" ]
+			          }
+			        }`, os.Getenv("ETCD_IP"), plugin)
+				_, _, session, _, _, _, err := CreateContainer(netconf)
+				Expect(err).Should(HaveOccurred())
+				Expect(session.Err.Contents()).Should(ContainSubstring("192.169.1.0/24) does not exist"))
+			})
+
+			It("fails to network the namespace", func() {
+				// Put the bogus pool first in the array
+				netconf := fmt.Sprintf(`
+			        {
+			          "name": "net1",
+			          "type": "calico",
+			          "etcd_endpoints": "http://%s:2379",
+			          "ipam": {
+			            "type": "%s",
+			            "assign_ipv4": "true",
+						"ipv4_pools": [ "192.169.1.0/24", "192.168.0.0/16" ]
+			          }
+			        }`, os.Getenv("ETCD_IP"), plugin)
+				_, _, session, _, _, _, err := CreateContainer(netconf)
+				Expect(err).Should(HaveOccurred())
+				Expect(session.Err.Contents()).Should(ContainSubstring("192.169.1.0/24) does not exist"))
+			})
+		})
+
+	})
+
 	Describe("Run IPAM plugin", func() {
 		netconf := fmt.Sprintf(`
 					{"name": "net1",

utils/utils.go
@@ -232,3 +232,23 @@ func CreateContextLogger(workload string) *log.Entry {
 
 	return contextLogger
 }
+
+// Takes as array of IPv4 or IPv6 pools and parses them into an array of IPnet's
+func ParsePools(pools []string, isv4 bool) ([]cnet.IPNet, error) {
+	result := []cnet.IPNet{}
+	for _, p := range pools {
+		_, cidr, err := net.ParseCIDR(p)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing pool %q: %s", p, err)
+		}
+		ip := cidr.IP
+		if isv4 && ip.To4() == nil {
+			return nil, fmt.Errorf("%q isn't a IPv4 address", ip)
+		}
+		if !isv4 && ip.To4() != nil {
+			return nil, fmt.Errorf("%q isn't a IPv16 address", ip)
+		}
+		result = append(result, cnet.IPNet{*cidr})
+	}
+	return result, nil
+}

projectcalico/calico

calico_cni_ipam_test.go
@@ -25,7 +25,7 @@ var _ = Describe("Calico IPAM Tests", func() {
 			DescribeTable("Request different numbers of IP addresses",
 				func(expectedIPv4, expectedIPv6 bool, netconf string) {
 
-					result, _ := RunIPAMPlugin(netconf, "ADD", "")
+					result, _, _ := RunIPAMPlugin(netconf, "ADD", "")
 
 					if expectedIPv4 {
 						Expect(result.IP4.IP.Mask.String()).Should(Equal("ffffffff"))
@@ -36,7 +36,7 @@ var _ = Describe("Calico IPAM Tests", func() {
 					}
 
 					// I can't find any testable side effects for this
-					_, _ = RunIPAMPlugin(netconf, "DEL", "")
+					_, _, _ = RunIPAMPlugin(netconf, "DEL", "")
 				},
 				Entry("IPAM with no configuration", true, false, fmt.Sprintf(`
 			{
@@ -83,6 +83,82 @@ var _ = Describe("Calico IPAM Tests", func() {
 		})
 	})
 
+	Describe("Run IPAM plugin - Verify IP Pools", func() {
+		Context("Pass valid pools", func() {
+			It("Uses the ipv4 pool", func() {
+				netconf := fmt.Sprintf(`
+                {
+                      "name": "net1",
+                      "type": "calico",
+                      "etcd_endpoints": "http://%s:2379",
+                      "ipam": {
+                        "type": "%s",
+                        "assign_ipv4": "true",
+                        "ipv4_pools": [ "192.168.0.0/16" ]
+                      }
+                }`, os.Getenv("ETCD_IP"), plugin)
+				result, _, _ := RunIPAMPlugin(netconf, "ADD", "")
+				Expect(result.IP4.IP.String()).Should(HavePrefix("192.168."))
+			})
+		})
+
+		Context("Pass more than one pool", func() {
+			It("Uses one of the ipv4 pools", func() {
+				testutils.CreateNewIPPool(*calicoClient, "192.169.1.0/24", false, false, true)
+				netconf := fmt.Sprintf(`
+                {
+                      "name": "net1",
+                      "type": "calico",
+                      "etcd_endpoints": "http://%s:2379",
+                      "ipam": {
+                        "type": "%s",
+                        "assign_ipv4": "true",
+                        "ipv4_pools": [ "192.169.1.0/24", "192.168.0.0/16" ]
+                      }
+                }`, os.Getenv("ETCD_IP"), plugin)
+				result, _, _ := RunIPAMPlugin(netconf, "ADD", "")
+				Expect(result.IP4.IP.String()).Should(Or(HavePrefix("192.168."), HavePrefix("192.169.1")))
+			})
+		})
+
+		Context("Pass an invalid pool", func() {
+			It("fails to get an IP", func() {
+				// Put the bogus pool last in the array
+				netconf := fmt.Sprintf(`
+                    {
+                      "name": "net1",
+                      "type": "calico",
+                      "etcd_endpoints": "http://%s:2379",
+                      "ipam": {
+                        "type": "%s",
+                        "assign_ipv4": "true",
+                        "ipv4_pools": [ "192.168.0.0/16", "192.169.1.0/24" ]
+                      }
+                    }`, os.Getenv("ETCD_IP"), plugin)
+				_, error, _ := RunIPAMPlugin(netconf, "ADD", "")
+				Expect(error.Msg).Should(ContainSubstring("192.169.1.0/24) does not exist"))
+			})
+
+			It("fails to get an IP", func() {
+				// Put the bogus pool first in the array
+				netconf := fmt.Sprintf(`
+                    {
+                      "name": "net1",
+                      "type": "calico",
+                      "etcd_endpoints": "http://%s:2379",
+                      "ipam": {
+                        "type": "%s",
+                        "assign_ipv4": "true",
+                        "ipv4_pools": [ "192.168.0.0/16", "192.169.1.0/24" ]
+                      }
+                    }`, os.Getenv("ETCD_IP"), plugin)
+				_, error, _ := RunIPAMPlugin(netconf, "ADD", "")
+				Expect(error.Msg).Should(ContainSubstring("192.169.1.0/24) does not exist"))
+			})
+		})
+
+	})
+
 	Describe("Run IPAM plugin", func() {
 		netconf := fmt.Sprintf(`
 					{"name": "net1",
@@ -94,20 +170,20 @@ var _ = Describe("Calico IPAM Tests", func() {
 					}`, os.Getenv("ETCD_IP"), plugin)
 		Context("Pass explicit IP address", func() {
 			It("Return the expected IP", func() {
-				result, _ := RunIPAMPlugin(netconf, "ADD", "IP=192.168.123.123")
+				result, _, _ := RunIPAMPlugin(netconf, "ADD", "IP=192.168.123.123")
 				Expect(result.IP4.IP.String()).Should(Equal("192.168.123.123/32"))
 			})
 			It("Return the expected IP twice after deleting in the middle", func() {
-				result, _ := RunIPAMPlugin(netconf, "ADD", "IP=192.168.123.123")
+				result, _, _ := RunIPAMPlugin(netconf, "ADD", "IP=192.168.123.123")
 				Expect(result.IP4.IP.String()).Should(Equal("192.168.123.123/32"))
-				_, _ = RunIPAMPlugin(netconf, "DEL", "IP=192.168.123.123")
-				result, _ = RunIPAMPlugin(netconf, "ADD", "IP=192.168.123.123")
+				_, _, _ = RunIPAMPlugin(netconf, "DEL", "IP=192.168.123.123")
+				result, _, _ = RunIPAMPlugin(netconf, "ADD", "IP=192.168.123.123")
 				Expect(result.IP4.IP.String()).Should(Equal("192.168.123.123/32"))
 			})
 			It("Doesn't allow an explicit IP to be assigned twice", func() {
-				result, _ := RunIPAMPlugin(netconf, "ADD", "IP=192.168.123.123")
+				result, _, _ := RunIPAMPlugin(netconf, "ADD", "IP=192.168.123.123")
 				Expect(result.IP4.IP.String()).Should(Equal("192.168.123.123/32"))
-				result, exitCode := RunIPAMPlugin(netconf, "ADD", "IP=192.168.123.123")
+				result, _, exitCode := RunIPAMPlugin(netconf, "ADD", "IP=192.168.123.123")
 				Expect(exitCode).Should(BeNumerically(">", 0))
 			})
 		})

utils/utils.go
@@ -232,3 +232,23 @@ func CreateContextLogger(workload string) *log.Entry {
 
 	return contextLogger
 }
+
+// Takes as array of IPv4 or IPv6 pools and parses them into an array of IPnet's
+func ParsePools(pools []string, isv4 bool) ([]cnet.IPNet, error) {
+	result := []cnet.IPNet{}
+	for _, p := range pools {
+		_, cidr, err := net.ParseCIDR(p)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing pool %q: %s", p, err)
+		}
+		ip := cidr.IP
+		if isv4 && ip.To4() == nil {
+			return nil, fmt.Errorf("%q isn't a IPv4 address", ip)
+		}
+		if !isv4 && ip.To4() != nil {
+			return nil, fmt.Errorf("%q isn't a IPv6 address", ip)
+		}
+		result = append(result, cnet.IPNet{*cidr})
+	}
+	return result, nil
+}