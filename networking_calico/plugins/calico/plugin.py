36fde2c1a4401e5d8c955caa0b73c4e8b8200568
Neil Jerram <neil@tigera.io>
2017-02-22 12:41:26 +0000

    Suppress spurious DHCP agent scheduling WARNINGs
    This change suppresses periodic Neutron server WARNING logs like
    these:
    
    neutron.scheduler.dhcp_agent_scheduler [REQ-ID - admin] No more DHCP agents
    neutron.api.rpc.agentnotifiers.dhcp_rpc_agent_api [REQ-ID - admin] Unable to schedule network NET-ID: no agents available; will retry on subsequent port and subnet creation events.
    
    (In Calico for OpenStack we use a Calico-specific DHCP agent that does
    not report to the Neutron server and does not require scheduling.)
    
    Change-Id: I2b59ae5baf76cbf4850db06f2b70d870f702300f

--- networking_calico/plugins/calico/plugin.py
+++ networking_calico/plugins/calico/plugin.py
@@ -36,6 +36,11 @@ class CalicoPlugin(Ml2Plugin, l3_db.L3_NAT_db_mixin):
         # Add the ability to handle floating IPs.
         self._supported_extension_aliases.extend(["router"])
 
+        # Suppress the Neutron server's DHCP agent scheduling.  This is useful
+        # because it suppresses many WARNING logs that would otherwise appear,
+        # but that are actually spurious in a Calico/OpenStack deployment.
+        self._supported_extension_aliases.remove("dhcp_agent_scheduler")
+
         # Set ML2 options so the user doesn't have to.
         LOG.info("Forcing ML2 mechanism_drivers to 'calico'")
         cfg.CONF.set_override('mechanism_drivers', ['calico'], group='ml2')
