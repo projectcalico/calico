a23bb76a7aa99d696861b0c290f3530117c5cd32
Dan Osborne <djosborne10@gmail.com>
2017-03-01 22:41:38 -0800

    Final fixups.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
-  var pathname = location.href.split('#')[0]; // remove anchor hash from page url if present
+  // remove anchor hash from page url if present
+  var pathname = location.href.split('#')[0];
 
+  // Open any collapses that should be opened when the page loads
   $('#sidebar a').each(function () {
     if (pathname === this.href) {
       $(this).parents('div.collapse').each(function(){
@@ -14,6 +16,7 @@ $(document).ready(function(){
 
   });
 
+  // Make arrows switch direction on collapse.
   $('#sidebar').on('show.bs.collapse', function(event) {
     $(event.target).parent().prev().find('span.glyphicon').removeClass('glyphicon-chevron-right').addClass('glyphicon-chevron-down');
   });
@@ -21,6 +24,7 @@ $(document).ready(function(){
     $(event.target).parent().prev().find('span.glyphicon').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-right');
   })
 
+  // Enable the sidebar collapse
   $('[data-toggle="offcanvas"]').click(function () {
     $('.row-offcanvas').toggleClass('active')
   });
