projectcalico/libcalico-go

Refactor all tests to e2edescribe

lib/backend/backend_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2016 Tigera, Inc. All rights reserved.
+// Copyright (c) 2016-2017 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,54 +18,49 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	"github.com/projectcalico/libcalico-go/lib/backend/etcd"
+	"github.com/projectcalico/libcalico-go/lib/api"
+	bapi "github.com/projectcalico/libcalico-go/lib/backend/api"
 	"github.com/projectcalico/libcalico-go/lib/backend/model"
 	"github.com/projectcalico/libcalico-go/lib/testutils"
 )
 
-// Setting localhost as the etcd endpoint location since that's where `make run-etcd` runs it.
-var etcdConfig = &etcd.EtcdConfig{
-	EtcdEndpoints: "http://127.0.0.1:2379",
-}
-
-var _ = Describe("Backend tests", func() {
-	etcdClient, _ := etcd.NewEtcdClient(etcdConfig)
-
+var _ = testutils.E2eDatastoreDescribe("Backend tests", testutils.DatastoreEtcdV2, func(config api.CalicoAPIConfig) {
 	var (
-		block model.KVPair
-		err   error
+		block  model.KVPair
+		client bapi.Client
+		err    error
 	)
 
 	BeforeEach(func() {
-		testutils.CleanEtcd()
-
+		c := testutils.CreateCleanClient(config)
+		client = c.Backend
 		block = model.KVPair{
 			Key: model.BlockKey{
 				CIDR: testutils.MustParseNetwork("10.0.0.0/26"),
 			},
-			Value: model.AllocationBlock{
+			Value: &model.AllocationBlock{
 				CIDR: testutils.MustParseNetwork("10.0.0.0/26"),
 			},
 		}
 
-		_, err = etcdClient.Create(&block)
+		_, err = client.Create(&block)
 		Expect(err).NotTo(HaveOccurred())
 	})
 
 	Describe("Create", func() {
 
 		It("persists a new kv pair", func() {
-			kv, err := etcdClient.Get(block.Key)
+			kv, err := client.Get(block.Key)
 			Expect(err).NotTo(HaveOccurred())
 
 			expectedCIDR := kv.Value.(*model.AllocationBlock).CIDR
-			persitedCIRD := block.Value.(model.AllocationBlock).CIDR
+			persitedCIRD := block.Value.(*model.AllocationBlock).CIDR
 
 			Expect(expectedCIDR).To(Equal(persitedCIRD))
 		})
 
 		It("sets revision field", func() {
-			kv, err := etcdClient.Get(block.Key)
+			kv, err := client.Get(block.Key)
 
 			Expect(err).NotTo(HaveOccurred())
 			Expect(kv.Revision).NotTo(BeNil())
@@ -76,21 +71,21 @@ var _ = Describe("Backend tests", func() {
 	Describe("Update", func() {
 
 		It("updates a kv pair", func() {
-			block.Value = model.AllocationBlock{
+			block.Value = &model.AllocationBlock{
 				CIDR: testutils.MustParseNetwork("192.168.0.0/26"),
 			}
 
-			kv, err := etcdClient.Update(&block)
+			kv, err := client.Update(&block)
 			Expect(err).NotTo(HaveOccurred())
 
-			expectedCIDR := kv.Value.(model.AllocationBlock).CIDR
-			persitedCIRD := block.Value.(model.AllocationBlock).CIDR
+			expectedCIDR := kv.Value.(*model.AllocationBlock).CIDR
+			persitedCIRD := block.Value.(*model.AllocationBlock).CIDR
 
 			Expect(expectedCIDR).To(Equal(persitedCIRD))
 		})
 
 		It("sets revision field", func() {
-			kv, err := etcdClient.Update(&block)
+			kv, err := client.Update(&block)
 
 			Expect(err).NotTo(HaveOccurred())
 			Expect(kv.Revision).NotTo(BeNil())
@@ -98,7 +93,7 @@ var _ = Describe("Backend tests", func() {
 
 		It("validates revision field", func() {
 			block.Revision = uint64(1000)
-			_, err := etcdClient.Update(&block)
+			_, err := client.Update(&block)
 			Expect(err).To(HaveOccurred())
 		})
 
@@ -107,15 +102,15 @@ var _ = Describe("Backend tests", func() {
 	Describe("Apply", func() {
 
 		It("updates a kv pair", func() {
-			block.Value = model.AllocationBlock{
+			block.Value = &model.AllocationBlock{
 				CIDR: testutils.MustParseNetwork("192.168.0.0/26"),
 			}
 
-			kv, err := etcdClient.Apply(&block)
+			kv, err := client.Apply(&block)
 			Expect(err).NotTo(HaveOccurred())
 
-			expectedCIDR := kv.Value.(model.AllocationBlock).CIDR
-			persitedCIRD := block.Value.(model.AllocationBlock).CIDR
+			expectedCIDR := kv.Value.(*model.AllocationBlock).CIDR
+			persitedCIRD := block.Value.(*model.AllocationBlock).CIDR
 
 			Expect(expectedCIDR).To(Equal(persitedCIRD))
 		})
@@ -125,21 +120,21 @@ var _ = Describe("Backend tests", func() {
 				CIDR: testutils.MustParseNetwork("192.168.0.0/26"),
 			}
 
-			kv, err := etcdClient.Apply(&block)
+			kv, err := client.Apply(&block)
 			Expect(err).NotTo(HaveOccurred())
 
-			expectedCIDR := kv.Value.(model.AllocationBlock).CIDR
-			persitedCIRD := block.Value.(model.AllocationBlock).CIDR
+			expectedCIDR := kv.Value.(*model.AllocationBlock).CIDR
+			persitedCIRD := block.Value.(*model.AllocationBlock).CIDR
 
 			Expect(expectedCIDR).To(Equal(persitedCIRD))
 		})
 
 		It("sets revision field", func() {
-			block.Value = model.AllocationBlock{
+			block.Value = &model.AllocationBlock{
 				CIDR: testutils.MustParseNetwork("192.168.0.0/26"),
 			}
 
-			kv, err := etcdClient.Apply(&block)
+			kv, err := client.Apply(&block)
 
 			Expect(err).NotTo(HaveOccurred())
 			Expect(kv.Revision).NotTo(BeNil())
@@ -147,7 +142,7 @@ var _ = Describe("Backend tests", func() {
 
 		It("validates revision field", func() {
 			block.Revision = uint64(1000)
-			_, err := etcdClient.Apply(&block)
+			_, err := client.Apply(&block)
 			Expect(err).NotTo(HaveOccurred())
 		})
 
@@ -156,10 +151,10 @@ var _ = Describe("Backend tests", func() {
 	Describe("Delete", func() {
 
 		It("deletes the kv pair", func() {
-			err := etcdClient.Delete(&block)
+			err := client.Delete(&block)
 			Expect(err).NotTo(HaveOccurred())
 
-			_, err = etcdClient.Get(block.Key)
+			_, err = client.Get(block.Key)
 			Expect(err).To(HaveOccurred())
 		})
 
@@ -170,15 +165,13 @@ var _ = Describe("Backend tests", func() {
 			IPVersion: 4,
 		}
 
-		bl, err := etcdClient.List(blockListOpt)
+		It("List", func() {
+			bl, err := client.List(blockListOpt)
 
-		for _, blv := range bl {
-			It("sets revision field", func() {
+			for _, blv := range bl {
 				Expect(err).NotTo(HaveOccurred())
 				Expect(blv.Revision).NotTo(BeNil())
-			})
-		}
-
+			}
+		})
 	})
-
 })

lib/client/node_e2e_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2016 Tigera, Inc. All rights reserved.
+// Copyright (c) 2016-2017 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -29,7 +29,6 @@ package client_test
 
 import (
 	"errors"
-	"log"
 	"reflect"
 
 	. "github.com/onsi/ginkgo"
@@ -42,25 +41,18 @@ import (
 	"github.com/projectcalico/libcalico-go/lib/testutils"
 )
 
-var _ = Describe("Node tests", func() {
+var _ = testutils.E2eDatastoreDescribe("Node tests", testutils.DatastoreEtcdV2, func(config api.CalicoAPIConfig) {
 	cidrv4 := testutils.MustParseCIDR("1.2.3.5/24")
 	cidrv6 := testutils.MustParseCIDR("aa::bb00:0001/104")
 	asn := numorstring.ASNumber(12345)
 
 	DescribeTable("Node e2e tests",
 		func(meta1, meta2 api.NodeMetadata, spec1, spec2 api.NodeSpec) {
-			// Erase etcd clean.
-			testutils.CleanEtcd()
-
-			// Create a new client.
-			c, err := testutils.NewClient("")
-			if err != nil {
-				log.Println("Error creating client:", err)
-			}
+			c := testutils.CreateCleanClient(config)
 
 			// Updating non-existent node1 should return an error
 			By("Updating the node before it is created")
-			_, err = c.Nodes().Update(&api.Node{Metadata: meta1, Spec: spec1})
+			_, err := c.Nodes().Update(&api.Node{Metadata: meta1, Spec: spec1})
 			Expect(err.Error()).To(Equal(errors.New("resource does not exist: Node(name=node1)").Error()))
 
 			// Create a new node1 with meta1 and spec1.  This should not error.
@@ -78,7 +70,8 @@ var _ = Describe("Node tests", func() {
 			_, err = c.Nodes().Apply(&api.Node{Metadata: meta2, Spec: spec2})
 			Expect(err).NotTo(HaveOccurred())
 
-			testutils.DumpEtcd()
+			err = testutils.DumpBackend(config)
+			Expect(err).NotTo(HaveOccurred())
 
 			// Get node1.  This should not error, spec should match spec1.
 			By("Getting node1 and comparing with spec1")
@@ -192,8 +185,7 @@ var _ = Describe("Node tests", func() {
 	)
 
 	Describe("Checking global config is set only once", func() {
-		testutils.CleanEtcd()
-		c, _ := testutils.NewClient("")
+		c := testutils.CreateCleanClient(config)
 		var guidOrig string
 		var guidNew string
 		var set bool
@@ -238,8 +230,7 @@ var _ = Describe("Node tests", func() {
 	})
 
 	Describe("Checking delete/get/list operations perform data validation", func() {
-		testutils.CleanEtcd()
-		c, _ := testutils.NewClient("")
+		c := testutils.CreateCleanClient(config)
 		var err error
 		valErrorType := reflect.TypeOf(cerrors.ErrorValidation{})
 

lib/testutils/client.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2016 Tigera, Inc. All rights reserved.
+// Copyright (c) 2016-2017 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,35 +15,21 @@
 package testutils
 
 import (
-	"log"
 	"net"
-	"os"
 
+	log "github.com/Sirupsen/logrus"
+	etcdclient "github.com/coreos/etcd/client"
 	"github.com/projectcalico/libcalico-go/lib/api"
 	"github.com/projectcalico/libcalico-go/lib/api/unversioned"
 	"github.com/projectcalico/libcalico-go/lib/client"
 	cnet "github.com/projectcalico/libcalico-go/lib/net"
+
+	"errors"
+	"fmt"
+	"golang.org/x/net/context"
+	"os/exec"
+	"strings"
 )
-
-// NewClient is a util function to create a new default client.
-// When passed empty string, it loads the default config instead from a config file.
-func NewClient(cf string) (*client.Client, error) {
-	if _, err := os.Stat(cf); err != nil {
-		cf = ""
-	}
-
-	cfg, err := client.LoadClientConfig(cf)
-	if err != nil {
-		return nil, err
-	}
-
-	c, err := client.New(*cfg)
-	if err != nil {
-		return nil, err
-	}
-
-	return c, err
-}
 
 // CreateNewIPPool takes a client.Client with a poolSubnet CIDR (in "192.168.1.0/24" format) with
 // ipip, natOut, and ipam bools for the pool to be setup and creates a new pool.
@@ -77,10 +63,18 @@ func CreateNewIPPool(c client.Client, poolSubnet string, ipip, natOut, ipam bool
 	if err != nil {
 		log.Printf("Error creating pool: %s\n", err)
 	}
-
 }
 
-// CleanIPPools removes all IP pool configuration from the datastore.
+func CreateClient(config api.CalicoAPIConfig) *client.Client {
+	c, err := client.New(config)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return c
+}
+
 func CleanIPPools(c *client.Client) {
 	if pools, err := c.IPPools().List(api.IPPoolMetadata{}); err == nil {
 		for _, pool := range pools.Items {
@@ -88,7 +82,69 @@ func CleanIPPools(c *client.Client) {
 				panic(err)
 			}
 		}
-	} else {
+	}
+}
+
+func CleanDatastore(config api.CalicoAPIConfig) {
+	var err error
+
+	log.Println(fmt.Sprintf("Cleaning datastore: %v", config.Spec.DatastoreType))
+
+	switch config.Spec.DatastoreType {
+	case api.EtcdV2:
+		cfg := etcdclient.Config{
+			Endpoints: []string{config.Spec.EtcdScheme + "://" + config.Spec.EtcdAuthority},
+		}
+		if config.Spec.EtcdEndpoints != "" {
+			cfg = etcdclient.Config{
+				Endpoints: strings.Split(config.Spec.EtcdEndpoints, ","),
+			}
+		}
+		if c, err := etcdclient.New(cfg); c != nil {
+			kapi := etcdclient.NewKeysAPI(c)
+			_, err = kapi.Delete(context.Background(), "/calico", &etcdclient.DeleteOptions{Dir: true, Recursive: true})
+		} else {
+			log.Errorf("Can't create etcd backend %v", err)
+		}
+	default:
+		err = errors.New(fmt.Sprintf("Unknown datastore type: %v", config.Spec.DatastoreType))
+	}
+
+	if err != nil {
 		panic(err)
 	}
+}
+
+// CleanBackend is a utility function to wipe clean "/calico" recursively from backend.
+func CreateCleanClient(config api.CalicoAPIConfig) *client.Client {
+	CleanDatastore(config)
+
+	return CreateClient(config)
+}
+
+// DumpBackend prints out a recursive dump of the contents of backend.
+func DumpBackend(config api.CalicoAPIConfig) error {
+	var output []byte
+	var err error
+
+	log.Println(fmt.Sprintf("Dumping datastore: %v", config.Spec.DatastoreType))
+
+	switch config.Spec.DatastoreType {
+	case api.EtcdV2:
+		output, err = exec.Command("curl", "http://127.0.0.1:2379/v2/keys?recursive=true").Output()
+	default:
+		err = errors.New(fmt.Sprintf("Unknown datastore type: %v", config.Spec.DatastoreType))
+	}
+
+	if err != nil {
+		if ee, ok := err.(*exec.ExitError); ok {
+			log.Printf("Dump backend return error: %s, %v", string(ee.Stderr), *ee.ProcessState)
+		} else {
+			log.Println(err)
+		}
+	} else {
+		log.Println(string(output))
+	}
+
+	return err
 }

projectcalico/calico

lib/backend/backend_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2016 Tigera, Inc. All rights reserved.
+// Copyright (c) 2016-2017 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,54 +18,49 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	"github.com/projectcalico/libcalico-go/lib/backend/etcd"
+	"github.com/projectcalico/libcalico-go/lib/api"
+	bapi "github.com/projectcalico/libcalico-go/lib/backend/api"
 	"github.com/projectcalico/libcalico-go/lib/backend/model"
 	"github.com/projectcalico/libcalico-go/lib/testutils"
 )
 
-// Setting localhost as the etcd endpoint location since that's where `make run-etcd` runs it.
-var etcdConfig = &etcd.EtcdConfig{
-	EtcdEndpoints: "http://127.0.0.1:2379",
-}
-
-var _ = Describe("Backend tests", func() {
-	etcdClient, _ := etcd.NewEtcdClient(etcdConfig)
-
+var _ = testutils.E2eDatastoreDescribe("Backend tests", testutils.DatastoreEtcdV2, func(config api.CalicoAPIConfig) {
 	var (
-		block model.KVPair
-		err   error
+		block  model.KVPair
+		client bapi.Client
+		err    error
 	)
 
 	BeforeEach(func() {
-		testutils.CleanEtcd()
-
+		c := testutils.CreateCleanClient(config)
+		client = c.Backend
 		block = model.KVPair{
 			Key: model.BlockKey{
 				CIDR: testutils.MustParseNetwork("10.0.0.0/26"),
 			},
-			Value: model.AllocationBlock{
+			Value: &model.AllocationBlock{
 				CIDR: testutils.MustParseNetwork("10.0.0.0/26"),
 			},
 		}
 
-		_, err = etcdClient.Create(&block)
+		_, err = client.Create(&block)
 		Expect(err).NotTo(HaveOccurred())
 	})
 
 	Describe("Create", func() {
 
 		It("persists a new kv pair", func() {
-			kv, err := etcdClient.Get(block.Key)
+			kv, err := client.Get(block.Key)
 			Expect(err).NotTo(HaveOccurred())
 
 			expectedCIDR := kv.Value.(*model.AllocationBlock).CIDR
-			persitedCIRD := block.Value.(model.AllocationBlock).CIDR
+			persitedCIDR := block.Value.(*model.AllocationBlock).CIDR
 
-			Expect(expectedCIDR).To(Equal(persitedCIRD))
+			Expect(expectedCIDR).To(Equal(persitedCIDR))
 		})
 
 		It("sets revision field", func() {
-			kv, err := etcdClient.Get(block.Key)
+			kv, err := client.Get(block.Key)
 
 			Expect(err).NotTo(HaveOccurred())
 			Expect(kv.Revision).NotTo(BeNil())
@@ -76,21 +71,21 @@ var _ = Describe("Backend tests", func() {
 	Describe("Update", func() {
 
 		It("updates a kv pair", func() {
-			block.Value = model.AllocationBlock{
+			block.Value = &model.AllocationBlock{
 				CIDR: testutils.MustParseNetwork("192.168.0.0/26"),
 			}
 
-			kv, err := etcdClient.Update(&block)
+			kv, err := client.Update(&block)
 			Expect(err).NotTo(HaveOccurred())
 
-			expectedCIDR := kv.Value.(model.AllocationBlock).CIDR
-			persitedCIRD := block.Value.(model.AllocationBlock).CIDR
+			expectedCIDR := kv.Value.(*model.AllocationBlock).CIDR
+			persitedCIDR := block.Value.(*model.AllocationBlock).CIDR
 
-			Expect(expectedCIDR).To(Equal(persitedCIRD))
+			Expect(expectedCIDR).To(Equal(persitedCIDR))
 		})
 
 		It("sets revision field", func() {
-			kv, err := etcdClient.Update(&block)
+			kv, err := client.Update(&block)
 
 			Expect(err).NotTo(HaveOccurred())
 			Expect(kv.Revision).NotTo(BeNil())
@@ -98,7 +93,7 @@ var _ = Describe("Backend tests", func() {
 
 		It("validates revision field", func() {
 			block.Revision = uint64(1000)
-			_, err := etcdClient.Update(&block)
+			_, err := client.Update(&block)
 			Expect(err).To(HaveOccurred())
 		})
 
@@ -107,17 +102,17 @@ var _ = Describe("Backend tests", func() {
 	Describe("Apply", func() {
 
 		It("updates a kv pair", func() {
-			block.Value = model.AllocationBlock{
+			block.Value = &model.AllocationBlock{
 				CIDR: testutils.MustParseNetwork("192.168.0.0/26"),
 			}
 
-			kv, err := etcdClient.Apply(&block)
+			kv, err := client.Apply(&block)
 			Expect(err).NotTo(HaveOccurred())
 
-			expectedCIDR := kv.Value.(model.AllocationBlock).CIDR
-			persitedCIRD := block.Value.(model.AllocationBlock).CIDR
+			expectedCIDR := kv.Value.(*model.AllocationBlock).CIDR
+			persitedCIDR := block.Value.(*model.AllocationBlock).CIDR
 
-			Expect(expectedCIDR).To(Equal(persitedCIRD))
+			Expect(expectedCIDR).To(Equal(persitedCIDR))
 		})
 
 		It("creates a kv pair", func() {
@@ -125,21 +120,21 @@ var _ = Describe("Backend tests", func() {
 				CIDR: testutils.MustParseNetwork("192.168.0.0/26"),
 			}
 
-			kv, err := etcdClient.Apply(&block)
+			kv, err := client.Apply(&block)
 			Expect(err).NotTo(HaveOccurred())
 
-			expectedCIDR := kv.Value.(model.AllocationBlock).CIDR
-			persitedCIRD := block.Value.(model.AllocationBlock).CIDR
+			expectedCIDR := kv.Value.(*model.AllocationBlock).CIDR
+			persitedCIDR := block.Value.(*model.AllocationBlock).CIDR
 
-			Expect(expectedCIDR).To(Equal(persitedCIRD))
+			Expect(expectedCIDR).To(Equal(persitedCIDR))
 		})
 
 		It("sets revision field", func() {
-			block.Value = model.AllocationBlock{
+			block.Value = &model.AllocationBlock{
 				CIDR: testutils.MustParseNetwork("192.168.0.0/26"),
 			}
 
-			kv, err := etcdClient.Apply(&block)
+			kv, err := client.Apply(&block)
 
 			Expect(err).NotTo(HaveOccurred())
 			Expect(kv.Revision).NotTo(BeNil())
@@ -147,7 +142,7 @@ var _ = Describe("Backend tests", func() {
 
 		It("validates revision field", func() {
 			block.Revision = uint64(1000)
-			_, err := etcdClient.Apply(&block)
+			_, err := client.Apply(&block)
 			Expect(err).NotTo(HaveOccurred())
 		})
 
@@ -156,10 +151,10 @@ var _ = Describe("Backend tests", func() {
 	Describe("Delete", func() {
 
 		It("deletes the kv pair", func() {
-			err := etcdClient.Delete(&block)
+			err := client.Delete(&block)
 			Expect(err).NotTo(HaveOccurred())
 
-			_, err = etcdClient.Get(block.Key)
+			_, err = client.Get(block.Key)
 			Expect(err).To(HaveOccurred())
 		})
 
@@ -170,15 +165,13 @@ var _ = Describe("Backend tests", func() {
 			IPVersion: 4,
 		}
 
-		bl, err := etcdClient.List(blockListOpt)
+		It("List", func() {
+			bl, err := client.List(blockListOpt)
+			Expect(err).NotTo(HaveOccurred())
 
-		for _, blv := range bl {
-			It("sets revision field", func() {
-				Expect(err).NotTo(HaveOccurred())
+			for _, blv := range bl {
 				Expect(blv.Revision).NotTo(BeNil())
-			})
-		}
-
+			}
+		})
 	})
-
 })

lib/client/node_e2e_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2016 Tigera, Inc. All rights reserved.
+// Copyright (c) 2016-2017 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -29,7 +29,6 @@ package client_test
 
 import (
 	"errors"
-	"log"
 	"reflect"
 
 	. "github.com/onsi/ginkgo"
@@ -42,25 +41,18 @@ import (
 	"github.com/projectcalico/libcalico-go/lib/testutils"
 )
 
-var _ = Describe("Node tests", func() {
+var _ = testutils.E2eDatastoreDescribe("Node tests", testutils.DatastoreEtcdV2, func(config api.CalicoAPIConfig) {
 	cidrv4 := testutils.MustParseCIDR("1.2.3.5/24")
 	cidrv6 := testutils.MustParseCIDR("aa::bb00:0001/104")
 	asn := numorstring.ASNumber(12345)
 
 	DescribeTable("Node e2e tests",
 		func(meta1, meta2 api.NodeMetadata, spec1, spec2 api.NodeSpec) {
-			// Erase etcd clean.
-			testutils.CleanEtcd()
-
-			// Create a new client.
-			c, err := testutils.NewClient("")
-			if err != nil {
-				log.Println("Error creating client:", err)
-			}
+			c := testutils.CreateCleanClient(config)
 
 			// Updating non-existent node1 should return an error
 			By("Updating the node before it is created")
-			_, err = c.Nodes().Update(&api.Node{Metadata: meta1, Spec: spec1})
+			_, err := c.Nodes().Update(&api.Node{Metadata: meta1, Spec: spec1})
 			Expect(err.Error()).To(Equal(errors.New("resource does not exist: Node(name=node1)").Error()))
 
 			// Create a new node1 with meta1 and spec1.  This should not error.
@@ -78,7 +70,8 @@ var _ = Describe("Node tests", func() {
 			_, err = c.Nodes().Apply(&api.Node{Metadata: meta2, Spec: spec2})
 			Expect(err).NotTo(HaveOccurred())
 
-			testutils.DumpEtcd()
+			err = testutils.DumpDatastore(config)
+			Expect(err).NotTo(HaveOccurred())
 
 			// Get node1.  This should not error, spec should match spec1.
 			By("Getting node1 and comparing with spec1")
@@ -192,8 +185,7 @@ var _ = Describe("Node tests", func() {
 	)
 
 	Describe("Checking global config is set only once", func() {
-		testutils.CleanEtcd()
-		c, _ := testutils.NewClient("")
+		c := testutils.CreateCleanClient(config)
 		var guidOrig string
 		var guidNew string
 		var set bool
@@ -238,8 +230,7 @@ var _ = Describe("Node tests", func() {
 	})
 
 	Describe("Checking delete/get/list operations perform data validation", func() {
-		testutils.CleanEtcd()
-		c, _ := testutils.NewClient("")
+		c := testutils.CreateCleanClient(config)
 		var err error
 		valErrorType := reflect.TypeOf(cerrors.ErrorValidation{})
 

lib/testutils/client.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2016 Tigera, Inc. All rights reserved.
+// Copyright (c) 2016-2017 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,35 +15,21 @@
 package testutils
 
 import (
-	"log"
 	"net"
-	"os"
 
+	log "github.com/Sirupsen/logrus"
+	etcdclient "github.com/coreos/etcd/client"
 	"github.com/projectcalico/libcalico-go/lib/api"
 	"github.com/projectcalico/libcalico-go/lib/api/unversioned"
 	"github.com/projectcalico/libcalico-go/lib/client"
 	cnet "github.com/projectcalico/libcalico-go/lib/net"
+
+	"errors"
+	"fmt"
+	"golang.org/x/net/context"
+	"os/exec"
+	"strings"
 )
-
-// NewClient is a util function to create a new default client.
-// When passed empty string, it loads the default config instead from a config file.
-func NewClient(cf string) (*client.Client, error) {
-	if _, err := os.Stat(cf); err != nil {
-		cf = ""
-	}
-
-	cfg, err := client.LoadClientConfig(cf)
-	if err != nil {
-		return nil, err
-	}
-
-	c, err := client.New(*cfg)
-	if err != nil {
-		return nil, err
-	}
-
-	return c, err
-}
 
 // CreateNewIPPool takes a client.Client with a poolSubnet CIDR (in "192.168.1.0/24" format) with
 // ipip, natOut, and ipam bools for the pool to be setup and creates a new pool.
@@ -77,10 +63,18 @@ func CreateNewIPPool(c client.Client, poolSubnet string, ipip, natOut, ipam bool
 	if err != nil {
 		log.Printf("Error creating pool: %s\n", err)
 	}
-
 }
 
-// CleanIPPools removes all IP pool configuration from the datastore.
+func CreateClient(config api.CalicoAPIConfig) *client.Client {
+	c, err := client.New(config)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return c
+}
+
 func CleanIPPools(c *client.Client) {
 	if pools, err := c.IPPools().List(api.IPPoolMetadata{}); err == nil {
 		for _, pool := range pools.Items {
@@ -88,7 +82,70 @@ func CleanIPPools(c *client.Client) {
 				panic(err)
 			}
 		}
-	} else {
+	}
+}
+
+func CleanDatastore(config api.CalicoAPIConfig) {
+	var err error
+
+	log.Println(fmt.Sprintf("Cleaning datastore: %v", config.Spec.DatastoreType))
+
+	switch config.Spec.DatastoreType {
+	case api.EtcdV2:
+		cfg := etcdclient.Config{
+			Endpoints: []string{config.Spec.EtcdScheme + "://" + config.Spec.EtcdAuthority},
+		}
+		if config.Spec.EtcdEndpoints != "" {
+			cfg = etcdclient.Config{
+				Endpoints: strings.Split(config.Spec.EtcdEndpoints, ","),
+			}
+		}
+		if c, err := etcdclient.New(cfg); c != nil {
+			kapi := etcdclient.NewKeysAPI(c)
+			_, err = kapi.Delete(context.Background(), "/calico", &etcdclient.DeleteOptions{Dir: true, Recursive: true})
+		} else {
+			log.Errorf("Can't create etcd backend %v", err)
+		}
+	default:
+		err = errors.New(fmt.Sprintf("Unknown datastore type: %v", config.Spec.DatastoreType))
+	}
+
+	if err != nil {
 		panic(err)
 	}
+}
+
+// CreateCleanClient is a utility function to wipe clean "/calico" recursively from backend
+// and to return confugred client to it
+func CreateCleanClient(config api.CalicoAPIConfig) *client.Client {
+	CleanDatastore(config)
+
+	return CreateClient(config)
+}
+
+// DumpDatastore prints out a recursive dump of the contents of backend.
+func DumpDatastore(config api.CalicoAPIConfig) error {
+	var output []byte
+	var err error
+
+	log.Println(fmt.Sprintf("Dumping datastore: %v", config.Spec.DatastoreType))
+
+	switch config.Spec.DatastoreType {
+	case api.EtcdV2:
+		output, err = exec.Command("curl", "http://127.0.0.1:2379/v2/keys?recursive=true").Output()
+	default:
+		err = errors.New(fmt.Sprintf("Unknown datastore type: %v", config.Spec.DatastoreType))
+	}
+
+	if err != nil {
+		if ee, ok := err.(*exec.ExitError); ok {
+			log.Printf("Dump backend return error: %s, %v", string(ee.Stderr), *ee.ProcessState)
+		} else {
+			log.Println(err)
+		}
+	} else {
+		log.Println(string(output))
+	}
+
+	return err
 }