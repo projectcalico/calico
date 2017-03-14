b05062f5adb59e724e7a9aad4c79df769403a59b
Neil Jerram <neil@tigera.io>
2017-03-14 14:55:30 +0000

    Self-review

--- k8sfv/calc_graph.go
+++ k8sfv/calc_graph.go
@@ -20,126 +20,7 @@ import (
 	"k8s.io/client-go/pkg/apis/meta/v1"
 )
 
-// Our objective here is to test the scalability of the Felix calculation graph.
-//
-// The calculation graph is the dataplane-independent part of Felix whose input is the whole Calico
-// data model, and whose output is everything that Felix needs to program on the local host,
-// comprising:
-//
-// - the set of active local endpoints, both workload and host, including the ordered profile and
-//   policy IDs that need to be applied for each of those endpoints (WorkloadEndpointUpdate,
-//   WorkloadEndpointRemove, HostEndpointUpdate, HostEndpointRemove)
-//
-// - the definition of each locally needed profile and policy, using IP set IDs to represent
-//   arbitrary sets of IP addresses, where a profile or policy (implicitly) uses those
-//   (ActivePolicyUpdate, ActivePolicyRemove, ActiveProfileUpdate, ActiveProfileRemove)
-//
-// - the definition of the current set of IP addresses for each IP set ID (IPSetDeltaUpdate,
-//   IPSetUpdate, IPSetRemove)
-//
-// - where IP-in-IP routes are needed to reach other compute hosts (HostMetadataUpdate,
-//   HostMetadataRemove)
-//
-// - where IP masquerading is needed for outgoing endpoint data (IPAMPoolUpdate, IPAMPoolRemove).
-//
-// (The names in brackets here are those of the protobuf messages that carry the corresponding
-// information from the calculation graph to the dataplane.)
-//
-// To narrow the focus further, we are only initially interested in the processing of
-// endpoint-related state that can be fed into Felix by the Kubernetes datastore driver, through the
-// Kubernetes API server.  This means that the interesting inputs become:
-//
-// - k8s namespaces (each of which gets mapped to a Calico profile and a policy)
-//
-// - k8s pods (which get mapped to Calico workload endpoints)
-//
-// - k8s network policies (which get mapped to Calico policies)
-//
-// The interesting outputs are now streamlined to:
-//
-// - the set of active local workload endpoints, including the policy and profile IDs for each
-//   endpoint (WorkloadEndpointUpdate, WorkloadEndpointRemove)
-//
-// - active policy, i.e. the definition of each locally needed profile ID and policy ID
-//   (ActivePolicyUpdate, ActivePolicyRemove, ActiveProfileUpdate, ActiveProfileRemove)
-//
-// - active IP sets, i.e. the definition of the IP addresses for each IP set ID that is referenced
-//   by the active policy (IPSetDeltaUpdate, IPSetUpdate, IPSetRemove).
-//
-// Let's consider what inputs can cause each of those outputs to change, and the complexity of the
-// calculation graph processing involved.
-//
-// The set of active local workload endpoints changes when a pod on the local host is created,
-// updated or deleted, or when a pod is moved to or from the local host.  The processing complexity
-// and effect on the set are 1:1 with the k8s pod input.
-//
-// The active policy set can change: if a pod is added to this host (if its labels match a policy
-// that previously didn't match any other local endpoints); or if a pod is deleted from this host
-// (if its labels were the only local match for a particular policy); or if a local pod's labels are
-// changed such that it matches different policy; or if a local pod's namespace's labels are changed
-// such that it matches different policy; or if the default deny/allow setting of a local pod's
-// namespace changes; or if the content of an active network policy changes; or if the selector of a
-// network policy changes so that it changes between active and inactive.
-//
-// The active IP sets can change: if an active network policy's rules are changed to use different
-// selectors; or if a pod (on any host) is updated and has/had labels matching a source selector of
-// an active network policy; or if a namespace is updated such that its pods has/had labels matching
-// a source selector of an active network policy.
-//
-// Pod added to local host (inc move from remote to local)
-// -> O(1) WorkloadEndpointUpdate New active local workload endpoint
-// -> O(<num defined policies>) ActivePolicyUpdate If pod labels match policies not previously used on this host
-//
-// Pod deleted from local host (inc move from local to remote)
-// -> O(1) WorkloadEndpointRemove Removed active local workload endpoint
-// -> O(1 or <num defined policies>) ActivePolicyRemove If pod labels matched policies that are otherwise not needed
-//
-// Pod on local host updated (inc changing its namespace name)
-// -> O(1) WorkloadEndpointUpdate Update content (addrs, policy/profile IDs) for local workload endpoint
-// -> O(<num defined policies>) ActivePolicyUpdate If new pod labels match policies not previously used on this host
-// -> O(1 or <num defined policies>) ActivePolicyRemove If old pod labels matched policies that are otherwise not needed
-//
-// Pod on any host is created or deleted, or has its labels updated
-// -> O(<num active policies> * <num source selectors in each policy>) IPSetUpdate/DeltaUpdate/Remove to create/update/delete IP sets, if pod labels changing.
-//
-// Namespace labels updated
-// -> O(<num local pods defined in that namespace>) <Pod on local host updated>
-// -> O(<num pods in namespace> * <num active policies> * <num source selectors in each policy>) IPSetUpdate/DeltaUpdate/Remove to create/update/delete IP sets, if pod labels changing.
-//
-// Namespace default deny/allow changed
-// -> O(<num local pods defined in that namespace>) ActivePolicyUpdate to change the default for the namespace-policy.
-//
-// Network policy selector changed
-// -> O(<num active local endpoints>) WorkloadEndpointUpdate if policy now applies, and before didn't, or vice versa.
-// -> O(<num active local endpoints>) <Active network policy rules changed> if policy didn't apply at all, but now does.
-// -> O(<num active local endpoints>) ActivePolicyRemove if policy now doesn't apply at all, but previously did.
-// -> O(<num active local endpoints> + <num source selectors in policy>) IPSetRemove if policy now doesn't apply at all, but previously did.
-//
-// Active network policy rules changed
-// -> O(1) ActivePolicyUpdate with new rules
-// -> O(<num changed source selectors in policy>) IPSetUpdate/DeltaUpdate/Remove to create/update/delete IP sets.
-//
-// So, some possible tests:
-//
-// 50 hosts (1 local + 49 remote)
-// 20 namespaces
-//  area=area1/2/3/4/5
-//  maturity=production/test/staging/experimental/out-of-service
-// 50 pods per namespace
-//  role=role1/2/3/4/5
-//  instance=instance1/2/3/4/5
-//  ha=active/backup
-//
-// Rotate maturity labels on the namespace, e.g. change all 'production' to 'out-of-service', then all 'staging' to 'production'.
-//
-// Churn pods, i.e. delete and recreate (with same properties), in a ring.
-//
-// Create and delete network policies, each with:
-//  selector = random set of labels to work on, random ops and values
-//  between 1 and 10 rules, each with random source selector (as above) and ports
-// Create say 10 of those, then churn by deleting the oldest, making a new one, etc.
-
-func calcGraph1(clientset *kubernetes.Clientset) error {
+func rotateLabels(clientset *kubernetes.Clientset) error {
 	nsMaturity := map[string]string{}
 	maturities := []string{"production", "test", "staging", "experimental"}
 
@@ -188,7 +69,8 @@ func calcGraph1(clientset *kubernetes.Clientset) error {
 	for _ = range nsMaturity {
 		<-waiter
 	}
-	// Churn the namespace labels.
+
+	// Rotate the namespace labels.
 	changeFrom := append(maturities, "out-of-service")
 	changeTo := append([]string{"out-of-service"}, maturities...)
 	for ii := range changeFrom {
--- k8sfv/scale_test.go
+++ k8sfv/scale_test.go
@@ -16,9 +16,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"math/rand"
-	"strconv"
 	"time"
 
 	log "github.com/Sirupsen/logrus"
@@ -27,96 +25,24 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
-func getMac() string {
-	// Gets a random mac address.
-	return fmt.Sprintf(
-		"%02x:%02x:%02x:%02x:%02x:%02x",
-		rand.Intn(256),
-		rand.Intn(256),
-		rand.Intn(256),
-		rand.Intn(256),
-		rand.Intn(256),
-		rand.Intn(256),
-	)
-}
+var _ = Describe("testing the test code", func() {
 
-func addEndpoints(clientset *kubernetes.Clientset, d deployment, numEndpoints int) {
-	//last_time = time.Now()
-	for i := 0; i < numEndpoints; i++ {
-		endpoint_id := fmt.Sprintf("endpoint_%d", i+1)
-
-		ip := fmt.Sprintf("%d.%d.%d.%d",
-			[]int{10, 52, 1}[rand.Intn(3)],
-			i/65536,
-			(i/256)%256,
-			i%256,
-		)
-
-		nsName := fmt.Sprintf("prof-%03d", i%1000)
-		createPod(clientset, d, nsName, podSpec{
-			mac:      getMac(),
-			ipv4Addr: ip,
-			ipv6Addr: fmt.Sprintf("dead:beef::%x/128", i%65536),
-			name:     fmt.Sprintf("%6.6d", i),
-			labels: map[string]string{
-				"foo":     "bar",
-				"alpha":   []string{"a", "b", "c", "d", "e", "f", "g", "h"}[rand.Intn(8)],
-				"id":      endpoint_id,
-				"mod1000": strconv.Itoa(i % 1000),
-				"mod100":  strconv.Itoa(i % 100),
-				"mod10":   strconv.Itoa(i % 10),
-			},
-		})
-	}
-
-	return
-}
-
-//        if random.random() > 0.5:
-//            # Delete first.
-//            path = "/calico/v1/host/%s/workload/%s/%s" % (HOST, ORCH, endpoint_id)
-//            tmp = subprocess.Popen(["etcdctl",  "rm", "--recursive", path],
-//                                   stdout=subprocess.PIPE,
-//                                   stderr=subprocess.PIPE)
-//        path = "/calico/v1/host/%s/workload/%s/%s/endpoint/%s" % (HOST, ORCH, endpoint_id, endpoint_id)
-//        tmp = subprocess.Popen(["etcdctl",  "set", path, json.dumps(data)],
-//                               stdout=subprocess.PIPE)
-//
-//        if (i + 1) % 100 == 0:
-//            now = time.time()
-//            duration = now - last_time
-//            rate = 100 / duration
-//            _log.info("Just done endpoint %d (%.1f/s)", i + 1, rate)
-//            last_time = now
-
-func addNamespaces(clientset *kubernetes.Clientset) {
-	for ii := 0; ii < 1000; ii++ {
-		nsName := fmt.Sprintf("prof-%03d", ii%1000)
-		createNamespace(clientset, nsName, nil)
-	}
-	return
-}
-
-var _ = Describe("100k scale test", func() {
-
-	var (
-		clientset *kubernetes.Clientset
-	)
-
-	BeforeEach(func() {
-		clientset = initialize(flag.Arg(0))
-	})
-
-	It("should get expected value from getMac", func() {
+	It("should get non-nil value from getMac", func() {
 		m := getMac()
 		log.WithField("mac", m).Info("Generated MAC address")
 		Expect(m).ToNot(BeNil())
 	})
+})
 
-	It("should create 100k endpoints", func() {
-		d := NewDeployment(1, false)
-		addNamespaces(clientset)
-		addEndpoints(clientset, d, 100000)
+var _ = Context("with a k8s clientset", func() {
+
+	var (
+		clientset *kubernetes.Clientset
+		d         deployment
+	)
+
+	BeforeEach(func() {
+		clientset = initialize(flag.Arg(0))
 	})
 
 	AfterEach(func() {
@@ -124,62 +50,81 @@ var _ = Describe("100k scale test", func() {
 		cleanupAll(clientset, "prof-")
 	})
 
-	It("should not leak memory", func() {
-		d := NewDeployment(1, false)
-		addNamespaces(clientset)
-		for ii := 0; ii < 10; ii++ {
-			addEndpoints(clientset, d, 10000)
-			time.Sleep(30 * time.Second)
-			cleanupAllPods(clientset, "prof-")
-			time.Sleep(30 * time.Second)
-		}
-		cleanupAll(clientset, "prof-")
-	})
+	Context("with 1 remote node", func() {
 
-	It("should handle a local endpoint", func() {
-		d := NewDeployment(0, true)
-		createPod(clientset, d, "test", podSpec{})
-		time.Sleep(3600 * time.Second)
-	})
+		BeforeEach(func() {
+			d = NewDeployment(1, false)
+		})
 
-	It("should handle 10 local endpoints", func() {
-		d := NewDeployment(0, true)
-		for ii := 0; ii < 10; ii++ {
-			createPod(clientset, d, "test", podSpec{})
-		}
-		time.Sleep(3600 * time.Second)
-	})
+		It("should create 100k endpoints", func() {
+			addNamespaces(clientset)
+			addEndpoints(clientset, d, 100000)
+		})
 
-	It("should handle 100 local endpoints", func() {
-		d := NewDeployment(0, true)
-		for ii := 0; ii < 100; ii++ {
-			createPod(clientset, d, "test", podSpec{})
-			//time.Sleep(10 * time.Millisecond)
-		}
-		time.Sleep(3600 * time.Second)
-	})
-
-	It("should handle 1000 local endpoints", func() {
-		d := NewDeployment(0, true)
-		for ii := 0; ii < 1000; ii++ {
-			createPod(clientset, d, "test", podSpec{})
-			time.Sleep(500 * time.Millisecond)
-		}
-		time.Sleep(3600 * time.Second)
-	})
-
-	It("10 nodes 1000 pods", func() {
-		d := NewDeployment(9, true)
-		createNamespace(clientset, "scale", nil)
-		for cycle := 0; cycle < 10; cycle++ {
-			for ii := 0; ii < 1000; ii++ {
-				createPod(clientset, d, "scale", podSpec{})
-				time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		It("should not leak memory", func() {
+			addNamespaces(clientset)
+			for ii := 0; ii < 10; ii++ {
+				addEndpoints(clientset, d, 10000)
+				time.Sleep(30 * time.Second)
+				cleanupAllPods(clientset, "prof-")
+				time.Sleep(30 * time.Second)
 			}
-			time.Sleep(5 * time.Second)
-			cleanupAllPods(clientset, "scale")
-			time.Sleep(1 * time.Second)
-		}
-		time.Sleep(3600 * time.Second)
+		})
+	})
+
+	Context("with 1 local node", func() {
+
+		BeforeEach(func() {
+			d = NewDeployment(0, true)
+		})
+
+		It("should handle a local endpoint", func() {
+			createPod(clientset, d, "test", podSpec{})
+			time.Sleep(3600 * time.Second)
+		})
+
+		It("should handle 10 local endpoints", func() {
+			for ii := 0; ii < 10; ii++ {
+				createPod(clientset, d, "test", podSpec{})
+			}
+			time.Sleep(3600 * time.Second)
+		})
+
+		It("should handle 100 local endpoints", func() {
+			for ii := 0; ii < 100; ii++ {
+				createPod(clientset, d, "test", podSpec{})
+				//time.Sleep(10 * time.Millisecond)
+			}
+			time.Sleep(3600 * time.Second)
+		})
+
+		It("should handle 1000 local endpoints", func() {
+			for ii := 0; ii < 1000; ii++ {
+				createPod(clientset, d, "test", podSpec{})
+				time.Sleep(500 * time.Millisecond)
+			}
+			time.Sleep(3600 * time.Second)
+		})
+	})
+
+	Context("with 1 local and 9 remote nodes", func() {
+
+		BeforeEach(func() {
+			d = NewDeployment(9, true)
+		})
+
+		It("should add and remove 1000 pods, of which about 100 on local node", func() {
+			createNamespace(clientset, "scale", nil)
+			for cycle := 0; cycle < 10; cycle++ {
+				for ii := 0; ii < 1000; ii++ {
+					createPod(clientset, d, "scale", podSpec{})
+					time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+				}
+				time.Sleep(5 * time.Second)
+				cleanupAllPods(clientset, "scale")
+				time.Sleep(1 * time.Second)
+			}
+			time.Sleep(3600 * time.Second)
+		})
 	})
 })
