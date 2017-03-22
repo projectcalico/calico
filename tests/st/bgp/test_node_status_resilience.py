630d66a32b16c30af9271048c70a74d01c7b4503
frnkdny <frank@tigera.io>
2017-03-21 17:10:26 -0700

    pkill the bird process, once a new process is started make sure the BGP status state can transition to Established. Validate new workloads can be created and connectivity can be Established.

---
+++ tests/st/bgp/test_node_status_resilience.py
@@ -0,0 +1,132 @@
+# Copyright (c) 2015-2016 Tigera, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import logging
+import time
+
+from nose.plugins.attrib import attr
+
+from tests.st.test_base import TestBase
+from tests.st.utils.docker_host import DockerHost
+from tests.st.utils.constants import (DEFAULT_IPV4_ADDR_1, DEFAULT_IPV4_ADDR_2,
+                                      DEFAULT_IPV4_ADDR_3,
+                                      DEFAULT_IPV4_POOL_CIDR, LARGE_AS_NUM)
+from tests.st.utils.utils import assert_network, assert_profile, \
+    assert_number_endpoints, get_profile_name, ETCD_CA, ETCD_CERT, \
+    ETCD_KEY, ETCD_HOSTNAME_SSL, ETCD_SCHEME, get_ip, check_bird_status, \
+    retry_until_success
+from tests.st.utils.exceptions import CommandExecError
+
+from .peer import ADDITIONAL_DOCKER_OPTIONS
+
+_log = logging.getLogger(__name__)
+_log.setLevel(logging.DEBUG)
+
+class TestBGPBackends(TestBase):
+
+    @attr('slow')
+    def test_bgp_backends(self):
+        """
+        Test using different BGP backends.
+
+        We run a multi-host test for this to test peering between two gobgp
+        backends and a single BIRD backend.
+        """
+        with DockerHost('host1',
+                        additional_docker_options=ADDITIONAL_DOCKER_OPTIONS,
+                        start_calico=False) as host1, \
+             DockerHost('host2',
+                        additional_docker_options=ADDITIONAL_DOCKER_OPTIONS,
+                        start_calico=False) as host2, \
+             DockerHost('host3',
+                        additional_docker_options=ADDITIONAL_DOCKER_OPTIONS,
+                        start_calico=True) as host3:
+
+            # Set the default AS number.
+            host1.calicoctl("config set asNumber %s" % LARGE_AS_NUM)
+
+            # Start host1 using the inherited AS, and host2 using a specified
+            # AS (same as default).  These hosts use the gobgp backend, whereas
+            # host3 uses BIRD.
+            host1.start_calico_node("--backend=gobgp")
+            host2.start_calico_node("--backend=gobgp --as=%s" % LARGE_AS_NUM)
+
+            # Create a network and a couple of workloads on each host.
+            network1 = host1.create_network("subnet1")
+            workload_host1 = host1.create_workload("workload1", network=network1)
+            workload_host2 = host2.create_workload("workload2", network=network1)
+            workload_host3 = host3.create_workload("workload3", network=network1)
+
+            # Allow network to converge
+            self.assert_true(workload_host1.check_can_ping(workload_host2.ip, retries=10))
+
+            # Check connectivity in both directions
+            self.assert_ip_connectivity(workload_list=[workload_host1,
+                                                       workload_host2,
+                                                       workload_host3],
+                                        ip_pass_list=[workload_host1.ip,
+                                                      workload_host2.ip,
+                                                      workload_host3.ip])
+
+            # Check the BGP status on the BIRD/GoBGP host.
+            _log.debug("==== docker exec -it calico-node ps -a  ====")
+            host3.execute("docker exec -it calico-node ps -a")
+            
+            hosts = [host1, host2, host3]
+            workloads = [workload_host1, workload_host2, workload_host3]
+            def check_connected(): 
+                for target in hosts:
+                    expected = [("node-to-node mesh", h.ip, "Established") for h in hosts if h is not target]
+                    _log.debug("expected : %s", expected)
+                    check_bird_status(target, expected)
+
+            def delete_workload(host, workload):
+                host.calicoctl("ipam release --ip=%s" % workload.ip)
+                host.execute("docker rm -f %s" % workload.name)
+                host.workloads.remove(workload)
+                
+            for iteration in range(1, 4):
+                _log.debug("Iteration %s", iteration) 
+                _log.debug("identify and pkill bird pid") 
+                host3.execute("docker exec -it calico-node pgrep bird")
+                host3.execute("docker exec -it calico-node pkill bird")
+                 
+                _log.debug('check connected and retry until "Established"') 
+                retry_until_success(check_connected, retries=10, ex_class=Exception) 
+                 
+                _log.debug("new bird pid") 
+                host3.execute("docker exec -it calico-node pgrep bird")
+                
+                new_workloads = []
+                extension = 'ip'
+                for workload in workloads:
+                    new_workload = "%s_%s" % (workload,iteration) 
+                    new_workloads.append(new_workload)
+                
+                index = 0
+                for new_workload in new_workloads: 
+                    new_workload = hosts[index].create_workload(new_workload, network=network1) 
+                    print(new_workload.name)
+                    _log.debug("host: %s and workload: %s", hosts[index].name, new_workload.name) 
+
+                    # Check connectivity in both directions
+                    self.assert_ip_connectivity(workload_list=[workload_host1,
+                                                               workload_host2,
+                                                               workload_host3, 
+                                                               new_workload],
+                                                 ip_pass_list=[workload_host1.ip,
+                                                               workload_host2.ip,
+                                                               workload_host3.ip, 
+                                                               new_workload.ip])
+                    delete_workload(hosts[index], new_workload) 
+                    index += 1
