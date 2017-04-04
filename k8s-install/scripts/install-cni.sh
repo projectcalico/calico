75501bf55464a49871089f4606b8da82699ebc5a
Erik Stidham <erik@tigera.io>
2017-04-04 16:00:30 -0700

    Change tmp cfg file path to be absolute

--- k8s-install/scripts/install-cni.sh
+++ k8s-install/scripts/install-cni.sh
@@ -5,17 +5,17 @@
 # - Expects the host CNI network config path to be mounted at /host/etc/cni/net.d.
 # - Expects the desired CNI config in the CNI_NETWORK_CONFIG env variable.
 
-# Ensure all variables are defined. 
-set -u 
+# Ensure all variables are defined.
+set -u
 
-# The directory on the host where CNI networks are installed. Defaults to 
+# The directory on the host where CNI networks are installed. Defaults to
 # /etc/cni/net.d, but can be overridden by setting CNI_NET_DIR.  This is used
 # for populating absolute paths in the CNI network config to assets
 # which are installed in the CNI network config directory.
 HOST_CNI_NET_DIR=${CNI_NET_DIR:-/etc/cni/net.d}
 HOST_SECRETS_DIR=${HOST_CNI_NET_DIR}/calico-tls
 
-# Directory where we expect that TLS assets will be mounted into 
+# Directory where we expect that TLS assets will be mounted into
 # the calico/cni container.
 SECRETS_MOUNT_DIR=${TLS_ASSETS_DIR:-/calico-secrets}
 
@@ -66,7 +66,7 @@ if [ -w "/host/opt/cni/bin/" ]; then
 	echo "CNI plugin version: $(/host/opt/cni/bin/calico -v)"
 fi
 
-# Place them in the secondary location if it exists and 
+# Place them in the secondary location if it exists and
 # is writeable.
 if [ -w "/host/secondary-bin-dir/" ]; then
 	cp /opt/cni/bin/calico /host/secondary-bin-dir/
@@ -85,16 +85,17 @@ if [ -w "/host/secondary-bin-dir/" ]; then
 	echo "CNI plugin version: $(/host/secondary-bin-dir/calico -v)"
 fi
 
+TMP_CONF='/calico.conf.tmp'
 # If specified, overwrite the network configuration file.
 if [ "${CNI_NETWORK_CONFIG:-}" != "" ]; then
-cat >calico.conf.tmp <<EOF
+cat >$TMP_CONF <<EOF
 ${CNI_NETWORK_CONFIG:-}
 EOF
 fi
 
 # Write a kubeconfig file for the CNI plugin.  Do this
 # to skip TLS verification for now.  We should eventually support
-# writing more complete kubeconfig files. This is only used 
+# writing more complete kubeconfig files. This is only used
 # if the provided CNI network config references it.
 cat > /host/etc/cni/net.d/calico-kubeconfig <<EOF
 # Kubeconfig file for Calico CNI plugin.
@@ -105,33 +106,33 @@ clusters:
   cluster:
     insecure-skip-tls-verify: true
 users:
-- name: calico 
+- name: calico
 contexts:
 - name: calico-context
   context:
     cluster: local
-    user: calico 
+    user: calico
 current-context: calico-context
 EOF
 
 # Insert any of the supported "auto" parameters.
 SERVICEACCOUNT_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-sed -i s/__KUBERNETES_SERVICE_HOST__/${KUBERNETES_SERVICE_HOST:-}/g calico.conf.tmp
-sed -i s/__KUBERNETES_SERVICE_PORT__/${KUBERNETES_SERVICE_PORT:-}/g calico.conf.tmp
-sed -i s/__KUBERNETES_NODE_NAME__/${KUBERNETES_NODE_NAME:-$(hostname)}/g calico.conf.tmp
-sed -i s/__SERVICEACCOUNT_TOKEN__/${SERVICEACCOUNT_TOKEN:-}/g calico.conf.tmp
-sed -i s/__KUBECONFIG_FILENAME__/calico-kubeconfig/g calico.conf.tmp
+sed -i s/__KUBERNETES_SERVICE_HOST__/${KUBERNETES_SERVICE_HOST:-}/g $TMP_CONF
+sed -i s/__KUBERNETES_SERVICE_PORT__/${KUBERNETES_SERVICE_PORT:-}/g $TMP_CONF
+sed -i s/__KUBERNETES_NODE_NAME__/${KUBERNETES_NODE_NAME:-$(hostname)}/g $TMP_CONF
+sed -i s/__SERVICEACCOUNT_TOKEN__/${SERVICEACCOUNT_TOKEN:-}/g $TMP_CONF
+sed -i s/__KUBECONFIG_FILENAME__/calico-kubeconfig/g $TMP_CONF
 
 # Use alternative command character "~", since these include a "/".
-sed -i s~__KUBECONFIG_FILEPATH__~${HOST_CNI_NET_DIR}/calico-kubeconfig~g calico.conf.tmp
-sed -i s~__ETCD_CERT_FILE__~${CNI_CONF_ETCD_CERT:-}~g calico.conf.tmp
-sed -i s~__ETCD_KEY_FILE__~${CNI_CONF_ETCD_KEY:-}~g calico.conf.tmp
-sed -i s~__ETCD_CA_CERT_FILE__~${CNI_CONF_ETCD_CA:-}~g calico.conf.tmp
-sed -i s~__ETCD_ENDPOINTS__~${ETCD_ENDPOINTS:-}~g calico.conf.tmp
+sed -i s~__KUBECONFIG_FILEPATH__~${HOST_CNI_NET_DIR}/calico-kubeconfig~g $TMP_CONF
+sed -i s~__ETCD_CERT_FILE__~${CNI_CONF_ETCD_CERT:-}~g $TMP_CONF
+sed -i s~__ETCD_KEY_FILE__~${CNI_CONF_ETCD_KEY:-}~g $TMP_CONF
+sed -i s~__ETCD_CA_CERT_FILE__~${CNI_CONF_ETCD_CA:-}~g $TMP_CONF
+sed -i s~__ETCD_ENDPOINTS__~${ETCD_ENDPOINTS:-}~g $TMP_CONF
 
 # Move the temporary CNI config into place.
 FILENAME=${CNI_CONF_NAME:-10-calico.conf}
-mv calico.conf.tmp /host/etc/cni/net.d/${FILENAME}
+mv $TMP_CONF /host/etc/cni/net.d/${FILENAME}
 echo "Wrote CNI config: $(cat /host/etc/cni/net.d/${FILENAME})"
 
 # Unless told otherwise, sleep forever.
@@ -140,12 +141,12 @@ should_sleep=${SLEEP:-"true"}
 echo "Done configuring CNI.  Sleep=$should_sleep"
 while [ "$should_sleep" == "true"  ]; do
 	# Kubernetes Secrets can be updated.  If so, we need to install the updated
-	# version to the host. Just check the timestamp on the certificate to see if it 
+	# version to the host. Just check the timestamp on the certificate to see if it
 	# has been updated.  A bit hokey, but likely good enough.
 	stat_output=$(stat -c%y ${SECRETS_MOUNT_DIR}/etcd-cert 2>/dev/null)
 	sleep 10;
 	if [ "$stat_output" != "$(stat -c%y ${SECRETS_MOUNT_DIR}/etcd-cert 2>/dev/null)" ]; then
 		echo "Updating installed secrets at: $(date)"
 		cp ${SECRETS_MOUNT_DIR}/* /host/etc/cni/net.d/calico-tls/
-	fi	
+	fi
 done
