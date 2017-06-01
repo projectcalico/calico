fb7e5c914e1758ce3ccb30c27497c3f938556788
Shaun Crampton <shaun@tigera.io>
2017-06-01 18:36:22 +0100

    Force the names of the gob messages to allow for vendoring.

--- pkg/syncproto/sync_proto.go
+++ pkg/syncproto/sync_proto.go
@@ -53,12 +53,14 @@ type MsgKVs struct {
 }
 
 func init() {
-	gob.Register(MsgClientHello{})
-	gob.Register(MsgServerHello{})
-	gob.Register(MsgSyncStatus{})
-	gob.Register(MsgPing{})
-	gob.Register(MsgPong{})
-	gob.Register(MsgKVs{})
+	// We need to use RegisterName here to force the name to be equal, even if this package gets vendored since the
+	// default name would include the vendor directory.
+	gob.RegisterName("github.com/projectcalico/typha/pkg/syncproto.MsgClientHello",MsgClientHello{})
+	gob.RegisterName("github.com/projectcalico/typha/pkg/syncproto.MsgServerHello",MsgServerHello{})
+	gob.RegisterName("github.com/projectcalico/typha/pkg/syncproto.MsgSyncStatus",MsgSyncStatus{})
+	gob.RegisterName("github.com/projectcalico/typha/pkg/syncproto.MsgPing",MsgPing{})
+	gob.RegisterName("github.com/projectcalico/typha/pkg/syncproto.MsgPong",MsgPong{})
+	gob.RegisterName("github.com/projectcalico/typha/pkg/syncproto.MsgKVs",MsgKVs{})
 }
 
 func SerializeUpdate(u api.Update) (kv SerializedUpdate, err error) {
