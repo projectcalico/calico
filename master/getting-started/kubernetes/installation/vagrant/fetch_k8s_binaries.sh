76982357482120d2d81af3c690102ce837099bac
Erik Stidham <erik@tigera.io>
2017-06-07 09:53:59 -0500

    Removed caching capabilities in Vagrant
    - expanded the time that the download on the master is allowed
    - created scripts in _includes to drop into Vagrantfile

--- master/getting-started/kubernetes/installation/vagrant/fetch_k8s_binaries.sh
+++ master/getting-started/kubernetes/installation/vagrant/fetch_k8s_binaries.sh
@@ -1,22 +1,4 @@
-#!/bin/bash
-k8s_ver="v1.6.2"
-
-if [ $# -eq 0 ]; then
-    binaries="kubectl kube-apiserver kube-controller-manager kubelet kube-proxy kube-scheduler"
-else
-    binaries="$@"
-fi
-
-mkdir -p k8s
-
-for x in $binaries; do
-  ver_binary=${x}-$k8s_ver
-  if [ ! -f k8s/${ver_binary}.downloaded ]; then
-    /usr/bin/wget -O k8s/${ver_binary} https://storage.googleapis.com/kubernetes-release/release/$k8s_ver/bin/linux/amd64/$x
-    if [ $? -eq 0 ]; then
-      touch k8s/${ver_binary}.downloaded
-    fi
-  else
-    echo "$x ($k8s_ver) already downloaded"
-  fi
-done
+---
+layout: null
+---
+{% include {{page.version}}/kubernetes/fetch_k8s_binaries.sh %}
