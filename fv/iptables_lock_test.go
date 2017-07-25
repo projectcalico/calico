fa0a5ae2c92d3a43a837275c57ca8515b1068c29
David Wilder <wilder@us.ibm.com>
2017-07-25 16:49:41 -0700

    Do not depend on the returned text string of iptables -w.
    The iptables_lock_test is looking for a specific text string to be
    returned by iptables. Unfortunately the output string varies depending
    on version. Checking the returned status is sufficient for this test.
    This problem was seen on a ppc64le. I verified the change with "make
    fv" on both x86 and ppc64le.
    
    Signed-off-by: David Wilder <wilder@us.ibm.com>

--- fv/iptables_lock_test.go
+++ fv/iptables_lock_test.go
@@ -115,7 +115,7 @@ var _ = Describe("with running container", func() {
 		It("iptables should succeed in getting the lock after 3s", func() {
 			iptCmd := cmdInContainer("iptables", "-w", "3", "-A", "FORWARD")
 			out, err := iptCmd.CombinedOutput()
-			Expect(string(out)).To(ContainSubstring("Another app is currently holding the xtables lock"))
+			log.Printf("iptables output='%s'", out)
 			Expect(err).NotTo(HaveOccurred())
 		})
 
