e066e5da293c34bd6f92acc1f0ac437c8465cfd8
Spike Curtis <spike@tigera.io>
2017-09-18 16:37:33 -0700

    Added support for Unix Domain Sockets

--- dikastes.go
+++ dikastes.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 
 	"tigera.io/dikastes/proto"
 	"tigera.io/dikastes/server"
@@ -14,6 +17,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"k8s.io/apimachinery/pkg/util/validation"
+	"time"
 )
 
 const usage = `Dikastes - the decider.
@@ -23,9 +27,11 @@ Usage:
   dikastes client [options]
 
 Options:
-  -h --help    Show this screen.`
+  -h --help           Show this screen.
+  -l --listen <port>  IP/port to listen on. [default: :50051]
+  -s --socket <sock>  Type of socket [default: tcp]
+  -d --dial <target>  Target to dial. [default: localhost:50051]`
 const version = "0.1"
-const default_port = ":50051"
 
 func main() {
 	log.SetLevel(log.DebugLevel)
@@ -37,7 +43,7 @@ func main() {
 	if arguments["server"].(bool) {
 		runServer(arguments)
 	} else if arguments["client"].(bool) {
-		runClient()
+		runClient(arguments)
 	}
 	return
 }
@@ -48,7 +54,8 @@ func runServer(arguments map[string]interface{}) {
 		println(usage)
 		fmt.Printf("Invalid <label> format: %v", err)
 	}
-	lis, err := net.Listen("tcp", default_port)
+	lis, err := net.Listen(arguments["--socket"].(string), arguments["--listen"].(string))
+	defer lis.Close()
 	if err != nil {
 		log.Fatalf("Unable to listen on %v")
 	}
@@ -59,9 +66,21 @@ func runServer(arguments map[string]interface{}) {
 	}
 	authz.RegisterAuthorizationServer(gs, ds)
 	reflection.Register(gs)
-	if err := gs.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+
+	// Run gRPC server on separate goroutine so we catch any signals and clean up the socket.
+	go func() {
+		if err := gs.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	// Use a buffered channel so we don't miss any signals
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGTERM)
+
+	// Block until a signal is received.
+	s := <-c
+	log.Infof("Got signal:", s)
 }
 
 // Parse labels in the form key=value and return a map.
@@ -85,9 +104,11 @@ func parseLabels(spec []string) (map[string]string, error) {
 	return labels, nil
 }
 
-func runClient() {
-	opts := []grpc.DialOption{grpc.WithInsecure()}
-	conn, err := grpc.Dial("127.0.0.1:50051", opts...)
+func runClient(arguments map[string]interface{}) {
+	opts := []grpc.DialOption{
+		grpc.WithInsecure(),
+		grpc.WithDialer(getDialer(arguments["--socket"].(string)))}
+	conn, err := grpc.Dial(arguments["--dial"].(string), opts...)
 	if err != nil {
 		log.Fatalf("fail to dial: %v", err)
 	}
@@ -102,4 +123,10 @@ func runClient() {
 	}
 	log.Infof("Check response:\n %v", resp)
 	return
+}
+
+func getDialer(proto string) func(string, time.Duration) (net.Conn, error) {
+	return func(target string, timeout time.Duration) (net.Conn, error) {
+		return net.DialTimeout(proto, target, timeout)
+	}
 }
