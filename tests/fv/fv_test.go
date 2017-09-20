20123b8b5718aa13061ab6c5d76fdbc2ceed4b89
Dan Osborne <djosborne10@gmail.com>
2017-09-20 13:07:27 -0700

    Add more fv tests

--- tests/fv/fv_test.go
+++ tests/fv/fv_test.go
@@ -23,6 +23,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/pkg/api/v1"
+	extensions "k8s.io/client-go/pkg/apis/extensions/v1beta1"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -85,52 +86,112 @@ var _ = Describe("PolicyController", func() {
 		time.Sleep(time.Second * 15)
 	})
 
-	It("should create profiles in etcd to represent k8s namespaces", func() {
-		// Create the namespace in Kubernetes
-		ns := &v1.Namespace{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "peanutbutter",
-			},
-			Spec: v1.NamespaceSpec{},
-		}
-		_, err := k8sClient.CoreV1().Namespaces().Create(ns)
-		Expect(err).NotTo(HaveOccurred())
-
-		// Check that the matching profile was created in etcd.
-		Eventually(func() error {
-			_, err = calicoClient.Profiles().Get(api.ProfileMetadata{Name: "k8s_ns.peanutbutter"})
-			return err
-		}).ShouldNot(HaveOccurred())
-	})
-
-	Context("when etcd data is lost", func() {
+	Context("profiles", func() {
+		var profName string
 		BeforeEach(func() {
-			// Write some data
+			nsName := "peanutbutter"
+			profName = "k8s_ns." + nsName
 			ns := &v1.Namespace{
 				ObjectMeta: metav1.ObjectMeta{
-					Name: "peanutbutter",
+					Name: nsName,
+					Labels: map[string]string{
+						"peanut": "butter",
+					},
 				},
 				Spec: v1.NamespaceSpec{},
 			}
 			_, err := k8sClient.CoreV1().Namespaces().Create(ns)
 			Expect(err).NotTo(HaveOccurred())
 
-			// Wait for it to appear in etcd
 			Eventually(func() *api.Profile {
 				profile, _ := calicoClient.Profiles().Get(api.ProfileMetadata{Name: "k8s_ns.peanutbutter"})
 				return profile
 			}).ShouldNot(BeNil())
 		})
-		It("should recover by rewriting the data to etcd ", func() {
-			Stop(apiserver)
-			err := calicoClient.Profiles().Delete(api.ProfileMetadata{Name: "k8s_ns.peanutbutter"})
-			Expect(err).ShouldNot(HaveOccurred())
 
-			Start(apiserver)
+		It("should write new profiles in etcd to match namespaces in k8s ", func() {
+			err := calicoClient.Profiles().Delete(api.ProfileMetadata{Name: profName})
+			Expect(err).ShouldNot(HaveOccurred())
 			Eventually(func() error {
-				_, err := calicoClient.Profiles().Get(api.ProfileMetadata{Name: "k8s_ns.peanutbutter"})
+				_, err := calicoClient.Profiles().Get(api.ProfileMetadata{Name: profName})
 				return err
 			}, time.Second*15, 500*time.Millisecond).ShouldNot(HaveOccurred())
+		})
+		It("should update existing profiles in etcd to match namespaces in k8s", func() {
+			profile, err := calicoClient.Profiles().Update(&api.Profile{Metadata: api.ProfileMetadata{Name: profName}})
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(profile.Metadata.Labels).To(BeNil())
+			Eventually(func() map[string]string {
+				prof, _ := calicoClient.Profiles().Get(api.ProfileMetadata{Name: profName})
+				return prof.Metadata.Labels
+			}, time.Second*15, 500*time.Millisecond).ShouldNot(BeEmpty())
+		})
+	})
+
+	Context("policies", func() {
+		var policyName string
+		var genPolicyName string
+
+		BeforeEach(func() {
+			// Create a Kubernetes NetworkPolicy.
+			policyName = "jelly"
+			genPolicyName = "knp.default.default." + policyName
+			var np *extensions.NetworkPolicy
+			np = &extensions.NetworkPolicy{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      policyName,
+					Namespace: "default",
+				},
+				Spec: extensions.NetworkPolicySpec{
+					PodSelector: metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"fools": "gold",
+						},
+					},
+				},
+			}
+			err := k8sClient.Extensions().RESTClient().
+				Post().
+				Resource("networkpolicies").
+				Namespace("default").
+				Body(np).
+				Do().Error()
+			Expect(err).NotTo(HaveOccurred())
+
+			// Wait for it to appear in Calico's etcd.
+			Eventually(func() *api.Policy {
+				policy, _ := calicoClient.Policies().Get(api.PolicyMetadata{Name: genPolicyName})
+				return policy
+			}).ShouldNot(BeNil())
+		})
+
+		It("should write new policies in etcd to match policies in k8s", func() {
+			// Delete the Policy.
+			err := calicoClient.Policies().Delete(api.PolicyMetadata{Name: genPolicyName})
+			Expect(err).ShouldNot(HaveOccurred())
+
+			// Wait for the policy-controller to write it back.
+			Eventually(func() error {
+				_, err := calicoClient.Policies().Get(api.PolicyMetadata{Name: genPolicyName})
+				return err
+			}, time.Second*15, 500*time.Millisecond).ShouldNot(HaveOccurred())
+		})
+
+		It("should update existing policies in etcd to match policies in k8s", func() {
+			// Change the selector of the policy in etcd.
+			_, err := calicoClient.Policies().Update(&api.Policy{
+				Metadata: api.PolicyMetadata{Name: genPolicyName},
+				Spec: api.PolicySpec{
+					Selector: "calico/k8s_ns == 'default' && ping == 'pong'",
+				},
+			})
+			Expect(err).ShouldNot(HaveOccurred())
+
+			// Wait for the policy-controller to set it back to its original value.
+			Eventually(func() string {
+				p, _ := calicoClient.Policies().Get(api.PolicyMetadata{Name: genPolicyName})
+				return p.Spec.Selector
+			}, time.Second*15, 500*time.Millisecond).ShouldNot(Equal("calico/k8s_ns == 'default' && ping == 'pong'"))
 		})
 	})
 })
