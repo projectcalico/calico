eba923269f5beea45d95c0479648f139a016e54e
Rob Brockbank <rob@tigera.io>
2017-10-11 22:18:56 -0700

    Udate confd to support a Calico client that specifically handles Calico v1 model data.

--- tests/test_suite_etcd.sh
+++ tests/test_suite_etcd.sh
@@ -1,22 +1,44 @@
 #!/bin/bash
 
+# This script runs a set of tests to verify the Calico client implementation of
+# confd.
+#
+# Data is programmed using calicoctl.
+
+# Make sure confd is not running
+pkill -9 confd
+
+# We should exit if any command fails.
+set -e
+
+# Add our bins to the PATH
+PATH=$PATH:/calico/bin:/calico/bin-node
+
+# Get this script directory, and source the common testsuite (which contains the actual test)
 script_dir="$(dirname "$0")"
-source ${script_dir}/utils.sh
+source "$script_dir/test_suite_common.sh"
 
-declare -a TESTS_TO_RUN=( 'mesh/ipip-always'
-                          'mesh/ipip-cross-subnet'
-                          'mesh/ipip-off'
-                          'explicit_peering/global'
-                          'explicit_peering/specific_node' )
+# Set the log output directory and ensure the directory exists.
+export LOGPATH=/tests/logs/etcd
 
-success=0
+# We are using etcdv3.  Set the datastore parms for calicoctl/confd/etcdctl
+export ETCDCTL_API=3
+export DATASTORE_TYPE=etcdv3
+export ETCD_ENDPOINTS=http://127.0.0.2:2379
 
-for test in "${TESTS_TO_RUN[@]}"; do
-    echo "Testing ${test}"
-    if ! . ${script_dir}/test_etcd.sh ${test}; then
-        success=1
-    fi
-    clean_etcd
-done
+# Clean etcd of all data
+echo "Cleaning out etcd and deleting old logs"
+etcdctl del --prefix /calico
 
-exit ${success}
+# Prepopulate etcd with data that cannot be populated through calicoctl.
+echo "Populating etcd with test data that cannot be handled by calicoctl"
+while read data; do
+    etcdctl put $data > /dev/null 2>&1
+done < /tests/mock_data/etcd/block
+
+# Run the tests a few times.
+execute_test_suite
+
+echo "Tests completed successfully"
+
+set +e
