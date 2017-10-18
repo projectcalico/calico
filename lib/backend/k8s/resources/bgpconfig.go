projectcalico/libcalico-go

Add watcher support for KDD CRD backed resources

lib/backend/k8s/resources/bgpconfig.go
@@ -66,7 +66,7 @@ func (_ BGPConfigConverter) NameToKey(name string) (model.Key, error) {
 	}, nil
 }
 
-func (c BGPConfigConverter) ToKVPair(r CustomK8sResource) (*model.KVPair, error) {
+func (c BGPConfigConverter) ToKVPair(r K8sResource) (*model.KVPair, error) {
 	t := r.(*apiv2.BGPConfiguration)
 
 	// Clear any CRD TypeMeta fields and then create a KVPair.
@@ -85,7 +85,7 @@ func (c BGPConfigConverter) ToKVPair(r CustomK8sResource) (*model.KVPair, error)
 	}, nil
 }
 
-func (c BGPConfigConverter) FromKVPair(kvp *model.KVPair) (CustomK8sResource, error) {
+func (c BGPConfigConverter) FromKVPair(kvp *model.KVPair) (K8sResource, error) {
 	v := kvp.Value.(*apiv2.BGPConfiguration)
 
 	crd := apiv2.BGPConfiguration{

lib/backend/k8s/resources/bgppeer.go
@@ -66,7 +66,7 @@ func (_ BGPPeerConverter) NameToKey(name string) (model.Key, error) {
 	}, nil
 }
 
-func (c BGPPeerConverter) ToKVPair(r CustomK8sResource) (*model.KVPair, error) {
+func (c BGPPeerConverter) ToKVPair(r K8sResource) (*model.KVPair, error) {
 	t := r.(*apiv2.BGPPeer)
 
 	// Clear any CRD TypeMeta fields and then create a KVPair.
@@ -85,7 +85,7 @@ func (c BGPPeerConverter) ToKVPair(r CustomK8sResource) (*model.KVPair, error) {
 	}, nil
 }
 
-func (c BGPPeerConverter) FromKVPair(kvp *model.KVPair) (CustomK8sResource, error) {
+func (c BGPPeerConverter) FromKVPair(kvp *model.KVPair) (K8sResource, error) {
 	v := kvp.Value.(*apiv2.BGPPeer)
 
 	return &apiv2.BGPPeer{

lib/backend/k8s/resources/globalnetworkpolicies.go
@@ -66,7 +66,7 @@ func (_ GlobalNetworkPolicyConverter) NameToKey(name string) (model.Key, error) 
 	}, nil
 }
 
-func (c GlobalNetworkPolicyConverter) ToKVPair(r CustomK8sResource) (*model.KVPair, error) {
+func (c GlobalNetworkPolicyConverter) ToKVPair(r K8sResource) (*model.KVPair, error) {
 	t := r.(*apiv2.GlobalNetworkPolicy)
 
 	// Clear any CRD TypeMeta fields and then create a KVPair.
@@ -85,7 +85,7 @@ func (c GlobalNetworkPolicyConverter) ToKVPair(r CustomK8sResource) (*model.KVPa
 	}, nil
 }
 
-func (c GlobalNetworkPolicyConverter) FromKVPair(kvp *model.KVPair) (CustomK8sResource, error) {
+func (c GlobalNetworkPolicyConverter) FromKVPair(kvp *model.KVPair) (K8sResource, error) {
 	v := kvp.Value.(*apiv2.GlobalNetworkPolicy)
 
 	return &apiv2.GlobalNetworkPolicy{

lib/backend/k8s/resources/ippool.go
@@ -66,7 +66,7 @@ func (_ IPPoolConverter) NameToKey(name string) (model.Key, error) {
 	}, nil
 }
 
-func (i IPPoolConverter) ToKVPair(r CustomK8sResource) (*model.KVPair, error) {
+func (i IPPoolConverter) ToKVPair(r K8sResource) (*model.KVPair, error) {
 	t := r.(*apiv2.IPPool)
 
 	// Clear any CRD TypeMeta fields and then create a KVPair.
@@ -86,7 +86,7 @@ func (i IPPoolConverter) ToKVPair(r CustomK8sResource) (*model.KVPair, error) {
 
 }
 
-func (i IPPoolConverter) FromKVPair(kvp *model.KVPair) (CustomK8sResource, error) {
+func (i IPPoolConverter) FromKVPair(kvp *model.KVPair) (K8sResource, error) {
 	v := kvp.Value.(*apiv2.IPPool)
 
 	return &apiv2.IPPool{

projectcalico/calico

lib/backend/k8s/resources/bgpconfig.go
@@ -66,7 +66,7 @@ func (_ BGPConfigConverter) NameToKey(name string) (model.Key, error) {
 	}, nil
 }
 
-func (c BGPConfigConverter) ToKVPair(r CustomK8sResource) (*model.KVPair, error) {
+func (c BGPConfigConverter) ToKVPair(r Resource) (*model.KVPair, error) {
 	t := r.(*apiv2.BGPConfiguration)
 
 	// Clear any CRD TypeMeta fields and then create a KVPair.
@@ -85,7 +85,7 @@ func (c BGPConfigConverter) ToKVPair(r CustomK8sResource) (*model.KVPair, error)
 	}, nil
 }
 
-func (c BGPConfigConverter) FromKVPair(kvp *model.KVPair) (CustomK8sResource, error) {
+func (c BGPConfigConverter) FromKVPair(kvp *model.KVPair) (Resource, error) {
 	v := kvp.Value.(*apiv2.BGPConfiguration)
 
 	crd := apiv2.BGPConfiguration{

lib/backend/k8s/resources/bgppeer.go
@@ -66,7 +66,7 @@ func (_ BGPPeerConverter) NameToKey(name string) (model.Key, error) {
 	}, nil
 }
 
-func (c BGPPeerConverter) ToKVPair(r CustomK8sResource) (*model.KVPair, error) {
+func (c BGPPeerConverter) ToKVPair(r Resource) (*model.KVPair, error) {
 	t := r.(*apiv2.BGPPeer)
 
 	// Clear any CRD TypeMeta fields and then create a KVPair.
@@ -85,7 +85,7 @@ func (c BGPPeerConverter) ToKVPair(r CustomK8sResource) (*model.KVPair, error) {
 	}, nil
 }
 
-func (c BGPPeerConverter) FromKVPair(kvp *model.KVPair) (CustomK8sResource, error) {
+func (c BGPPeerConverter) FromKVPair(kvp *model.KVPair) (Resource, error) {
 	v := kvp.Value.(*apiv2.BGPPeer)
 
 	return &apiv2.BGPPeer{

lib/backend/k8s/resources/globalnetworkpolicies.go
@@ -66,7 +66,7 @@ func (_ GlobalNetworkPolicyConverter) NameToKey(name string) (model.Key, error) 
 	}, nil
 }
 
-func (c GlobalNetworkPolicyConverter) ToKVPair(r CustomK8sResource) (*model.KVPair, error) {
+func (c GlobalNetworkPolicyConverter) ToKVPair(r Resource) (*model.KVPair, error) {
 	t := r.(*apiv2.GlobalNetworkPolicy)
 
 	// Clear any CRD TypeMeta fields and then create a KVPair.
@@ -85,7 +85,7 @@ func (c GlobalNetworkPolicyConverter) ToKVPair(r CustomK8sResource) (*model.KVPa
 	}, nil
 }
 
-func (c GlobalNetworkPolicyConverter) FromKVPair(kvp *model.KVPair) (CustomK8sResource, error) {
+func (c GlobalNetworkPolicyConverter) FromKVPair(kvp *model.KVPair) (Resource, error) {
 	v := kvp.Value.(*apiv2.GlobalNetworkPolicy)
 
 	return &apiv2.GlobalNetworkPolicy{

lib/backend/k8s/resources/ippool.go
@@ -66,7 +66,7 @@ func (_ IPPoolConverter) NameToKey(name string) (model.Key, error) {
 	}, nil
 }
 
-func (i IPPoolConverter) ToKVPair(r CustomK8sResource) (*model.KVPair, error) {
+func (i IPPoolConverter) ToKVPair(r Resource) (*model.KVPair, error) {
 	t := r.(*apiv2.IPPool)
 
 	// Clear any CRD TypeMeta fields and then create a KVPair.
@@ -86,7 +86,7 @@ func (i IPPoolConverter) ToKVPair(r CustomK8sResource) (*model.KVPair, error) {
 
 }
 
-func (i IPPoolConverter) FromKVPair(kvp *model.KVPair) (CustomK8sResource, error) {
+func (i IPPoolConverter) FromKVPair(kvp *model.KVPair) (Resource, error) {
 	v := kvp.Value.(*apiv2.IPPool)
 
 	return &apiv2.IPPool{