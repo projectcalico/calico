d8b8801bfea4977d6b40ba22aa8b1483438478fc
gunjan5 <patelgunjan5@gmail.com>
2017-10-26 15:44:00 -0700

    Remove support for ETCD_AUTHORITY and ETCD_SCHEME

--- lib/apiconfig/apiconfig.go
+++ lib/apiconfig/apiconfig.go
@@ -48,8 +48,6 @@ type CalicoAPIConfigSpec struct {
 }
 
 type EtcdConfig struct {
-	EtcdScheme     string `json:"etcdScheme" envconfig:"ETCD_SCHEME" default:"http"`
-	EtcdAuthority  string `json:"etcdAuthority" envconfig:"ETCD_AUTHORITY" default:"127.0.0.1:2379"`
 	EtcdEndpoints  string `json:"etcdEndpoints" envconfig:"ETCD_ENDPOINTS"`
 	EtcdUsername   string `json:"etcdUsername" envconfig:"ETCD_USERNAME"`
 	EtcdPassword   string `json:"etcdPassword" envconfig:"ETCD_PASSWORD"`
