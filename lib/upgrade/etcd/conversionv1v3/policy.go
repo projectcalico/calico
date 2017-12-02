gunjan5/libcalico-go

Add v1 to v3 conversion code and tests for Policy to GNP

lib/upgrade/etcd/conversionv1v3/policy.go
@@ -1,0 +1,130 @@
+// Copyright (c) 2017 Tigera, Inc. All rights reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package conversionv1v3
+
+import (
+	"fmt"
+	"strings"
+
+	apiv1 "github.com/projectcalico/libcalico-go/lib/apis/v1"
+	"github.com/projectcalico/libcalico-go/lib/apis/v1/unversioned"
+	apiv3 "github.com/projectcalico/libcalico-go/lib/apis/v3"
+	"github.com/projectcalico/libcalico-go/lib/backend/model"
+)
+
+// Policy implements the Converter interface.
+type Policy struct{}
+
+// APIV1ToBackendV1 converts v1 Policy API to v1 Policy KVPair.
+func (_ Policy) APIV1ToBackendV1(a unversioned.Resource) (*model.KVPair, error) {
+	ap := a.(apiv1.Policy)
+
+	d := model.KVPair{
+		Key: model.PolicyKey{
+			Name: ap.Metadata.Name,
+		},
+		Value: &model.Policy{
+			Order:          ap.Spec.Order,
+			InboundRules:   rulesAPIToBackend(ap.Spec.IngressRules),
+			OutboundRules:  rulesAPIToBackend(ap.Spec.EgressRules),
+			Selector:       ap.Spec.Selector,
+			DoNotTrack:     ap.Spec.DoNotTrack,
+			Annotations:    ap.Metadata.Annotations,
+			PreDNAT:        ap.Spec.PreDNAT,
+			ApplyOnForward: ap.Spec.ApplyOnForward,
+			Types:          nil, // filled in below
+		},
+	}
+
+	if len(ap.Spec.Types) == 0 {
+		// Default the Types field according to what inbound and outbound rules are present
+		// in the policy.
+		if len(ap.Spec.EgressRules) == 0 {
+			// Policy has no egress rules, so apply this policy to ingress only.  (Note:
+			// intentionally including the case where the policy also has no ingress
+			// rules.)
+			d.Value.(*model.Policy).Types = []string{string(apiv1.PolicyTypeIngress)}
+		} else if len(ap.Spec.IngressRules) == 0 {
+			// Policy has egress rules but no ingress rules, so apply this policy to
+			// egress only.
+			d.Value.(*model.Policy).Types = []string{string(apiv1.PolicyTypeEgress)}
+		} else {
+			// Policy has both ingress and egress rules, so apply this policy to both
+			// ingress and egress.
+			d.Value.(*model.Policy).Types = []string{string(apiv1.PolicyTypeIngress), string(apiv1.PolicyTypeEgress)}
+		}
+	} else {
+		// Convert from the API-specified Types.
+		d.Value.(*model.Policy).Types = make([]string, len(ap.Spec.Types))
+		for i, t := range ap.Spec.Types {
+			d.Value.(*model.Policy).Types[i] = string(t)
+		}
+	}
+
+	return &d, nil
+}
+
+// BackendV1ToAPIV3 converts v1 Policy KVPair to v3 API.
+func (_ Policy) BackendV1ToAPIV3(kvp *model.KVPair) (Resource, error) {
+	bp, ok := kvp.Value.(*model.Policy)
+	if !ok {
+		return nil, fmt.Errorf("value is not a valid Policy resource")
+	}
+	bk, ok := kvp.Key.(model.PolicyKey)
+	if !ok {
+		return nil, fmt.Errorf("value is not a valid Policy resource key")
+	}
+
+	ap := apiv3.NewGlobalNetworkPolicy()
+	ap.Name = convertName(bk.Name)
+	ap.Annotations = bp.Annotations
+	ap.Spec.Order = bp.Order
+	ap.Spec.Ingress = rulesV1BackendToV3API(bp.InboundRules)
+	ap.Spec.Egress = rulesV1BackendToV3API(bp.OutboundRules)
+	ap.Spec.Selector = bp.Selector
+	ap.Spec.DoNotTrack = bp.DoNotTrack
+	ap.Spec.PreDNAT = bp.PreDNAT
+	ap.Spec.ApplyOnForward = bp.ApplyOnForward
+	ap.Spec.Types = nil // Set later.
+
+	if !bp.ApplyOnForward && (bp.DoNotTrack || bp.PreDNAT) {
+		// This case happens when there is a pre-existing policy in the datastore, from before
+		// the ApplyOnForward feature was available. DoNotTrack or PreDNAT policy applies to
+		// forward traffic by nature. So in this case we return ApplyOnForward flag as true.
+		ap.Spec.ApplyOnForward = true
+	}
+
+	if len(bp.Types) == 0 {
+		// This case happens when there is a pre-existing policy in an etcd datastore, from
+		// before the explicit Types feature was available.  Calico's previous behaviour was
+		// always to apply policy to both ingress and egress traffic, so in this case we
+		// return Types as [ ingress, egress ].
+		ap.Spec.Types = []apiv3.PolicyType{apiv3.PolicyTypeIngress, apiv3.PolicyTypeEgress}
+	} else {
+		// Convert from the backend-specified Types.
+		ap.Spec.Types = make([]apiv3.PolicyType, len(bp.Types))
+		for i, t := range bp.Types {
+			if strings.ToLower(t) == "ingress" {
+				ap.Spec.Types[i] = apiv3.PolicyTypeIngress
+			} else if strings.ToLower(t) == "egress" {
+				ap.Spec.Types[i] = apiv3.PolicyTypeEgress
+			} else {
+				return nil, fmt.Errorf("invalid policy type: '%s'", t)
+			}
+		}
+	}
+
+	return ap, nil
+}

lib/upgrade/etcd/conversionv1v3/policy_test.go
@@ -1,0 +1,297 @@
+// Copyright (c) 2017 Tigera, Inc. All rights reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package conversionv1v3
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	apiv1 "github.com/projectcalico/libcalico-go/lib/apis/v1"
+	"github.com/projectcalico/libcalico-go/lib/apis/v1/unversioned"
+	apiv3 "github.com/projectcalico/libcalico-go/lib/apis/v3"
+	"github.com/projectcalico/libcalico-go/lib/backend/model"
+)
+
+var order1 = 1000.00
+var order2 = 999.99
+var policyTable = []struct {
+	description string
+	v1API       unversioned.Resource
+	v1KVP       *model.KVPair
+	v3API       apiv3.GlobalNetworkPolicy
+}{
+	{
+		description: "fully populated Policy",
+		v1API: apiv1.Policy{
+			Metadata: apiv1.PolicyMetadata{
+				Name: "namyMcPolicyName",
+			},
+			Spec: apiv1.PolicySpec{
+				Order:          &order1,
+				IngressRules:   []apiv1.Rule{V1InRule1, V1InRule2},
+				EgressRules:    []apiv1.Rule{V1EgressRule1, V1EgressRule2},
+				Selector:       "thing == 'value'",
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true,
+				Types:          []apiv1.PolicyType{apiv1.PolicyTypeIngress},
+			},
+		},
+		v1KVP: &model.KVPair{
+			Key: model.PolicyKey{
+				Name: "namyMcPolicyName",
+			},
+			Value: &model.Policy{
+				Order:          &order1,
+				InboundRules:   []model.Rule{V1ModelInRule1, V1ModelInRule2},
+				OutboundRules:  []model.Rule{V1ModelEgressRule1, V1ModelEgressRule2},
+				Selector:       "thing == 'value'",
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true,
+				Types:          []string{"ingress"},
+			},
+		},
+		v3API: apiv3.GlobalNetworkPolicy{
+			ObjectMeta: v1.ObjectMeta{
+				Name: "namymcpolicyname",
+			},
+			Spec: apiv3.GlobalNetworkPolicySpec{
+				Order:          &order1,
+				Ingress:        []apiv3.Rule{V3InRule1, V3InRule2},
+				Egress:         []apiv3.Rule{V3EgressRule1, V3EgressRule2},
+				Selector:       "thing == 'value'",
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true,
+				Types:          []apiv3.PolicyType{apiv3.PolicyTypeIngress},
+			},
+		},
+	},
+	{
+		description: "policy name conversion",
+		v1API: apiv1.Policy{
+			Metadata: apiv1.PolicyMetadata{
+				Name: "MaKe.-.MaKe",
+			},
+			Spec: apiv1.PolicySpec{
+				Order:          &order1,
+				IngressRules:   []apiv1.Rule{V1InRule2},
+				EgressRules:    []apiv1.Rule{V1EgressRule1},
+				Selector:       "thing == 'value'",
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true,
+				Types:          []apiv1.PolicyType{apiv1.PolicyTypeIngress},
+			},
+		},
+		v1KVP: &model.KVPair{
+			Key: model.PolicyKey{
+				Name: "MaKe.-.MaKe",
+			},
+			Value: &model.Policy{
+				Order:          &order1,
+				InboundRules:   []model.Rule{V1ModelInRule2},
+				OutboundRules:  []model.Rule{V1ModelEgressRule1},
+				Selector:       "thing == 'value'",
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true,
+				Types:          []string{"ingress"},
+			},
+		},
+		v3API: apiv3.GlobalNetworkPolicy{
+			ObjectMeta: v1.ObjectMeta{
+				Name: "make.make",
+			},
+			Spec: apiv3.GlobalNetworkPolicySpec{
+				Order:          &order1,
+				Ingress:        []apiv3.Rule{V3InRule2},
+				Egress:         []apiv3.Rule{V3EgressRule1},
+				Selector:       "thing == 'value'",
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true,
+				Types:          []apiv3.PolicyType{apiv3.PolicyTypeIngress},
+			},
+		},
+	},
+	{
+		description: "policy with ApplyOnForward set to it's zero value (missing), and PreDNAT and DoNotTrack set to true " +
+			"should convert ApplyOnForward to true in v3 API",
+		v1API: apiv1.Policy{
+			Metadata: apiv1.PolicyMetadata{
+				Name: "RAWR",
+			},
+			Spec: apiv1.PolicySpec{
+				Order:          &order1,
+				IngressRules:   []apiv1.Rule{V1InRule2},
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: false,
+				Types:          []apiv1.PolicyType{apiv1.PolicyTypeIngress},
+			},
+		},
+		v1KVP: &model.KVPair{
+			Key: model.PolicyKey{
+				Name: "RAWR",
+			},
+			Value: &model.Policy{
+				Order:          &order1,
+				InboundRules:   []model.Rule{V1ModelInRule2},
+				OutboundRules:  []model.Rule{},
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: false,
+				Types:          []string{"ingress"},
+			},
+		},
+		v3API: apiv3.GlobalNetworkPolicy{
+			ObjectMeta: v1.ObjectMeta{
+				Name: "rawr",
+			},
+			Spec: apiv3.GlobalNetworkPolicySpec{
+				Order:          &order1,
+				Ingress:        []apiv3.Rule{V3InRule2},
+				Egress:         []apiv3.Rule{},
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true, // notice this gets converted to true, because DoNotTrack and PreDNAT are true.
+				Types:          []apiv3.PolicyType{apiv3.PolicyTypeIngress},
+			},
+		},
+	},
+	{
+		description: "policy with ApplyOnForward set to it's zero value (missing), and PreDNAT and DoNotTrack both " +
+			"set to false should NOT convert ApplyOnForward to true in v3 API",
+		v1API: apiv1.Policy{
+			Metadata: apiv1.PolicyMetadata{
+				Name: "meow",
+			},
+			Spec: apiv1.PolicySpec{
+				Order:          &order1,
+				IngressRules:   []apiv1.Rule{V1InRule2},
+				DoNotTrack:     false,
+				PreDNAT:        false,
+				ApplyOnForward: true,
+				Types:          []apiv1.PolicyType{apiv1.PolicyTypeIngress},
+			},
+		},
+		v1KVP: &model.KVPair{
+			Key: model.PolicyKey{
+				Name: "meow",
+			},
+			Value: &model.Policy{
+				Order:          &order1,
+				InboundRules:   []model.Rule{V1ModelInRule2},
+				OutboundRules:  []model.Rule{},
+				DoNotTrack:     false,
+				PreDNAT:        false,
+				ApplyOnForward: true,
+				Types:          []string{"ingress"},
+			},
+		},
+		v3API: apiv3.GlobalNetworkPolicy{
+			ObjectMeta: v1.ObjectMeta{
+				Name: "meow",
+			},
+			Spec: apiv3.GlobalNetworkPolicySpec{
+				Order:          &order1,
+				Ingress:        []apiv3.Rule{V3InRule2},
+				Egress:         []apiv3.Rule{},
+				DoNotTrack:     false,
+				PreDNAT:        false,
+				ApplyOnForward: true, // notice this gets converted to true, because DoNotTrack and PreDNAT are true.
+				Types:          []apiv3.PolicyType{apiv3.PolicyTypeIngress},
+			},
+		},
+	},
+	{
+		description: "policy with non-strictly masked CIDR should get converted to strictly masked CIDR in v3 API",
+		v1API: apiv1.Policy{
+			Metadata: apiv1.PolicyMetadata{
+				Name: "MaKe.-.MaKe",
+			},
+			Spec: apiv1.PolicySpec{
+				Order: &order1,
+				// Source Nets selector in V1InRule1 and V1EgressRule1 are non-strictly masked CIDRs.
+				IngressRules:   []apiv1.Rule{V1InRule1},
+				EgressRules:    []apiv1.Rule{V1EgressRule1},
+				Selector:       "thing == 'value'",
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true,
+				Types:          []apiv1.PolicyType{apiv1.PolicyTypeIngress, apiv1.PolicyTypeEgress},
+			},
+		},
+		v1KVP: &model.KVPair{
+			Key: model.PolicyKey{
+				Name: "MaKe.-.MaKe",
+			},
+			Value: &model.Policy{
+				Order: &order1,
+				// Source Nets selector in V1ModelInRule1 and V1ModelEgressRule1 are non-strictly masked CIDRs.
+				InboundRules:   []model.Rule{V1ModelInRule1},
+				OutboundRules:  []model.Rule{V1ModelEgressRule1},
+				Selector:       "thing == 'value'",
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true,
+				Types:          []string{"ingress", "egress"},
+			},
+		},
+		v3API: apiv3.GlobalNetworkPolicy{
+			ObjectMeta: v1.ObjectMeta{
+				Name: "make.make",
+			},
+			Spec: apiv3.GlobalNetworkPolicySpec{
+				Order: &order1,
+				// Source Nets selector in V3InRule1 and V3EgressRule1 are strictly masked CIDRs.
+				Ingress:        []apiv3.Rule{V3InRule1},
+				Egress:         []apiv3.Rule{V3EgressRule1},
+				Selector:       "thing == 'value'",
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true,
+				Types:          []apiv3.PolicyType{apiv3.PolicyTypeIngress, apiv3.PolicyTypeEgress},
+			},
+		},
+	},
+}
+
+func TestCanConvertV1ToV3Policy(t *testing.T) {
+
+	for _, entry := range policyTable {
+		t.Run(entry.description, func(t *testing.T) {
+			RegisterTestingT(t)
+
+			p := Policy{}
+
+			// Test and assert v1 API to v1 backend logic.
+			v1KVPResult, err := p.APIV1ToBackendV1(entry.v1API)
+			Expect(err).NotTo(HaveOccurred(), entry.description)
+			Expect(v1KVPResult.Key.(model.PolicyKey).Name).To(Equal(entry.v1KVP.Key.(model.PolicyKey).Name))
+			Expect(v1KVPResult.Value.(*model.Policy)).To(Equal(entry.v1KVP.Value))
+
+			// Test and assert v1 backend to v3 API logic.
+			v3APIResult, err := p.BackendV1ToAPIV3(entry.v1KVP)
+			Expect(err).NotTo(HaveOccurred(), entry.description)
+			Expect(v3APIResult.(*apiv3.GlobalNetworkPolicy).Name).To(Equal(entry.v3API.Name), entry.description)
+			Expect(v3APIResult.(*apiv3.GlobalNetworkPolicy).Spec).To(Equal(entry.v3API.Spec), entry.description)
+		})
+	}
+}

projectcalico/calico

lib/upgrade/etcd/conversionv1v3/policy.go
@@ -1,0 +1,142 @@
+// Copyright (c) 2017 Tigera, Inc. All rights reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package conversionv1v3
+
+import (
+	"fmt"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+
+	apiv1 "github.com/projectcalico/libcalico-go/lib/apis/v1"
+	"github.com/projectcalico/libcalico-go/lib/apis/v1/unversioned"
+	apiv3 "github.com/projectcalico/libcalico-go/lib/apis/v3"
+	"github.com/projectcalico/libcalico-go/lib/backend/model"
+)
+
+// Policy implements the Converter interface.
+type Policy struct{}
+
+// APIV1ToBackendV1 converts v1 Policy API to v1 Policy KVPair.
+func (_ Policy) APIV1ToBackendV1(a unversioned.Resource) (*model.KVPair, error) {
+	ap := a.(apiv1.Policy)
+
+	d := model.KVPair{
+		Key: model.PolicyKey{
+			Name: ap.Metadata.Name,
+		},
+		Value: &model.Policy{
+			Order:          ap.Spec.Order,
+			InboundRules:   rulesAPIV1ToBackend(ap.Spec.IngressRules),
+			OutboundRules:  rulesAPIV1ToBackend(ap.Spec.EgressRules),
+			Selector:       ap.Spec.Selector,
+			DoNotTrack:     ap.Spec.DoNotTrack,
+			Annotations:    ap.Metadata.Annotations,
+			PreDNAT:        ap.Spec.PreDNAT,
+			ApplyOnForward: ap.Spec.ApplyOnForward,
+			Types:          nil, // filled in below
+		},
+	}
+
+	if len(ap.Spec.Types) == 0 {
+		// Default the Types field according to what inbound and outbound rules are present
+		// in the policy.
+		if len(ap.Spec.EgressRules) == 0 {
+			// Policy has no egress rules, so apply this policy to ingress only.  (Note:
+			// intentionally including the case where the policy also has no ingress
+			// rules.)
+			d.Value.(*model.Policy).Types = []string{string(apiv1.PolicyTypeIngress)}
+		} else if len(ap.Spec.IngressRules) == 0 {
+			// Policy has egress rules but no ingress rules, so apply this policy to
+			// egress only.
+			d.Value.(*model.Policy).Types = []string{string(apiv1.PolicyTypeEgress)}
+		} else {
+			// Policy has both ingress and egress rules, so apply this policy to both
+			// ingress and egress.
+			d.Value.(*model.Policy).Types = []string{string(apiv1.PolicyTypeIngress), string(apiv1.PolicyTypeEgress)}
+		}
+	} else {
+		// Convert from the API-specified Types.
+		d.Value.(*model.Policy).Types = make([]string, len(ap.Spec.Types))
+		for i, t := range ap.Spec.Types {
+			d.Value.(*model.Policy).Types[i] = string(t)
+		}
+	}
+
+	log.WithFields(log.Fields{
+		"APIv1":  ap,
+		"KVPair": d,
+	}).Debugf("Converted Policy '%s' V1 API to V1 backend", ap.Metadata.Name)
+
+	return &d, nil
+}
+
+// BackendV1ToAPIV3 converts v1 Policy KVPair to v3 API.
+func (_ Policy) BackendV1ToAPIV3(kvp *model.KVPair) (Resource, error) {
+	bp, ok := kvp.Value.(*model.Policy)
+	if !ok {
+		return nil, fmt.Errorf("value is not a valid Policy resource")
+	}
+	bk, ok := kvp.Key.(model.PolicyKey)
+	if !ok {
+		return nil, fmt.Errorf("value is not a valid Policy resource key")
+	}
+
+	ap := apiv3.NewGlobalNetworkPolicy()
+	ap.Name = convertName(bk.Name)
+	ap.Annotations = bp.Annotations
+	ap.Spec.Order = bp.Order
+	ap.Spec.Ingress = rulesV1BackendToV3API(bp.InboundRules)
+	ap.Spec.Egress = rulesV1BackendToV3API(bp.OutboundRules)
+	ap.Spec.Selector = convertSelector(bp.Selector)
+	ap.Spec.DoNotTrack = bp.DoNotTrack
+	ap.Spec.PreDNAT = bp.PreDNAT
+	ap.Spec.ApplyOnForward = bp.ApplyOnForward
+	ap.Spec.Types = nil // Set later.
+
+	if !bp.ApplyOnForward && (bp.DoNotTrack || bp.PreDNAT) {
+		// This case happens when there is a pre-existing policy in the datastore, from before
+		// the ApplyOnForward feature was available. DoNotTrack or PreDNAT policy applies to
+		// forward traffic by nature. So in this case we return ApplyOnForward flag as true.
+		ap.Spec.ApplyOnForward = true
+	}
+
+	if len(bp.Types) == 0 {
+		// This case happens when there is a pre-existing policy in an etcd datastore, from
+		// before the explicit Types feature was available.  Calico's previous behaviour was
+		// always to apply policy to both ingress and egress traffic, so in this case we
+		// return Types as [ ingress, egress ].
+		ap.Spec.Types = []apiv3.PolicyType{apiv3.PolicyTypeIngress, apiv3.PolicyTypeEgress}
+	} else {
+		// Convert from the backend-specified Types.
+		ap.Spec.Types = make([]apiv3.PolicyType, len(bp.Types))
+		for i, t := range bp.Types {
+			if strings.ToLower(t) == "ingress" {
+				ap.Spec.Types[i] = apiv3.PolicyTypeIngress
+			} else if strings.ToLower(t) == "egress" {
+				ap.Spec.Types[i] = apiv3.PolicyTypeEgress
+			} else {
+				return nil, fmt.Errorf("invalid policy type: '%s'", t)
+			}
+		}
+	}
+
+	log.WithFields(log.Fields{
+		"KVPairV1": bp,
+		"APIv3":    ap,
+	}).Debugf("Converted Policy '%s' V1 backend to GlobalNetworkPolicy '%s' V3 API", bk.Name, ap.Name)
+
+	return ap, nil
+}

lib/upgrade/etcd/conversionv1v3/policy_test.go
@@ -1,0 +1,297 @@
+// Copyright (c) 2017 Tigera, Inc. All rights reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package conversionv1v3
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	apiv1 "github.com/projectcalico/libcalico-go/lib/apis/v1"
+	"github.com/projectcalico/libcalico-go/lib/apis/v1/unversioned"
+	apiv3 "github.com/projectcalico/libcalico-go/lib/apis/v3"
+	"github.com/projectcalico/libcalico-go/lib/backend/model"
+)
+
+var order1 = 1000.00
+var order2 = 999.99
+var policyTable = []struct {
+	description string
+	v1API       unversioned.Resource
+	v1KVP       *model.KVPair
+	v3API       apiv3.GlobalNetworkPolicy
+}{
+	{
+		description: "fully populated Policy",
+		v1API: apiv1.Policy{
+			Metadata: apiv1.PolicyMetadata{
+				Name: "nameyMcPolicyName",
+			},
+			Spec: apiv1.PolicySpec{
+				Order:          &order1,
+				IngressRules:   []apiv1.Rule{V1InRule1, V1InRule2},
+				EgressRules:    []apiv1.Rule{V1EgressRule1, V1EgressRule2},
+				Selector:       "calico/k8s_ns == 'default' || thing == 'value'",
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true,
+				Types:          []apiv1.PolicyType{apiv1.PolicyTypeIngress},
+			},
+		},
+		v1KVP: &model.KVPair{
+			Key: model.PolicyKey{
+				Name: "nameyMcPolicyName",
+			},
+			Value: &model.Policy{
+				Order:          &order1,
+				InboundRules:   []model.Rule{V1ModelInRule1, V1ModelInRule2},
+				OutboundRules:  []model.Rule{V1ModelEgressRule1, V1ModelEgressRule2},
+				Selector:       "calico/k8s_ns == 'default' || thing == 'value'",
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true,
+				Types:          []string{"ingress"},
+			},
+		},
+		v3API: apiv3.GlobalNetworkPolicy{
+			ObjectMeta: v1.ObjectMeta{
+				Name: "nameymcpolicyname",
+			},
+			Spec: apiv3.GlobalNetworkPolicySpec{
+				Order:          &order1,
+				Ingress:        []apiv3.Rule{V3InRule1, V3InRule2},
+				Egress:         []apiv3.Rule{V3EgressRule1, V3EgressRule2},
+				Selector:       "projectcalico.org/namespace == 'default' || thing == 'value'",
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true,
+				Types:          []apiv3.PolicyType{apiv3.PolicyTypeIngress},
+			},
+		},
+	},
+	{
+		description: "policy name conversion",
+		v1API: apiv1.Policy{
+			Metadata: apiv1.PolicyMetadata{
+				Name: "MaKe.-.MaKe",
+			},
+			Spec: apiv1.PolicySpec{
+				Order:          &order1,
+				IngressRules:   []apiv1.Rule{V1InRule2},
+				EgressRules:    []apiv1.Rule{V1EgressRule1},
+				Selector:       "thing == 'value'",
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true,
+				Types:          []apiv1.PolicyType{apiv1.PolicyTypeIngress},
+			},
+		},
+		v1KVP: &model.KVPair{
+			Key: model.PolicyKey{
+				Name: "MaKe.-.MaKe",
+			},
+			Value: &model.Policy{
+				Order:          &order1,
+				InboundRules:   []model.Rule{V1ModelInRule2},
+				OutboundRules:  []model.Rule{V1ModelEgressRule1},
+				Selector:       "thing == 'value'",
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true,
+				Types:          []string{"ingress"},
+			},
+		},
+		v3API: apiv3.GlobalNetworkPolicy{
+			ObjectMeta: v1.ObjectMeta{
+				Name: "make.make",
+			},
+			Spec: apiv3.GlobalNetworkPolicySpec{
+				Order:          &order1,
+				Ingress:        []apiv3.Rule{V3InRule2},
+				Egress:         []apiv3.Rule{V3EgressRule1},
+				Selector:       "thing == 'value'",
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true,
+				Types:          []apiv3.PolicyType{apiv3.PolicyTypeIngress},
+			},
+		},
+	},
+	{
+		description: "policy with ApplyOnForward set to it's zero value (missing), and PreDNAT and DoNotTrack set to true " +
+			"should convert ApplyOnForward to true in v3 API",
+		v1API: apiv1.Policy{
+			Metadata: apiv1.PolicyMetadata{
+				Name: "RAWR",
+			},
+			Spec: apiv1.PolicySpec{
+				Order:          &order1,
+				IngressRules:   []apiv1.Rule{V1InRule2},
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: false,
+				Types:          []apiv1.PolicyType{apiv1.PolicyTypeIngress},
+			},
+		},
+		v1KVP: &model.KVPair{
+			Key: model.PolicyKey{
+				Name: "RAWR",
+			},
+			Value: &model.Policy{
+				Order:          &order1,
+				InboundRules:   []model.Rule{V1ModelInRule2},
+				OutboundRules:  []model.Rule{},
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: false,
+				Types:          []string{"ingress"},
+			},
+		},
+		v3API: apiv3.GlobalNetworkPolicy{
+			ObjectMeta: v1.ObjectMeta{
+				Name: "rawr",
+			},
+			Spec: apiv3.GlobalNetworkPolicySpec{
+				Order:          &order1,
+				Ingress:        []apiv3.Rule{V3InRule2},
+				Egress:         []apiv3.Rule{},
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true, // notice this gets converted to true, because DoNotTrack and PreDNAT are true.
+				Types:          []apiv3.PolicyType{apiv3.PolicyTypeIngress},
+			},
+		},
+	},
+	{
+		description: "policy with ApplyOnForward set to it's zero value (missing), and PreDNAT and DoNotTrack both " +
+			"set to false should NOT convert ApplyOnForward to true in v3 API",
+		v1API: apiv1.Policy{
+			Metadata: apiv1.PolicyMetadata{
+				Name: "meow",
+			},
+			Spec: apiv1.PolicySpec{
+				Order:          &order1,
+				IngressRules:   []apiv1.Rule{V1InRule2},
+				DoNotTrack:     false,
+				PreDNAT:        false,
+				ApplyOnForward: true,
+				Types:          []apiv1.PolicyType{apiv1.PolicyTypeIngress},
+			},
+		},
+		v1KVP: &model.KVPair{
+			Key: model.PolicyKey{
+				Name: "meow",
+			},
+			Value: &model.Policy{
+				Order:          &order1,
+				InboundRules:   []model.Rule{V1ModelInRule2},
+				OutboundRules:  []model.Rule{},
+				DoNotTrack:     false,
+				PreDNAT:        false,
+				ApplyOnForward: true,
+				Types:          []string{"ingress"},
+			},
+		},
+		v3API: apiv3.GlobalNetworkPolicy{
+			ObjectMeta: v1.ObjectMeta{
+				Name: "meow",
+			},
+			Spec: apiv3.GlobalNetworkPolicySpec{
+				Order:          &order1,
+				Ingress:        []apiv3.Rule{V3InRule2},
+				Egress:         []apiv3.Rule{},
+				DoNotTrack:     false,
+				PreDNAT:        false,
+				ApplyOnForward: true, // notice this gets converted to true, because DoNotTrack and PreDNAT are true.
+				Types:          []apiv3.PolicyType{apiv3.PolicyTypeIngress},
+			},
+		},
+	},
+	{
+		description: "policy with non-strictly masked CIDR should get converted to strictly masked CIDR in v3 API",
+		v1API: apiv1.Policy{
+			Metadata: apiv1.PolicyMetadata{
+				Name: "MaKe.-.MaKe",
+			},
+			Spec: apiv1.PolicySpec{
+				Order: &order1,
+				// Source Nets selector in V1InRule1 and V1EgressRule1 are non-strictly masked CIDRs.
+				IngressRules:   []apiv1.Rule{V1InRule1},
+				EgressRules:    []apiv1.Rule{V1EgressRule1},
+				Selector:       "thing == 'value'",
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true,
+				Types:          []apiv1.PolicyType{apiv1.PolicyTypeIngress, apiv1.PolicyTypeEgress},
+			},
+		},
+		v1KVP: &model.KVPair{
+			Key: model.PolicyKey{
+				Name: "MaKe.-.MaKe",
+			},
+			Value: &model.Policy{
+				Order: &order1,
+				// Source Nets selector in V1ModelInRule1 and V1ModelEgressRule1 are non-strictly masked CIDRs.
+				InboundRules:   []model.Rule{V1ModelInRule1},
+				OutboundRules:  []model.Rule{V1ModelEgressRule1},
+				Selector:       "thing == 'value'",
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true,
+				Types:          []string{"ingress", "egress"},
+			},
+		},
+		v3API: apiv3.GlobalNetworkPolicy{
+			ObjectMeta: v1.ObjectMeta{
+				Name: "make.make",
+			},
+			Spec: apiv3.GlobalNetworkPolicySpec{
+				Order: &order1,
+				// Source Nets selector in V3InRule1 and V3EgressRule1 are strictly masked CIDRs.
+				Ingress:        []apiv3.Rule{V3InRule1},
+				Egress:         []apiv3.Rule{V3EgressRule1},
+				Selector:       "thing == 'value'",
+				DoNotTrack:     true,
+				PreDNAT:        true,
+				ApplyOnForward: true,
+				Types:          []apiv3.PolicyType{apiv3.PolicyTypeIngress, apiv3.PolicyTypeEgress},
+			},
+		},
+	},
+}
+
+func TestCanConvertV1ToV3Policy(t *testing.T) {
+
+	for _, entry := range policyTable {
+		t.Run(entry.description, func(t *testing.T) {
+			RegisterTestingT(t)
+
+			p := Policy{}
+
+			// Test and assert v1 API to v1 backend logic.
+			v1KVPResult, err := p.APIV1ToBackendV1(entry.v1API)
+			Expect(err).NotTo(HaveOccurred(), entry.description)
+			Expect(v1KVPResult.Key.(model.PolicyKey).Name).To(Equal(entry.v1KVP.Key.(model.PolicyKey).Name))
+			Expect(v1KVPResult.Value.(*model.Policy)).To(Equal(entry.v1KVP.Value))
+
+			// Test and assert v1 backend to v3 API logic.
+			v3APIResult, err := p.BackendV1ToAPIV3(entry.v1KVP)
+			Expect(err).NotTo(HaveOccurred(), entry.description)
+			Expect(v3APIResult.(*apiv3.GlobalNetworkPolicy).Name).To(Equal(entry.v3API.Name), entry.description)
+			Expect(v3APIResult.(*apiv3.GlobalNetworkPolicy).Spec).To(Equal(entry.v3API.Spec), entry.description)
+		})
+	}
+}