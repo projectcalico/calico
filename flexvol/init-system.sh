8e753153824b898331f769bc7fe8296141c254e4
Saurabh Mohan <saurabh+github@tigera.io>
2017-12-08 11:51:20 -0800

    No need for a init-system script.

--- flexvol/init-system.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Initialize the host/node for use with flexvolume driver
-FLEXVOL_HOME=/usr/libexec/kubernetes/kubelet-plugins/volume/exec/nodeagent~uds/
-sudo mkdir -p ${FLEXVOL_HOME}
-sudo cp flexvoldriver ${FLEXVOL_HOME}/uds
-sudo mkdir -p /tmp/udsuspver/
-sudo mkdir -p /tmp/nodeagent/
