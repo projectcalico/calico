0f30e1873cc1a78265f0f093c9de4ece4f674634
Shaun Crampton <shaun@tigera.io>
2018-01-22 18:13:15 +0000

    Add a basic connectivity test for network sets.

--- fv/netsets_test.go
+++ fv/netsets_test.go
@@ -20,11 +20,13 @@ import (
 	"context"
 	"fmt"
 	"math/rand"
+	"strconv"
 	"sync"
 	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/projectcalico/felix/fv/utils"
 	"github.com/projectcalico/libcalico-go/lib/options"
 
 	log "github.com/sirupsen/logrus"
@@ -41,15 +43,17 @@ const (
 	numNetworkSets = 100
 )
 
-var _ = Context("with initialized Felix and etcd datastore", func() {
+var _ = FContext("Network sets connectivity tests with initialized Felix and etcd datastore", func() {
+
+})
+
+var _ = Context("Network sets churn test with initialized Felix and etcd datastore", func() {
 
 	var (
-		etcd        *containers.Container
-		felix       *containers.Container
-		felixPID    int
-		client      client.Interface
-		policies    []*api.GlobalNetworkPolicy
-		networkSets []*api.GlobalNetworkSet
+		etcd     *containers.Container
+		felix    *containers.Container
+		felixPID int
+		client   client.Interface
 	)
 
 	getFelixPIDs := func() []int {
@@ -76,49 +80,6 @@ var _ = Context("with initialized Felix and etcd datastore", func() {
 			FelixLogSeverity: "info",
 		})
 		updateFelixPID()
-
-		// Start a workload so we have something to add policy to
-		w := workload.Run(
-			felix,
-			"w",
-			"cali12345",
-			"10.65.0.2",
-			"8055",
-			"tcp",
-		)
-		w.Configure(client)
-
-		// Generate policies and network sets.
-		for i := 0; i < numPolicies; i++ {
-			pol := api.NewGlobalNetworkPolicy()
-			pol.Name = fmt.Sprintf("policy-%d", i)
-			pol.Spec = api.GlobalNetworkPolicySpec{
-				Selector: "all()",
-				Ingress: []api.Rule{
-					{
-						Action: "Allow",
-						Source: api.EntityRule{
-							Selector: fmt.Sprintf("label == '%d'", i),
-						}},
-				},
-			}
-			policies = append(policies, pol)
-		}
-		for i := 0; i < numNetworkSets; i++ {
-			ns := api.NewGlobalNetworkSet()
-			ns.Name = fmt.Sprintf("netset-%d", i)
-			ns.Labels = map[string]string{
-				"foo":   "bar",
-				"baz":   "biff",
-				"label": fmt.Sprintf("%d", i%numPolicies),
-			}
-			nets := generateNets(i * 100)
-			ns.Spec = api.GlobalNetworkSetSpec{
-				Nets: nets,
-			}
-			networkSets = append(networkSets, ns)
-		}
-		log.SetLevel(log.InfoLevel)
 	})
 
 	AfterEach(func() {
@@ -133,86 +94,253 @@ var _ = Context("with initialized Felix and etcd datastore", func() {
 		etcd.Stop()
 	})
 
-	churnPolicies := func(iterations int) {
-		log.Info("Churning policies...")
-		created := false
-		for i := 0; i < iterations; i++ {
-			if created {
+	Describe("connectivity tests", func() {
+		var (
+			w  [4]*workload.Workload
+			cc *workload.ConnectivityChecker
+		)
+
+		createPolicy := func(policy *api.NetworkPolicy) {
+			log.WithField("policy", dumpPolicy(policy)).Info("Creating policy")
+			_, err := client.NetworkPolicies().Create(utils.Ctx, policy, utils.NoOptions)
+			Expect(err).NotTo(HaveOccurred())
+		}
+
+		BeforeEach(func() {
+			for ii := range w {
+				iiStr := strconv.Itoa(ii)
+				var ports string
+
+				ports = "3000"
+				w[ii] = workload.Run(
+					felix,
+					"w"+iiStr,
+					"cali0"+iiStr,
+					"10.65."+iiStr+".1", // IPs each in their own /24
+					ports,
+					"tcp",
+				)
+
+				w[ii].DefaultPort = "3000"
+				w[ii].Configure(client)
+			}
+
+			cc = &workload.ConnectivityChecker{
+				Protocol: "tcp",
+			}
+
+			pol := api.NewNetworkPolicy()
+			pol.Namespace = "fv"
+			pol.Name = "policy-1"
+			pol.Spec.Ingress = []api.Rule{
+				{
+					Action: "Allow",
+					Source: api.EntityRule{
+						Selector: "has(allow-as-source)",
+					},
+					Destination: api.EntityRule{
+						Selector: "has(allow-as-dest)",
+					},
+				},
+			}
+			pol.Spec.Selector = "all()"
+
+			createPolicy(pol)
+		})
+
+		It("with no matching network sets, should deny all", func() {
+			cc.ExpectNone(w[0], w[1])
+			cc.ExpectNone(w[0], w[2])
+			cc.ExpectNone(w[0], w[3])
+			cc.ExpectNone(w[1], w[0])
+			cc.ExpectNone(w[1], w[2])
+			cc.ExpectNone(w[1], w[3])
+			cc.ExpectNone(w[2], w[0])
+			cc.ExpectNone(w[2], w[1])
+			cc.ExpectNone(w[2], w[3])
+			cc.ExpectNone(w[3], w[0])
+			cc.ExpectNone(w[3], w[1])
+			cc.ExpectNone(w[3], w[2])
+		})
+
+		It("with network sets matching some workloads, should have expected connectivity", func() {
+			srcNS := api.NewGlobalNetworkSet()
+			srcNS.Name = "ns-1"
+			srcNS.Spec.Nets = []string{
+				"10.65.0.0/24",
+				"10.65.1.0/24",
+				"10.65.2.0/24",
+			}
+			srcNS.Labels = map[string]string{
+				"allow-as-source": "",
+			}
+			client.GlobalNetworkSets().Create(utils.Ctx, srcNS, utils.NoOptions)
+
+			destNS := api.NewGlobalNetworkSet()
+			destNS.Name = "ns-2"
+			destNS.Spec.Nets = []string{
+				"10.65.2.0/24",
+				"10.65.3.0/24",
+			}
+			destNS.Labels = map[string]string{
+				"allow-as-dest": "",
+			}
+			client.GlobalNetworkSets().Create(utils.Ctx, destNS, utils.NoOptions)
+
+			cc.ExpectNone(w[0], w[1]) // not in dest net set
+			cc.ExpectSome(w[0], w[2])
+			cc.ExpectSome(w[0], w[3])
+			cc.ExpectNone(w[1], w[0]) // not in dest net set
+			cc.ExpectSome(w[1], w[2])
+			cc.ExpectSome(w[1], w[3])
+			cc.ExpectNone(w[2], w[0]) // not in dest net set
+			cc.ExpectNone(w[2], w[1]) // not in dest net set
+			cc.ExpectSome(w[2], w[3])
+			// 3 isn't in the source net set so it can't talk to anyone.
+			cc.ExpectNone(w[3], w[0])
+			cc.ExpectNone(w[3], w[1])
+			cc.ExpectNone(w[3], w[2])
+		})
+
+		AfterEach(func() {
+			for ii := range w {
+				w[ii].Stop()
+			}
+		})
+	})
+
+	Describe("churn tests", func() {
+		var (
+			policies    []*api.GlobalNetworkPolicy
+			networkSets []*api.GlobalNetworkSet
+		)
+
+		BeforeEach(func() {
+			// Start a workload so we have something to add policy to
+			w := workload.Run(
+				felix,
+				"w",
+				"cali12345",
+				"10.65.0.2",
+				"8055",
+				"tcp",
+			)
+			w.Configure(client)
+
+			// Generate policies and network sets.
+			for i := 0; i < numPolicies; i++ {
+				pol := api.NewGlobalNetworkPolicy()
+				pol.Name = fmt.Sprintf("policy-%d", i)
+				pol.Spec = api.GlobalNetworkPolicySpec{
+					Selector: "all()",
+					Ingress: []api.Rule{
+						{
+							Action: "Allow",
+							Source: api.EntityRule{
+								Selector: fmt.Sprintf("label == '%d'", i),
+							}},
+					},
+				}
+				policies = append(policies, pol)
+			}
+			for i := 0; i < numNetworkSets; i++ {
+				ns := api.NewGlobalNetworkSet()
+				ns.Name = fmt.Sprintf("netset-%d", i)
+				ns.Labels = map[string]string{
+					"foo":   "bar",
+					"baz":   "biff",
+					"label": fmt.Sprintf("%d", i%numPolicies),
+				}
+				nets := generateNets(i * 100)
+				ns.Spec = api.GlobalNetworkSetSpec{
+					Nets: nets,
+				}
+				networkSets = append(networkSets, ns)
+			}
+			log.SetLevel(log.InfoLevel)
+		})
+
+		churnPolicies := func(iterations int) {
+			log.Info("Churning policies...")
+			created := false
+			for i := 0; i < iterations; i++ {
+				if created {
+					for _, pol := range policies {
+						log.WithField("policy", pol.Name).Info("Deleting policy")
+						cxt, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+						_, err := client.GlobalNetworkPolicies().Delete(cxt, pol.Name, options.DeleteOptions{})
+						cancel()
+						if err != nil {
+							log.WithError(err).Panic("Failed to delete policy")
+						}
+						time.Sleep(13 * time.Millisecond)
+					}
+				}
 				for _, pol := range policies {
-					log.WithField("policy", pol.Name).Info("Deleting policy")
+					log.WithField("policy", pol.Name).Info("Creating policy")
 					cxt, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-					_, err := client.GlobalNetworkPolicies().Delete(cxt, pol.Name, options.DeleteOptions{})
+					_, err := client.GlobalNetworkPolicies().Create(cxt, pol, options.SetOptions{})
 					cancel()
 					if err != nil {
-						log.WithError(err).Panic("Failed to delete policy")
+						log.WithError(err).WithField("name", pol.Name).Panic("Failed to add policy")
 					}
-					time.Sleep(13 * time.Millisecond)
+					time.Sleep(17 * time.Millisecond)
 				}
+				created = true
 			}
-			for _, pol := range policies {
-				log.WithField("policy", pol.Name).Info("Creating policy")
-				cxt, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-				_, err := client.GlobalNetworkPolicies().Create(cxt, pol, options.SetOptions{})
-				cancel()
-				if err != nil {
-					log.WithError(err).WithField("name", pol.Name).Panic("Failed to add policy")
-				}
-				time.Sleep(17 * time.Millisecond)
-			}
-			created = true
 		}
-	}
 
-	churnNetworkSets := func(iterations int) {
-		log.Info("Churning network sets...")
-		created := false
-		for i := 0; i < iterations; i++ {
-			if created {
+		churnNetworkSets := func(iterations int) {
+			log.Info("Churning network sets...")
+			created := false
+			for i := 0; i < iterations; i++ {
+				if created {
+					for _, ns := range networkSets {
+						log.WithField("name", ns.Name).Info("Deleting network set")
+						cxt, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+						_, err := client.GlobalNetworkSets().Delete(cxt, ns.Name, options.DeleteOptions{})
+						cancel()
+						if err != nil {
+							log.WithError(err).Panic("Failed to delete network set")
+						}
+						time.Sleep(19 * time.Millisecond)
+					}
+				}
 				for _, ns := range networkSets {
-					log.WithField("name", ns.Name).Info("Deleting network set")
+					log.WithField("name", ns.Name).Info("Creating network set")
 					cxt, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-					_, err := client.GlobalNetworkSets().Delete(cxt, ns.Name, options.DeleteOptions{})
+					_, err := client.GlobalNetworkSets().Create(cxt, ns, options.SetOptions{})
 					cancel()
 					if err != nil {
-						log.WithError(err).Panic("Failed to delete network set")
+						log.WithError(err).Panic("Failed to add network set")
 					}
-					time.Sleep(19 * time.Millisecond)
+					time.Sleep(29 * time.Millisecond)
 				}
+				created = true
 			}
-			for _, ns := range networkSets {
-				log.WithField("name", ns.Name).Info("Creating network set")
-				cxt, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-				_, err := client.GlobalNetworkSets().Create(cxt, ns, options.SetOptions{})
-				cancel()
-				if err != nil {
-					log.WithError(err).Panic("Failed to add network set")
-				}
-				time.Sleep(29 * time.Millisecond)
-			}
-			created = true
 		}
-	}
 
-	It("should withstand churn", func() {
-		var wg sync.WaitGroup
-		wg.Add(2)
+		It("should withstand churn", func() {
+			var wg sync.WaitGroup
+			wg.Add(2)
 
-		go func() {
-			defer wg.Done()
-			defer log.Info("Finished churning policies")
-			churnPolicies(3)
-		}()
-		go func() {
-			RegisterFailHandler(Fail)
-			defer wg.Done()
-			defer log.Info("Finished churning network sets")
-			churnNetworkSets(3)
-		}()
+			go func() {
+				defer wg.Done()
+				defer log.Info("Finished churning policies")
+				churnPolicies(3)
+			}()
+			go func() {
+				RegisterFailHandler(Fail)
+				defer wg.Done()
+				defer log.Info("Finished churning network sets")
+				churnNetworkSets(3)
+			}()
 
-		wg.Wait()
+			wg.Wait()
 
-		// getFelixPIDs may return more than one PID, transiently due to Felix calling fork().
-		Eventually(getFelixPIDs).Should(ConsistOf(felixPID))
+			// getFelixPIDs may return more than one PID, transiently due to Felix calling fork().
+			Eventually(getFelixPIDs).Should(ConsistOf(felixPID))
+		})
 	})
 })
 
