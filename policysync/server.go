40a692d36e36011941f02a4c83fc5b77d24b4023
Spike Curtis <spike@tigera.io>
2018-01-26 15:55:55 -0800

    WIP Policy Sync server

--- policysync/server.go
+++ policysync/server.go
@@ -14,14 +14,59 @@
 
 package policysync
 
+import (
+	"errors"
+
+	"github.com/projectcalico/felix/proto"
+
+	mwi "github.com/colabsaumoh/proto-udsuspver/mgmtwlhintf"
+	wlh "github.com/colabsaumoh/proto-udsuspver/workloadhandler"
+	"google.golang.org/grpc"
+)
+
+const SockName = "/policysync.sock"
+const OrchestratorId = "k8s"
+const EndpointId = "eth0"
+
 type Server struct {
-
+	Joins chan<- JoinRequest
 }
 
-func NewServer() *Server {
-	return &Server{
+func NewServer(joins chan<- JoinRequest) *Server {
+	return &Server{Joins: joins}
+}
 
+func NewWlServer(joins chan<- JoinRequest) *mwi.WlServer {
+	s := NewServer(joins)
+	return &mwi.WlServer{SockFile: SockName, RegAPI: s.RegisterGrpc}
+}
+
+func (s *Server) RegisterGrpc(g *grpc.Server) {
+	proto.RegisterPolicySyncServer(g, s)
+}
+
+func (s *Server) Sync(_ *proto.SyncRequest, stream proto.PolicySync_SyncServer) error {
+	cxt := stream.Context()
+	creds, ok := wlh.CallerFromContext(cxt)
+	if !ok {
+		return errors.New("Unable to authenticate client.")
 	}
+	updates := make(chan proto.ToDataplane)
+	s.Joins <- JoinRequest{
+		EndpointID: proto.WorkloadEndpointID{
+			OrchestratorId: OrchestratorId,
+			EndpointId:     EndpointId,
+			WorkloadId:     creds.Uid,
+		},
+		C: updates,
+		//TODO: JoinCount?
+	}
+	for update := range updates {
+		err := stream.Send(&update)
+		if err != nil {
+			// TODO: maybe don't just blindly send errors?
+			return err
+		}
+	}
+	return nil
 }
-
-
