984dd812eacfc7cef8e2911b0b99c65482585d61
Spike Curtis <spike@tigera.io>
2018-03-07 10:08:46 -0800

    Refactor profile_decoder.go to use type switch
    Signed-off-by: Spike Curtis <spike@tigera.io>

--- calc/profile_decoder.go
+++ calc/profile_decoder.go
@@ -26,14 +26,6 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-type kind int
-
-const (
-	KindUnknown kind = iota
-	KindServiceAccount
-	KindNamespace
-)
-
 // ProfileDecoder takes updates from a dispatcher, determines if the profile is a Kubernetes Service Account or
 // Kubernetes Namespace, and if it is, generates a dataplane update or remove for it.
 type ProfileDecoder struct {
@@ -53,53 +45,47 @@ func (p *ProfileDecoder) OnUpdate(update api.Update) (filterOut bool) {
 	// This type assertion is safe because we only registered for ProfileLabels updates.
 	key := update.Key.(model.ProfileLabelsKey)
 	log.WithField("key", key.String()).Debug("Decoding ProfileLabels")
-	id, knd := p.classifyProfile(key)
-	switch knd {
-	case KindUnknown:
+	idInterface := p.classifyProfile(key)
+	switch id := idInterface.(type) {
+	case nil:
 		log.WithField("key", key.String()).Debug("Ignoring ProfileLabels")
-	case KindServiceAccount:
-		id := id.(proto.ServiceAccountID)
+	case proto.ServiceAccountID:
 		if update.Value == nil {
 			p.callbacks.OnServiceAccountRemove(id)
 		} else {
 			labels := update.Value.(map[string]string)
-			msg := proto.ServiceAccountUpdate{Id: &id, Labels: decodeLabels(knd, labels)}
+			msg := proto.ServiceAccountUpdate{
+				Id: &id, Labels: decodeLabels(conversion.ServiceAccountLabelPrefix, labels)}
 			p.callbacks.OnServiceAccountUpdate(&msg)
 		}
-	case KindNamespace:
-		id := id.(proto.NamespaceID)
+	case proto.NamespaceID:
 		if update.Value == nil {
 			p.callbacks.OnNamespaceRemove(id)
 		} else {
 			labels := update.Value.(map[string]string)
-			msg := proto.NamespaceUpdate{Id: &id, Labels: decodeLabels(knd, labels)}
+			msg := proto.NamespaceUpdate{
+				Id: &id, Labels: decodeLabels(conversion.NamespaceLabelPrefix, labels)}
 			p.callbacks.OnNamespaceUpdate(&msg)
 		}
 	}
 	return false
 }
 
-func (p *ProfileDecoder) classifyProfile(key model.ProfileLabelsKey) (interface{}, kind) {
+func (p *ProfileDecoder) classifyProfile(key model.ProfileLabelsKey) interface{} {
 	namespace, name, err := p.converter.ProfileNameToServiceAccount(key.Name)
 	if err == nil {
-		return proto.ServiceAccountID{Name: name, Namespace: namespace}, KindServiceAccount
+		return proto.ServiceAccountID{Name: name, Namespace: namespace}
 	}
 	name, err = p.converter.ProfileNameToNamespace(key.Name)
 	if err == nil {
-		return proto.NamespaceID{Name: name}, KindNamespace
+		return proto.NamespaceID{Name: name}
 	}
-	return nil, KindUnknown
+	return nil
 }
 
 // decodeLabels strips the special prefix we add to Profile labels when converting. This gives us the original labels on
 // the ServiceAccount or Namespace object.
-func decodeLabels(k kind, in map[string]string) map[string]string {
-	var prefix string
-	if k == KindServiceAccount {
-		prefix = conversion.ServiceAccountLabelPrefix
-	} else if k == KindNamespace {
-		prefix = conversion.NamespaceLabelPrefix
-	}
+func decodeLabels(prefix string, in map[string]string) map[string]string {
 	out := make(map[string]string)
 	for k, v := range in {
 		k = strings.TrimPrefix(k, prefix)
