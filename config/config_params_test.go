83cc178ca8e360a1e27f15d3ff5691b732e0f00b
Neil Jerram <neil@tigera.io>
2018-05-10 11:47:25 +0100

    Check that capitalized 'Append' works for ChainInsertMode
    (In relation to https://github.com/projectcalico/calico/pull/1968)

--- config/config_params_test.go
+++ config/config_params_test.go
@@ -161,6 +161,7 @@ var _ = DescribeTable("Config parsing",
 	Entry("InterfaceExclude list", "InterfaceExclude", "kube-ipvs0,dummy", "kube-ipvs0,dummy"),
 
 	Entry("ChainInsertMode append", "ChainInsertMode", "append", "append"),
+	Entry("ChainInsertMode append", "ChainInsertMode", "Append", "append"),
 
 	Entry("IptablesPostWriteCheckIntervalSecs", "IptablesPostWriteCheckIntervalSecs",
 		"1.5", 1500*time.Millisecond),
