cd733a3858af2de3a19a4bb43ed4e0a084be8a75
Saurabh Mohan <saurabh+github@tigera.io>
2018-06-07 11:20:14 -0700

    Track generateName from k8s object metadata.

--- lib/backend/syncersv1/updateprocessors/workloadendpointprocessor.go
+++ lib/backend/syncersv1/updateprocessors/workloadendpointprocessor.go
@@ -140,18 +140,19 @@ func convertWorkloadEndpointV2ToV1Value(val interface{}) (interface{}, error) {
 	}
 
 	v1value := &model.WorkloadEndpoint{
-		State:       "active",
-		Name:        v3res.Spec.InterfaceName,
-		Mac:         cmac,
-		ProfileIDs:  v3res.Spec.Profiles,
-		IPv4Nets:    ipv4Nets,
-		IPv6Nets:    ipv6Nets,
-		IPv4NAT:     ipv4NAT,
-		IPv6NAT:     ipv6NAT,
-		Labels:      labels,
-		IPv4Gateway: ipv4Gateway,
-		IPv6Gateway: ipv6Gateway,
-		Ports:       ports,
+		State:        "active",
+		Name:         v3res.Spec.InterfaceName,
+		Mac:          cmac,
+		ProfileIDs:   v3res.Spec.Profiles,
+		IPv4Nets:     ipv4Nets,
+		IPv6Nets:     ipv6Nets,
+		IPv4NAT:      ipv4NAT,
+		IPv6NAT:      ipv6NAT,
+		Labels:       labels,
+		IPv4Gateway:  ipv4Gateway,
+		IPv6Gateway:  ipv6Gateway,
+		Ports:        ports,
+		GenerateName: v3res.GenerateName,
 	}
 
 	return v1value, nil
