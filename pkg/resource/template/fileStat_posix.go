b8a05cfcbb8491954c6d68c5a2db2d1e68654328
Saurabh Mohan <saurabh+github@tigera.io>
2018-08-03 13:31:22 -0700

    Fix linter issues and ignore notExist error for remove.

--- pkg/resource/template/fileStat_posix.go
+++ pkg/resource/template/fileStat_posix.go
@@ -9,6 +9,8 @@ import (
 	"io"
 	"os"
 	"syscall"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // fileStat return a fileInfo describing the named file.
@@ -18,13 +20,22 @@ func fileStat(name string) (fi fileInfo, err error) {
 		if err != nil {
 			return fi, err
 		}
-		defer f.Close()
-		stats, _ := f.Stat()
+		defer func() {
+			if e := f.Close(); e != nil {
+				log.WithError(e).WithField("filename", name).Error("error closing file")
+			}
+		}()
+		stats, err := f.Stat()
+		if err != nil {
+			return fi, err
+		}
 		fi.Uid = stats.Sys().(*syscall.Stat_t).Uid
 		fi.Gid = stats.Sys().(*syscall.Stat_t).Gid
 		fi.Mode = stats.Mode()
 		h := md5.New()
-		io.Copy(h, f)
+		if _, err := io.Copy(h, f); err != nil {
+			return fileInfo{}, err
+		}
 		fi.Md5 = fmt.Sprintf("%x", h.Sum(nil))
 		return fi, nil
 	}
