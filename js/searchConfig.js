be2dd076a3d86b7d9d0fc6c4d496d0d504530b87
Artem Rymarchik <artemrymarchik@gmail.com>
2018-10-05 14:20:19 +0300

    Added basic version of InstantSearch

--- js/searchConfig.js
+++ js/searchConfig.js
@@ -1,5 +1,6 @@
-$(document).ready(function () {
-    window.initializeSearch = function (currentDocVersion, searchInputSelector, searchResultsSelector) {
+(function () {
+    window.initializeSearch = function (currentDocVersion, searchInputSelector, searchContentSelector,
+        searchResultsSelector, searchPaginationSelector) {
         if (!!currentDocVersion || typeof (currentDocVersion) !== string) {
             throw new Error('Provide a version');
         }
@@ -8,17 +9,115 @@ $(document).ready(function () {
             throw new Error('Provide a search input selector');
         }
 
+        if (!!searchContentSelector || typeof (searchContentSelector) !== string) {
+            throw new Error('Provide a search content selector');
+        }
+
         if (!!searchResultsSelector || typeof (searchResultsSelector) !== string) {
             throw new Error('Provide a search results selector');
         }
 
-        // Initialize search here. Note the algoliaOptions setting.
-        // docsearch({
-        //     apiKey: '99def7ba73ea2430f7f42383148fe57a',
-        //     indexName: 'projectcalico',
-        //     inputSelector: targetElementSelector,
-        //     debug: false,
-        //     algoliaOptions: { 'facetFilters': ['version:' + currentDocVersion] }
-        // });
+        if (!!searchPaginationSelector || typeof (searchPaginationSelector) !== string) {
+            throw new Error('Provide a search pagination selector');
+        }
+
+        $(document).ready(function () {
+            // Initialize search here. Note the algoliaOptions setting.
+            // docsearch({
+            //     apiKey: '99def7ba73ea2430f7f42383148fe57a',
+            //     indexName: 'projectcalico',
+            //     inputSelector: targetElementSelector,
+            //     debug: false,
+            //     algoliaOptions: { 'facetFilters': ['version:' + currentDocVersion] }
+            // });
+            initializeInstantSearch(searchInputSelector, searchResultsSelector, searchPaginationSelector);
+            initializePopover(searchContentSelector, searchInputSelector);
+            hidePopoversOnClickOutside();
+        });
     };
-});
+
+    var searchResultTemplate = '\
+            <div class="search-result ais-result">\
+                {{#hierarchy.lvl0}}\
+                <h3 class="search-result__category ais-lvl0">\
+                    <a href="{{url}}" >\
+                        {{{_highlightResult.hierarchy.lvl0.value}}}\
+                    </a>\
+                </h3>\
+                {{/hierarchy.lvl0}}\
+                {{#hierarchy.lvl1}}\
+                <h4 class="search-result__subcategory ais-lvl1">\
+                    <a href="{{url}}">\
+                        {{{_highlightResult.hierarchy.lvl1.value}}}\
+                    </a>\
+                </h4>\
+                {{/hierarchy.lvl1}}\
+                {{#hierarchy.lvl2}}\
+                <h5 class="search-result__subsubcategory ais-lvl2">\
+                    <a href="{{url}}">\
+                        {{{_highlightResult.hierarchy.lvl2.value}}}\
+                    </a>\
+                </h5>\
+                {{/hierarchy.lvl2}}\
+                {{#content}}\
+                <div class="search-result__content ais-content">\
+                    <a href="{{url}}" >\
+                        {{{_snippetResult.content.value}}}\
+                    </a>\
+                </div>\
+                {{/content}}\
+            </div>\
+        ';
+
+    function initializeInstantSearch(inputSelector, resultsSelector, paginationSelector) {
+        // TODO:
+        var search = instantsearch({
+            appId: 'BH4D9OD16A',
+            apiKey: 'e574b72c1301f8a28e49ec9644096c79',
+            indexName: 'apereo',
+            routing: true
+        });
+        search.addWidget(instantsearch.widgets.searchBox({
+            container: inputSelector,
+            placeholder: 'Search in the documentation',
+            autofocus: false,
+            poweredBy: true
+        }));
+        search.addWidget(instantsearch.widgets.hits({
+            container: resultsSelector,
+            templates: {
+                empty: 'No results',
+                item: searchResultTemplate
+            },
+            hitsPerPage: 6
+        }));
+        search.addWidget(instantsearch.widgets.pagination({
+            container: paginationSelector,
+            maxPages: 20,
+            scrollTo: false
+        }));
+        search.start();
+    }
+
+    function initializePopover(searchContentSelector, searchInputSelector) {
+        var content = $(searchContentSelector).children();
+        $(searchInputSelector).popover({
+            html: true,
+            content: function () {
+                return content;
+            }
+        });
+    }
+
+    function hidePopoversOnClickOutside() {
+        $('body').on('click', function (e) {
+            $('[data-toggle="popover"]').each(function () {
+                if (!$(this).is(e.target)
+                    && $(this).has(e.target).length === 0
+                    && $('.popover').has(e.target).length === 0) {
+                    $(this).popover('hide');
+                }
+            });
+        });
+    }
+})();
