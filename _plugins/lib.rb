0095e21493f76e264f43e95a28df6be5a03d9926
ozdanborne <dan@projectcalico.org>
2019-03-27 18:16:52 -0700

    refactor imagenames to allow unset versions

--- _plugins/lib.rb
+++ _plugins/lib.rb
@@ -1,29 +1,50 @@
-def gen_values(versions, imageNames, version, imageRegistry)
-    components = versions[version][0]["components"]
+def gen_values(versions, imageNames, imageRegistry)
     versionsYml = <<~EOF
     node:
       image: #{imageRegistry}#{imageNames["node"]}
-      tag: #{components["calico/node"]["version"]}
+      tag: #{versions["calico/node"]}
     calicoctl:
       image: #{imageRegistry}#{imageNames["calicoctl"]}
-      tag: #{components["calicoctl"]["version"]}
+      tag: #{versions["calicoctl"]}
     typha:
       image: #{imageRegistry}#{imageNames["typha"]}
-      tag: #{components["typha"]["version"]}
+      tag: #{versions["typha"]}
     cni:
       image: #{imageRegistry}#{imageNames["cni"]}
-      tag: #{components["calico/cni"]["version"]}
+      tag: #{versions["calico/cni"]}
     kubeControllers:
       image: #{imageRegistry}#{imageNames["kubeControllers"]}
-      tag: #{components["calico/kube-controllers"]["version"]}
+      tag: #{versions["calico/kube-controllers"]}
     flannel:
       image: #{imageNames["flannel"]}
-      tag: #{components["flannel"]["version"]}
+      tag: #{versions["flannel"]}
     dikastes:
       image: #{imageRegistry}#{imageNames["dikastes"]}
-      tag: #{components["calico/dikastes"]["version"]}
+      tag: #{versions["calico/dikastes"]}
     flexvol:
       image: #{imageRegistry}#{imageNames["flexvol"]}
-      tag: #{components["flexvol"]["version"]}
+      tag: #{versions["flexvol"]}
     EOF
+end
+
+
+# Takes versions_yml which is structured as follows:
+#
+# {"v3.6"=>
+#     ["components"=>
+#        {"calico/node"=>{"version"=>"v3.6.0"},
+#         "typha"=>{"version"=>"v3.6.0"}}]
+#
+# And for a given version, return a Hash of each components' version by component name e.g:
+#
+# {"calico/node"=>"v3.6.0",
+#   "typha"=>"v3.6.0"}
+#
+def parse_versions(versions_yml, version)
+  if not versions_yml.key?(version)
+    raise IndexError.new "requested version '#{version}' not present in versions.yml"
+  end
+
+  components = versions_yml[version][0]["components"].clone
+  return components.each { |key,val| components[key] = val["version"] }
 end
