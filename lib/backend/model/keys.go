ea102c37634b1eb635cd83de2427d6b42db0e87b
Shaun Crampton <shaun@tigera.io>
2019-04-17 11:17:26 +0100

    Add parsing for keys expected by the multi-syncer.

--- lib/backend/model/keys.go
+++ lib/backend/model/keys.go
@@ -265,6 +265,10 @@ func KeyFromDefaultPath(path string) Key {
 		return k
 	} else if k := (ResourceListOptions{Kind: v3.KindBGPPeer}).KeyFromDefaultPath(path); k != nil {
 		return k
+	} else if k := (ResourceListOptions{Kind: v3.KindNetworkPolicy}).KeyFromDefaultPath(path); k != nil {
+		return k
+	} else if k := (ResourceListOptions{Kind: v3.KindIPPool}).KeyFromDefaultPath(path); k != nil {
+		return k
 	} else if k := (HostEndpointStatusListOptions{}).KeyFromDefaultPath(path); k != nil {
 		return k
 	} else if k := (WorkloadEndpointStatusListOptions{}).KeyFromDefaultPath(path); k != nil {
