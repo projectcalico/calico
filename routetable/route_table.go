ee60cff02567575b24054cad667ab7a243ce1749
Shaun Crampton <shaun@tigera.io>
2019-07-30 10:22:51 +0100

    Fix interaction between source address check and grace period.
    - Only allow grace period for unexpected routes (i.e. skip
      it for routes that have incorrect source address).
    - Only drop conntrack entries for unexpected routes.
    - Refactor the route sync calculation to emit reason for route
      deletion to the log.

--- routetable/route_table.go
+++ routetable/route_table.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2016-2018 Tigera, Inc. All rights reserved.
+// Copyright (c) 2016-2019 Tigera, Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -429,7 +429,7 @@ func (r *RouteTable) syncRoutesForLink(ifaceName string) error {
 	// before learning about the endpoint, we give each interface a grace period after we first
 	// see it before we remove routes that we're not expecting.  Check whether the grace period
 	// applies to this interface.
-	inGracePeriod := r.time.Since(r.ifaceNameToFirstSeen[ifaceName]) < cleanupGracePeriod
+	ifaceInGracePeriod := r.time.Since(r.ifaceNameToFirstSeen[ifaceName]) < cleanupGracePeriod
 	leaveDirty := false
 
 	// If this is a modify or delete, grab a copy of the existing targets so we can clean up
@@ -511,7 +511,7 @@ func (r *RouteTable) syncRoutesForLink(ifaceName string) error {
 		return filteredErr
 	}
 
-	seenCIDRs := set.New()
+	alreadyCorrectCIDRs := set.New()
 	updatesFailed := false
 	for _, route := range oldRoutes {
 		var dest ip.CIDR
@@ -519,32 +519,33 @@ func (r *RouteTable) syncRoutesForLink(ifaceName string) error {
 			dest = ip.CIDRFromIPNet(route.Dst)
 		}
 		logCxt := logCxt.WithField("dest", dest)
-		seenCIDRs.Add(dest)
-		if expectedCIDRs.Contains(dest) {
-			logCxt.Debug("Syncing routes: Found expected route.")
-			// Check if we need to update this route with a source address
-			if !r.deviceRouteSourceAddress.Equal(route.Src) {
-				logCxt.Debug("Syncing routes: existing route needs modification. Removing.")
-				// Don't skip adding this route
-				seenCIDRs.Discard(dest)
-			} else {
-				continue
-			}
+		routeExpected := expectedCIDRs.Contains(dest)
+		var routeProblems []string
+		if !routeExpected {
+			routeProblems = append(routeProblems, "unexpected route")
 		}
-		if !r.vxlan && inGracePeriod {
-			// Don't remove routes from interfaces created recently. VXLAN routes don't have a grace period.
+		if !r.deviceRouteSourceAddress.Equal(route.Src) {
+			routeProblems = append(routeProblems, "incorrect source address")
+		}
+		if len(routeProblems) == 0 {
+			alreadyCorrectCIDRs.Add(dest)
+			continue
+		}
+		gracePeriodApplies := ifaceInGracePeriod && !routeExpected && !r.vxlan
+		if gracePeriodApplies {
+			// Don't remove unexpected routes from interfaces created recently. VXLAN routes don't have a grace period.
 			logCxt.Info("Syncing routes: found unexpected route; ignoring due to grace period.")
 			leaveDirty = true
 			continue
 		}
+		logCxt = logCxt.WithField("routeProblems", routeProblems)
 		logCxt.Info("Syncing routes: removing old route.")
 		if err := nl.RouteDel(&route); err != nil {
 			// Probably a race with the interface being deleted.
-			logCxt.WithError(err).Info(
-				"Route deletion failed, assuming someone got there first.")
+			logCxt.WithError(err).Info("Route deletion failed, assuming someone got there first.")
 			updatesFailed = true
 		}
-		if dest != nil {
+		if !routeExpected && dest != nil {
 			// Collect any old route CIDRs that we find in the dataplane so we
 			// can remove their conntrack entries later.
 			oldCIDRs.Add(dest)
@@ -552,7 +553,7 @@ func (r *RouteTable) syncRoutesForLink(ifaceName string) error {
 	}
 	for _, target := range expectedTargets {
 		cidr := target.CIDR
-		if !seenCIDRs.Contains(cidr) {
+		if !alreadyCorrectCIDRs.Contains(cidr) {
 			logCxt := logCxt.WithField("targetCIDR", target.CIDR)
 			logCxt.Info("Syncing routes: adding new route.")
 			ipNet := cidr.ToIPNet()
