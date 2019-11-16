4d86866626f9e8a15174021349aba6588028d858
jayunit100 <jay@apache.org>
2019-11-16 10:00:21 -0500

    Updates, added env var controls per casey's comments

--- hack/development-environment/kind-local-up.sh
+++ hack/development-environment/kind-local-up.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
 # Thanks to https://alexbrand.dev/post/creating-a-kind-cluster-with-calico-networking/ for this snippet :)
 cat << EOF > calico-conf.yaml
 kind: Cluster
@@ -11,20 +12,14 @@ EOF
 
 # Where all your source lives...
 ROOT_CALICO_REPOS_DIR="${ROOT_CALICO_REPOS_DIR:-/home/$USER/calico_all}"
-
-if [[ -v ROOT_CALICO_REPOS_DIR ]] ;  then
-    echo "foudn input var for ROOT_CALICO_REPOS_DIR =  $ROOT_CALICO_REPOS_DIR"
-else
-    ROOT_CALICO_REPOS_DIR ?="~/calico_all/"
-fi
-echo "calico source ---> $ROOT_CALICO_REPOS_DIR"
-
-echo "$ROOT_CALICO_REPOS_DIR is the input dir for calico sources"
-if [[ ! -d $ROOT_CALICO_REPOS_DIR/node ]]; then
-    ls -altrh $ROOT_CALICO_REPOS_DIR
-    echo "clone down all the calico repos before starting/"
-    exit 1
-fi
+function check() {
+	if [[ ! -v ROOT_CALICO_REPOS_DIR ]] ;  then
+		echo "Need to specify ROOT_CALICO_REPOS_DIR = "
+	fi
+	if [[ ! -v BUILD_CALICO ]] ; then 
+		BUILD_CALICO="true"
+	fi
+}
 
 function build() {
     pushd $ROOT_CALICO_REPOS_DIR/calico/
@@ -39,16 +34,15 @@ function load_images() {
 	# Now, we need to copy local docker images into the container
 	# daemon running inside kind.
 	echo "Copying images into kind cluster !!!"
-	for i in cni-plugin node pod2daemon kube-controllers; do 
+	for i in "cni-plugin" "node" "pod2daemon" "kube-controllers"; do 
 		echo "...$i"
-		kind load docker-image cd/$i-amd64
+		kind load docker-image cd/$i:latest-amd64 --name calico-test
 	done
-
 }
 function install_k8s() {
-    kind create cluster --config calico-conf.yaml
-    export KUBECONFIG="$(kind get kubeconfig-path --name="kind")"
-    chmod 755 ~/.kube/kind-config-kind
+    kind delete cluster --name calico-test
+    kind create cluster --name calico-test --config calico-conf.yaml
+    export KUBECONFIG="$(kind get kubeconfig-path --name=calico-test)"
     until kubectl cluster-info;  do
         echo "`date`waiting for cluster..."
         sleep 2
@@ -57,10 +51,10 @@ function install_k8s() {
 
 function install_calico() {
     kubectl get pods
-        pushd $ROOT_CALICO_REPOS_DIR/calico/_output/dev-manifests
-            kubectl apply -f ./calico.yaml 
-            kubectl get pods -n kube-system
-        popd
+    pushd ${ROOT_CALICO_REPOS_DIR}/calico/_output/dev-manifests
+    	kubectl apply -f ./calico.yaml 
+    	kubectl get pods -n kube-system
+    popd
     sleep 5 ; kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
     sleep 5 ; kubectl -n kube-system get pods | grep calico-node
     echo "will wait for calico to start running now... "
@@ -70,7 +64,14 @@ function install_calico() {
     done
 }
 
-build
+check
+if [[ ! "${BUILD_CALICO}" == "false" ]] ; then
+	build
+fi
+if [[ ! -d ${ROOT_CALICO_REPOS_DIR}/calico/_output ]] ; then
+	echo "No build output directory ! Provide a build of calico before we finish installation"
+	exit 1
+fi
 install_k8s
 load_images
 install_calico
