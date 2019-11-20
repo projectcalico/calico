a76831957593a2b211934e925372faca5c0c1e09
Anton Klokau <anton.klokau@gmail.com>
2019-11-20 08:42:57 -0500

    Fixed breadcrumbs

--- _plugins/breadcrumbs.rb
+++ _plugins/breadcrumbs.rb
@@ -11,8 +11,8 @@ Jekyll::Hooks.register [:pages, :documents], :pre_render do |side, payload|  # d
     payload["breadcrumbs"] = []
     path = side.url.split("/")
 
-    1.upto(path.size - 1) do |int|
-      joined_path = path[1..int].join("/")
+    0.upto(path.size - 1) do |int|
+      joined_path = path[0..int].join("/")
       sides = [].concat(side.site.pages).concat(side.site.documents)
       item = sides.find { |side_| joined_path == "" && side_.url == "/" || side_.url.chomp("/") == joined_path }
       payload["breadcrumbs"] << drop.new(item, payload) if item
