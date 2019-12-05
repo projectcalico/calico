b3f5b169681e4d55ee4820680cae382d7c20df51
Tomas Hruby <tomas@tigera.io>
2019-12-05 10:54:07 -0800

    bpf k8s proxy tests follow ginkgo patterns
    Replaced It with By to make tests that check state transitions
    deterministing when randomized.
    
    Easier cleanup after a test fails.

--- bpf/proxy/proxy_test.go
+++ bpf/proxy/proxy_test.go
@@ -53,15 +53,17 @@ var _ = Describe("BPF Proxy", func() {
 		p, err := proxy.New(k8s, dp, "testnode", proxy.WithImmediateSync())
 		Expect(err).NotTo(HaveOccurred())
 
+		defer func() {
+			close(syncStop)
+			p.Stop()
+		}()
+
 		dp.checkState(func(s proxy.DPSyncerState) {
 			Expect(len(s.SvcMap)).To(Equal(0))
 			Expect(len(s.EpsMap)).To(Equal(0))
 			Expect(len(s.StaleUDPEps)).To(Equal(0))
 			Expect(len(s.StaleUDPSvcs)).To(Equal(0))
 		})
-
-		close(syncStop)
-		p.Stop()
 	})
 
 	testSvc := &v1.Service{
@@ -144,279 +146,288 @@ var _ = Describe("BPF Proxy", func() {
 		},
 	}
 
-	var p proxy.Proxy
-	var dp *mockSyncer
-	k8s := fake.NewSimpleClientset(testSvc, testSvcEps, secondSvc, secondSvcEps)
+	Describe("with k8s client", func() {
+		var p proxy.Proxy
+		var dp *mockSyncer
+		k8s := fake.NewSimpleClientset(testSvc, testSvcEps, secondSvc, secondSvcEps)
 
-	It("should create proxy with fake client and mock syncer", func() {
-		var err error
+		BeforeEach(func() {
+			By("creating proxy with fake client and mock syncer", func() {
+				var err error
 
-		syncStop = make(chan struct{})
-		dp = newMockSyncer(syncStop)
+				syncStop = make(chan struct{})
+				dp = newMockSyncer(syncStop)
 
-		p, err = proxy.New(k8s, dp, "testnode", proxy.WithImmediateSync())
-		Expect(err).NotTo(HaveOccurred())
-	})
-
-	It("should get the initial sync", func() {
-		dp.checkState(func(s proxy.DPSyncerState) {
-			Expect(len(s.SvcMap)).To(Equal(2))
-			Expect(len(s.EpsMap)).To(Equal(2))
-			Expect(len(s.StaleUDPEps)).To(Equal(0))
-			Expect(len(s.StaleUDPSvcs)).To(Equal(0))
-		})
-	})
-
-	It("should add a service", func() {
-		err := k8s.Tracker().Add(
-			&v1.Service{
-				TypeMeta:   typeMetaV1("Service"),
-				ObjectMeta: objectMeataV1("added"),
-				Spec: v1.ServiceSpec{
-					ClusterIP: "10.1.0.3",
-					Type:      v1.ServiceTypeClusterIP,
-					Selector: map[string]string{
-						"app": "test",
-					},
-					Ports: []v1.ServicePort{
-						{
-							Protocol: v1.ProtocolTCP,
-							Port:     1221,
-						},
-					},
-				},
-			},
-		)
-		Expect(err).NotTo(HaveOccurred())
-
-		dp.checkState(func(s proxy.DPSyncerState) {
-			Expect(len(s.SvcMap)).To(Equal(3))
-			Expect(len(s.EpsMap)).To(Equal(2))
-			Expect(len(s.StaleUDPEps)).To(Equal(0))
-			Expect(len(s.StaleUDPSvcs)).To(Equal(0))
-		})
-	})
-
-	It("should delete the last added service", func() {
-		err := k8s.Tracker().Delete(v1.SchemeGroupVersion.WithResource("services"), "default", "added")
-		Expect(err).NotTo(HaveOccurred())
-
-		dp.checkState(func(s proxy.DPSyncerState) {
-			Expect(len(s.SvcMap)).To(Equal(2))
-			Expect(len(s.EpsMap)).To(Equal(2))
-			Expect(len(s.StaleUDPEps)).To(Equal(0))
-			Expect(len(s.StaleUDPSvcs)).To(Equal(0))
-		})
-	})
-
-	It("should delete an endpoint of the second-service", func() {
-		err := k8s.Tracker().Update(v1.SchemeGroupVersion.WithResource("endpoints"),
-			&v1.Endpoints{
-				TypeMeta:   typeMetaV1("Endpoints"),
-				ObjectMeta: objectMeataV1("second-service"),
-				Subsets: []v1.EndpointSubset{
-					{
-						Addresses: []v1.EndpointAddress{
-							{
-								IP: "10.1.2.11",
-							},
-						},
-						Ports: []v1.EndpointPort{
-							{
-								Port: 1221,
-							},
-						},
-					},
-				},
-			},
-			"default")
-		Expect(err).NotTo(HaveOccurred())
-
-		secondSvcEpsKey := k8sp.ServicePortName{
-			NamespacedName: types.NamespacedName{
-				Namespace: secondSvcEps.ObjectMeta.Namespace,
-				Name:      secondSvcEps.ObjectMeta.Name,
-			},
-		}
-
-		dp.checkState(func(s proxy.DPSyncerState) {
-			Expect(len(s.SvcMap)).To(Equal(2))
-			Expect(len(s.EpsMap)).To(Equal(2))
-			Expect(len(s.EpsMap[secondSvcEpsKey])).To(Equal(1))
-			Expect(len(s.StaleUDPEps)).To(Equal(0))
-			Expect(len(s.StaleUDPSvcs)).To(Equal(0))
-		})
-	})
-
-	It("should delete the second-service", func() {
-		err := k8s.Tracker().Delete(v1.SchemeGroupVersion.WithResource("services"),
-			"default", "second-service")
-		Expect(err).NotTo(HaveOccurred())
-
-		dp.checkState(func(s proxy.DPSyncerState) {
-			Expect(len(s.SvcMap)).To(Equal(1))
-			Expect(len(s.EpsMap)).To(Equal(2))
-			Expect(len(s.StaleUDPEps)).To(Equal(0))
-			Expect(len(s.StaleUDPSvcs)).To(Equal(1))
-		})
-	})
-
-	It("should add Endpoints with named ports", func() {
-		httpSvcEps := &v1.Endpoints{
-			TypeMeta:   typeMetaV1("Endpoints"),
-			ObjectMeta: objectMeataV1("http-service"),
-			Subsets: []v1.EndpointSubset{
-				{
-					Addresses: []v1.EndpointAddress{
-						{
-							IP:       "10.1.2.111",
-							NodeName: strPtr("testnode"),
-						},
-						{
-							IP:       "10.1.2.222",
-							NodeName: strPtr("anothertestnode"),
-						},
-					},
-					Ports: []v1.EndpointPort{
-						{
-							Name: "http",
-							Port: 80,
-						},
-						{
-							Name: "http-alt",
-							Port: 8080,
-						},
-						{
-							Name: "https",
-							Port: 443,
-						},
-					},
-				},
-			},
-		}
-
-		err := k8s.Tracker().Add(httpSvcEps)
-		Expect(err).NotTo(HaveOccurred())
-
-		dp.checkState(func(s proxy.DPSyncerState) {
-			for _, port := range httpSvcEps.Subsets[0].Ports {
-				Expect(len(s.SvcMap)).To(Equal(1))
-				Expect(len(s.EpsMap)).To(Equal(5))
-				Expect(len(s.StaleUDPEps)).To(Equal(0))
-				Expect(len(s.StaleUDPSvcs)).To(Equal(0))
-
-				ep := s.EpsMap[k8sp.ServicePortName{
-					NamespacedName: types.NamespacedName{
-						Namespace: httpSvcEps.ObjectMeta.Namespace,
-						Name:      httpSvcEps.ObjectMeta.Name,
-					},
-					Port: port.Name,
-				}]
-
-				Expect(len(ep)).To(Equal(2))
-				Expect(ep[0].GetIsLocal).NotTo(Equal(ep[1].GetIsLocal))
-			}
-		})
-	})
-
-	It("should include endpoints without service", func() {
-		err := k8s.Tracker().Add(
-			&v1.Endpoints{
-				TypeMeta:   typeMetaV1("Endpoints"),
-				ObjectMeta: objectMeataV1("noservice"),
-				Subsets: []v1.EndpointSubset{
-					{
-						Addresses: []v1.EndpointAddress{
-							{
-								IP: "10.1.2.244",
-							},
-						},
-						Ports: []v1.EndpointPort{
-							{
-								Port: 666,
-							},
-						},
-					},
-				},
+				p, err = proxy.New(k8s, dp, "testnode", proxy.WithImmediateSync())
+				Expect(err).NotTo(HaveOccurred())
 			})
-		Expect(err).NotTo(HaveOccurred())
-
-		dp.checkState(func(s proxy.DPSyncerState) {
-			Expect(len(s.SvcMap)).To(Equal(1))
-			Expect(len(s.EpsMap)).To(Equal(6))
-			Expect(len(s.StaleUDPEps)).To(Equal(0))
-			Expect(len(s.StaleUDPSvcs)).To(Equal(0))
 		})
-	})
 
-	It("should add a NodePort", func() {
-		nodeport := &v1.Service{
-			TypeMeta:   typeMetaV1("Service"),
-			ObjectMeta: objectMeataV1("nodeport"),
-			Spec: v1.ServiceSpec{
-				ClusterIP: "10.1.0.1",
-				Type:      v1.ServiceTypeNodePort,
-				Selector: map[string]string{
-					"app": "test",
-				},
-				Ports: []v1.ServicePort{
-					{
-						Protocol: v1.ProtocolTCP,
-						Port:     1234,
-						NodePort: 32678,
-					},
-				},
-			},
-		}
+		AfterEach(func() {
+			By("stopping the proxy", func() {
+				close(syncStop)
+				p.Stop()
+			})
+		})
 
-		nodeportEps := &v1.Endpoints{
-			TypeMeta:   typeMetaV1("Endpoints"),
-			ObjectMeta: objectMeataV1("nodeport"),
-			Subsets: []v1.EndpointSubset{
-				{
-					Addresses: []v1.EndpointAddress{
-						{
-							IP: "10.1.2.1",
+		It("should make the right transitions", func() {
+
+			By("getting the initial sync", func() {
+				dp.checkState(func(s proxy.DPSyncerState) {
+					Expect(len(s.SvcMap)).To(Equal(2))
+					Expect(len(s.EpsMap)).To(Equal(2))
+					Expect(len(s.StaleUDPEps)).To(Equal(0))
+					Expect(len(s.StaleUDPSvcs)).To(Equal(0))
+				})
+			})
+
+			By("adding a service", func() {
+				err := k8s.Tracker().Add(
+					&v1.Service{
+						TypeMeta:   typeMetaV1("Service"),
+						ObjectMeta: objectMeataV1("added"),
+						Spec: v1.ServiceSpec{
+							ClusterIP: "10.1.0.3",
+							Type:      v1.ServiceTypeClusterIP,
+							Selector: map[string]string{
+								"app": "test",
+							},
+							Ports: []v1.ServicePort{
+								{
+									Protocol: v1.ProtocolTCP,
+									Port:     1221,
+								},
+							},
 						},
 					},
-					Ports: []v1.EndpointPort{
-						{
-							Port: 1234,
+				)
+				Expect(err).NotTo(HaveOccurred())
+
+				dp.checkState(func(s proxy.DPSyncerState) {
+					Expect(len(s.SvcMap)).To(Equal(3))
+					Expect(len(s.EpsMap)).To(Equal(2))
+					Expect(len(s.StaleUDPEps)).To(Equal(0))
+					Expect(len(s.StaleUDPSvcs)).To(Equal(0))
+				})
+			})
+
+			By("deleting the last added service", func() {
+				err := k8s.Tracker().Delete(v1.SchemeGroupVersion.WithResource("services"), "default", "added")
+				Expect(err).NotTo(HaveOccurred())
+
+				dp.checkState(func(s proxy.DPSyncerState) {
+					Expect(len(s.SvcMap)).To(Equal(2))
+					Expect(len(s.EpsMap)).To(Equal(2))
+					Expect(len(s.StaleUDPEps)).To(Equal(0))
+					Expect(len(s.StaleUDPSvcs)).To(Equal(0))
+				})
+			})
+
+			By("deleting an endpoint of the second-service", func() {
+				err := k8s.Tracker().Update(v1.SchemeGroupVersion.WithResource("endpoints"),
+					&v1.Endpoints{
+						TypeMeta:   typeMetaV1("Endpoints"),
+						ObjectMeta: objectMeataV1("second-service"),
+						Subsets: []v1.EndpointSubset{
+							{
+								Addresses: []v1.EndpointAddress{
+									{
+										IP: "10.1.2.11",
+									},
+								},
+								Ports: []v1.EndpointPort{
+									{
+										Port: 1221,
+									},
+								},
+							},
 						},
 					},
-				},
-			},
-		}
+					"default")
+				Expect(err).NotTo(HaveOccurred())
 
-		err := k8s.Tracker().Add(nodeport)
-		Expect(err).NotTo(HaveOccurred())
-		dp.checkState(func(s proxy.DPSyncerState) { /* just consume the event */ })
+				secondSvcEpsKey := k8sp.ServicePortName{
+					NamespacedName: types.NamespacedName{
+						Namespace: secondSvcEps.ObjectMeta.Namespace,
+						Name:      secondSvcEps.ObjectMeta.Name,
+					},
+				}
 
-		err = k8s.Tracker().Add(nodeportEps)
-		Expect(err).NotTo(HaveOccurred())
+				dp.checkState(func(s proxy.DPSyncerState) {
+					Expect(len(s.SvcMap)).To(Equal(2))
+					Expect(len(s.EpsMap)).To(Equal(2))
+					Expect(len(s.EpsMap[secondSvcEpsKey])).To(Equal(1))
+					Expect(len(s.StaleUDPEps)).To(Equal(0))
+					Expect(len(s.StaleUDPSvcs)).To(Equal(0))
+				})
+			})
 
-		dp.checkState(func(s proxy.DPSyncerState) {
-			Expect(len(s.SvcMap)).To(Equal(2))
-			Expect(len(s.EpsMap)).To(Equal(7))
-			Expect(len(s.StaleUDPEps)).To(Equal(0))
-			Expect(len(s.StaleUDPSvcs)).To(Equal(0))
+			By("deleting the second-service", func() {
+				err := k8s.Tracker().Delete(v1.SchemeGroupVersion.WithResource("services"),
+					"default", "second-service")
+				Expect(err).NotTo(HaveOccurred())
 
-			npKey := k8sp.ServicePortName{
-				NamespacedName: types.NamespacedName{
-					Name:      "nodeport",
-					Namespace: "default",
-				},
-			}
+				dp.checkState(func(s proxy.DPSyncerState) {
+					Expect(len(s.SvcMap)).To(Equal(1))
+					Expect(len(s.EpsMap)).To(Equal(2))
+					Expect(len(s.StaleUDPEps)).To(Equal(0))
+					Expect(len(s.StaleUDPSvcs)).To(Equal(1))
+				})
+			})
 
-			Expect(s.SvcMap[npKey].(*k8sp.BaseServiceInfo).Port).
-				To(Equal(int(nodeport.Spec.Ports[0].Port)))
-			Expect(s.SvcMap[npKey].GetNodePort()).To(Equal(int(nodeport.Spec.Ports[0].NodePort)))
+			By("adding Endpoints with named ports", func() {
+				httpSvcEps := &v1.Endpoints{
+					TypeMeta:   typeMetaV1("Endpoints"),
+					ObjectMeta: objectMeataV1("http-service"),
+					Subsets: []v1.EndpointSubset{
+						{
+							Addresses: []v1.EndpointAddress{
+								{
+									IP:       "10.1.2.111",
+									NodeName: strPtr("testnode"),
+								},
+								{
+									IP:       "10.1.2.222",
+									NodeName: strPtr("anothertestnode"),
+								},
+							},
+							Ports: []v1.EndpointPort{
+								{
+									Name: "http",
+									Port: 80,
+								},
+								{
+									Name: "http-alt",
+									Port: 8080,
+								},
+								{
+									Name: "https",
+									Port: 443,
+								},
+							},
+						},
+					},
+				}
+
+				err := k8s.Tracker().Add(httpSvcEps)
+				Expect(err).NotTo(HaveOccurred())
+
+				dp.checkState(func(s proxy.DPSyncerState) {
+					for _, port := range httpSvcEps.Subsets[0].Ports {
+						Expect(len(s.SvcMap)).To(Equal(1))
+						Expect(len(s.EpsMap)).To(Equal(5))
+						Expect(len(s.StaleUDPEps)).To(Equal(0))
+						Expect(len(s.StaleUDPSvcs)).To(Equal(0))
+
+						ep := s.EpsMap[k8sp.ServicePortName{
+							NamespacedName: types.NamespacedName{
+								Namespace: httpSvcEps.ObjectMeta.Namespace,
+								Name:      httpSvcEps.ObjectMeta.Name,
+							},
+							Port: port.Name,
+						}]
+
+						Expect(len(ep)).To(Equal(2))
+						Expect(ep[0].GetIsLocal).NotTo(Equal(ep[1].GetIsLocal))
+					}
+				})
+			})
+
+			By("including endpoints without service", func() {
+				err := k8s.Tracker().Add(
+					&v1.Endpoints{
+						TypeMeta:   typeMetaV1("Endpoints"),
+						ObjectMeta: objectMeataV1("noservice"),
+						Subsets: []v1.EndpointSubset{
+							{
+								Addresses: []v1.EndpointAddress{
+									{
+										IP: "10.1.2.244",
+									},
+								},
+								Ports: []v1.EndpointPort{
+									{
+										Port: 666,
+									},
+								},
+							},
+						},
+					})
+				Expect(err).NotTo(HaveOccurred())
+
+				dp.checkState(func(s proxy.DPSyncerState) {
+					Expect(len(s.SvcMap)).To(Equal(1))
+					Expect(len(s.EpsMap)).To(Equal(6))
+					Expect(len(s.StaleUDPEps)).To(Equal(0))
+					Expect(len(s.StaleUDPSvcs)).To(Equal(0))
+				})
+			})
+
+			By("adding a NodePort", func() {
+				nodeport := &v1.Service{
+					TypeMeta:   typeMetaV1("Service"),
+					ObjectMeta: objectMeataV1("nodeport"),
+					Spec: v1.ServiceSpec{
+						ClusterIP: "10.1.0.1",
+						Type:      v1.ServiceTypeNodePort,
+						Selector: map[string]string{
+							"app": "test",
+						},
+						Ports: []v1.ServicePort{
+							{
+								Protocol: v1.ProtocolTCP,
+								Port:     1234,
+								NodePort: 32678,
+							},
+						},
+					},
+				}
+
+				nodeportEps := &v1.Endpoints{
+					TypeMeta:   typeMetaV1("Endpoints"),
+					ObjectMeta: objectMeataV1("nodeport"),
+					Subsets: []v1.EndpointSubset{
+						{
+							Addresses: []v1.EndpointAddress{
+								{
+									IP: "10.1.2.1",
+								},
+							},
+							Ports: []v1.EndpointPort{
+								{
+									Port: 1234,
+								},
+							},
+						},
+					},
+				}
+
+				err := k8s.Tracker().Add(nodeport)
+				Expect(err).NotTo(HaveOccurred())
+				dp.checkState(func(s proxy.DPSyncerState) { /* just consume the event */ })
+
+				err = k8s.Tracker().Add(nodeportEps)
+				Expect(err).NotTo(HaveOccurred())
+
+				dp.checkState(func(s proxy.DPSyncerState) {
+					Expect(len(s.SvcMap)).To(Equal(2))
+					Expect(len(s.EpsMap)).To(Equal(7))
+					Expect(len(s.StaleUDPEps)).To(Equal(0))
+					Expect(len(s.StaleUDPSvcs)).To(Equal(0))
+
+					npKey := k8sp.ServicePortName{
+						NamespacedName: types.NamespacedName{
+							Name:      "nodeport",
+							Namespace: "default",
+						},
+					}
+
+					Expect(s.SvcMap[npKey].(*k8sp.BaseServiceInfo).Port).
+						To(Equal(int(nodeport.Spec.Ports[0].Port)))
+					Expect(s.SvcMap[npKey].GetNodePort()).To(Equal(int(nodeport.Spec.Ports[0].NodePort)))
+				})
+			})
 		})
-	})
-
-	It("should stop the proxy", func() {
-		close(syncStop)
-		p.Stop()
 	})
 })
 
