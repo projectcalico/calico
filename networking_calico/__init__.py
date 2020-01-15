7748d18e83639753de6a230a34fbcd242bf49ff0
Neil Jerram <neil@tigera.io>
2020-01-15 23:19:57 +0000

    Remove PBR
    Change-Id: I1caf3bb559786a8f4886da6fd20e63a547a1042f

--- networking_calico/__init__.py
+++ networking_calico/__init__.py
@@ -11,9 +11,3 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
-import pbr.version
-
-
-__version__ = pbr.version.VersionInfo(
-    'networking-calico').version_string()
--- setup.py
+++ setup.py
@@ -14,16 +14,6 @@
 # limitations under the License.
 
 # THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
-import setuptools
+from setuptools import setup, find_packages
 
-# In python < 2.7.4, a lazy loading of package `pbr` will break
-# setuptools if some other modules registered functions in `atexit`.
-# solution from: http://bugs.python.org/issue15881#msg170215
-try:
-    import multiprocessing  # noqa
-except ImportError:
-    pass
-
-setuptools.setup(
-    setup_requires=['pbr'],
-    pbr=True)
+setup(packages=find_packages())
