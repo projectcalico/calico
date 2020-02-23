5db599840df56a8863713f3abb93f56cb269b04b
Tomas Hruby <tomas@tigera.io>
2020-02-22 16:13:25 -0800

    Fix ICMP reply size calculation and trimming

--- bpf-gpl/icmp.h
+++ bpf-gpl/icmp.h
@@ -23,6 +23,7 @@
 #include <linux/icmp.h>
 
 #include "bpf.h"
+#include "log.h"
 #include "skb.h"
 
 static CALI_BPF_INLINE int icmp_v4_reply(struct __sk_buff *skb,
@@ -35,49 +36,78 @@ static CALI_BPF_INLINE int icmp_v4_reply(struct __sk_buff *skb,
 	int ret;
 
 	if (skb_too_short(skb)) {
-		CALI_DEBUG_NO_FLAG("ICMP v4 reply: too short after making room\n");
+		CALI_DEBUG("ICMP v4 reply: too short before making room\n");
 		return -1;
 	}
 
 	ip = skb_iphdr(skb);
 
-	CALI_DEBUG_NO_FLAG("ip->ihl: %d\n", ip->ihl);
+	CALI_DEBUG("ip->ihl: %d\n", ip->ihl);
 	if (ip->ihl > 5) {
-		CALI_DEBUG_NO_FLAG("ICMP v4 reply: IP options\n");
+		CALI_DEBUG("ICMP v4 reply: IP options\n");
 		return -1;
 	}
 
 	ip_orig = *ip;
 
-	/* make room for the new IP + ICMP header */
-	len = sizeof(struct iphdr) + sizeof(struct icmphdr);
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,2,0)
-	ret = bpf_skb_adjust_room(skb, len, BPF_ADJ_ROOM_MAC, 0);
-#else
-	uint32_t ip_inner_off = sizeof(struct ethhdr) + len;
-	ret = bpf_skb_adjust_room(skb, len, BPF_ADJ_ROOM_NET, 0);
-#endif
-	if (ret) {
-		CALI_DEBUG_NO_FLAG("ICMP v4 reply: failed to make room\n");
+	/* Trim the packet to the desired length. ICMP requires min 8 bytes of
+	 * payload but the SKB implementation gets upset if we try to trim
+	 * part-way through the UDP/TCP header.
+	 */
+	len = sizeof(struct ethhdr) + sizeof(struct iphdr) + 64;
+	switch (ip->protocol) {
+	case IPPROTO_TCP:
+		len += sizeof(struct tcphdr);
+		break;
+	case IPPROTO_UDP:
+		len += sizeof(struct udphdr);
+		break;
+	default:
+		len += 8;
+		break;
+	}
+
+	int err = bpf_skb_change_tail(skb, len,  0);
+	if (err) {
+		CALI_DEBUG("ICMP v4 reply: early bpf_skb_change_tail (len=%d) failed (err=%d)\n", len, err);
 		return -1;
 	}
 
-	/* ICMP reply carries the IP header + 8 bytes of data */
-	len += sizeof(struct ethhdr) + sizeof(struct iphdr) + 8;
+	// Revalidate.
+	if (skb_too_short(skb)) {
+		CALI_DEBUG("ICMP v4 reply: too short after trimming packet\n");
+		return -1;
+	}
+
+	/* make room for the new IP + ICMP header */
+	int new_hdrs_len = sizeof(struct iphdr) + sizeof(struct icmphdr);
+#if 1
+	ret = bpf_skb_adjust_room(skb, new_hdrs_len, BPF_ADJ_ROOM_MAC, 0);
+#else
+	uint32_t ip_inner_off = sizeof(struct ethhdr) + len;
+	ret = bpf_skb_adjust_room(skb, new_hdrs_len, BPF_ADJ_ROOM_NET, 0);
+#endif
+	if (ret) {
+		CALI_DEBUG("ICMP v4 reply: failed to make room\n");
+		return -1;
+	}
+
+	/* ICMP reply carries the IP header + at least 8 bytes of data. */
+	len += new_hdrs_len;
 
 	if (skb_shorter(skb, len)) {
-		CALI_DEBUG_NO_FLAG("ICMP v4 reply: too short after making room\n");
+		CALI_DEBUG("ICMP v4 reply: too short after making room\n");
 		return -1;
 	}
 
 	/* N.B. getting the ip pointer here again makes verifier happy */
 	ip = skb_iphdr(skb);
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5,2,0)
+#if 0
 	struct iphdr *ip_inner;
 
 	if (skb_shorter(skb, ip_inner_off + sizeof(struct iphdr))) {
-		CALI_DEBUG_NO_FLAG("ICMP v4 reply: too short to move ip header\n");
+		CALI_DEBUG("ICMP v4 reply: too short to move ip header\n");
 		return -1;
 	}
 
@@ -86,9 +116,9 @@ static CALI_BPF_INLINE int icmp_v4_reply(struct __sk_buff *skb,
 	*ip_inner = ip_orig;
 #endif
 
-	/* we do not touch ethhdr, we rely on linux to rewrite it after routing */
-	/* XXX we might want to swap MACs and bounce it back from the same device */
-
+	/* we do not touch ethhdr, we rely on linux to rewrite it after routing
+	 * XXX we might want to swap MACs and bounce it back from the same device
+	 */
 	ip->version = 4;
 	ip->ihl = 5;
 	ip->tos = 0;
@@ -103,7 +133,7 @@ static CALI_BPF_INLINE int icmp_v4_reply(struct __sk_buff *skb,
 	 * we only call this function because of NodePOrt encap
 	 */
 	if (ip_orig.daddr != cali_host_ip()) {
-		CALI_DEBUG_NO_FLAG("ICMP v4 reply: ip_orig.daddr != cali_host_ip() 0x%x\n", ip_orig.daddr);
+		CALI_DEBUG("ICMP v4 reply: ip_orig.daddr != cali_host_ip() 0x%x\n", ip_orig.daddr);
 	}
 #endif
 
@@ -118,31 +148,28 @@ static CALI_BPF_INLINE int icmp_v4_reply(struct __sk_buff *skb,
 	icmp->checksum = 0;
 
 	ip_csum = bpf_csum_diff(0, 0, (void *)ip, sizeof(*ip), 0);
-	icmp_csum = bpf_csum_diff(0, 0, (void *)icmp, sizeof(*icmp) + sizeof(struct iphdr) + 8 , 0);
+	icmp_csum = bpf_csum_diff(0, 0, (void *)icmp, len -  sizeof(*ip) - sizeof(struct ethhdr), 0);
 
 	ret = bpf_l3_csum_replace(skb,
 			skb_offset(skb, ip) + offsetof(struct iphdr, check), 0, ip_csum, 0);
 	if (ret) {
-		CALI_DEBUG_NO_FLAG("ICMP v4 reply: set ip csum failed\n");
+		CALI_DEBUG("ICMP v4 reply: set ip csum failed\n");
 		return -1;
 	}
 
 	if (skb_shorter(skb, len)) {
-		CALI_DEBUG_NO_FLAG("ICMP v4 reply: too short after ip csum fix\n");
+		CALI_DEBUG("ICMP v4 reply: too short after ip csum fix\n");
 		return -1;
 	}
 
 	ret = bpf_l3_csum_replace(skb, sizeof(struct ethhdr) + sizeof(struct iphdr) +
 					offsetof(struct icmphdr, checksum), 0, icmp_csum, 0);
 	if (ret) {
-		CALI_DEBUG_NO_FLAG("ICMP v4 reply: set icmp csum failed\n");
+		CALI_DEBUG("ICMP v4 reply: set icmp csum failed\n");
 		return -1;
 	}
 
-	/* trim the packet to the desired length */
-	if (bpf_skb_change_tail(skb, len,  0)) {
-		return -1;
-	}
+	CALI_DEBUG("ICMP v4 reply: change tail succeeded\n");
 
 	return 0;
 }
