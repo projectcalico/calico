a658e42db5c5a06507eea16ea984f2813d034db5
Spike Curtis <spike@tigera.io>
2020-03-31 16:06:11 -0700

    Update KubeControllersConfiguration to use metav1.Duration
    Signed-off-by: Spike Curtis <spike@tigera.io>

--- calicoctl/resourcemgr/kubecontrollersconfig_test.go
+++ calicoctl/resourcemgr/kubecontrollersconfig_test.go
@@ -15,8 +15,11 @@
 package resourcemgr_test
 
 import (
+	"time"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	api "github.com/projectcalico/libcalico-go/lib/apis/v3"
 )
@@ -54,20 +57,20 @@ spec:
 		Expect(kcc.Name).To(Equal("default"))
 		Expect(kcc.Spec.LogSeverityScreen).To(Equal("Info"))
 		Expect(kcc.Spec.HealthChecks).To(Equal(api.Enabled))
-		Expect(kcc.Spec.EtcdV3CompactionPeriod).To(Equal("10m30s"))
+		Expect(kcc.Spec.EtcdV3CompactionPeriod).To(Equal(&v1.Duration{Duration: (time.Minute * 10) + (time.Second * 30)}))
 		Expect(kcc.Spec.Controllers.Node).To(Equal(&api.NodeControllerConfig{
-			ReconcilerPeriod: "1m",
+			ReconcilerPeriod: &v1.Duration{Duration: time.Minute},
 			SyncLabels:       api.Enabled,
 			HostEndpoint:     &api.AutoHostEndpointConfig{AutoCreate: api.Disabled},
 		}))
 		Expect(kcc.Spec.Controllers.Policy).
-			To(Equal(&api.PolicyControllerConfig{ReconcilerPeriod: "2m"}))
+			To(Equal(&api.PolicyControllerConfig{ReconcilerPeriod: &v1.Duration{Duration: time.Minute * 2}}))
 		Expect(kcc.Spec.Controllers.WorkloadEndpoint).
-			To(Equal(&api.WorkloadEndpointControllerConfig{ReconcilerPeriod: "3m"}))
+			To(Equal(&api.WorkloadEndpointControllerConfig{ReconcilerPeriod: &v1.Duration{Duration: time.Minute * 3}}))
 		Expect(kcc.Spec.Controllers.ServiceAccount).
-			To(Equal(&api.ServiceAccountControllerConfig{ReconcilerPeriod: "4m"}))
+			To(Equal(&api.ServiceAccountControllerConfig{ReconcilerPeriod: &v1.Duration{Duration: time.Minute * 4}}))
 		Expect(kcc.Spec.Controllers.Namespace).
-			To(Equal(&api.NamespaceControllerConfig{ReconcilerPeriod: "5m"}))
+			To(Equal(&api.NamespaceControllerConfig{ReconcilerPeriod: &v1.Duration{Duration: time.Minute * 5}}))
 
 		// Status
 		Expect(kcc.Status.EnvironmentVars).To(BeNil())
@@ -78,7 +81,7 @@ spec:
 		Expect(kcc.Status.RunningConfig.Controllers.Namespace).To(BeNil())
 		Expect(kcc.Status.RunningConfig.LogSeverityScreen).To(Equal(""))
 		Expect(kcc.Status.RunningConfig.HealthChecks).To(Equal(""))
-		Expect(kcc.Status.RunningConfig.EtcdV3CompactionPeriod).To(Equal(""))
+		Expect(kcc.Status.RunningConfig.EtcdV3CompactionPeriod).To(BeNil())
 	})
 
 })
