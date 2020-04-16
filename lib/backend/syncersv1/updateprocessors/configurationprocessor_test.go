8e6b5c436d6a3e3576b726347bf2e65f58cb6a6f
Shaun Crampton <shaun@tigera.io>
2020-04-16 15:13:52 +0100

    Add BPFDisableUnprivileged config param.

--- lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
+++ lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
@@ -72,7 +72,7 @@ var _ = Describe("Test the generic configuration update processor and the concre
 		Kind: apiv3.KindBGPConfiguration,
 		Name: "node.bgpnode1",
 	}
-	numFelixConfigs := 76
+	numFelixConfigs := 77
 	numClusterConfigs := 4
 	numNodeClusterConfigs := 3
 	numBgpConfigs := 5
