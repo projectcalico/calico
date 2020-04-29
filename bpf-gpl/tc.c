8a98291c6f596a04cc51e440039c84a84aee46b7
Tomas Hruby <tomas@tigera.io>
2020-04-28 18:35:42 -0700

    bpf: do not override fib=false if no forward
    If the backend workload for for the a NodePort is local, no tunnel
    forwarding happens, we can still use local kernel to reliably do RPF
    check for us if we think it is needed.

--- bpf-gpl/tc.c
+++ bpf-gpl/tc.c
@@ -919,9 +919,6 @@ static CALI_BPF_INLINE struct fwd calico_tc_skb_accepted(struct __sk_buff *skb,
 			CALI_DEBUG("rt found for 0x%x\n", be32_to_host(state->post_nat_ip_dst));
 
 			encap_needed = !cali_rt_is_local(rt);
-
-			/* We cannot enforce RPF check on encapped traffic, do FIB if you can */
-			fib = true;
 		}
 
 		/* We have not created the conntrack yet since we did not know
@@ -945,6 +942,10 @@ static CALI_BPF_INLINE struct fwd calico_tc_skb_accepted(struct __sk_buff *skb,
 			state->ip_src = HOST_IP;
 			state->ip_dst = cali_rt_is_workload(rt) ? rt->next_hop : state->post_nat_ip_dst;
 			seen_mark = CALI_SKB_MARK_BYPASS_FWD;
+
+			/* We cannot enforce RPF check on encapped traffic, do FIB if you can */
+			fib = true;
+
 			goto nat_encap;
 		}
 
