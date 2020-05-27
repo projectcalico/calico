4d8170f720f955b28f8f374749b2b924d9e88c5f
sridhar <sridhar@tigera.io>
2020-05-27 15:29:59 +0530

    Handling endianness explicitly when copying prefix length

--- bpf/nat/maps.go
+++ bpf/nat/maps.go
@@ -82,7 +82,7 @@ func NewNATKeySrc(addr net.IP, port uint16, protocol uint8, cidr ip.V4CIDR) Fron
 	if len(addr) != 4 {
 		log.WithField("ip", addr).Panic("Bad IP")
 	}
-	k[0] = uint8(uint32(prefixlen) + uint32(cidr.Prefix()))
+	binary.LittleEndian.PutUint32(k[:4], uint32(prefixlen) + uint32(cidr.Prefix()))
 	copy(k[4:8], addr)
 	binary.LittleEndian.PutUint16(k[8:10], port)
 	k[10] = protocol
