tomastigera/project-calico-felix

bpf: conntrack cleaning only in kube-proxy
kube-proxy runs cleaning periodically, both for removing expired
conntrack entries as well as removing entries for connections that do
not have active services/backends.

bpf_conntrack_mgr removed

bpf/proxy/kube-proxy.go
@@ -23,6 +23,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 
 	"github.com/projectcalico/felix/bpf"
+	"github.com/projectcalico/felix/bpf/conntrack"
 	"github.com/projectcalico/felix/bpf/routes"
 )
 
@@ -45,6 +46,9 @@ type KubeProxy struct {
 	ctMap       bpf.Map
 	rt          *RTCache
 	opts        []Option
+
+	conntrackTimeouts conntrack.Timeouts
+	dsrEnabled        bool
 }
 
 // StartKubeProxy start a new kube-proxy if there was no error
@@ -63,6 +67,12 @@ func StartKubeProxy(k8s kubernetes.Interface, hostname string,
 
 		hostIPUpdates: make(chan []net.IP, 1),
 		exiting:       make(chan struct{}),
+	}
+
+	for _, o := range opts {
+		if err := o(kp); err != nil {
+			return nil, errors.WithMessage(err, "applying option to kube-proxy")
+		}
 	}
 
 	go func() {
@@ -97,12 +107,18 @@ func (kp *KubeProxy) run(hostIPs []net.IP) error {
 	copy(withLocalNP, hostIPs)
 	withLocalNP = append(withLocalNP, podNPIP)
 
-	syncer, err := NewSyncer(withLocalNP, kp.frontendMap, kp.backendMap, kp.affinityMap, kp.ctMap, kp.rt)
+	syncer, err := NewSyncer(withLocalNP, kp.frontendMap, kp.backendMap, kp.affinityMap, kp.rt)
 	if err != nil {
 		return errors.WithMessage(err, "new bpf syncer")
 	}
 
-	proxy, err := New(kp.k8s, syncer, kp.hostname, kp.opts...)
+	lc := conntrack.NewLivenessScanner(kp.conntrackTimeouts, kp.dsrEnabled)
+	connScan := conntrack.NewScanner(kp.ctMap,
+		lc.ScanEntry,
+		conntrack.NewStaleNATScanner(syncer.ConntrackFrontnedHasBackend),
+	)
+
+	proxy, err := New(kp.k8s, syncer, connScan, kp.hostname, kp.opts...)
 	if err != nil {
 		return errors.WithMessage(err, "new proxy")
 	}

bpf/proxy/proxy.go
@@ -18,6 +18,7 @@
 package proxy
 
 import (
+	"net"
 	"strings"
 	"sync"
 	"time"
@@ -39,6 +40,9 @@ import (
 	"k8s.io/kubernetes/pkg/proxy/config"
 	"k8s.io/kubernetes/pkg/proxy/healthcheck"
 	"k8s.io/kubernetes/pkg/util/async"
+
+	"github.com/projectcalico/felix/bpf/conntrack"
+	"github.com/projectcalico/felix/jitter"
 )
 
 // Proxy watches for updates of Services and Endpoints, maintains their mapping
@@ -59,6 +63,9 @@ type DPSyncerState struct {
 // observed changes to the dataplane
 type DPSyncer interface {
 	Apply(state DPSyncerState) error
+	ConntrackScanStart()
+	ConntrackScanEnd()
+	ConntrackFrontnedHasBackend(ip net.IP, port uint16, backendIP net.IP, backendPort uint16, proto uint8) bool
 	Stop()
 }
 
@@ -94,6 +101,8 @@ type proxy struct {
 	svcHealthServer healthcheck.ServiceHealthServer
 	healthzServer   healthcheck.ProxierHealthUpdater
 
+	connScan *conntrack.Scanner
+
 	stopCh   chan struct{}
 	stopWg   sync.WaitGroup
 	stopOnce sync.Once
@@ -104,7 +113,8 @@ type stoppableRunner interface {
 }
 
 // New returns a new Proxy for the given k8s interface
-func New(k8s kubernetes.Interface, dp DPSyncer, hostname string, opts ...Option) (Proxy, error) {
+func New(k8s kubernetes.Interface, dp DPSyncer, connScan *conntrack.Scanner,
+	hostname string, opts ...Option) (Proxy, error) {
 
 	if k8s == nil {
 		return nil, errors.Errorf("no k8s client")
@@ -117,6 +127,7 @@ func New(k8s kubernetes.Interface, dp DPSyncer, hostname string, opts ...Option)
 	p := &proxy{
 		k8s:      k8s,
 		dpSyncer: dp,
+		connScan: connScan,
 		hostname: hostname,
 		svcMap:   make(k8sp.ServiceMap),
 		epsMap:   make(k8sp.EndpointsMap),
@@ -190,14 +201,20 @@ func New(k8s kubernetes.Interface, dp DPSyncer, hostname string, opts ...Option)
 	p.startRoutine(func() { informerFactory.Start(p.stopCh) })
 	p.startRoutine(func() { svcConfig.Run(p.stopCh) })
 
+	if p.connScan != nil {
+		p.startRoutine(func() { p.conntrackCleaner(p.stopCh) })
+	}
+
 	return p, nil
 }
 
 func (p *proxy) Stop() {
 	p.stopOnce.Do(func() {
+		log.Info("Proxy stopping")
 		p.dpSyncer.Stop()
 		close(p.stopCh)
 		p.stopWg.Wait()
+		log.Info("Proxy stopped")
 	})
 }
 
@@ -324,6 +341,26 @@ func (p *proxy) OnEndpointSliceDelete(eps *discovery.EndpointSlice) {
 func (p *proxy) OnEndpointSlicesSynced() {
 	p.setEpsSynced()
 	p.forceSyncDP()
+}
+
+func (p *proxy) conntrackCleaner(stopCh <-chan struct{}) {
+	log.Debug("Conntrack cleanup thread started")
+	defer log.Debug("Conntrack cleanup thread stopped")
+
+	ticker := jitter.NewTicker(10*time.Second, 100*time.Millisecond)
+
+	for {
+		select {
+		case <-ticker.C:
+			log.Debug("Conntrack cleanup timer popped")
+			p.dpSyncer.ConntrackScanStart()
+			p.connScan.Scan()
+			p.dpSyncer.ConntrackScanEnd()
+		case <-stopCh:
+			log.Debug("Conntrack cleanup got stop signal")
+			return
+		}
+	}
 }
 
 type initState struct {

bpf/proxy/proxy_test.go
@@ -16,6 +16,7 @@ package proxy_test
 
 import (
 	"fmt"
+	"net"
 	"time"
 
 	. "github.com/onsi/ginkgo"
@@ -39,10 +40,10 @@ var _ = Describe("BPF Proxy", func() {
 	var syncStop chan struct{}
 
 	It("should fail without k8s client", func() {
-		_, err := proxy.New(nil, nil, "testnode")
+		_, err := proxy.New(nil, nil, nil, "testnode")
 		Expect(err).To(HaveOccurred())
 
-		_, err = proxy.New(fake.NewSimpleClientset(), nil, "testnode")
+		_, err = proxy.New(fake.NewSimpleClientset(), nil, nil, "testnode")
 		Expect(err).To(HaveOccurred())
 	})
 
@@ -52,7 +53,7 @@ var _ = Describe("BPF Proxy", func() {
 		syncStop = make(chan struct{})
 		dp := newMockSyncer(syncStop)
 
-		p, err := proxy.New(k8s, dp, "testnode", proxy.WithImmediateSync())
+		p, err := proxy.New(k8s, dp, nil, "testnode", proxy.WithImmediateSync())
 		Expect(err).NotTo(HaveOccurred())
 
 		defer func() {
@@ -174,7 +175,7 @@ var _ = Describe("BPF Proxy", func() {
 					opts = append(opts, proxy.WithEndpointsSlices())
 				}
 
-				p, err = proxy.New(k8s, dp, "testnode", opts...)
+				p, err = proxy.New(k8s, dp, nil, "testnode", opts...)
 				Expect(err).NotTo(HaveOccurred())
 			})
 		})
@@ -556,7 +557,7 @@ var _ = Describe("BPF Proxy", func() {
 					opts = append(opts, proxy.WithEndpointsSlices())
 				}
 
-				p, err = proxy.New(k8s, dp, testNodeName, opts...)
+				p, err = proxy.New(k8s, dp, nil, testNodeName, opts...)
 				Expect(err).NotTo(HaveOccurred())
 			})
 		})
@@ -592,6 +593,7 @@ var _ = Describe("BPF Proxy", func() {
 })
 
 type mockSyncer struct {
+	syncerConntrackAPIDummy
 	out  chan proxy.DPSyncerState
 	in   chan error
 	stop chan struct{}
@@ -616,6 +618,15 @@ func (s *mockSyncer) Apply(state proxy.DPSyncerState) error {
 	case <-s.stop:
 		return nil
 	}
+}
+
+type syncerConntrackAPIDummy struct{}
+
+func (*syncerConntrackAPIDummy) ConntrackScanStart() {}
+func (*syncerConntrackAPIDummy) ConntrackScanEnd()   {}
+func (*syncerConntrackAPIDummy) ConntrackFrontnedHasBackend(ip net.IP, port uint16, backendIP net.IP,
+	backendPort uint16, proto uint8) bool {
+	return false
 }
 
 func (s *mockSyncer) checkState(f func(proxy.DPSyncerState)) {

bpf/proxy/syncer.go
@@ -31,7 +31,6 @@ import (
 	k8sp "k8s.io/kubernetes/pkg/proxy"
 
 	"github.com/projectcalico/felix/bpf"
-	"github.com/projectcalico/felix/bpf/conntrack"
 	"github.com/projectcalico/felix/bpf/nat"
 	"github.com/projectcalico/felix/bpf/routes"
 	"github.com/projectcalico/felix/ip"
@@ -120,8 +119,7 @@ type Syncer struct {
 	activeSvcsMap map[ipPortProto]uint32
 	activeEpsMap  map[uint32]map[ipPort]struct{}
 
-	// We never have more than one thread accessing the [prev|new][Svc|Eps]Map,
-	// this is to just make sure and to make the --race checker happy
+	// Protects acessing the [prev|new][Svc|Eps]Map,
 	mapsLck sync.Mutex
 
 	// synced is true after reconciling the first Apply
@@ -139,8 +137,6 @@ type Syncer struct {
 	stickySvcs       map[nat.FrontEndAffinityKey]stickyFrontend
 	stickyEps        map[uint32]map[nat.BackendValue]struct{}
 	stickySvcDeleted bool
-
-	conntrackScanner *conntrack.Scanner
 }
 
 type ipPort struct {
@@ -189,7 +185,7 @@ func uniqueIPs(ips []net.IP) []net.IP {
 }
 
 // NewSyncer returns a new Syncer
-func NewSyncer(nodePortIPs []net.IP, svcsmap, epsmap, affmap, ctmap bpf.Map, rt Routes) (*Syncer, error) {
+func NewSyncer(nodePortIPs []net.IP, svcsmap, epsmap, affmap bpf.Map, rt Routes) (*Syncer, error) {
 	s := &Syncer{
 		bpfSvcs:     svcsmap,
 		bpfEps:      epsmap,
@@ -200,8 +196,6 @@ func NewSyncer(nodePortIPs []net.IP, svcsmap, epsmap, affmap, ctmap bpf.Map, rt 
 		prevEpsMap:  make(k8sp.EndpointsMap),
 		stop:        make(chan struct{}),
 	}
-
-	s.conntrackScanner = conntrack.NewScanner(ctmap, conntrack.NewStaleNATScanner(s.frontendHasBackend))
 
 	if err := s.loadOrigs(); err != nil {
 		return nil, err
@@ -503,15 +497,6 @@ func (s *Syncer) apply(state DPSyncerState) error {
 	s.newSvcMap = make(map[svcKey]svcInfo)
 	s.newEpsMap = make(k8sp.EndpointsMap)
 
-	s.activeSvcsMap = make(map[ipPortProto]uint32)
-	s.activeEpsMap = make(map[uint32]map[ipPort]struct{})
-
-	defer func() {
-		// free the maps when the iteration is complete
-		s.activeSvcsMap = nil
-		s.activeEpsMap = nil
-	}()
-
 	var expNPMisses []*expandMiss
 
 	// insert or update existing services
@@ -583,23 +568,9 @@ func (s *Syncer) apply(state DPSyncerState) error {
 		log.Infof("removed stale service %q", skey)
 	}
 
-	// build active maps for conntrack cleaning
-	for skey, sinfo := range s.newSvcMap {
-		if sinfo.count == 0 {
-			continue
-		}
-
-		if isSvcKeyDerived(skey) {
-			s.addActiveEps(sinfo.id, sinfo.svc, nil)
-		} else {
-			s.addActiveEps(sinfo.id, sinfo.svc, s.newEpsMap[skey.sname])
-		}
-	}
-
 	log.Debugf("new state written")
 
 	s.runExpandNPFixup(expNPMisses)
-	s.conntrackScanner.Scan()
 
 	return nil
 }
@@ -1050,11 +1021,13 @@ func (s *Syncer) stopExpandNPFixup() {
 	s.expFixupWg.Wait()
 }
 
-// Stop sto pthe syncer
+// Stop stops the syncer
 func (s *Syncer) Stop() {
 	s.stopOnce.Do(func() {
+		log.Info("Syncer stopping")
 		close(s.stop)
 		s.expFixupWg.Wait()
+		log.Info("Syncer stopped")
 	})
 }
 
@@ -1121,7 +1094,10 @@ func (s *Syncer) cleanupSticky() error {
 	return nil
 }
 
-func (s *Syncer) frontendHasBackend(ip net.IP, port uint16, backendIP net.IP, backendPort uint16, proto uint8) bool {
+// ConntrackFrontnedHasBackend returns true if the given front-backend pair exists
+func (s *Syncer) ConntrackFrontnedHasBackend(ip net.IP, port uint16,
+	backendIP net.IP, backendPort uint16, proto uint8) bool {
+
 	id, ok := s.activeSvcsMap[ipPortProto{ipPort{ip.String(), int(port)}, proto}]
 	if !ok {
 		return false
@@ -1135,6 +1111,36 @@ func (s *Syncer) frontendHasBackend(ip net.IP, port uint16, backendIP net.IP, ba
 	_, ok = backends[ipPort{backendIP.String(), int(backendPort)}]
 
 	return ok
+}
+
+// ConntrackScanStart excludes Apply from running and builds the acite maps from
+// ConntrackFrontnedHasBackend
+func (s *Syncer) ConntrackScanStart() {
+	s.mapsLck.Lock()
+
+	s.activeSvcsMap = make(map[ipPortProto]uint32)
+	s.activeEpsMap = make(map[uint32]map[ipPort]struct{})
+
+	// build active maps for conntrack cleaning
+	for skey, sinfo := range s.newSvcMap {
+		if sinfo.count == 0 {
+			continue
+		}
+
+		if isSvcKeyDerived(skey) {
+			s.addActiveEps(sinfo.id, sinfo.svc, nil)
+		} else {
+			s.addActiveEps(sinfo.id, sinfo.svc, s.newEpsMap[skey.sname])
+		}
+	}
+}
+
+// ConntrackScanEnd enables Apply and frees active maps
+func (s *Syncer) ConntrackScanEnd() {
+	// free the maps when the iteration is complete
+	s.activeSvcsMap = nil
+	s.activeEpsMap = nil
+	s.mapsLck.Unlock()
 }
 
 func serviceInfoFromK8sServicePort(sport k8sp.ServicePort) *serviceInfo {

projectcalico/calico

bpf/proxy/kube-proxy.go
@@ -23,6 +23,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 
 	"github.com/projectcalico/felix/bpf"
+	"github.com/projectcalico/felix/bpf/conntrack"
 	"github.com/projectcalico/felix/bpf/routes"
 )
 
@@ -45,6 +46,9 @@ type KubeProxy struct {
 	ctMap       bpf.Map
 	rt          *RTCache
 	opts        []Option
+
+	conntrackTimeouts conntrack.Timeouts
+	dsrEnabled        bool
 }
 
 // StartKubeProxy start a new kube-proxy if there was no error
@@ -63,6 +67,12 @@ func StartKubeProxy(k8s kubernetes.Interface, hostname string,
 
 		hostIPUpdates: make(chan []net.IP, 1),
 		exiting:       make(chan struct{}),
+	}
+
+	for _, o := range opts {
+		if err := o(kp); err != nil {
+			return nil, errors.WithMessage(err, "applying option to kube-proxy")
+		}
 	}
 
 	go func() {
@@ -97,12 +107,18 @@ func (kp *KubeProxy) run(hostIPs []net.IP) error {
 	copy(withLocalNP, hostIPs)
 	withLocalNP = append(withLocalNP, podNPIP)
 
-	syncer, err := NewSyncer(withLocalNP, kp.frontendMap, kp.backendMap, kp.affinityMap, kp.ctMap, kp.rt)
+	syncer, err := NewSyncer(withLocalNP, kp.frontendMap, kp.backendMap, kp.affinityMap, kp.rt)
 	if err != nil {
 		return errors.WithMessage(err, "new bpf syncer")
 	}
 
-	proxy, err := New(kp.k8s, syncer, kp.hostname, kp.opts...)
+	lc := conntrack.NewLivenessScanner(kp.conntrackTimeouts, kp.dsrEnabled)
+	connScan := conntrack.NewScanner(kp.ctMap,
+		lc.ScanEntry,
+		conntrack.NewStaleNATScanner(syncer.ConntrackFrontendHasBackend),
+	)
+
+	proxy, err := New(kp.k8s, syncer, connScan, kp.hostname, kp.opts...)
 	if err != nil {
 		return errors.WithMessage(err, "new proxy")
 	}

bpf/proxy/proxy.go
@@ -18,6 +18,7 @@
 package proxy
 
 import (
+	"net"
 	"strings"
 	"sync"
 	"time"
@@ -39,6 +40,9 @@ import (
 	"k8s.io/kubernetes/pkg/proxy/config"
 	"k8s.io/kubernetes/pkg/proxy/healthcheck"
 	"k8s.io/kubernetes/pkg/util/async"
+
+	"github.com/projectcalico/felix/bpf/conntrack"
+	"github.com/projectcalico/felix/jitter"
 )
 
 // Proxy watches for updates of Services and Endpoints, maintains their mapping
@@ -59,6 +63,9 @@ type DPSyncerState struct {
 // observed changes to the dataplane
 type DPSyncer interface {
 	Apply(state DPSyncerState) error
+	ConntrackScanStart()
+	ConntrackScanEnd()
+	ConntrackFrontendHasBackend(ip net.IP, port uint16, backendIP net.IP, backendPort uint16, proto uint8) bool
 	Stop()
 }
 
@@ -94,6 +101,8 @@ type proxy struct {
 	svcHealthServer healthcheck.ServiceHealthServer
 	healthzServer   healthcheck.ProxierHealthUpdater
 
+	connScan *conntrack.Scanner
+
 	stopCh   chan struct{}
 	stopWg   sync.WaitGroup
 	stopOnce sync.Once
@@ -104,7 +113,8 @@ type stoppableRunner interface {
 }
 
 // New returns a new Proxy for the given k8s interface
-func New(k8s kubernetes.Interface, dp DPSyncer, hostname string, opts ...Option) (Proxy, error) {
+func New(k8s kubernetes.Interface, dp DPSyncer, connScan *conntrack.Scanner,
+	hostname string, opts ...Option) (Proxy, error) {
 
 	if k8s == nil {
 		return nil, errors.Errorf("no k8s client")
@@ -117,6 +127,7 @@ func New(k8s kubernetes.Interface, dp DPSyncer, hostname string, opts ...Option)
 	p := &proxy{
 		k8s:      k8s,
 		dpSyncer: dp,
+		connScan: connScan,
 		hostname: hostname,
 		svcMap:   make(k8sp.ServiceMap),
 		epsMap:   make(k8sp.EndpointsMap),
@@ -190,14 +201,20 @@ func New(k8s kubernetes.Interface, dp DPSyncer, hostname string, opts ...Option)
 	p.startRoutine(func() { informerFactory.Start(p.stopCh) })
 	p.startRoutine(func() { svcConfig.Run(p.stopCh) })
 
+	if p.connScan != nil {
+		p.startRoutine(func() { p.conntrackCleaner(p.stopCh) })
+	}
+
 	return p, nil
 }
 
 func (p *proxy) Stop() {
 	p.stopOnce.Do(func() {
+		log.Info("Proxy stopping")
 		p.dpSyncer.Stop()
 		close(p.stopCh)
 		p.stopWg.Wait()
+		log.Info("Proxy stopped")
 	})
 }
 
@@ -324,6 +341,28 @@ func (p *proxy) OnEndpointSliceDelete(eps *discovery.EndpointSlice) {
 func (p *proxy) OnEndpointSlicesSynced() {
 	p.setEpsSynced()
 	p.forceSyncDP()
+}
+
+func (p *proxy) conntrackCleaner(stopCh <-chan struct{}) {
+	log.Debug("Conntrack cleanup thread started")
+	defer log.Debug("Conntrack cleanup thread stopped")
+
+	ticker := jitter.NewTicker(10*time.Second, 100*time.Millisecond)
+
+	for {
+		select {
+		case <-ticker.C:
+			log.Debug("Conntrack cleanup timer popped")
+			// N.B. syncer needs to be told about when scanning starts and ends so that it
+			// can get its internal structs ready, take/release any locks and do cleanup.
+			p.dpSyncer.ConntrackScanStart()
+			p.connScan.Scan()
+			p.dpSyncer.ConntrackScanEnd()
+		case <-stopCh:
+			log.Debug("Conntrack cleanup got stop signal")
+			return
+		}
+	}
 }
 
 type initState struct {

bpf/proxy/proxy_test.go
@@ -16,6 +16,7 @@ package proxy_test
 
 import (
 	"fmt"
+	"net"
 	"time"
 
 	. "github.com/onsi/ginkgo"
@@ -39,10 +40,10 @@ var _ = Describe("BPF Proxy", func() {
 	var syncStop chan struct{}
 
 	It("should fail without k8s client", func() {
-		_, err := proxy.New(nil, nil, "testnode")
+		_, err := proxy.New(nil, nil, nil, "testnode")
 		Expect(err).To(HaveOccurred())
 
-		_, err = proxy.New(fake.NewSimpleClientset(), nil, "testnode")
+		_, err = proxy.New(fake.NewSimpleClientset(), nil, nil, "testnode")
 		Expect(err).To(HaveOccurred())
 	})
 
@@ -52,7 +53,7 @@ var _ = Describe("BPF Proxy", func() {
 		syncStop = make(chan struct{})
 		dp := newMockSyncer(syncStop)
 
-		p, err := proxy.New(k8s, dp, "testnode", proxy.WithImmediateSync())
+		p, err := proxy.New(k8s, dp, nil, "testnode", proxy.WithImmediateSync())
 		Expect(err).NotTo(HaveOccurred())
 
 		defer func() {
@@ -174,7 +175,7 @@ var _ = Describe("BPF Proxy", func() {
 					opts = append(opts, proxy.WithEndpointsSlices())
 				}
 
-				p, err = proxy.New(k8s, dp, "testnode", opts...)
+				p, err = proxy.New(k8s, dp, nil, "testnode", opts...)
 				Expect(err).NotTo(HaveOccurred())
 			})
 		})
@@ -556,7 +557,7 @@ var _ = Describe("BPF Proxy", func() {
 					opts = append(opts, proxy.WithEndpointsSlices())
 				}
 
-				p, err = proxy.New(k8s, dp, testNodeName, opts...)
+				p, err = proxy.New(k8s, dp, nil, testNodeName, opts...)
 				Expect(err).NotTo(HaveOccurred())
 			})
 		})
@@ -592,6 +593,7 @@ var _ = Describe("BPF Proxy", func() {
 })
 
 type mockSyncer struct {
+	syncerConntrackAPIDummy
 	out  chan proxy.DPSyncerState
 	in   chan error
 	stop chan struct{}
@@ -616,6 +618,15 @@ func (s *mockSyncer) Apply(state proxy.DPSyncerState) error {
 	case <-s.stop:
 		return nil
 	}
+}
+
+type syncerConntrackAPIDummy struct{}
+
+func (*syncerConntrackAPIDummy) ConntrackScanStart() {}
+func (*syncerConntrackAPIDummy) ConntrackScanEnd()   {}
+func (*syncerConntrackAPIDummy) ConntrackFrontendHasBackend(ip net.IP, port uint16, backendIP net.IP,
+	backendPort uint16, proto uint8) bool {
+	return false
 }
 
 func (s *mockSyncer) checkState(f func(proxy.DPSyncerState)) {

bpf/proxy/syncer.go
@@ -31,7 +31,6 @@ import (
 	k8sp "k8s.io/kubernetes/pkg/proxy"
 
 	"github.com/projectcalico/felix/bpf"
-	"github.com/projectcalico/felix/bpf/conntrack"
 	"github.com/projectcalico/felix/bpf/nat"
 	"github.com/projectcalico/felix/bpf/routes"
 	"github.com/projectcalico/felix/ip"
@@ -120,8 +119,7 @@ type Syncer struct {
 	activeSvcsMap map[ipPortProto]uint32
 	activeEpsMap  map[uint32]map[ipPort]struct{}
 
-	// We never have more than one thread accessing the [prev|new][Svc|Eps]Map,
-	// this is to just make sure and to make the --race checker happy
+	// Protects acessing the [prev|new][Svc|Eps]Map,
 	mapsLck sync.Mutex
 
 	// synced is true after reconciling the first Apply
@@ -139,8 +137,6 @@ type Syncer struct {
 	stickySvcs       map[nat.FrontEndAffinityKey]stickyFrontend
 	stickyEps        map[uint32]map[nat.BackendValue]struct{}
 	stickySvcDeleted bool
-
-	conntrackScanner *conntrack.Scanner
 }
 
 type ipPort struct {
@@ -189,7 +185,7 @@ func uniqueIPs(ips []net.IP) []net.IP {
 }
 
 // NewSyncer returns a new Syncer
-func NewSyncer(nodePortIPs []net.IP, svcsmap, epsmap, affmap, ctmap bpf.Map, rt Routes) (*Syncer, error) {
+func NewSyncer(nodePortIPs []net.IP, svcsmap, epsmap, affmap bpf.Map, rt Routes) (*Syncer, error) {
 	s := &Syncer{
 		bpfSvcs:     svcsmap,
 		bpfEps:      epsmap,
@@ -200,8 +196,6 @@ func NewSyncer(nodePortIPs []net.IP, svcsmap, epsmap, affmap, ctmap bpf.Map, rt 
 		prevEpsMap:  make(k8sp.EndpointsMap),
 		stop:        make(chan struct{}),
 	}
-
-	s.conntrackScanner = conntrack.NewScanner(ctmap, conntrack.NewStaleNATScanner(s.frontendHasBackend))
 
 	if err := s.loadOrigs(); err != nil {
 		return nil, err
@@ -504,15 +498,6 @@ func (s *Syncer) apply(state DPSyncerState) error {
 	s.newSvcMap = make(map[svcKey]svcInfo)
 	s.newEpsMap = make(k8sp.EndpointsMap)
 
-	s.activeSvcsMap = make(map[ipPortProto]uint32)
-	s.activeEpsMap = make(map[uint32]map[ipPort]struct{})
-
-	defer func() {
-		// free the maps when the iteration is complete
-		s.activeSvcsMap = nil
-		s.activeEpsMap = nil
-	}()
-
 	var expNPMisses []*expandMiss
 
 	// insert or update existing services
@@ -584,23 +569,9 @@ func (s *Syncer) apply(state DPSyncerState) error {
 		log.Infof("removed stale service %q", skey)
 	}
 
-	// build active maps for conntrack cleaning
-	for skey, sinfo := range s.newSvcMap {
-		if sinfo.count == 0 {
-			continue
-		}
-
-		if isSvcKeyDerived(skey) {
-			s.addActiveEps(sinfo.id, sinfo.svc, nil)
-		} else {
-			s.addActiveEps(sinfo.id, sinfo.svc, s.newEpsMap[skey.sname])
-		}
-	}
-
 	log.Debugf("new state written")
 
 	s.runExpandNPFixup(expNPMisses)
-	s.conntrackScanner.Scan()
 
 	return nil
 }
@@ -1051,11 +1022,13 @@ func (s *Syncer) stopExpandNPFixup() {
 	s.expFixupWg.Wait()
 }
 
-// Stop sto pthe syncer
+// Stop stops the syncer
 func (s *Syncer) Stop() {
 	s.stopOnce.Do(func() {
+		log.Info("Syncer stopping")
 		close(s.stop)
 		s.expFixupWg.Wait()
+		log.Info("Syncer stopped")
 	})
 }
 
@@ -1122,7 +1095,10 @@ func (s *Syncer) cleanupSticky() error {
 	return nil
 }
 
-func (s *Syncer) frontendHasBackend(ip net.IP, port uint16, backendIP net.IP, backendPort uint16, proto uint8) bool {
+// ConntrackFrontenddHasBackend returns true if the given front-backend pair exists
+func (s *Syncer) ConntrackFrontendHasBackend(ip net.IP, port uint16,
+	backendIP net.IP, backendPort uint16, proto uint8) bool {
+
 	id, ok := s.activeSvcsMap[ipPortProto{ipPort{ip.String(), int(port)}, proto}]
 	if !ok {
 		return false
@@ -1136,6 +1112,36 @@ func (s *Syncer) frontendHasBackend(ip net.IP, port uint16, backendIP net.IP, ba
 	_, ok = backends[ipPort{backendIP.String(), int(backendPort)}]
 
 	return ok
+}
+
+// ConntrackScanStart excludes Apply from running and builds the active maps from
+// ConntrackFrontendHasBackend
+func (s *Syncer) ConntrackScanStart() {
+	s.mapsLck.Lock()
+
+	s.activeSvcsMap = make(map[ipPortProto]uint32)
+	s.activeEpsMap = make(map[uint32]map[ipPort]struct{})
+
+	// build active maps for conntrack cleaning
+	for skey, sinfo := range s.newSvcMap {
+		if sinfo.count == 0 {
+			continue
+		}
+
+		if isSvcKeyDerived(skey) {
+			s.addActiveEps(sinfo.id, sinfo.svc, nil)
+		} else {
+			s.addActiveEps(sinfo.id, sinfo.svc, s.newEpsMap[skey.sname])
+		}
+	}
+}
+
+// ConntrackScanEnd enables Apply and frees active maps
+func (s *Syncer) ConntrackScanEnd() {
+	// free the maps when the iteration is complete
+	s.activeSvcsMap = nil
+	s.activeEpsMap = nil
+	s.mapsLck.Unlock()
 }
 
 func serviceInfoFromK8sServicePort(sport k8sp.ServicePort) *serviceInfo {