c68a6e116a150dd3ecca0ccd9ffd0a42e8aeead7
Shaun Crampton <shaun@tigera.io>
2020-08-18 09:06:16 +0100

    Merge pull request #2459 from fasaxc/fix-cleanup-race
    Fix that we never cleaned up the BPF endpoint cache
    
    (cherry picked from commit 1695af1394158c8a79d221ee735a9ef5d60dfb23)

--- dataplane/linux/bpf_ep_mgr.go
+++ dataplane/linux/bpf_ep_mgr.go
@@ -29,6 +29,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/sync/semaphore"
 	"golang.org/x/sys/unix"
@@ -46,6 +47,27 @@ import (
 )
 
 const jumpMapCleanupInterval = 10 * time.Second
+
+var (
+	bpfEndpointsGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "felix_bpf_dataplane_endpoints",
+		Help: "Number of BPF endpoints managed in the dataplane.",
+	})
+	bpfDirtyEndpointsGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "felix_bpf_dirty_dataplane_endpoints",
+		Help: "Number of BPF endpoints managed in the dataplane that are left dirty after a failure.",
+	})
+	bpfHappyEndpointsGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "felix_bpf_happy_dataplane_endpoints",
+		Help: "Number of BPF endpoints that are successfully programmed.",
+	})
+)
+
+func init() {
+	prometheus.MustRegister(bpfEndpointsGauge)
+	prometheus.MustRegister(bpfDirtyEndpointsGauge)
+	prometheus.MustRegister(bpfHappyEndpointsGauge)
+}
 
 type bpfInterface struct {
 	// info contains the information about the interface sent to us from external sources. For example,
@@ -175,12 +197,12 @@ func (m *bpfEndpointManager) withIface(ifaceName string, fn func(iface *bpfInter
 	if iface == zeroIface {
 		logCtx.Debug("Interface info is now empty.")
 		delete(m.nameToIface, ifaceName)
+	} else {
+		// Always store the result (rather than checking hte dirty flag) because dirty only covers the info..
+		m.nameToIface[ifaceName] = iface
 	}
 
 	dirty = dirty || iface.info != ifaceCopy.info
-
-	// Store the result in case iface.state was changed.
-	m.nameToIface[ifaceName] = iface
 
 	if !dirty {
 		return
@@ -368,11 +390,15 @@ func (m *bpfEndpointManager) CompleteDeferredWork() error {
 	m.applyProgramsToDirtyDataInterfaces()
 	m.updateWEPsInDataplane()
 
+	bpfEndpointsGauge.Set(float64(len(m.nameToIface)))
+	bpfDirtyEndpointsGauge.Set(float64(m.dirtyIfaceNames.Len()))
+
 	if m.happyWEPsDirty {
 		chains := m.ruleRenderer.WorkloadInterfaceAllowChains(m.happyWEPs)
 		m.iptablesFilterTable.UpdateChains(chains)
 		m.happyWEPsDirty = false
 	}
+	bpfHappyEndpointsGauge.Set(float64(len(m.happyWEPs)))
 
 	return nil
 }
@@ -470,6 +496,7 @@ func (m *bpfEndpointManager) applyProgramsToDirtyDataInterfaces() {
 			log.WithField("iface", iface).Debug(
 				"Tried to apply BPF program to interface but the interface wasn't present.  " +
 					"Will retry if it shows up.")
+			return set.RemoveItem
 		}
 		log.WithError(err).Warn("Failed to apply policy to interface")
 		return nil
@@ -550,12 +577,12 @@ func (m *bpfEndpointManager) updateWEPsInDataplane() {
 			log.WithField("wep", wlID).Debug(
 				"Tried to apply BPF program to interface but the interface wasn't present.  " +
 					"Will retry if it shows up.")
-			return nil
+			return set.RemoveItem
 		}
 		log.WithError(err).WithFields(log.Fields{
 			"wepID": wlID,
 			"name":  ifaceName,
-		}).Warn("Failed to apply policy to endpoint")
+		}).Warn("Failed to apply policy to endpoint, leaving it dirty")
 		return nil
 	})
 }
