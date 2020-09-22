3a6d85fd4ff5589af9ec01caccb70c0b84b08b43
Brian McMahon <brianmcmahon135@gmail.com>
2020-09-22 09:59:07 -0700

    Fix recrusive Error() call for ErrorValidation struct

--- lib/errors/errors.go
+++ lib/errors/errors.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2016 Tigera, Inc. All rights reserved.
+// Copyright (c) 2020 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -100,7 +100,7 @@ func (e ErroredField) String() string {
 
 func (e ErrorValidation) Error() string {
 	if len(e.ErroredFields) == 0 {
-		return fmt.Sprintf("unknown validation error: %v", e)
+		return "unknown validation error"
 	} else if len(e.ErroredFields) == 1 {
 		f := e.ErroredFields[0]
 		return fmt.Sprintf("error with field %s", f)
