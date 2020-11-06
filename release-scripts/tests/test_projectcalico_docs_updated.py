cc1018e9806932bc7e52d65a70c236c561157994
Matt Leung <mleung975@gmail.com>
2020-11-06 11:43:54 -0800

    Update release tests (#4147)

--- release-scripts/tests/test_projectcalico_docs_updated.py
+++ release-scripts/tests/test_projectcalico_docs_updated.py
@@ -11,17 +11,9 @@ def test_http_redirects_correctly():
     assert req.status_code == 200
 
 
-def test_latest_redirects_correctly():
-    req = requests.get("https://docs.projectcalico.org/latest")
-    assert req.status_code == 200
-
-    redirect = BeautifulSoup(req.content, features="html.parser").find('a', href=True)
-    assert redirect['href'] == "https://docs.projectcalico.org/%s/" % RELEASE_STREAM
-
-
 def test_latest_releases_redirects_correctly():
     req = requests.get("https://docs.projectcalico.org/latest/release-notes")
     assert req.status_code == 200
 
-    redirect = BeautifulSoup(req.content, features="html.parser").find('a', href=True)
-    assert redirect['href'] == "https://docs.projectcalico.org/%s/release-notes/" % RELEASE_STREAM
+    version = BeautifulSoup(req.content, features="html.parser").find('strong')
+    assert version.get_text() == RELEASE_STREAM
