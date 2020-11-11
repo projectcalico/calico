e20dc37a72706fca5651fd754f28de4099202e74
matt <mleung975@gmail.com>
2020-11-10 17:29:07 -0800

    Fix update CNI binary logic

--- pkg/install/install.go
+++ pkg/install/install.go
@@ -49,7 +49,7 @@ type config struct {
 
 	// UpdateCNIBinaries controls whether or not to overwrite any binaries with the same name
 	// on the host.
-	UpdateCNIBinaries bool `envconfig:"UPDATE_CNI_BINARIES"`
+	UpdateCNIBinaries bool `envconfig:"UPDATE_CNI_BINARIES" default:"true"`
 
 	// The CNI network configuration to install.
 	CNINetworkConfig     string `envconfig:"CNI_NETWORK_CONFIG"`
@@ -201,7 +201,7 @@ func Install() error {
 			if c.skipBinary(binary.Name()) {
 				continue
 			}
-			if fileExists(target) && c.UpdateCNIBinaries {
+			if fileExists(target) && !c.UpdateCNIBinaries {
 				logrus.Infof("Skipping installation of %s", target)
 				continue
 			}
