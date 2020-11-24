3889d436745f246c760090293990c26cca050a78
Casey Davenport <caseydavenport@users.noreply.github.com>
2020-11-24 10:15:27 -0800

    Merge pull request #4210 from lwr20/lwr-fix-tests-3.17
    Fix post release tests

--- release-scripts/tests/test_tags_tars.py
+++ release-scripts/tests/test_tags_tars.py
@@ -4,119 +4,137 @@ import requests
 import yaml
 from parameterized import parameterized
 
-DOCS_PATH = '/docs' if os.environ.get('CALICO_DOCS_PATH') is None else os.environ.get('CALICO_DOCS_PATH')
-RELEASE_STREAM = os.environ.get('RELEASE_STREAM')
-EXCLUDED_IMAGES = ['calico/pilot-webhook',
-                   'calico/upgrade',
-                   'quay.io/coreos/flannel']
+DOCS_PATH = (
+    "/docs"
+    if os.environ.get("CALICO_DOCS_PATH") is None
+    else os.environ.get("CALICO_DOCS_PATH")
+)
+RELEASE_STREAM = os.environ.get("RELEASE_STREAM")
+EXCLUDED_IMAGES = ["calico/pilot-webhook", "calico/upgrade", "quay.io/coreos/flannel"]
 
-EXPECTED_ARCHS = ['amd64', 'arm64', 'ppc64le']
+EXPECTED_ARCHS = ["amd64", "arm64", "ppc64le"]
 
-with open('%s/_data/versions.yml' % DOCS_PATH) as f:
+with open("%s/_data/versions.yml" % DOCS_PATH) as f:
     versions = yaml.safe_load(f)
-    RELEASE_VERSION = versions[0]['title']
-    print('[INFO] using _data/versions.yaml, discovered version: %s' % RELEASE_VERSION)
+    RELEASE_VERSION = versions[0]["title"]
+    print("[INFO] using _data/versions.yaml, discovered version: %s" % RELEASE_VERSION)
 
-TAG_URL_TEMPL = "https://github.com/projectcalico/{component}/releases/tag/{component_version}"
-ZIP_URL_TEMPL = "https://github.com/projectcalico/{component}/archive/{component_version}.zip"
-TAR_URL_TEMPL = "https://github.com/projectcalico/{component}/archive/{component_version}.tar.gz"
+TAG_URL_TEMPL = (
+    "https://github.com/projectcalico/{component}/releases/tag/{component_version}"
+)
+ZIP_URL_TEMPL = (
+    "https://github.com/projectcalico/{component}/archive/{component_version}.zip"
+)
+TAR_URL_TEMPL = (
+    "https://github.com/projectcalico/{component}/archive/{component_version}.tar.gz"
+)
 
 CNI_URL_TEMPL = "https://github.com/projectcalico/{component}/releases/download/{component_version}/{binary}-{arch}"
 CTL_URL_TEMPL = "https://github.com/projectcalico/{component}/releases/download/{component_version}/{binary}"
 
-FLANNEL_TAG_URL_TEMPL = "https://github.com/coreos/{component}/releases/tag/{component_version}"
-FLANNEL_ZIP_URL_TEMPL = "https://github.com/coreos/{component}/archive/{component_version}.zip"
-FLANNEL_TAR_URL_TEMPL = "https://github.com/coreos/{component}/archive/{component_version}.tar.gz"
+FLANNEL_TAG_URL_TEMPL = (
+    "https://github.com/coreos/{component}/releases/tag/{component_version}"
+)
+FLANNEL_ZIP_URL_TEMPL = (
+    "https://github.com/coreos/{component}/archive/{component_version}.zip"
+)
+FLANNEL_TAR_URL_TEMPL = (
+    "https://github.com/coreos/{component}/archive/{component_version}.tar.gz"
+)
 
-DOCS_TAR_URL_TEMPL = "https://github.com/projectcalico/{component}/releases/download/" \
-                     "{component_version}/release-{component_version}.tgz"
+DOCS_TAR_URL_TEMPL = (
+    "https://github.com/projectcalico/{component}/releases/download/"
+    "{component_version}/release-{component_version}.tgz"
+)
 
 components = [
-    {"name": "typha",
-     "lookup": "typha",
-     "urls": []},
-    {"name": "calicoctl",
-     "lookup": "calicoctl",
-     "urls": []},
-    {"name": "node",
-     "lookup": "calico/node",
-     "urls": []},
-    {"name": "cni-plugin",
-     "lookup": "calico/cni",
-     "urls": []},
-    {"name": "kube-controllers",
-     "lookup": "calico/kube-controllers",
-     "urls": []},
-    {"name": "networking-calico",
-     "lookup": "networking-calico",
-     "urls": []},
-    {"name": "flannel",
-     "lookup": "flannel",
-     "urls": []},
-    {"name": "app-policy",
-     "lookup": "calico/dikastes",
-     "urls": []},
-    {"name": "pod2daemon",
-     "lookup": "flexvol",
-     "urls": []},
-    {"name": "calico",
-     "lookup": "calico/node",
-     "urls": []},
+    {"name": "typha", "lookup": "typha", "urls": []},
+    {"name": "calicoctl", "lookup": "calicoctl", "urls": []},
+    {"name": "node", "lookup": "calico/node", "urls": []},
+    {"name": "cni-plugin", "lookup": "calico/cni", "urls": []},
+    {"name": "kube-controllers", "lookup": "calico/kube-controllers", "urls": []},
+    {"name": "networking-calico", "lookup": "networking-calico", "urls": []},
+    {"name": "flannel", "lookup": "flannel", "urls": []},
+    {"name": "app-policy", "lookup": "calico/dikastes", "urls": []},
+    {"name": "pod2daemon", "lookup": "flexvol", "urls": []},
+    {"name": "calico", "lookup": "calico/node", "urls": []},
 ]
 
 for component in components:
-    component['urls'] = [
+    component["urls"] = [
         TAG_URL_TEMPL.format(
-            component=component['name'],
-            component_version=versions[0]['components'][component['lookup']]['version']),
+            component=component["name"],
+            component_version=versions[0]["components"][component["lookup"]]["version"],
+        ),
         ZIP_URL_TEMPL.format(
-            component=component['name'],
-            component_version=versions[0]['components'][component['lookup']]['version']),
+            component=component["name"],
+            component_version=versions[0]["components"][component["lookup"]]["version"],
+        ),
         TAR_URL_TEMPL.format(
-            component=component['name'],
-            component_version=versions[0]['components'][component['lookup']]['version']),
+            component=component["name"],
+            component_version=versions[0]["components"][component["lookup"]]["version"],
+        ),
     ]
-    if component['name'] == "calico":
-        component['urls'].append(
+    if component["name"] == "calico":
+        component["urls"].append(
             DOCS_TAR_URL_TEMPL.format(
-                component=component['name'],
-                component_version=versions[0]['components'][component['lookup']]['version']
+                component=component["name"],
+                component_version=versions[0]["components"][component["lookup"]][
+                    "version"
+                ],
             )
         )
-    if component['name'] == "cni-plugin":
-        for binary in ['calico', 'calico-ipam']:
-            for arch in ['amd64', 'arm64', 'ppc64le']:
-                component['urls'].append(
+    if component["name"] == "cni-plugin":
+        for binary in ["calico", "calico-ipam"]:
+            for arch in ["amd64", "arm64", "ppc64le"]:
+                component["urls"].append(
                     CNI_URL_TEMPL.format(
-                        component=component['name'],
-                        component_version=versions[0]['components'][component['lookup']]['version'],
+                        component=component["name"],
+                        component_version=versions[0]["components"][
+                            component["lookup"]
+                        ]["version"],
                         binary=binary,
-                        arch=arch
-                    ))
-    elif component['name'] == "calicoctl":
-        for binary in ['calicoctl',
-                       'calicoctl-darwin-amd64',
-                       'calicoctl-linux-amd64',
-                       'calicoctl-linux-arm64',
-                       'calicoctl-linux-ppc64le',
-                       'calicoctl-windows-amd64.exe'
-                       ]:
-            component['urls'].append(CTL_URL_TEMPL.format(
-                component=component['name'],
-                component_version=versions[0]['components'][component['lookup']]['version'],
-                binary=binary,
-            ))
-    elif component['name'] == "flannel":
-        component['urls'] = [
+                        arch=arch,
+                    )
+                )
+    elif component["name"] == "calicoctl":
+        for binary in [
+            "calicoctl",
+            "calicoctl-darwin-amd64",
+            "calicoctl-linux-amd64",
+            "calicoctl-linux-arm64",
+            "calicoctl-linux-ppc64le",
+            "calicoctl-windows-amd64.exe",
+        ]:
+            component["urls"].append(
+                CTL_URL_TEMPL.format(
+                    component=component["name"],
+                    component_version=versions[0]["components"][component["lookup"]][
+                        "version"
+                    ],
+                    binary=binary,
+                )
+            )
+    elif component["name"] == "flannel":
+        component["urls"] = [
             FLANNEL_TAG_URL_TEMPL.format(
-                component=component['name'],
-                component_version=versions[0]['components'][component['lookup']]['version']),
+                component=component["name"],
+                component_version=versions[0]["components"][component["lookup"]][
+                    "version"
+                ],
+            ),
             FLANNEL_ZIP_URL_TEMPL.format(
-                component=component['name'],
-                component_version=versions[0]['components'][component['lookup']]['version']),
+                component=component["name"],
+                component_version=versions[0]["components"][component["lookup"]][
+                    "version"
+                ],
+            ),
             FLANNEL_TAR_URL_TEMPL.format(
-                component=component['name'],
-                component_version=versions[0]['components'][component['lookup']]['version']),
+                component=component["name"],
+                component_version=versions[0]["components"][component["lookup"]][
+                    "version"
+                ],
+            ),
         ]
     # The intent of all the above looping and lookups is to generate a list of component URLs that must be present.
     # We then pass that list of URLs into the simple test below (which gets the URL
@@ -162,7 +180,7 @@ for component in components:
 
 unrolled_urls = []
 for component in components:
-    for url in component['urls']:
+    for url in component["urls"]:
         unrolled_urls.append(url)
 
 
