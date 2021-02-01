e72b5433fbcd2db545a42b84baf19c36cc736704
Neil Jerram <neil@tigera.io>
2021-02-01 11:45:30 +0000

    Make CALI_CT_FLAG values easier to read

--- bpf-gpl/conntrack_types.h
+++ bpf-gpl/conntrack_types.h
@@ -36,10 +36,10 @@ enum cali_ct_type {
 					 */
 };
 
-#define CALI_CT_FLAG_NAT_OUT	(1 << 0)
-#define CALI_CT_FLAG_DSR_FWD	(1 << 1) /* marks entry into the tunnel on the fwd node when dsr */
-#define CALI_CT_FLAG_NP_FWD	(1 << 2) /* marks entry into the tunnel on the fwd node */
-#define CALI_CT_FLAG_SKIP_FIB	(1 << 3) /* marks traffic that should pass through host IP stack */
+#define CALI_CT_FLAG_NAT_OUT	0x01
+#define CALI_CT_FLAG_DSR_FWD	0x02 /* marks entry into the tunnel on the fwd node when dsr */
+#define CALI_CT_FLAG_NP_FWD	0x04 /* marks entry into the tunnel on the fwd node */
+#define CALI_CT_FLAG_SKIP_FIB	0x08 /* marks traffic that should pass through host IP stack */
 
 struct calico_ct_leg {
 	__u32 seqno;
