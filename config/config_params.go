be25e7ff81c47e0c22d7ff91b87c3fd5facf46f4
matt <mleung975@gmail.com>
2021-03-26 14:40:25 -0700

    Add handling for IPv6 networks in failsafe rules

--- config/config_params.go
+++ config/config_params.go
@@ -242,8 +242,8 @@ type Config struct {
 	PrometheusGoMetricsEnabled      bool   `config:"bool;true"`
 	PrometheusProcessMetricsEnabled bool   `config:"bool;true"`
 
-	FailsafeInboundHostPorts  []ProtoPort `config:"port-list;tcp:0.0.0.0/0:22,udp:0.0.0.0/0:68,tcp:0.0.0.0/0:179,tcp:0.0.0.0/0:2379,tcp:0.0.0.0/0:2380,tcp:0.0.0.0/0:5473,tcp:0.0.0.0/0:6443,tcp:0.0.0.0/0:6666,tcp:0.0.0.0/0:6667;die-on-fail"`
-	FailsafeOutboundHostPorts []ProtoPort `config:"port-list;udp:0.0.0.0/0:53,udp:0.0.0.0/0:67,tcp:0.0.0.0/0:179,tcp:0.0.0.0/0:2379,tcp:0.0.0.0/0:2380,tcp:0.0.0.0/0:5473,tcp:0.0.0.0/0:6443,tcp:0.0.0.0/0:6666,tcp:0.0.0.0/0:6667;die-on-fail"`
+	FailsafeInboundHostPorts  []ProtoPort `config:"port-list;tcp:22,udp:68,tcp:179,tcp:2379,tcp:2380,tcp:5473,tcp:6443,tcp:6666,tcp:6667;die-on-fail"`
+	FailsafeOutboundHostPorts []ProtoPort `config:"port-list;udp:53,udp:67,tcp:179,tcp:2379,tcp:2380,tcp:5473,tcp:6443,tcp:6666,tcp:6667;die-on-fail"`
 
 	KubeNodePortRanges []numorstring.Port `config:"portrange-list;30000:32767"`
 	NATPortRange       numorstring.Port   `config:"portrange;"`
