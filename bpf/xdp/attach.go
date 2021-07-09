d565fa9e5ee6808dc9252ea40ae5e8b0e7027679
Neil Jerram <neil@tigera.io>
2021-07-09 15:21:01 +0100

    WIP commenting for xdp/attach.go

--- bpf/xdp/attach.go
+++ bpf/xdp/attach.go
@@ -31,6 +31,58 @@ func (ap *AttachPoint) IsAttached() (bool, error) {
 }
 
 func (ap *AttachPoint) AttachProgram() error {
+	//func (b *BPFLib) LoadXDPAuto(ifName string, mode XDPMode) error {
+	//	return b.LoadXDP(xdpFilename, ifName, mode)
+	//}
+
+	//func (b *BPFLib) LoadXDP(objPath, ifName string, mode XDPMode) error {
+	//	mapArgs, err := b.getMapArgs(ifName)
+	//	if err != nil {
+	//		return err
+	//	}
+	//
+	//	return b.loadXDPRaw(objPath, ifName, mode, mapArgs)
+	//}
+
+	//func (b *BPFLib) loadXDPRaw(objPath, ifName string, mode XDPMode, mapArgs []string) error {
+	//	objPath = path.Join(b.binDir, objPath)
+	//
+	//	if _, err := os.Stat(objPath); os.IsNotExist(err) {
+	//		return fmt.Errorf("cannot find XDP object %q", objPath)
+	//	}
+	//
+	//	progName := getProgName(ifName)
+	//	progPath := filepath.Join(b.xdpDir, progName)
+	//
+	//	if err := b.loadBPF(objPath, progPath, "xdp", mapArgs); err != nil {
+	//		return err
+	//	}
+	//
+	//	prog := "ip"
+	//	args := []string{
+	//		"link",
+	//		"set",
+	//		"dev",
+	//		ifName,
+	//		mode.String(),
+	//		"pinned",
+	//		progPath}
+	//
+	//	printCommand(prog, args...)
+	//	output, err := exec.Command(prog, args...).CombinedOutput()
+	//	log.Debugf("out:\n%v", string(output))
+	//
+	//	if err != nil {
+	//		if removeErr := os.Remove(progPath); removeErr != nil {
+	//			return fmt.Errorf("failed to attach XDP program (%s) to %s: %s (also failed to remove the pinned program: %s)\n%s", progPath, ifName, err, removeErr, output)
+	//		} else {
+	//			return fmt.Errorf("failed to attach XDP program (%s) to %s: %s\n%s", progPath, ifName, err, output)
+	//		}
+	//	}
+	//
+	//	return nil
+	//}
+
 	return nil
 }
 
