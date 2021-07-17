projectcalico/libcalico-go

Fix parsing of v3 resource keys to allow them to be transmitted over typha

lib/backend/model/keys.go
@@ -23,12 +23,11 @@ import (
 	"strings"
 	"time"
 
-	libapiv3 "github.com/projectcalico/libcalico-go/lib/apis/v3"
-
 	log "github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/types"
 
 	apiv3 "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
+	"github.com/projectcalico/libcalico-go/lib/namespace"
 	"github.com/projectcalico/libcalico-go/lib/net"
 )
 
@@ -214,6 +213,29 @@ func KeyFromDefaultPath(path string) Key {
 		return NetworkSetKey{
 			Name: unescapeName(m[1]),
 		}
+	} else if m := matchGlobalResource.FindStringSubmatch(path); m != nil {
+		ri := resourceInfoByPlural[unescapeName(m[1])]
+		if namespace.IsNamespaced(ri.kind) {
+			log.Infof("(BUG) Path is a global resource, but resource is namespaced: %v", path)
+			return nil
+		}
+		log.Debugf("Path is a global resource: %v", path)
+		return ResourceKey{
+			Kind: ri.kind,
+			Name: unescapeName(m[2]),
+		}
+	} else if m := matchNamespacedResource.FindStringSubmatch(path); m != nil {
+		ri := resourceInfoByPlural[unescapeName(m[1])]
+		if !namespace.IsNamespaced(ri.kind) {
+			log.Infof("(BUG) Path is a namespaced resource, but resource is global: %v", path)
+			return nil
+		}
+		log.Infof("Path is a namespaced resource: %v", path)
+		return ResourceKey{
+			Kind:      resourceInfoByPlural[unescapeName(m[1])].kind,
+			Namespace: unescapeName(m[2]),
+			Name:      unescapeName(m[3]),
+		}
 	} else if m := matchPolicy.FindStringSubmatch(path); m != nil {
 		log.Debugf("Path is a policy: %v", path)
 		return PolicyKey{
@@ -266,18 +288,6 @@ func KeyFromDefaultPath(path string) Key {
 	} else if k := (BlockAffinityListOptions{}).KeyFromDefaultPath(path); k != nil {
 		return k
 	} else if k := (BlockListOptions{}).KeyFromDefaultPath(path); k != nil {
-		return k
-	} else if k := (ResourceListOptions{Kind: libapiv3.KindNode}).KeyFromDefaultPath(path); k != nil {
-		return k
-	} else if k := (ResourceListOptions{Kind: apiv3.KindBGPPeer}).KeyFromDefaultPath(path); k != nil {
-		return k
-	} else if k := (ResourceListOptions{Kind: apiv3.KindBGPConfiguration}).KeyFromDefaultPath(path); k != nil {
-		return k
-	} else if k := (ResourceListOptions{Kind: apiv3.KindNetworkPolicy}).KeyFromDefaultPath(path); k != nil {
-		return k
-	} else if k := (ResourceListOptions{Kind: apiv3.KindIPPool}).KeyFromDefaultPath(path); k != nil {
-		return k
-	} else if k := (ResourceListOptions{Kind: apiv3.KindProfile}).KeyFromDefaultPath(path); k != nil {
 		return k
 	} else if k := (HostEndpointStatusListOptions{}).KeyFromDefaultPath(path); k != nil {
 		return k

projectcalico/calico

lib/backend/model/keys.go
@@ -23,12 +23,10 @@ import (
 	"strings"
 	"time"
 
-	libapiv3 "github.com/projectcalico/libcalico-go/lib/apis/v3"
-
 	log "github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/types"
 
-	apiv3 "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
+	"github.com/projectcalico/libcalico-go/lib/namespace"
 	"github.com/projectcalico/libcalico-go/lib/net"
 )
 
@@ -214,6 +212,29 @@ func KeyFromDefaultPath(path string) Key {
 		return NetworkSetKey{
 			Name: unescapeName(m[1]),
 		}
+	} else if m := matchGlobalResource.FindStringSubmatch(path); m != nil {
+		ri := resourceInfoByPlural[unescapeName(m[1])]
+		if namespace.IsNamespaced(ri.kind) {
+			log.Warnf("(BUG) Path is a global resource, but resource is namespaced: %v", path)
+			return nil
+		}
+		log.Debugf("Path is a global resource: %v", path)
+		return ResourceKey{
+			Kind: ri.kind,
+			Name: unescapeName(m[2]),
+		}
+	} else if m := matchNamespacedResource.FindStringSubmatch(path); m != nil {
+		ri := resourceInfoByPlural[unescapeName(m[1])]
+		if !namespace.IsNamespaced(ri.kind) {
+			log.Warnf("(BUG) Path is a namespaced resource, but resource is global: %v", path)
+			return nil
+		}
+		log.Debugf("Path is a namespaced resource: %v", path)
+		return ResourceKey{
+			Kind:      resourceInfoByPlural[unescapeName(m[1])].kind,
+			Namespace: unescapeName(m[2]),
+			Name:      unescapeName(m[3]),
+		}
 	} else if m := matchPolicy.FindStringSubmatch(path); m != nil {
 		log.Debugf("Path is a policy: %v", path)
 		return PolicyKey{
@@ -266,18 +287,6 @@ func KeyFromDefaultPath(path string) Key {
 	} else if k := (BlockAffinityListOptions{}).KeyFromDefaultPath(path); k != nil {
 		return k
 	} else if k := (BlockListOptions{}).KeyFromDefaultPath(path); k != nil {
-		return k
-	} else if k := (ResourceListOptions{Kind: libapiv3.KindNode}).KeyFromDefaultPath(path); k != nil {
-		return k
-	} else if k := (ResourceListOptions{Kind: apiv3.KindBGPPeer}).KeyFromDefaultPath(path); k != nil {
-		return k
-	} else if k := (ResourceListOptions{Kind: apiv3.KindBGPConfiguration}).KeyFromDefaultPath(path); k != nil {
-		return k
-	} else if k := (ResourceListOptions{Kind: apiv3.KindNetworkPolicy}).KeyFromDefaultPath(path); k != nil {
-		return k
-	} else if k := (ResourceListOptions{Kind: apiv3.KindIPPool}).KeyFromDefaultPath(path); k != nil {
-		return k
-	} else if k := (ResourceListOptions{Kind: apiv3.KindProfile}).KeyFromDefaultPath(path); k != nil {
 		return k
 	} else if k := (HostEndpointStatusListOptions{}).KeyFromDefaultPath(path); k != nil {
 		return k