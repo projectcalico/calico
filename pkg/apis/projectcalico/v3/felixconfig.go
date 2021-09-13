1cf77f48576ba88522ef7295118123204503509c
Tomas Hruby <tomas@tigera.io>
2021-09-13 10:32:03 -0700

    BPFPSNATPorts fix default value

--- pkg/apis/projectcalico/v3/felixconfig.go
+++ pkg/apis/projectcalico/v3/felixconfig.go
@@ -353,7 +353,8 @@ type FelixConfigurationSpec struct {
 	// collision. This should be within the ephemeral range as defined by RFC 6056 (1024–65535) and
 	// preferably outside the  ephemeral ranges used by common operating systems. Linux uses
 	// 32768–60999, while others mostly use the IANA defined range 49152–65535. It is not necessarily
-	// a problem if this range overlaps with the operating systems. [Default: 20000:20999]
+	// a problem if this range overlaps with the operating systems. Both ends of the range are
+	// inclusive. [Default: 20000:29999]
 	BPFPSNATPorts *numorstring.Port `json:"bpfPSNATPorts,omitempty"`
 
 	// RouteSource configures where Felix gets its routing information.
