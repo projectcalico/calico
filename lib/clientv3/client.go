projectcalico/libcalico-go

Added NodeBGPStatus resource

lib/clientv3/client.go
@@ -149,6 +149,11 @@ func (c client) KubeControllersConfiguration() KubeControllersConfigurationInter
 	return kubeControllersConfiguration{client: c}
 }
 
+// NodeBGPStatus returns an interface for managing the NodeBGPStatus resource.
+func (c client) NodeBGPStatus() NodeBGPStatusInterface {
+	return nodeBGPStatus{client: c}
+}
+
 type poolAccessor struct {
 	client *client
 }

projectcalico/calico

lib/clientv3/client.go
@@ -154,6 +154,11 @@ func (c client) KubeControllersConfiguration() KubeControllersConfigurationInter
 	return kubeControllersConfiguration{client: c}
 }
 
+// CalicoNodeStatus returns an interface for managing the CalicoNodeStatus resource.
+func (c client) CalicoNodeStatus() CalicoNodeStatusInterface {
+	return calicoNodeStatus{client: c}
+}
+
 type poolAccessor struct {
 	client *client
 }