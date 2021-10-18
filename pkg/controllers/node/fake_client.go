83badae3e55e0a5683a92b6fe537e421dadeb0d7
Neil Jerram <neil@tigera.io>
2021-10-18 15:44:27 +0100

    Add CalicoNodeStatus() stub to FakeCalicoClient

--- pkg/controllers/node/fake_client.go
+++ pkg/controllers/node/fake_client.go
@@ -1,3 +1,16 @@
+// Copyright (c) 2021 Tigera, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package node
 
 import (
@@ -112,6 +125,10 @@ func (f *FakeCalicoClient) ClusterInformation() clientv3.ClusterInformationInter
 // KubeControllersConfiguration returns an interface for managing the
 // KubeControllersConfiguration resource.
 func (f *FakeCalicoClient) KubeControllersConfiguration() clientv3.KubeControllersConfigurationInterface {
+	panic("not implemented")
+}
+
+func (f *FakeCalicoClient) CalicoNodeStatus() clientv3.CalicoNodeStatusInterface {
 	panic("not implemented")
 }
 
