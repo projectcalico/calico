3afd61913f7395fbf6e0bed0de9c036a1c538e49
Abhijeet Kasurde <akasurde@redhat.com>
2021-12-17 19:27:50 +0530

    Move hacking scripts to central location
    Moved hacking scripts to a central location after monorepo move

--- api/build/update-client-gen.sh
+++ api/build/update-client-gen.sh
@@ -25,19 +25,19 @@ BINDIR=${REPO_ROOT}/bin
 
 # Generate the versioned clientset (pkg/client/clientset_generated/clientset)
 client-gen "$@" \
-		--go-header-file "${REPO_ROOT}/hack/boilerplate/boilerplate.go.txt" \
+		--go-header-file "${REPO_ROOT}/../hack/boilerplate/boilerplate.go.txt" \
 		--input-base "github.com/projectcalico/api/pkg/apis/" \
 		--input "projectcalico/v3" \
 		--clientset-path "github.com/projectcalico/api/pkg/client/clientset_generated/" \
 		--clientset-name "clientset"
 # generate lister
 lister-gen "$@" \
-		--go-header-file "${REPO_ROOT}/hack/boilerplate/boilerplate.go.txt" \
+		--go-header-file "${REPO_ROOT}/../hack/boilerplate/boilerplate.go.txt" \
 		--input-dirs="github.com/projectcalico/api/pkg/apis/projectcalico/v3" \
 		--output-package "github.com/projectcalico/api/pkg/client/listers_generated"
 # generate informer
 informer-gen "$@" \
-		--go-header-file "${REPO_ROOT}/hack/boilerplate/boilerplate.go.txt" \
+		--go-header-file "${REPO_ROOT}/../hack/boilerplate/boilerplate.go.txt" \
 		--input-dirs "github.com/projectcalico/api/pkg/apis/projectcalico/v3" \
 		--versioned-clientset-package "github.com/projectcalico/api/pkg/client/clientset_generated/clientset" \
 		--listers-package "github.com/projectcalico/api/pkg/client/listers_generated" \
