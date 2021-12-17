a8b3f9e57190c11f09d9699aee5b35edd3e63643
Casey Davenport <davenport.cas@gmail.com>
2021-12-16 17:14:21 -0800

    Update apiserver to use new k8s version w/ certs

--- apiserver/test/integration/framework.go
+++ apiserver/test/integration/framework.go
@@ -82,7 +82,7 @@ func withConfigGetFreshApiserverServerAndClient(
 	options.RecommendedOptions.SecureServing.BindPort = securePort
 	// Set this so that we avoid RecommendedOptions.CoreAPI's initialization from calling InClusterConfig()
 	// and uses our fv kubeconfig instead.
-	options.RecommendedOptions.CoreAPI.CoreAPIKubeconfigPath = "../test-apiserver-kubeconfig.conf"
+	options.RecommendedOptions.CoreAPI.CoreAPIKubeconfigPath = "../certs/kubeconfig"
 
 	var err error
 	pcs, err := server.PrepareServer(options)
