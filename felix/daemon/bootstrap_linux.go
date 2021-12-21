b796ca82bcec3bd17a02e69e38f55640d6798abd
Alex O'Regan <alex.oregan@tigera.io>
2021-12-21 11:14:39 +0000

    sentence case logging and handle bootstrap errors in daemon

--- felix/daemon/bootstrap_linux.go
+++ felix/daemon/bootstrap_linux.go
@@ -15,17 +15,12 @@
 package daemon
 
 import (
-	log "github.com/sirupsen/logrus"
 
 	"github.com/projectcalico/calico/felix/wireguard"
 	"github.com/projectcalico/calico/libcalico-go/lib/clientv3"
 )
 
-func bootstrapWireguard(ifaceName string, felixHostName string, v3Client clientv3.Interface) {
-	err := wireguard.BootstrapHostConnectivity(
+func bootstrapWireguard(ifaceName string, felixHostName string, v3Client clientv3.Interface) error {
+	return wireguard.BootstrapHostConnectivity(
 		ifaceName, felixHostName, v3Client)
-
-	if err != nil {
-		log.WithError(err).Info("couldn't bootstrap WireGuard host connectivity")
-	}
 }
