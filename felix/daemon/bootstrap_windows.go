39547b8b27dbab2040ae5b06f6cf0cb4740d3b25
Alex O'Regan <alex.oregan@tigera.io>
2021-12-21 11:44:54 +0000

    correct return type for windows no-op

--- felix/daemon/bootstrap_windows.go
+++ felix/daemon/bootstrap_windows.go
@@ -18,4 +18,6 @@ import (
 	"github.com/projectcalico/calico/libcalico-go/lib/clientv3"
 )
 
-func bootstrapWireguard(ifaceName string, nodeName string, v3client clientv3.Interface) error {} // no-op
+func bootstrapWireguard(ifaceName string, nodeName string, v3client clientv3.Interface) error {
+	return nil
+} // no-op
