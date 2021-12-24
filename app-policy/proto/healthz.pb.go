28638dbe860bce89fe6ce1b721d9978137dcb976
Casey Davenport <davenport.cas@gmail.com>
2021-12-23 18:05:18 -0800

    Make clean when checking protobuf, to make sure we catch diffs

--- app-policy/proto/healthz.pb.go
+++ app-policy/proto/healthz.pb.go
@@ -3,19 +3,14 @@
 
 package proto
 
-import (
-	fmt "fmt"
+import proto1 "github.com/gogo/protobuf/proto"
+import fmt "fmt"
+import math "math"
 
-	proto1 "github.com/gogo/protobuf/proto"
+import context "golang.org/x/net/context"
+import grpc "google.golang.org/grpc"
 
-	math "math"
-
-	context "golang.org/x/net/context"
-
-	grpc "google.golang.org/grpc"
-
-	io "io"
-)
+import io "io"
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto1.Marshal
