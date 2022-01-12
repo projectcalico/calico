b0b2a21d10bbdf5c884fda6cf7c58e83d0dbcff3
Casey Davenport <davenport.cas@gmail.com>
2022-01-12 10:22:06 -0800

    Move nodeselector, don't need to restart

--- node/tests/k8st/deploy_resources_on_kind_cluster.sh
+++ node/tests/k8st/deploy_resources_on_kind_cluster.sh
@@ -120,10 +120,6 @@ openssl req -x509 -nodes -newkey rsa:4096 -keyout apiserver.key -out apiserver.c
 ${kubectl} create secret -n calico-apiserver generic calico-apiserver-certs --from-file=apiserver.key --from-file=apiserver.crt
 ${kubectl} patch apiservice v3.projectcalico.org -p \
     "{\"spec\": {\"caBundle\": \"$(${kubectl} get secret -n calico-apiserver calico-apiserver-certs -o go-template='{{ index .data "apiserver.crt" }}')\"}}"
-
-# TODO: This is a code bug!
-echo "Patch Calico apiserver to run on master, this would make sure communications to Calico apiserver won't be affected by test cases"
-${kubectl} patch deployment calico-apiserver -n calico-apiserver -p '{"spec":{"template":{"spec":{"nodeSelector":{ "kubernetes.io/hostname": "kind-control-plane" }}}}}'
 time ${kubectl} wait pod -l k8s-app=calico-apiserver --for=condition=Ready -n calico-apiserver --timeout=30s
 echo "Calico apiserver is running."
 
