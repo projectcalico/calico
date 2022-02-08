db7674b2825ddb1a3a3350115d00debd7fce3fd3
Seth Malaki <seth@tigera.io>
2022-02-08 01:10:18 +0000

    revert 'debugging' stuff

--- felix/fv/containers/containers.go
+++ felix/fv/containers/containers.go
@@ -564,11 +564,7 @@ func (c *Container) WaitUntilRunning() {
 	}()
 
 	for {
-		select {
-		case <-stoppedChan:
-			return
-		default:
-		}
+		Expect(stoppedChan).NotTo(BeClosed(), fmt.Sprintf("Container %s failed before being listed in 'docker ps'", c.Name))
 
 		cmd := utils.Command("docker", "ps")
 		out, err := cmd.CombinedOutput()
