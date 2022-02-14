87820c044a86355611c15b910f546ff29c6c9fc3
Casey Davenport <davenport.cas@gmail.com>
2022-02-14 12:06:47 -0800

    Update remaining ReleaseIPs calls

--- kube-controllers/pkg/controllers/node/fake_client.go
+++ kube-controllers/pkg/controllers/node/fake_client.go
@@ -160,7 +160,6 @@ func (f *fakeNodeClient) Create(ctx context.Context, res *apiv3.Node, opts optio
 
 	if _, ok := f.nodes[res.Name]; ok {
 		return nil, cerrors.ErrorResourceAlreadyExists{Identifier: res.Name}
-
 	}
 	f.nodes[res.Name] = res
 	return res, nil
@@ -230,7 +229,7 @@ func (f *fakeIPAMClient) AutoAssign(ctx context.Context, args ipam.AutoAssignArg
 
 // ReleaseIPs releases any of the given IP addresses that are currently assigned,
 // so that they are available to be used in another assignment.
-func (f *fakeIPAMClient) ReleaseIPs(ctx context.Context, ips []cnet.IP) ([]cnet.IP, error) {
+func (f *fakeIPAMClient) ReleaseIPs(ctx context.Context, ips []ipam.ReleaseOptions) ([]cnet.IP, error) {
 	panic("not implemented") // TODO: Implement
 }
 
