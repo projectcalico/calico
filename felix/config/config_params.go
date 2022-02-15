8444cae7a8f3b06e1fdc6dbf46b1dc411a1fa5e7
sridhar <sridhar@tigera.io>
2022-02-15 14:16:57 -0800

    Change map size defaults

--- felix/config/config_params.go
+++ felix/config/config_params.go
@@ -179,10 +179,10 @@ type Config struct {
 	BPFKubeProxyEndpointSlicesEnabled  bool             `config:"bool;false"`
 	BPFExtToServiceConnmark            int              `config:"int;0"`
 	BPFPSNATPorts                      numorstring.Port `config:"portrange;20000:29999"`
-	BPFMapSizeNATFrontend              int              `config:"int;511000;non-zero"`
-	BPFMapSizeNATBackend               int              `config:"int;510000;non-zero"`
-	BPFMapSizeNATAffinity              int              `config:"int;510000;non-zero"`
-	BPFMapSizeRoute                    int              `config:"int;1048576;non-zero"`
+	BPFMapSizeNATFrontend              int              `config:"int;65536;non-zero"`
+	BPFMapSizeNATBackend               int              `config:"int;262144;non-zero"`
+	BPFMapSizeNATAffinity              int              `config:"int;65536;non-zero"`
+	BPFMapSizeRoute                    int              `config:"int;262144;non-zero"`
 	BPFMapSizeConntrack                int              `config:"int;512000;non-zero"`
 	BPFMapSizeIPSets                   int              `config:"int;1048576;non-zero"`
 
