8cf0272dc44da5c6f519b511a5ab289d4263da5d
freecaykes <edbert@tigera.io>
2022-02-18 12:49:43 -0800

    revert(helm.rb): keep bin/helm running there

--- calico/_plugins/helm.rb
+++ calico/_plugins/helm.rb
@@ -47,11 +47,13 @@ module Jekyll
       end
 
       # substitute --execute with --show-only for helm v3 compatibility.
-      extra_args.gsub!(/--execute (\S*)/) do |f|
-        # operator CRDs have moved to root
-        if $1.start_with? "templates/crds/" then f.sub('--execute templates/crds/', '--show-only ')
-        # all other requests need to use --show-only instead of --execute for helm v3
-        else f.sub('--execute', '--show-only')
+      if @chart == "tigera-operator" then
+        extra_args.gsub!(/--execute (\S*)/) do |f|
+          # operator CRDs have moved to root
+          if $1.start_with? "templates/crds/" then f.sub('--execute templates/crds/', '--show-only ')
+          # all other requests need to use --show-only instead of --execute for helm v3
+          else f.sub('--execute', '--show-only')
+          end
         end
       end
 
@@ -85,7 +87,7 @@ module Jekyll
           -f #{tv.path} \
           -f #{t.path}"""
       else
-        cmd = """bin/helm3 template _includes/charts/#{@chart} \
+        cmd = """bin/helm template _includes/charts/#{@chart} \
         -f #{tv.path} \
         -f #{t.path} \
         --set etcd.endpoints='http://<ETCD_IP>:<ETCD_PORT>'"""
