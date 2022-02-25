4be60a6b5b4131c07d3cc8af11ce21592b760404
Shaun Crampton <shaun@tigera.io>
2022-02-25 17:43:02 +0000

    Fix number of configs.

--- libcalico-go/lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
+++ libcalico-go/lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
@@ -45,7 +45,7 @@ const (
 )
 
 const (
-	numBaseFelixConfigs = 105
+	numBaseFelixConfigs = 106
 )
 
 var _ = Describe("Test the generic configuration update processor and the concrete implementations", func() {
