778d0c574119bdd8318601a993bc8b8ee8f53a35
Casey Davenport <caseydavenport@users.noreply.github.com>
2022-02-25 10:12:11 -0800

    Allow for safer IP address release (#5613)

--- kube-controllers/tests/fv/flannel_migration_test.go
+++ kube-controllers/tests/fv/flannel_migration_test.go
@@ -58,9 +58,7 @@ const (
 	flannelSubnetEnv = "FLANNEL_NETWORK=192.168.0.0/16;FLANNEL_SUBNET=192.168.1.1/24;FLANNEL_MTU=8951;FLANNEL_IPMASQ=true;"
 )
 
-var (
-	emptyLabel = map[string]string{}
-)
+var emptyLabel = map[string]string{}
 
 var _ = Describe("flannel-migration-controller FV test", func() {
 	var (
@@ -178,7 +176,7 @@ var _ = Describe("flannel-migration-controller FV test", func() {
 			Eventually(func() *api.ClusterInformation {
 				info, _ = calicoClient.ClusterInformation().Get(context.Background(), "default", options.GetOptions{})
 				return info
-			}).ShouldNot(BeNil())
+			}, 10*time.Second).ShouldNot(BeNil())
 
 			Expect(info.Spec.ClusterGUID).To(MatchRegexp("^[a-f0-9]{32}$"))
 			Expect(info.Spec.ClusterType).To(Equal("k8s,kdd"))
