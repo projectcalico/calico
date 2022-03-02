f1edf5add1d5abbfa8ccc5a6d6889d1a3acc7d59
Tomas Hruby <tomas@tigera.io>
2022-03-01 16:32:16 -0800

    felix/bpf: set up jump to host ct conflict resolution program

--- felix/bpf/tc/attach.go
+++ felix/bpf/tc/attach.go
@@ -648,20 +648,19 @@ func (ap *AttachPoint) setMapSize(m *libbpf.Map) error {
 
 // nolint
 func updateJumpMap(obj *libbpf.Obj, isHost bool, ipv6Enabled bool) error {
-	ipVersions := set.New()
-	ipVersions.Add("IPv4")
+	ipVersions := []string{"IPv4"}
 	if ipv6Enabled {
-		ipVersions.Add("IPv6")
+		ipVersions = append(ipVersions, "IPv6")
 	}
 
-	ipVersions.Iter(func(ipFamily interface{}) error {
+	for _, ipFamily := range ipVersions {
 		// Since in IPv4, we don't add prologue to the jump map, and hence the first
 		// program is policy, the base index should be set to -1 to properly offset the
 		// policy program (base+1) to the first entry in the jump map, i.e. 0. However,
 		// in IPv6, we add the prologue program to the jump map, and the first entry is 3.
 		base := -1
 		if ipFamily == "IPv6" {
-			base = 3
+			base = 4
 		}
 
 		// Update prologue program, but only in IPv6. IPv4 prologue program is the start
@@ -689,8 +688,14 @@ func updateJumpMap(obj *libbpf.Obj, isHost bool, ipv6Enabled bool) error {
 		if err != nil {
 			return fmt.Errorf("error updating %v icmp program: %v", ipFamily, err)
 		}
-		return nil
-	})
+		if ipFamily != "IPv6" {
+			iIndex := base + 4
+			err = obj.UpdateJumpMap("cali_jump", string(programNames[iIndex]), iIndex)
+			if err != nil {
+				return fmt.Errorf("error updating %v host CT conflict program: %v", ipFamily, err)
+			}
+		}
+	}
 
 	return nil
 }
