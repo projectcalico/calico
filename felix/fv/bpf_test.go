183ad828faa9df27105f4f0d934deec1b0dea36e
Tomas Hruby <tomas@tigera.io>
2022-03-10 13:11:13 -0800

    bpf/fv: loopback is tested elsewhere

--- felix/fv/bpf_test.go
+++ felix/fv/bpf_test.go
@@ -2319,7 +2319,6 @@ func describeBPFTests(opts ...bpfTestOpt) bool {
 						clusterIP := testSvc.Spec.ClusterIP
 						port := uint16(testSvc.Spec.Ports[0].Port)
 
-						cc.ExpectSome(w[0][0], TargetIP(clusterIP), port)
 						cc.ExpectSome(w[0][1], TargetIP(clusterIP), port)
 						cc.ExpectSome(w[1][0], TargetIP(clusterIP), port)
 						cc.ExpectSome(w[1][1], TargetIP(clusterIP), port)
