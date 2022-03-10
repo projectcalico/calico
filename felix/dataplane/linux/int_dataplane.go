620b757e7a59b79e90f15050e9d332342fc7df45
Olli Janatuinen <olli.janatuinen@gmail.com>
2022-03-10 21:06:14 +0100

    Add support for REJECT instead of DROP
    Based on draft from https://github.com/projectcalico/felix/pull/2161
    but additionally implements tests and BPF support.
    
    In tests:
    - Using DROP action on iptables mode
    - Using REJECT action on IPVS mode

--- felix/dataplane/linux/int_dataplane.go
+++ felix/dataplane/linux/int_dataplane.go
@@ -264,6 +264,7 @@ type InternalDataplane struct {
 	allManagers             []Manager
 	managersWithRouteTables []ManagerWithRouteTables
 	ruleRenderer            rules.RuleRenderer
+	defaultRuleRenderer     rules.DefaultRuleRenderer
 
 	// dataplaneNeedsSync is set if the dataplane is dirty in some way, i.e. we need to
 	// call apply().
@@ -1104,8 +1105,8 @@ func (d *InternalDataplane) setUpIptablesBPF() {
 			},
 			iptables.Rule{
 				Match:   iptables.Match().MarkMatchesWithMask(tcdefs.MarkSeenFallThrough, tcdefs.MarkSeenFallThroughMask),
-				Comment: []string{"Drop packets from unknown flows."},
-				Action:  iptables.DropAction{},
+				Comment: []string{fmt.Sprintf("%s packets from unknown flows.", d.defaultRuleRenderer.DropActionOverride)},
+				Action:  d.defaultRuleRenderer.DropActionOverride,
 			},
 		)
 
@@ -1124,10 +1125,10 @@ func (d *InternalDataplane) setUpIptablesBPF() {
 
 		for _, prefix := range rulesConfig.WorkloadIfacePrefixes {
 			fwdRules = append(fwdRules,
-				// Drop packets that have come from a workload but have not been through our BPF program.
+				// Drop/reject packets that have come from a workload but have not been through our BPF program.
 				iptables.Rule{
 					Match:   iptables.Match().InInterface(prefix+"+").NotMarkMatchesWithMask(tcdefs.MarkSeen, tcdefs.MarkSeenMask),
-					Action:  iptables.DropAction{},
+					Action:  d.defaultRuleRenderer.DropActionOverride,
 					Comment: []string{"From workload without BPF seen mark"},
 				},
 			)
@@ -1144,7 +1145,7 @@ func (d *InternalDataplane) setUpIptablesBPF() {
 			// Catch any workload to host packets that haven't been through the BPF program.
 			inputRules = append(inputRules, iptables.Rule{
 				Match:  iptables.Match().InInterface(prefix+"+").NotMarkMatchesWithMask(tcdefs.MarkSeen, tcdefs.MarkSeenMask),
-				Action: iptables.DropAction{},
+				Action: d.defaultRuleRenderer.DropActionOverride,
 			})
 		}
 
@@ -1153,7 +1154,7 @@ func (d *InternalDataplane) setUpIptablesBPF() {
 				// In BPF mode, we don't support IPv6 yet.  Drop it.
 				fwdRules = append(fwdRules, iptables.Rule{
 					Match:   iptables.Match().OutInterface(prefix + "+"),
-					Action:  iptables.DropAction{},
+					Action:  d.defaultRuleRenderer.DropActionOverride,
 					Comment: []string{"To workload, drop IPv6."},
 				})
 			}
@@ -1227,7 +1228,7 @@ func (d *InternalDataplane) setUpIptablesBPF() {
 
 		// Do the full RPF check and dis-allow accept_local for anything else.
 		rpfRules = append(rpfRules, rules.RPFilter(t.IPVersion, tcdefs.MarkSeen, tcdefs.MarkSeenMask,
-			rulesConfig.OpenStackSpecialCasesEnabled, false)...)
+			rulesConfig.OpenStackSpecialCasesEnabled, false, d.defaultRuleRenderer.DropActionOverride)...)
 
 		rpfChain := []*iptables.Chain{{
 			Name:  rules.ChainNamePrefix + "RPF",
