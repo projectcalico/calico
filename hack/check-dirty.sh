711fca4b2472f572ebd191c1b141c7a4586fabf0
Abhijeet Kasurde <akasurde@redhat.com>
2022-03-14 14:42:07 +0530

    Review comments
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- hack/check-dirty.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ $(git status --porcelain) != '' ]]; then
-	echo "$(git status)"
-	echo ""
-	echo "ERROR: Local working tree is not clean. Make sure clean builds do not produce a dirty tree."
-	echo ""
-	exit 1
-fi
