4501ea77f9eb13e481970940be384b90e9f36e40
Casey Davenport <davenport.cas@gmail.com>
2022-03-14 12:37:31 -0700

    Fix upload of RPMs

--- utils/lib.sh
+++ utils/lib.sh
@@ -153,6 +153,7 @@ function copy_rpms_to_host {
 	# Some packages are produced in the calico dir.
 	set -- `find calico/dist/rpms-el7 -name "*.$arch.rpm"`
 	if test $# -gt 0; then
+	    $ssh_host -- mkdir -p $rpmdir/$reponame/$arch/
 	    $scp_host "$@" ${HOST}:$rpmdir/$reponame/$arch/
 	fi
     done
