6a8666c3ef7256bfb917dda3f7035ab711c0a557
Johannes Scheerer <johannes.scheerer@sap.com>
2022-04-14 15:13:13 +0200

    Use TokenRequest API instead of calico-nodes service account token for CNI kubeconfig.
    With projected service account tokens in kubernetes, the service account tokens of pods
    are bound to the lifetime of the corresponding pod. Therefore, it may lead to problems
    if an external process re-uses the token of a pod.
    The CNI binaries used the token of calico-node. However, in case calico-node got stopped
    the corresponding token lost its validity and hence could no longer be used for CNI
    operations. Usually, this automatically resolves over time, but there are some edge cases
    where this is not possible, e.g. if calico-node is autoscaled in terms of resources and
    the new resource requests would require preemption/eviction of an existing pod the CNI
    operation to delete the network sandbox will fail due to the no longer valid token
    (as calico-node was stopped beforehand).
    This change switches over to using the TokenRequest API instead, i.e. creating new
    tokens with limited validity. It would have been good to bind the token to an object,
    e.g. to the corresponding node, but as of now only secret and pod are supported types
    for binding tokens. Hence, the tokens are only limited in time and not bound to any
    other kubernetes object.

---
+++ node/pkg/cni/token_watch_test.go
@@ -0,0 +1,93 @@
+package cni_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/projectcalico/calico/node/pkg/cni"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+var _ = Describe("FV tests", func() {
+	const serviceAccountName = "cni-token-watch-test"
+	const namespace = metav1.NamespaceSystem
+
+	var clientset *kubernetes.Clientset
+
+	BeforeEach(func() {
+		clientset = createKubernetesClient()
+		setupServiceAccount(clientset, serviceAccountName, namespace)
+	})
+
+	AfterEach(func() {
+		cleanupServiceAccount(clientset, serviceAccountName, namespace)
+	})
+
+	It("should create a token successfully", func() {
+		tr := cni.NewTokenRefresher(clientset, namespace, serviceAccountName)
+		tu, err := tr.UpdateToken()
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(tu.Token).NotTo(BeEmpty())
+	})
+
+	It("should create a token successfully and deliver it through the channel", func() {
+		tr := cni.NewTokenRefresher(clientset, namespace, serviceAccountName)
+		tokenChan := tr.TokenChan()
+		go tr.Run()
+		defer tr.Stop()
+		var receivedToken cni.TokenUpdate
+		Eventually(tokenChan, 5*time.Second).Should(Receive(&receivedToken))
+		Expect(receivedToken.Token).NotTo(BeEmpty())
+	})
+
+	It("should create multiple tokens successfully and deliver them through the channel", func() {
+		const iterations = 5
+		// kube-apiserver does not allow smaller validity periods than 10 minutes
+		const tokenValiditySeconds = 600
+
+		tr := cni.NewTokenRefresherWithCustomTiming(clientset, namespace, serviceAccountName, tokenValiditySeconds, 1*time.Nanosecond, 600000)
+		tokenChan := tr.TokenChan()
+		go tr.Run()
+		defer tr.Stop()
+		for i := 0; i < iterations; i++ {
+			var receivedToken cni.TokenUpdate
+			Eventually(tokenChan, 5*time.Second).Should(Receive(&receivedToken))
+			Expect(receivedToken.Token).NotTo(BeEmpty())
+		}
+	})
+})
+
+func createKubernetesClient() *kubernetes.Clientset {
+	kubeconfigPath := os.Getenv("KUBECONFIG")
+	kubeconfig, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	if err != nil {
+		Fail(fmt.Sprintf("Failed to create kubernetes config: %v", err))
+	}
+	clientset, err := kubernetes.NewForConfig(kubeconfig)
+	if err != nil {
+		Fail(fmt.Sprintf("Could not create kubernetes client: %v", err))
+	}
+	return clientset
+}
+
+func setupServiceAccount(clientset *kubernetes.Clientset, name string, namespace string) {
+	serviceAccount := &v1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+	}
+	_, err := clientset.CoreV1().ServiceAccounts(namespace).Create(context.Background(), serviceAccount, metav1.CreateOptions{})
+	Expect(err).ShouldNot(HaveOccurred())
+}
+
+func cleanupServiceAccount(clientset *kubernetes.Clientset, name string, namespace string) {
+	err := clientset.CoreV1().ServiceAccounts(namespace).Delete(context.Background(), name, metav1.DeleteOptions{})
+	Expect(err).ShouldNot(HaveOccurred())
+}
