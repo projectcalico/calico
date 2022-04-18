eb05330ae3090511a16d4613df038f5c8042617b
Laurence Man <laurence@tigera.io>
2022-04-18 16:31:13 -0700

    Restart kube services post install if managed by Calico and running
    If kubelet/kube-proxy are installed by Calico and running, we restart
    the services so that the possibly updated service files are reloaded.

--- node/windows-packaging/host-process-install.ps1
+++ node/windows-packaging/host-process-install.ps1
@@ -13,11 +13,10 @@
 # limitations under the License.
 
 # This script is the entrypoint for the install container for the HostProcess
-# container installation.
-# - If Calico for Windows services are running, run the uninstall.
-Write-Host "Uninstalling any existing Calico install before proceeding with installation..."
-
+$ErrorActionPreference = "Stop"
 $rootDir = "c:\CalicoWindows"
+
+Write-Host "Uninstalling any existing Calico install before proceeding with installation..."
 $calicoSvcsRunning = (Get-Service | where Name -Like 'Calico*') -NE $null
 if ((Get-Service | where Name -Like 'Calico*') -NE $null) {
     Write-Host "Calico services running. Executing $rootDir\uninstall-calico.ps1..."
@@ -27,7 +26,7 @@ if ((Get-Service | where Name -Like 'Calico*') -NE $null) {
 # removing any Calico CNI config install
 elseif (Test-Path $RootDir) {
     # Load existing config and check if Calico CNI was configured.
-	if ((Test-Path "$rootDir\config.ps1") -and $env:CALICO_NETWORKING_BACKEND -NE "none") {
+    if ((Test-Path "$rootDir\config.ps1") -and $env:CALICO_NETWORKING_BACKEND -NE "none") {
         . $RootDir\config.ps1
         Write-Host "Root dir $rootDir exists. Removing Calico CNI plugin if installed..."
         Remove-CNIPlugin
@@ -35,7 +34,45 @@ elseif (Test-Path $RootDir) {
 } else {
     Write-Host "No Calico services found."
 }
+
 # Finally, start the install script.
 & ".\install-calico-windows.ps1" -StartCalico no
-exit $LastExitCode
+if ($LastExitCode -NE 0) {
+    exit $LastExitCode
+}
 
+# Restart kubelet and/or kube-proxy services if they are installed by Calico and
+# running since their service files may have been updated in-place.
+$kubeProxySvc = Get-CimInstance -Query 'select * from win32_service where name="kube-proxy"'
+if ($kubeProxySvc.PathName.StartsWith($rootDir, 'CurrentCultureIgnoreCase')) {
+    if ($kubeProxySvc.State -EQ "Running") {
+        Write-Host "Restarting running kube-proxy service managed by Calico to reload service"
+        Restart-Service kube-proxy
+        $svc = Get-Service kube-proxy
+
+        try {
+            $svc.WaitForStatus("Running", "00:00:10")
+        } catch {
+            Write-Host "Error waiting for kube-proxy service to be Running"
+            Write-Host $_
+            exit $LastExitCode
+        }
+    }
+}
+
+$kubeletSvc = Get-CimInstance -Query 'select * from win32_service where name="kubelet"'
+if ($kubeletSvc.PathName.StartsWith($rootDir, 'CurrentCultureIgnoreCase')) {
+    if ($kubeletSvc.State -EQ "Running") {
+        Write-Host "Restarting running kubelet service managed by Calico to reload service"
+        Restart-Service kubelet
+        $svc = Get-Service kubelet
+
+        try {
+            $svc.WaitForStatus("Running", "00:00:10")
+        } catch {
+            Write-Host "Error waiting for kubelet service to be Running"
+            Write-Host $_
+            exit $LastExitCode
+        }
+    }
+}
