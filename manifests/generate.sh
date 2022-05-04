dc900c93313154f27ad64847fa82e3f37f375de5
Casey Davenport <davenport.cas@gmail.com>
2022-05-04 16:26:17 -0700

    WIP: Start removing manifests from the docs build

---
+++ manifests/generate.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# This script updates the manifsts in this directory using helm.
+# Values files for the manifests in this directory can be found in 
+# ../calico/charts/values.
+
+##########################################################################
+# Build the operator manifest. 
+##########################################################################
+cat <<EOF > tigera-operator.yaml
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: tigera-operator
+  labels:
+    name: tigera-operator
+EOF
+
+helm -n tigera-operator template \
+	--include-crds \
+	../charts/tigera-operator \
+	-f ../charts/values/tigera-operator.yaml >> tigera-operator.yaml
+
+##########################################################################
+# Build Calico manifests.
+#
+# To add a new manifest to this directory, define
+# a values file in ../charts/values/ and then add it to VALUES_FILES.
+##########################################################################
+VALUES_FILES="calico-typha.yaml
+	calico-bpf.yaml
+	calico-vxlan.yaml
+	calico.yaml
+	calico-etcd.yaml 
+	calico-policy-only.yaml
+	flannel-migration/calico.yaml
+	"
+
+for FILE in $VALUES_FILES; do
+	helm -n kube-system template \
+		../charts/calico \
+		-f ../charts/values/$FILE \
+		-f ../charts/values/values.common.yaml > $FILE
+done
