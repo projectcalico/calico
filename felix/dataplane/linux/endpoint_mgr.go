3e70cb3f84331c8997ac89686c317b04b5ab7199
Tomas Hruby <tomas@tigera.io>
2022-05-12 16:23:15 -0700

    felix/bpf: add comment that RPF is done by BPF now.

--- felix/dataplane/linux/endpoint_mgr.go
+++ felix/dataplane/linux/endpoint_mgr.go
@@ -1075,7 +1075,7 @@ func (m *endpointManager) updateHostEndpoints() {
 		m.activeHostIfaceToMangleIngressChains = newHostIfaceMangleIngressChains
 	}
 
-	if m.ipVersion == 4 || !m.bpfEnabled {
+	if m.ipVersion == 4 || !m.bpfEnabled /* BPF enforces RPF on its own */ {
 		// Build iptables chains for untracked host endpoint policy.
 		newHostIfaceRawChains := map[string][]*iptables.Chain{}
 		for ifaceName, id := range newUntrackedIfaceNameToHostEpID {
