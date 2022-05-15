9afd092ff23038aa290360a4b652fc77e89e092f
Mazdak Nasab <mazdak.nasab@gmail.com>
2022-05-14 17:45:47 -0700

    update license year

--- node/clean-up-filesystem.sh
+++ node/clean-up-filesystem.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2020 Tigera, Inc. All rights reserved.
+# Copyright (c) 2020-2022 Tigera, Inc. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
