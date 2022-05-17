f8ebf2ccb4b8fa0b4ee345f438e5be008df745b2
Mazdak Nasab <mazdak.nasab@gmail.com>
2022-05-17 11:40:50 -0700

    check if cgroup2 fs is already mounted

--- node/pkg/nodeinit/calico-init_linux.go
+++ node/pkg/nodeinit/calico-init_linux.go
@@ -84,21 +84,34 @@ func ensureBPFFilesystem() error {
 // with PID 1 running on a host to allow felix running in calico-node access the root of cgroup namespace.
 // This is needed by felix to attach CTLB programs and implement k8s services correctly.
 func ensureCgroupV2Filesystem() error {
-	umountCmd := exec.Command(
-		"nsenter", "--cgroup=/rootns/cgroup", "--mount=/rootns/mnt",
-		"umount", bpf.CgroupV2Path)
-
-	out, err := umountCmd.Output()
+	// Check if the Cgroup2 filesystem is mounted at the expected location.
+	logrus.Info("Checking if Cgroup2 filesystem is mounted.")
+	mounts, err := os.Open("/node-proc/1/mountinfo")
 	if err != nil {
-		logrus.Errorf("Unmouting cgroup2 fs failed. output: %v", out)
-		return fmt.Errorf("failed to unmount cgroup2 filesystem: %w", err)
+		return fmt.Errorf("failed to open /node-proc/1/mountinfo: %w", err)
+	}
+	scanner := bufio.NewScanner(mounts)
+	for scanner.Scan() {
+		line := scanner.Text()
+		mountPoint := strings.Split(line, " ")[4] // 4 is the index to mount points in mountinfo files
+
+		if mountPoint == bpf.CgroupV2Path && strings.Contains(line, "cgroup2") {
+			logrus.Info("Cgroup2 filesystem is mounted.")
+			return nil
+		}
+	}
+	if scanner.Err() != nil {
+		return fmt.Errorf("failed to read /node-proc/1/mountinfo: %w", scanner.Err())
 	}
 
+	// If we get here, the Cgroup2 filesystem is not mounted.  Try to mount it.
+	logrus.Info("Cgroup2 filesystem is not mounted.  Trying to mount it...")
+
 	mountCmd := exec.Command(
-		"nsenter", "--cgroup=/rootns/cgroup", "--mount=/rootns/mnt",
+		"nsenter", "--cgroup=/node-proc/1/ns/cgroup", "--mount=/node-proc/1/ns/mnt",
 		"mount", "-t", "cgroup2", "none", bpf.CgroupV2Path)
 
-	out, err = mountCmd.Output()
+	out, err := mountCmd.Output()
 	if err != nil {
 		logrus.Errorf("Mouting cgroup2 fs failed. output: %v", out)
 		return fmt.Errorf("failed to mount cgroup2 filesystem: %w", err)
