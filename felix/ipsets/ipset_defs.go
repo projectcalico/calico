181c9e33ddb7a74b996e3a4e1be0803f58d2399a
Lancelot Robson <lancelot.robson@gmail.com>
2022-05-20 16:29:31 +0100

    make it build

--- felix/ipsets/ipset_defs.go
+++ felix/ipsets/ipset_defs.go
@@ -149,7 +149,7 @@ func (t IPSetType) CanonicaliseMember(member string) IPSetMember {
 		default:
 			log.WithField("member", member).Panic("Unknown protocol")
 		}
-		port, err := strconv.ParseUInt(parts[1], 10, 16)
+		port, err := strconv.ParseUint(parts[1], 10, 16)
 		if err != nil {
 			log.WithField("member", member).WithError(err).Panic("Bad port (should be between 0 and 65535)")
 		}
