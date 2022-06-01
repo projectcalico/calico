cb3c2dde2a38d78e033723091cea6bf839b9c97d
Tomas Hruby <tomas@tigera.io>
2022-05-31 17:47:24 -0700

    felix/bpf: ifstate map contains iface name
    For easier debugging and more robustnes in detecting what has changed
    and what was the previous state.

--- felix/bpf-gpl/ifstate.h
+++ felix/bpf-gpl/ifstate.h
@@ -5,9 +5,14 @@
 #ifndef __CALI_IFSTATE_H__
 #define __CALI_IFSTATE_H__
 
+struct ifstate_val {
+	__u32 flags;
+	char  name[16];
+};
+
 CALI_MAP(cali_iface, 2,
 		BPF_MAP_TYPE_HASH,
-		__u32, __u32,
+		__u32, struct ifstate_val,
 		1000, BPF_F_NO_PREALLOC, MAP_PIN_GLOBAL)
 
 #define IFACE_STATE_WEP		0x1
