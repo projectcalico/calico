76451995a29717fc8438fbe08e520e8d50df3d22
Pedro Coutinho <pedro@tigera.io>
2022-06-02 16:28:01 -0700

    Data types/messages changes for IPv6 wireguard: - Add new proto.HostMetadataV6Update/Remove messages - proto.WireguardEndpointUpdate/Remove   - Add interface_ipv6_addr and public_key_v6 fields - proto.WireguardStatusUpdate   - Add ip_version field - model.Wireguard   - Add InterfaceIPv6Addr and PublicKeyV6 attributes - v3.NodeWireguardSpec   - Add InterfaceIPv6Address attribute - v3.NodeStatus   - Add WireguardPublicKeyV6 attribute - calc.l3rrNodeInfo (L3RouteResolver)   - Add WireguardV6Addr attribute - Extend IPAM/allocateip related data types for IPv6 wireguard   - Add ipam.AttributeTypeWireguardV6 = model.IPAMBlockAttributeTypeWireguardV6   - Add model.IPAMBlockAttributeTypeWireguardV6 = "wireguardV6TunnelAddress" - Extend local wireguard related configuration for IPv6   - Attributes: WireguardEnabledV6, WireguardInterfaceNameV6, WireguardMTUV6, RoutingTableIndexV6   - Locations: wireguard.Config (RoutingTableIndexV6), FelixConfiguration, ConfigParams, rules.Config

--- felix/wireguard/config.go
+++ felix/wireguard/config.go
@@ -18,12 +18,16 @@ import "time"
 type Config struct {
 	// Wireguard configuration
 	Enabled             bool
+	EnabledV6           bool
 	ListeningPort       int
 	FirewallMark        int
 	RoutingRulePriority int
 	RoutingTableIndex   int
+	RoutingTableIndexV6 int
 	InterfaceName       string
+	InterfaceNameV6     string
 	MTU                 int
+	MTUV6               int
 	RouteSource         string
 	EncryptHostTraffic  bool
 	PersistentKeepAlive time.Duration
