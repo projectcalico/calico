576d166461387648b9592b05431feeb8198a3cea
Alex O Regan <alex.oregan@tigera.io>
2022-06-13 09:53:43 +0100

    Sync Operator CRD's (Kubernetes Provider) (#4803)
    * generator thrashing
    
    * copy operator installations CRD
    
    * generate files from updated operator installations CRD

--- libcalico-go/lib/backend/watchersyncer/watchersyncer_test.go
+++ libcalico-go/lib/backend/watchersyncer/watchersyncer_test.go
@@ -28,6 +28,8 @@ import (
 	apiv3 "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+
 	"github.com/projectcalico/calico/libcalico-go/lib/backend/api"
 	"github.com/projectcalico/calico/libcalico-go/lib/backend/model"
 	"github.com/projectcalico/calico/libcalico-go/lib/backend/watchersyncer"
