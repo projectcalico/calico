5907564a3ed5b4a0255420306510a3750d105cec
sridhar <sridhar@tigera.io>
2022-07-21 12:11:30 -0700

    Fix annotation formatting

--- felix/cmd/calico-bpf/commands/policy_debug.go
+++ felix/cmd/calico-bpf/commands/policy_debug.go
@@ -76,13 +76,13 @@ func parseArgs(args []string) (string, string, error) {
 }
 
 func printInsn(cmd *cobra.Command, insn asm.Insn) {
-	cmd.Printf("      ")
+	cmd.Printf("%-6s","")
 	for _, value := range insn.Instruction {
 		cmd.Printf("%02x", value)
 	}
-	cmd.Printf(" %v", insn)
+	cmd.Printf(" %-80v", insn)
 	if insn.Annotation != "" {
-		cmd.Printf("           %s", insn.Annotation)
+		cmd.Printf("%s", insn.Annotation)
 	}
 	cmd.Println()
 }
@@ -111,7 +111,7 @@ func dumpPolicyInfo(cmd *cobra.Command, iface, hook string) error {
 	cmd.Println("Policy Info:")
 	for _, insn := range policyDbg.PolicyInfo {
 		for _, comment := range insn.Comments {
-			cmd.Printf("      // %s\n", comment)
+			cmd.Printf("//%s\n", comment)
 		}
 		for _, label := range insn.Labels {
 			cmd.Printf("%s:\n", label)
