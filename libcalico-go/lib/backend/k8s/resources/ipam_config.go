d0810e17a02058b789ee4ead409382d78e055faf
Casey Davenport <davenport.cas@gmail.com>
2022-07-28 18:16:30 -0700

    Try doing it in the apiserver instead

--- libcalico-go/lib/backend/k8s/resources/ipam_config.go
+++ libcalico-go/lib/backend/k8s/resources/ipam_config.go
@@ -50,7 +50,7 @@ func NewIPAMConfigClient(c *kubernetes.Clientset, r *rest.RESTClient) K8sResourc
 				APIVersion: apiv3.GroupVersionCurrent,
 			},
 			k8sListType:  reflect.TypeOf(libapiv3.IPAMConfigList{}),
-			resourceKind: apiv3.KindIPAMConfiguration,
+			resourceKind: libapiv3.KindIPAMConfig,
 		},
 	}
 }
