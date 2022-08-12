d49acdc3d33ad18b3fae48ba83d2f3cc7fb2cb9c
Mazdak Nasab <mazdak.nasab@gmail.com>
2022-08-12 14:23:20 -0700

    Parse IPv6 fix header (#6531)

--- felix/bpf-gpl/tcv6.h
+++ felix/bpf-gpl/tcv6.h
@@ -9,6 +9,55 @@ SEC("classifier/tc/prologue_v6")
 int calico_tc_v6(struct __sk_buff *skb)
 {
 	CALI_DEBUG("Entering IPv6 prologue program\n");
+	struct cali_tc_ctx ctx = {
+		.state = state_get(),
+		.counters = counters_get(),
+		.skb = skb,
+		.fwd = {
+			.res = TC_ACT_UNSPEC,
+			.reason = CALI_REASON_UNKNOWN,
+		},
+		.ipheader_len = IPv6_SIZE,
+	};
+
+	if (!ctx.state) {
+		CALI_DEBUG("State map lookup failed: DROP\n");
+		return TC_ACT_SHOT;
+	}
+
+	if (!ctx.counters) {
+		CALI_DEBUG("Counters map lookup failed: DROP\n");
+		// We don't want to drop packets just because counters initialization fails, but
+		// failing here normally should not happen.
+		return TC_ACT_SHOT;
+	}
+	// TODO: Add IPv6 counters
+
+	if (CALI_LOG_LEVEL >= CALI_LOG_LEVEL_INFO) {
+		ctx.state->prog_start_time = bpf_ktime_get_ns();
+	}
+
+	if (skb_refresh_validate_ptrs(&ctx, UDP_SIZE)) {
+		DENY_REASON(&ctx, CALI_REASON_SHORT);
+		CALI_DEBUG("Too short\n");
+		goto deny;
+	}
+
+	tc_state_fill_from_ipv6hdr(&ctx);
+
+	/* Parse out the source/dest ports (or type/code for ICMP). */
+	switch (tc_state_fill_from_nexthdr(&ctx)) {
+	case PARSING_ERROR:
+		goto deny;
+	case PARSING_ALLOW_WITHOUT_ENFORCING_POLICY:
+		goto allow;
+	}
+
+	CALI_LOG_IPV6(ipv6_hdr(&ctx));
+	CALI_DEBUG("proto=%d\n", ctx.state->ip_proto);
+	CALI_DEBUG("sport=%d\n", ctx.state->sport);
+	CALI_DEBUG("dport=%d\n", ctx.state->dport);
+
 	if (CALI_F_WEP) {
 		CALI_DEBUG("IPv6 from workload: drop\n");
 		goto deny;
@@ -17,6 +66,9 @@ int calico_tc_v6(struct __sk_buff *skb)
 	CALI_DEBUG("About to jump to normal policy program\n");
 	CALI_JUMP_TO(skb, PROG_INDEX_V6_POLICY);
 	CALI_DEBUG("Tail call to normal policy program failed: DROP\n");
+
+allow:
+	return TC_ACT_UNSPEC;
 
 deny:
 	return TC_ACT_SHOT;
