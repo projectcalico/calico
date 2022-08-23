631f14fd473f96acb425f5a54e9cc929bfe64c1e
sridhar <sridhar@tigera.io>
2022-08-23 14:48:47 -0700

    Add BPFL3IfacePattern config, which is same as BPFDataIfacePattern but in addition specifies which devices are L3. This will allow users to set different tunnel devices such as tailscale as a L3 device, so that appropriate BPF programs are attached to such interfaces.

--- felix/config/config_params.go
+++ felix/config/config_params.go
@@ -176,7 +176,8 @@ type Config struct {
 	BPFEnabled                         bool             `config:"bool;false"`
 	BPFDisableUnprivileged             bool             `config:"bool;true"`
 	BPFLogLevel                        string           `config:"oneof(off,info,debug);off;non-zero"`
-	BPFDataIfacePattern                *regexp.Regexp   `config:"regexp;^((en|wl|ww|sl|ib)[opsx].*|(eth|wlan|wwan).*|tunl0$|vxlan.calico$|wireguard.cali$|wg-v6.cali$)"`
+	BPFDataIfacePattern                *regexp.Regexp   `config:"regexp;^((en|wl|ww|sl|ib)[opsx].*|(eth|wlan|wwan).*|tunl0$|vxlan.calico$)"`
+	BPFL3IfacePattern                  *regexp.Regexp   `config:"regexp;^(wireguard.cali$|wg-v6.cali$)"`
 	BPFConnectTimeLoadBalancingEnabled bool             `config:"bool;true"`
 	BPFExternalServiceMode             string           `config:"oneof(tunnel,dsr);tunnel;non-zero"`
 	BPFKubeProxyIptablesCleanupEnabled bool             `config:"bool;true"`
