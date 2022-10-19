cca248c4de1060a8f5ac196fff962d88c9814214
Josh Lucas <josh.lucas@tigera.io>
2022-10-19 11:19:12 -0400

    -Add new ttlSecurity field to documentation table for BGPPeer resource -Expand API description to include additional details

--- api/pkg/apis/projectcalico/v3/bgppeer.go
+++ api/pkg/apis/projectcalico/v3/bgppeer.go
@@ -100,7 +100,8 @@ type BGPPeerSpec struct {
 	// This removes BGP loop prevention and should only be used if absolutely necesssary.
 	// +optional
 	NumAllowedLocalASNumbers *int32 `json:"numAllowedLocalASNumbers,omitempty"`
-	// TTLSecurity enables the generalized TTL security mechanism (GTSM). The provided value is the number of hops
+	// TTLSecurity enables the generalized TTL security mechanism (GTSM) which protects against spoofed packets by
+	// ignoring received packets with a smaller than expected TTL value. The provided value is the number of hops
 	// (edges) between the peers.
 	// +optional
 	TTLSecurity *uint32 `json:"ttlSecurity,omitempty"`
