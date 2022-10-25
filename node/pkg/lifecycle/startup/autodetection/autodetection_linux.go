5c4724ceb0ff5b97882f6721c996b84455ec242d
Casey Davenport <davenport.cas@gmail.com>
2022-10-25 10:21:01 -0700

    Skip vxlan.calico in IP auto-detection

--- node/pkg/lifecycle/startup/autodetection/autodetection_linux.go
+++ node/pkg/lifecycle/startup/autodetection/autodetection_linux.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 Tigera, Inc. All rights reserved.
+// Copyright (c) 2021-2022 Tigera, Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,4 +19,5 @@ var DEFAULT_INTERFACES_TO_EXCLUDE []string = []string{
 	"^docker.*", "^cbr.*", "^dummy.*",
 	"^virbr.*", "^lxcbr.*", "^veth.*", "^lo",
 	"^cali.*", "^tunl.*", "^flannel.*", "^kube-ipvs.*", "^cni.*",
+	"^vxlan.calico.*",
 }
