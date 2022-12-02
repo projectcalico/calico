31258f70e9c2068105c76b0f87b03afaaa5e097f
rene-dekker <rene@tigera.io>
2022-12-02 15:03:18 -0800

    Fix the CI by removing an unused method

--- typha/pkg/syncserver/sync_server.go
+++ typha/pkg/syncserver/sync_server.go
@@ -620,19 +620,6 @@ func (s *Server) TerminateRandomConnection(logCtx *log.Entry, reason string) boo
 	return false
 }
 
-// allowedCiphers returns the set of allowed cipher suites for the server.
-// The list is taken from https://github.com/golang/go/blob/dev.boringcrypto.go1.13/src/crypto/tls/boring.go#L54
-func (s *Server) allowedCiphers() []uint16 {
-	return []uint16{
-		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-		tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-		tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-	}
-}
-
 func (s *Server) reportHealth() {
 	if s.config.HealthAggregator != nil {
 		s.config.HealthAggregator.Report(healthName, &health.HealthReport{Live: true})
