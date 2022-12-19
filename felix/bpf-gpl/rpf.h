abf36fc021ed96df334133a91bdc49a9d6fa817c
Steven Boland <steven@tigera.io>
2022-12-19 15:33:03 +0000

    Default to disabled if the option is not set as suggested

--- felix/bpf-gpl/rpf.h
+++ felix/bpf-gpl/rpf.h
@@ -34,12 +34,12 @@ static CALI_BPF_INLINE bool hep_rpf_check(struct cali_tc_ctx *ctx)
 	bool ret = false;
     bool strict;
 
-	if (GLOBAL_FLAGS & CALI_GLOBALS_RPF_OPTION_DISABLED) {
+	if (!(GLOBAL_FLAGS & CALI_GLOBALS_RPF_OPTION_ENABLED)) {
 		CALI_DEBUG("Host RPF check disabled\n");
 		return true;
 	}
 
-    strict = GLOBAL_FLAGS & CALI_GLOBALS_RPF_OPTION_STRICT;
+	strict = GLOBAL_FLAGS & CALI_GLOBALS_RPF_OPTION_STRICT;
 	struct bpf_fib_lookup fib_params = {
 		.family = 2, /* AF_INET */
 		.tot_len = 0,
