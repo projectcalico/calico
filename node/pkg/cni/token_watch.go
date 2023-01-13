a1d66ef0baba5c9a9aaccc2e93208acb5cd4fa1c
Michal Fupso <michal@tigera.io>
2023-01-13 14:45:24 -0800

    naming update

--- node/pkg/cni/token_watch.go
+++ node/pkg/cni/token_watch.go
@@ -240,9 +240,9 @@ func Run() {
 // CNIServiceAccountName returns the name of the serviceaccount to use for the CNI plugin token request.
 // This can be set via the CALICO_CNI_SERVICE_ACCOUNT environment variable, and defaults to "calico-cni-plugin" otherwise.
 func CNIServiceAccountName() string {
-	if na := os.Getenv("CALICO_NODE_SERVICE_ACCOUNT"); na != "" {
-		logrus.WithField("name", na).Debug("Using service account from CALICO_NODE_SERVICE_ACCOUNT")
-		return na
+	if sa := os.Getenv("CALICO_CNI_SERVICE_ACCOUNT"); sa != "" {
+		logrus.WithField("name", sa).Debug("Using service account from CALICO_NODE_SERVICE_ACCOUNT")
+		return sa
 	}
 	return defaultServiceAccountName
 }
