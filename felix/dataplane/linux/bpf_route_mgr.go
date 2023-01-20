238f55ff9d150e72961248a3d546f4a1358e0df4
Tomas Hruby <tomas@tigera.io>
2023-01-20 10:44:29 -0800

    [BPF] BPFDSROptoutCIDRs can overlaps with other CIDRs
    For instance 0.0.0.0/0 would optout all clients from DSR. Those CIDRs
    must be either disjoint with other CIDRs in the cluster or their
    super-sets. That means, for instance, if an IP pool overlaps only
    partially with the opt out CIDR, the optout may or may not be applied as
    it is applied to the route blocks on each node, but does not split those
    blocks.
    
    The BPFDSROptoutCIDRs is meant to be used with external entities to the
    cluser. It has no effect on in-cluster entities since DSR does not
    have much meaning fot them anyway.
    
    A useful case may be to apply this to external nodes, provider specific
    ip like AKS healt check IPs etc.

--- felix/dataplane/linux/bpf_route_mgr.go
+++ felix/dataplane/linux/bpf_route_mgr.go
@@ -67,7 +67,7 @@ type bpfRouteManager struct {
 	externalNodeCIDRs set.Set[ip.V4CIDR]
 	// Set of CIDRs for which we need to update the BPF routes.
 	dirtyCIDRs     set.Set[ip.V4CIDR]
-	dsrOptoutCIDRs set.Set[ip.V4CIDR]
+	dsrOptoutCIDRs *ip.CIDRTrie
 
 	// These fields track the desired state of the dataplane and the set of inconsistencies
 	// between that and the real state of the dataplane.
@@ -107,7 +107,8 @@ func newBPFRouteManager(config *Config, maps *bpfmap.Maps,
 		extCIDRs.Add(cidr.(ip.V4CIDR))
 		dirtyCIDRs.Add(cidr.(ip.V4CIDR))
 	}
-	noDsrCIDRs := set.New[ip.V4CIDR]()
+	noDsrCIDRs := ip.NewCIDRTrie()
+	something := new(struct{})
 	for _, cidrStr := range config.BPFDSROptoutCIDRs {
 		if strings.Contains(cidrStr, ":") {
 			log.WithField("cidr", cidrStr).Debug("Ignoring IPv6 DSR optout CIDR")
@@ -118,7 +119,7 @@ func newBPFRouteManager(config *Config, maps *bpfmap.Maps,
 			log.WithError(err).WithField("cidr", cidr).Error(
 				"Failed to parse DSR optout CIDR (which should have been validated already).")
 		}
-		noDsrCIDRs.Add(cidr.(ip.V4CIDR))
+		noDsrCIDRs.Update(cidr.(ip.V4CIDR), something) // We need to store something
 		dirtyCIDRs.Add(cidr.(ip.V4CIDR))
 	}
 
@@ -246,7 +247,7 @@ func (m *bpfRouteManager) calculateRoute(cidr ip.V4CIDR) *routes.Value {
 		flags |= routes.FlagHost
 	}
 
-	if m.dsrOptoutCIDRs.Contains(cidr) {
+	if m.dsrOptoutCIDRs.Covers(cidr) {
 		log.WithField("cidr", cidr).Debug("CIDR is optout from DSR.")
 		flags |= routes.FlagNoDSR
 	}
