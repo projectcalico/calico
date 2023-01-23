3abd0b304e077df45b63de75de96be5f9cfe5814
Tomas Hruby <tomas@tigera.io>
2023-01-23 09:40:27 -0800

    [BPF] fix typo in COUTNERS_TC_*

--- felix/bpf-gpl/counters.h
+++ felix/bpf-gpl/counters.h
@@ -14,8 +14,8 @@ struct counters_key {
 	__u32 hook;
 };
 
-#define COUTNERS_TC_INGRESS	0
-#define COUTNERS_TC_EGRESS	1
+#define COUNTERS_TC_INGRESS	0
+#define COUNTERS_TC_EGRESS	1
 #define COUNTERS_XDP		2
 
 CALI_MAP(cali_counters, 2,
@@ -32,13 +32,13 @@ static CALI_BPF_INLINE counters_t *counters_get(int ifindex)
 	if (CALI_F_XDP) {
 		key.hook = COUNTERS_XDP;
 	} else if (CALI_F_TO_HEP) {
-		key.hook = COUTNERS_TC_EGRESS;
+		key.hook = COUNTERS_TC_EGRESS;
 	} else if (CALI_F_FROM_HEP) {
-		key.hook = COUTNERS_TC_INGRESS;
+		key.hook = COUNTERS_TC_INGRESS;
 	} else if (CALI_F_TO_WEP) {
-		key.hook = COUTNERS_TC_EGRESS;
+		key.hook = COUNTERS_TC_EGRESS;
 	} else if (CALI_F_FROM_WEP) {
-		key.hook = COUTNERS_TC_INGRESS;
+		key.hook = COUNTERS_TC_INGRESS;
 	}
 
 	void * val = cali_counters_lookup_elem(&key);
