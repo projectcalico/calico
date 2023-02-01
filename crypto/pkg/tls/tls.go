8c47fe03cf157932cf3bb157a5df93cdf1d0a1f6
rene-dekker <rene@tigera.io>
2023-01-31 16:50:32 -0800

    Lower log level for NewTLSConfig

--- crypto/pkg/tls/tls.go
+++ crypto/pkg/tls/tls.go
@@ -21,14 +21,33 @@ import (
 )
 
 // NewTLSConfig returns a tls.Config with the recommended default settings for Calico Enterprise components.
-// Read more here:
-// https://csrc.nist.gov/projects/cryptographic-module-validation-program/certificate/4095
-func NewTLSConfig(_ bool) *tls.Config {
-	log.WithField("BuiltWithBoringCrypto", BuiltWithBoringCrypto).Debug("creating a TLS config")
-	// When we build with GOEXPERIMENT and tag boringcrypto, the tls settings in the config will automatically
-	// be overwritten and set to strict mode, due to the fipsonly import in fipstls.go.
-	return &tls.Config{
+// Read more recommendations here in Chapter 3:
+// https://www.gsa.gov/cdnstatic/SSL_TLS_Implementation_%5BCIO_IT_Security_14-69_Rev_6%5D_04-06-2021docx.pdf
+// When built with GOEXPERIMENT and tag boringcrypto, the TLS settings in the config will automatically
+// be overwritten and set to strict mode, due to the fipsonly import in fipstls.go.
+func NewTLSConfig(fipsMode bool) *tls.Config {
+	log.WithField("BuiltWithBoringCrypto", BuiltWithBoringCrypto).
+		WithField("fipsMode", fipsMode).
+		Debug("creating a TLS config")
+
+	cfg := &tls.Config{
 		MinVersion: tls.VersionTLS12,
 		MaxVersion: tls.VersionTLS13,
 	}
+
+	if fipsMode {
+		cfg.CipherSuites = []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+		}
+		cfg.CurvePreferences = []tls.CurveID{tls.CurveP384, tls.CurveP256}
+		// Our certificate for FIPS validation not mention validation for v1.3.
+		cfg.MaxVersion = tls.VersionTLS12
+		cfg.Renegotiation = tls.RenegotiateNever
+	}
+	return cfg
 }
