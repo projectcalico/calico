fa6c0d540d00c19f12c49c8d1a02bb86d6d4c74c
matt <mleung975@gmail.com>
2023-02-24 09:08:05 -0800

    Update the K8s version

--- apiserver/pkg/storage/calico/felixConfig_storage.go
+++ apiserver/pkg/storage/calico/felixConfig_storage.go
@@ -10,7 +10,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apiserver/pkg/registry/generic/registry"
 	"k8s.io/apiserver/pkg/storage"
-	etcd "k8s.io/apiserver/pkg/storage/etcd3"
 	"k8s.io/apiserver/pkg/storage/storagebackend/factory"
 
 	aapi "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
@@ -54,7 +53,7 @@ func NewFelixConfigurationStorage(opts Options) (registry.DryRunnableStorage, fa
 	dryRunnableStorage := registry.DryRunnableStorage{Storage: &resourceStore{
 		client:            c,
 		codec:             opts.RESTOptions.StorageConfig.Codec,
-		versioner:         etcd.APIObjectVersioner{},
+		versioner:         APIObjectVersioner{},
 		aapiType:          reflect.TypeOf(aapi.FelixConfiguration{}),
 		aapiListType:      reflect.TypeOf(aapi.FelixConfigurationList{}),
 		libCalicoType:     reflect.TypeOf(api.FelixConfiguration{}),
