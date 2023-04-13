910e53b57260ed1aae948bf84dd349dac346edbe
detailyang <detailyang@gmail.com>
2023-04-13 13:54:56 +0800

    chore:apply make fix

--- felix/routetable/route_table.go
+++ felix/routetable/route_table.go
@@ -1036,8 +1036,8 @@ func (r *RouteTable) syncL2RoutesForLink(ifaceName string) error {
 
 	for _, existing := range existingNeigh {
 		if existing.HardwareAddr == nil {
-				log.WithField("entry", entry).Debug("Ignoring existing ARP entry with no hardware addr")
-				continue
+			log.WithField("entry", entry).Debug("Ignoring existing ARP entry with no hardware addr")
+			continue
 		}
 		if _, ok := expected[existing.HardwareAddr.String()]; !ok {
 			logCxt.WithField("neighbor", existing).Debug("Neighbor should no longer be programmed")
