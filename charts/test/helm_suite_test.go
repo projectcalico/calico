9ca05e4ef42287221e0b505e8dda1ab06a22ca2d
ozdanborne <dan@projectcalico.org>
2023-04-26 11:58:51 -0400

    allow reference to existing secret as imagePullSecret
    allows reference to existing secrets for imagePullSecrets without
    passing the secret itself. this enables management of secrets by an external
    system like sealedsecrets and prevents the secret data from being stored in helm.
    
    it works by allowing use of the installation's imagePullSecret field
    directly instead of the toplevel imagePullSecrets field

---
+++ charts/test/helm_suite_test.go
@@ -0,0 +1,29 @@
+package charttest
+
+import (
+	"os/exec"
+	"testing"
+
+	"github.com/onsi/ginkgo/reporters"
+	"github.com/projectcalico/calico/libcalico-go/lib/testutils"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func init() {
+	testutils.HookLogrusForGinkgo()
+}
+
+func TestHelm(t *testing.T) {
+	// testutils.HookLogrusForGinkgo()
+	RegisterFailHandler(Fail)
+	junitReporter := reporters.NewJUnitReporter("../../report/helm_suite.xml")
+
+	_, err := exec.LookPath("helm")
+	if err != nil {
+		t.Skip("skipping exec tests since 'helm' is not installed")
+	}
+
+	RunSpecsWithDefaultAndCustomReporters(t, "Helm Suite", []Reporter{junitReporter})
+}
