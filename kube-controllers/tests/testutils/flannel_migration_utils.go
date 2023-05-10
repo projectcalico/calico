bfa940ef3c2837b2ee88b2baf9260bc4378f1a37
Pedro Coutinho <pedro@tigera.io>
2023-05-10 11:34:20 -0700

    Backport lib.Makefile and Makefile changes from master to build binaries with/without CGO etc

--- kube-controllers/tests/testutils/flannel_migration_utils.go
+++ kube-controllers/tests/testutils/flannel_migration_utils.go
@@ -108,7 +108,7 @@ func (f *FlannelCluster) Reset() {
 }
 
 func (f *FlannelCluster) AddFlannelNode(nodeName, podCidr, backend, mac, ip string, labels map[string]string, isMaster bool) *v1.Node {
-	defaultLabels := map[string]string{"beta.kubernetes.io/os": "linux"}
+	defaultLabels := map[string]string{"kubernetes.io/os": "linux"}
 	if isMaster {
 		defaultLabels["node-role.kubernetes.io/master"] = ""
 	}
