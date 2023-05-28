1bd249b2684d25429bd2e00c964d1daa994ae901
Daniel Fox <dan.fox@tigera.io>
2023-05-27 19:36:16 -0700

    Updates for v3.26.0 release (#7696)
    * Update values.yaml files for v3.26.0
    * Generate manifests
    * Add release notes
    * Fix deployment file paths

--- hack/release/pkg/builder/builder.go
+++ hack/release/pkg/builder/builder.go
@@ -332,7 +332,7 @@ func (r *ReleaseBuilder) collectGithubArtifacts(ver string) error {
 	if _, err := r.runner.Run("cp", []string{fmt.Sprintf("bin/tigera-operator-%s.tgz", ver), uploadDir}, nil); err != nil {
 		return err
 	}
-	if _, err := r.runner.Run("cp", []string{"manifests/ocp.tgz", ver, uploadDir}, nil); err != nil {
+	if _, err := r.runner.Run("cp", []string{"bin/ocp.tgz", uploadDir}, nil); err != nil {
 		return err
 	}
 
@@ -416,7 +416,7 @@ func (r *ReleaseBuilder) buildReleaseTar(ver string, targetDir string) error {
 	}
 
 	// Add in manifests directory generated from the docs.
-	if _, err := r.runner.Run("cp", []string{"-r", "/manifests", releaseBase}, nil); err != nil {
+	if _, err := r.runner.Run("cp", []string{"-r", "manifests", releaseBase}, nil); err != nil {
 		return err
 	}
 
