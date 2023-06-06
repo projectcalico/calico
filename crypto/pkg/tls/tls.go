069340cad22ce7c49b3073158e81d7322de3d797
sridhar <sridhar@tigera.io>
2023-06-06 15:18:07 -0700

    Update ciphers for tls 1.2, tls 1.3 for non-fips mode

--- crypto/pkg/tls/tls.go
+++ crypto/pkg/tls/tls.go
@@ -20,6 +20,23 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// Ciphers supported by TLS 1.2
+var tls12Ciphers = []uint16{
+	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+	tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+}
+
+// Ciphers supported by TLS 1.3
+var tls13Ciphers = []uint16{
+	tls.TLS_AES_256_GCM_SHA384,
+	tls.TLS_CHACHA20_POLY1305_SHA256,
+	tls.TLS_AES_128_GCM_SHA256,
+}
+
 // NewTLSConfig returns a tls.Config with the recommended default settings for Calico Enterprise components.
 // Read more recommendations here in Chapter 3:
 // https://www.gsa.gov/cdnstatic/SSL_TLS_Implementation_%5BCIO_IT_Security_14-69_Rev_6%5D_04-06-2021docx.pdf
@@ -35,19 +52,14 @@ func NewTLSConfig(fipsMode bool) *tls.Config {
 		MaxVersion: tls.VersionTLS13,
 	}
 
+	cfg.CipherSuites = append(cfg.CipherSuites, tls12Ciphers...)
 	if fipsMode {
-		cfg.CipherSuites = []uint16{
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-		}
 		cfg.CurvePreferences = []tls.CurveID{tls.CurveP384, tls.CurveP256}
 		// Our certificate for FIPS validation not mention validation for v1.3.
 		cfg.MaxVersion = tls.VersionTLS12
 		cfg.Renegotiation = tls.RenegotiateNever
+	} else {
+		cfg.CipherSuites = append(cfg.CipherSuites, tls13Ciphers...)
 	}
 	return cfg
 }
