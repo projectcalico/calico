39efc90706ad562860f3f13db2c46cc0ee42a48b
Matt Leung <mleung975@gmail.com>
2023-06-07 09:20:10 -0700

    Merge pull request #7756 from sridhartigera/tls-config-change
    Update ciphers for tls 1.2, tls 1.3 for non-fips mode

--- crypto/pkg/tls/tls.go
+++ crypto/pkg/tls/tls.go
@@ -20,6 +20,40 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// Ciphers supported by TLS 1.2 in fips mode
+var tls12CiphersFIPS = []uint16{
+	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+	tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+}
+
+// Ciphers supported by TLS 1.2
+var tls12Ciphers = []uint16{
+	tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+	tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+	tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+	tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+	tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+	tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+	tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+}
+
+// Ciphers supported by TLS 1.3
+var tls13Ciphers = []uint16{
+	tls.TLS_AES_256_GCM_SHA384,
+	tls.TLS_CHACHA20_POLY1305_SHA256,
+	tls.TLS_AES_128_GCM_SHA256,
+}
+
 // NewTLSConfig returns a tls.Config with the recommended default settings for Calico Enterprise components.
 // Read more recommendations here in Chapter 3:
 // https://www.gsa.gov/cdnstatic/SSL_TLS_Implementation_%5BCIO_IT_Security_14-69_Rev_6%5D_04-06-2021docx.pdf
@@ -36,18 +70,14 @@ func NewTLSConfig(fipsMode bool) *tls.Config {
 	}
 
 	if fipsMode {
-		cfg.CipherSuites = []uint16{
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-		}
 		cfg.CurvePreferences = []tls.CurveID{tls.CurveP384, tls.CurveP256}
+		cfg.CipherSuites = tls12CiphersFIPS
 		// Our certificate for FIPS validation not mention validation for v1.3.
 		cfg.MaxVersion = tls.VersionTLS12
 		cfg.Renegotiation = tls.RenegotiateNever
+	} else {
+		cfg.CipherSuites = tls12Ciphers
+		cfg.CipherSuites = append(cfg.CipherSuites, tls13Ciphers...)
 	}
 	return cfg
 }
