7a26a467fe9cd0e8aad743c37a7950492cfce62a
Pedro Coutinho <pedro@tigera.io>
2023-07-12 13:47:28 -0700

    Fix panic in 'calicoctl get nodes'
    Fix panic when bgpConfig.Spec.ASNumber was nil in calicoctl/calicoctl/commands/common/printer.go (used by calicoctl/calicoctl/resourcemgr/node.go when running 'calicoctl get nodes', https://github.com/projectcalico/calico/issues/7755 for more info).
    
    Add printer config() test to calicoctl/calicoctl/commands/common/printer_test.go.
    
    Run ginkgo bootstrap in calicoctl/calicoctl/commands/common/ (tests were not being run previously).
    
    Consider $(GINKGO_ARGS) when running the 'ut' target in calicoctl/Makefile.

--- calicoctl/calicoctl/commands/common/printer_test.go
+++ calicoctl/calicoctl/commands/common/printer_test.go
@@ -1,8 +1,35 @@
+// Copyright (c) 2019-2023 Tigera, Inc. All rights reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package common
 
 import (
+	"context"
+	"errors"
+
+	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	apiv3 "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
+	"github.com/projectcalico/api/pkg/lib/numorstring"
+
+	"github.com/projectcalico/calico/libcalico-go/lib/clientv3"
+	cerrors "github.com/projectcalico/calico/libcalico-go/lib/errors"
+	"github.com/projectcalico/calico/libcalico-go/lib/options"
 )
 
 var nilSlice []int
@@ -37,3 +64,83 @@ var _ = DescribeTable("Testing joinAndTruncate",
 	Entry("slice no truncate", []int{123456}, ",", 6, "123456"),
 	Entry("string", "HelloWorld", ",", 0, "HelloWorld"),
 )
+
+var _ = Describe("Testing printer config()", func() {
+	var client *mockClient
+	BeforeEach(func() {
+		client = newMockClient()
+	})
+
+	It("prints the default asnumber 64512 when BGPConfig.Spec.ASNumber is nil", func() {
+		Expect(config(client)("asnumber")).To(Equal("64512"))
+	})
+
+	It("prints the default asnumber 64512 when the default BGPConfig cannot be found", func() {
+		client.bgpConfig = nil
+		Expect(config(client)("asnumber")).To(Equal("64512"))
+	})
+
+	It("prints the asnumber from BGPConfig.Spec.ASNumber when it is present", func() {
+		asNumber := numorstring.ASNumber(12345)
+		bgpConfig := apiv3.BGPConfiguration{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "default", ResourceVersion: "1234",
+				CreationTimestamp: metav1.Now(),
+				UID:               "test-printer-bgpconfig",
+			},
+			Spec: apiv3.BGPConfigurationSpec{
+				ASNumber: &asNumber,
+			},
+		}
+		_, _ = client.BGPConfigurations().Update(context.Background(), &bgpConfig, options.SetOptions{})
+		Expect(config(client)("asnumber")).To(Equal(asNumber.String()))
+	})
+
+	It("prints 'unknown' when there is an error getting the default BGPConfig", func() {
+		client.throwError = true
+		Expect(config(client)("asnumber")).To(Equal("unknown"))
+	})
+})
+
+type mockClient struct {
+	clientv3.Interface
+	clientv3.BGPConfigurationInterface
+	bgpConfig  *apiv3.BGPConfiguration
+	throwError bool
+}
+
+func newMockClient() *mockClient {
+	return &mockClient{
+		bgpConfig: &apiv3.BGPConfiguration{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "default", ResourceVersion: "1234",
+				CreationTimestamp: metav1.Now(),
+				UID:               "test-printer-bgpconfig",
+			},
+			Spec: apiv3.BGPConfigurationSpec{},
+		},
+		throwError: false,
+	}
+}
+
+func (c *mockClient) BGPConfigurations() clientv3.BGPConfigurationInterface {
+	return c
+}
+
+func (c *mockClient) Get(_ context.Context, name string, _ options.GetOptions) (*apiv3.BGPConfiguration, error) {
+	if c.throwError {
+		return nil, errors.New("mock error for testing")
+	}
+
+	if c.bgpConfig == nil {
+		return nil, cerrors.ErrorResourceDoesNotExist{}
+
+	}
+
+	return c.bgpConfig, nil
+}
+
+func (c *mockClient) Update(_ context.Context, res *apiv3.BGPConfiguration, _ options.SetOptions) (*apiv3.BGPConfiguration, error) {
+	c.bgpConfig = res
+	return c.bgpConfig, nil
+}
