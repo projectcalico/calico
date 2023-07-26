12e4b5510d3676b79f816f9d73f27e74e06b00fb
Shaun Crampton <shaun@tigera.io>
2023-07-26 15:27:59 +0100

    Insert a deduplicating buffer ahead of other syncer clients.

--- typha/pkg/syncclientutils/startsyncerclient.go
+++ typha/pkg/syncclientutils/startsyncerclient.go
@@ -20,7 +20,7 @@ import (
 	log "github.com/sirupsen/logrus"
 
 	"github.com/projectcalico/calico/libcalico-go/lib/backend/api"
-
+	"github.com/projectcalico/calico/libcalico-go/lib/backend/syncersv1/dedupebuffer"
 	"github.com/projectcalico/calico/typha/pkg/discovery"
 	"github.com/projectcalico/calico/typha/pkg/syncclient"
 	"github.com/projectcalico/calico/typha/pkg/syncproto"
@@ -53,12 +53,24 @@ func MustStartSyncerClientIfTyphaConfigured(
 		return false
 	}
 
+	var dedupeBuf *dedupebuffer.DedupeBuffer
+	if db, ok := cbs.(*dedupebuffer.DedupeBuffer); ok {
+		log.Debug("Callback is already a dedupe buffer.")
+		dedupeBuf = db
+	} else {
+		// Include a de-duplicating buffer to avoid putting backpressure on the
+		// Typha client.
+		log.Debug("Inserting dedupe buffer ahead of callbacks.")
+		dedupeBuf = dedupebuffer.New()
+		go dedupeBuf.SendToSinkForever(cbs)
+	}
+
 	// Use a remote Syncer, via the Typha server.
 	log.WithField("addr", typhaAddrs).Info("Connecting to Typha.")
 	typhaConnection := syncclient.New(
 		discoverer,
 		myVersion, myHostname, myInfo,
-		cbs,
+		dedupeBuf,
 		&syncclient.Options{
 			SyncerType:      syncerType,
 			ReadTimeout:     typhaConfig.ReadTimeout,
