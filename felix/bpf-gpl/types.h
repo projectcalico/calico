fbe5274759477fe66e03281df027bd588189c8b5
Tomas Hruby <tomas@tigera.io>
2023-08-15 10:31:18 -0700

    [BPF] use explicit scratch pointer in state
    Makes it always aligned and easy to reach.

--- felix/bpf-gpl/types.h
+++ felix/bpf-gpl/types.h
@@ -185,7 +185,7 @@ struct cali_tc_ctx {
 				CALI_LOG_IF(CALI_LOG_LEVEL_DEBUG, "no globals: DROP\n");		\
 				bpf_exit(TC_ACT_SHOT);				\
 			}							\
-			struct pkt_scratch *scratch = (void *)(gl + 1); 	\
+			struct pkt_scratch *scratch = (void *)(gl->__scratch); 	\
 			(struct cali_tc_ctx) {					\
 				.state = state,					\
 				.counters = counters,				\
