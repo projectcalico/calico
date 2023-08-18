6ef2d7c068487f16eaa6a3383e38074929eed194
sridhar <sridhar@tigera.io>
2023-08-17 18:21:57 -0700

    Remove header

--- felix/bpf/libbpf/libbpf.go
+++ felix/bpf/libbpf/libbpf.go
@@ -30,7 +30,6 @@ import (
 // #cgo amd64 LDFLAGS: -L${SRCDIR}/../../bpf-gpl/include/libbpf/src/amd64 -lbpf -lelf -lz
 // #cgo arm64 LDFLAGS: -L${SRCDIR}/../../bpf-gpl/include/libbpf/src/arm64 -lbpf -lelf -lz
 // #cgo armv7 LDFLAGS: -L${SRCDIR}/../../bpf-gpl/include/libbpf/src/armv7 -lbpf -lelf -lz
-// #include <linux/bpf.h>
 // #include "libbpf_api.h"
 import "C"
 
