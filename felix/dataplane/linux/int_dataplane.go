6f320ff902611f9db484e69fdbe596fdb84d8c63
Tomas Hruby <tomas@tigera.io>
2023-08-31 16:12:51 -0700

    [BPF] CLTBv6 works

--- felix/dataplane/linux/int_dataplane.go
+++ felix/dataplane/linux/int_dataplane.go
@@ -800,8 +800,13 @@ func NewIntDataplaneDriver(config Config) *InternalDataplane {
 					logLevel = "off"
 				}
 			}
+
+			ipFamily := 4
+			if config.BPFIpv6Enabled {
+				ipFamily = 6
+			}
 			// Activate the connect-time load balancer.
-			err = bpfnat.InstallConnectTimeLoadBalancer(
+			err = bpfnat.InstallConnectTimeLoadBalancer(ipFamily,
 				config.BPFCgroupV2, logLevel, config.BPFConntrackTimeouts.UDPLastSeen, excludeUDP)
 			if err != nil {
 				log.WithError(err).Panic("BPFConnTimeLBEnabled but failed to attach connect-time load balancer, bailing out.")
