b04065a07b9439f980e4d2b4b1bcd1bb026c7b51
Jiawei Huang <jiawei@tigera.io>
2023-10-10 22:11:49 -0700

    Use custom go-build

--- metadata.mk
+++ metadata.mk
@@ -3,7 +3,8 @@
 #################################################################################################
 
 # The version of github.com/projectcalico/go-build to use.
-GO_BUILD_VER = v0.89
+GO_BUILD_VER = ubi8
+
 # Env var to ACK Ginkgo deprecation warnings, may need updating with go-build.
 ACK_GINKGO=ACK_GINKGO_DEPRECATIONS=1.16.5
 
