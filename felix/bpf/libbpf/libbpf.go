d27b80cfd6b81852aafd03a22d60e808bc23b632
Jiawei Huang <jiawei@tigera.io>
2023-10-13 17:21:45 -0700

    Drop 32 bit linux/arm support
    k8s dropped 32 bit linux/arm support from v1.27+ [1].
    
    [1] https://github.com/kubernetes/kubernetes/pull/115742

--- felix/bpf/libbpf/libbpf.go
+++ felix/bpf/libbpf/libbpf.go
@@ -29,7 +29,6 @@ import (
 // #cgo CFLAGS: -I${SRCDIR}/../../bpf-gpl/include/libbpf/src -I${SRCDIR}/../../bpf-gpl/include/libbpf/include/uapi -I${SRCDIR}/../../bpf-gpl -Werror
 // #cgo amd64 LDFLAGS: -L${SRCDIR}/../../bpf-gpl/include/libbpf/src/amd64 -lbpf -lelf -lz
 // #cgo arm64 LDFLAGS: -L${SRCDIR}/../../bpf-gpl/include/libbpf/src/arm64 -lbpf -lelf -lz
-// #cgo armv7 LDFLAGS: -L${SRCDIR}/../../bpf-gpl/include/libbpf/src/armv7 -lbpf -lelf -lz
 // #include "libbpf_api.h"
 import "C"
 
