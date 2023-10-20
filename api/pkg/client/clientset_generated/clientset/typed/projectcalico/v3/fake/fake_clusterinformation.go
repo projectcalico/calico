0288b2e4fa1a92836b4c263e966a5a4fa223ed53
Jiawei Huang <jiawei@tigera.io>
2023-10-19 19:48:28 -0700

    make generate

--- api/pkg/client/clientset_generated/clientset/typed/projectcalico/v3/fake/fake_clusterinformation.go
+++ api/pkg/client/clientset_generated/clientset/typed/projectcalico/v3/fake/fake_clusterinformation.go
@@ -10,7 +10,6 @@ import (
 	v3 "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	labels "k8s.io/apimachinery/pkg/labels"
-	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
 	testing "k8s.io/client-go/testing"
@@ -21,9 +20,9 @@ type FakeClusterInformations struct {
 	Fake *FakeProjectcalicoV3
 }
 
-var clusterinformationsResource = schema.GroupVersionResource{Group: "projectcalico.org", Version: "v3", Resource: "clusterinformations"}
+var clusterinformationsResource = v3.SchemeGroupVersion.WithResource("clusterinformations")
 
-var clusterinformationsKind = schema.GroupVersionKind{Group: "projectcalico.org", Version: "v3", Kind: "ClusterInformation"}
+var clusterinformationsKind = v3.SchemeGroupVersion.WithKind("ClusterInformation")
 
 // Get takes name of the clusterInformation, and returns the corresponding clusterInformation object, and an error if there is any.
 func (c *FakeClusterInformations) Get(ctx context.Context, name string, options v1.GetOptions) (result *v3.ClusterInformation, err error) {
