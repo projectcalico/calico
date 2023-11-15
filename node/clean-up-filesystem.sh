63db843273c3eb185853364223c712b6d809885e
Shaun Crampton <shaun@tigera.io>
2023-11-15 10:58:31 +0000

    Fix missing libraries when building node image.

--- node/clean-up-filesystem.sh
+++ node/clean-up-filesystem.sh
@@ -238,6 +238,7 @@ done < <(find /usr/lib64 \( -type f -or -type l \))
 # binaries and libraries that we don't want.
 packages_to_keep=(
   bash
+  bzip2-libs
   ca-certificates
   conntrack-tools
   coreutils-single
@@ -268,6 +269,8 @@ packages_to_keep=(
   libpcap
   libpwquality
   libselinux
+  libzstd
+  libz
   ncurses
   net-tools
   openssl-libs
