9d03240b9aa55916fe7b21211d3ce51dd4cfdccf
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-11-16 10:25:27 -0500

    spelling: validating
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- libcalico-go/lib/ipam/ipam_block_reader_writer_test.go
+++ libcalico-go/lib/ipam/ipam_block_reader_writer_test.go
@@ -254,7 +254,7 @@ var _ = testutils.E2eDatastoreDescribe("IPAM affine block allocation tests", tes
 
 				// Validate the affinities.
 				for _, a := range affs.KVPairs {
-					log.Infof("Validaing affinity: %+v", a)
+					log.Infof("Validating affinity: %+v", a)
 					b, err := bc.Get(ctx, model.BlockKey{CIDR: a.Key.(model.BlockAffinityKey).CIDR}, "")
 					Expect(err).NotTo(HaveOccurred())
 					Expect(b).NotTo(BeNil())
@@ -345,7 +345,7 @@ var _ = testutils.E2eDatastoreDescribe("IPAM affine block allocation tests", tes
 
 				// For each affinity, expect the corresponding block to have the same affinity.
 				for _, a := range affs.KVPairs {
-					log.Infof("Validaing affinity: %+v", a)
+					log.Infof("Validating affinity: %+v", a)
 					b, err := bc.Get(ctx, model.BlockKey{CIDR: a.Key.(model.BlockAffinityKey).CIDR}, "")
 					Expect(err).NotTo(HaveOccurred())
 					Expect(*b.Value.(*model.AllocationBlock).Affinity).To(Equal(fmt.Sprintf("host:%s", a.Key.(model.BlockAffinityKey).Host)))
@@ -411,7 +411,7 @@ var _ = testutils.E2eDatastoreDescribe("IPAM affine block allocation tests", tes
 
 				// Validate the affinities.
 				for _, a := range affs.KVPairs {
-					log.Infof("Validaing affinity: %+v", a)
+					log.Infof("Validating affinity: %+v", a)
 					b, err := bc.Get(ctx, model.BlockKey{CIDR: a.Key.(model.BlockAffinityKey).CIDR}, "")
 					Expect(err).NotTo(HaveOccurred())
 
