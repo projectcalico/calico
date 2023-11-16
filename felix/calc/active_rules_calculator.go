b080b9cc34e13d38ce8bb5ef1e421f126d530eed
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-11-16 03:31:12 -0500

    spelling: are
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- felix/calc/active_rules_calculator.go
+++ felix/calc/active_rules_calculator.go
@@ -55,7 +55,7 @@ type PolicyMatchListener interface {
 //
 // When looking at policies, the ActiveRules calculator is only interested in the selector
 // attached to the policy itself (which determines the set of endpoints that it applies to).
-// The rules in a policy may also contain selectors; those are are ignored here; they are
+// The rules in a policy may also contain selectors; those are ignored here; they are
 // mapped to IP sets by the RuleScanner.
 type ActiveRulesCalculator struct {
 	// Caches of all known policies/profiles.
