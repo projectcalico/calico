d6ec3ce2ac218c8b5f97119c520b15d163e4076e
Tomas Hruby <tomas@tigera.io>
2023-11-20 19:59:31 -0800

    [BPF] fix ct cleaning for host processes w/o ctlb using service
    If there is an overlay, forwarding entry may not have a matching client
    IP as it may start with host's main IP, but then it is turned into
    host's tunnel IP. We cannot determine what is the client/service/backend
    from that entry only. We need to lookup the reverse entry. If the
    reverse entry does not exist, we delete the forarding entry anyway as
    there is no use for it without the reverse half.

--- felix/bpf/conntrack/cleanup.go
+++ felix/bpf/conntrack/cleanup.go
@@ -212,14 +212,17 @@ func NewStaleNATScanner(frontendHasBackend NATChecker) *StaleNATScanner {
 }
 
 // Check checks the conntrack entry
-func (sns *StaleNATScanner) Check(k KeyInterface, v ValueInterface, _ EntryGet) ScanVerdict {
+func (sns *StaleNATScanner) Check(k KeyInterface, v ValueInterface, get EntryGet) ScanVerdict {
 	debug := log.GetLevel() >= log.DebugLevel
+
+again:
 
 	switch v.Type() {
 	case TypeNormal:
 		// skip non-NAT entry
 
 	case TypeNATReverse:
+
 		proto := k.Proto()
 		ipA := k.AddrA()
 		ipB := k.AddrB()
@@ -288,8 +291,16 @@ func (sns *StaleNATScanner) Check(k KeyInterface, v ValueInterface, _ EntryGet) 
 				svcIP = kA
 				svcPort = kAport
 			} else {
-				log.WithFields(log.Fields{"key": k, "value": v}).Error("Mismatch between key and rev key")
-				return ScanVerdictOK // don't touch, will get deleted when expired
+				rv, err := get(revKey)
+				if err != nil {
+					// There is no match for the reverse key, delete it, its useless - we
+					// can get here due to host networked program accessing service
+					// without ctlb when the backed is accessible via tunnel.
+					return ScanVerdictDelete
+				}
+				k = revKey
+				v = rv
+				goto reverse // handle it based on the reverse entry
 			}
 		} else {
 			// snatPort is the new client port. It does not match the client
@@ -316,8 +327,16 @@ func (sns *StaleNATScanner) Check(k KeyInterface, v ValueInterface, _ EntryGet) 
 				svcIP = kA
 				svcPort = kAport
 			} else {
-				log.WithFields(log.Fields{"key": k, "value": v}).Error("Mismatch between key and rev key")
-				return ScanVerdictOK // don't touch, will get deleted when expired
+				rv, err := get(revKey)
+				if err != nil {
+					// There is no match for the reverse key, delete it, its useless - we
+					// can get here due to host networked program accessing service
+					// without ctlb when the backed is accessible via tunnel.
+					return ScanVerdictDelete
+				}
+				k = revKey
+				v = rv
+				goto reverse // handle it based on the reverse entry
 			}
 		}
 
@@ -336,6 +355,13 @@ func (sns *StaleNATScanner) Check(k KeyInterface, v ValueInterface, _ EntryGet) 
 	}
 
 	return ScanVerdictOK
+
+reverse:
+	if v.Type() == TypeNATReverse {
+		goto again
+	}
+
+	return ScanVerdictDelete
 }
 
 // IterationStart satisfies EntryScannerSynced
