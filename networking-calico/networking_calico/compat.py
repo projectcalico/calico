8d5d676b02379c83242985bda82405f0be1cd69b
Nell Jerram <nell@tigera.io>
2023-11-24 09:11:35 +0000

    Change to trigger CI

--- networking-calico/networking_calico/compat.py
+++ networking-calico/networking_calico/compat.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Change to trigger CI
+
 try:
     from neutron_lib import constants
     from neutron_lib import exceptions as n_exc
