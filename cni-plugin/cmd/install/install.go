b9318ac66eac058b102b7167a508010c9554ff31
Pedro Coutinho <pedro@tigera.io>
2024-01-24 20:17:10 -0800

    Improvements to cni-plugin binaries

--- cni-plugin/cmd/install/install.go
+++ cni-plugin/cmd/install/install.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2018,2020 Tigera, Inc. All rights reserved.
+// Copyright (c) 2023 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,25 +15,17 @@
 package main
 
 import (
-	"os"
-	"path/filepath"
+	"github.com/sirupsen/logrus"
 
-	"github.com/projectcalico/calico/cni-plugin/pkg/ipamplugin"
-	"github.com/projectcalico/calico/cni-plugin/pkg/plugin"
+	"github.com/projectcalico/calico/cni-plugin/pkg/install"
 )
 
 // VERSION is filled out during the build process (using git describe output)
 var VERSION string
 
 func main() {
-	// Use the name of the binary to determine which routine to run.
-	_, filename := filepath.Split(os.Args[0])
-	switch filename {
-	case "calico", "calico.exe":
-		plugin.Main(VERSION)
-	case "calico-ipam", "calico-ipam.exe":
-		ipamplugin.Main(VERSION)
-	default:
-		panic("Unknown binary name: " + filename)
+	err := install.Install()
+	if err != nil {
+		logrus.WithError(err).Fatal("Error installing CNI plugin")
 	}
 }
