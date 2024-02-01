9593fb90a767b0e095d335f41ee9d6f2d815668d
Alex O'Regan <alex.oregan@tigera.io>
2024-02-01 12:37:32 +0000

    Adds endpoint-status file-writing and endpoint-status CNI waiting

---
+++ felix/fv/pod_setup_wait_test.go
@@ -0,0 +1,164 @@
+// Copyright (c) 2020 Tigera, Inc. All rights reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build fvtests
+
+package fv_test
+
+import (
+	"fmt"
+	"path/filepath"
+	"regexp"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/sirupsen/logrus"
+
+	"github.com/projectcalico/calico/felix/fv/infrastructure"
+	"github.com/projectcalico/calico/felix/fv/workload"
+	"github.com/projectcalico/calico/libcalico-go/lib/apiconfig"
+
+	"github.com/projectcalico/calico/libcalico-go/lib/names"
+)
+
+var _ = infrastructure.DatastoreDescribe("_BPF-SAFE_ Pod setup status wait", []apiconfig.DatastoreType{apiconfig.EtcdV3, apiconfig.Kubernetes}, func(getInfra infrastructure.InfraFactory) {
+	const nodeCount = 1
+
+	var (
+		infra                    infrastructure.DatastoreInfra
+		topologyOptions          infrastructure.TopologyOptions
+		tc                       infrastructure.TopologyContainers
+		dummyWorkloads           [2]*workload.Workload
+		dataplaneInSyncReceivedC chan struct{}
+	)
+
+	BeforeEach(func() {
+		infra = getInfra()
+		topologyOptions = infrastructure.DefaultTopologyOptions()
+		topologyOptions.DelayFelixStart = true
+		topologyOptions.EnableIPv6 = false
+		// /tmp should be automatically mounted by an internal call to RunFelix
+		// by the infrastructure pkg when Topology is started.
+		topologyOptions.ExtraEnvVars["FELIX_ENDPOINTSTATUSPATHPREFIX"] = "/tmp"
+		topologyOptions.ExtraEnvVars["FELIX_DEBUGDISABLElOGDROPPING"] = "true"
+		topologyOptions.FelixLogSeverity = "Debug"
+		topologyOptions.FelixDebugFilenameRegex = "status_file_reporter"
+
+		tc, _ = infrastructure.StartNNodeTopology(nodeCount, topologyOptions, infra)
+		tc.Felixes[0].Exec("rm", "-rf", "/tmp/endpoint-status")
+		dataplaneInSyncReceivedC = tc.Felixes[0].WatchStdoutFor(regexp.MustCompile("DataplaneInSync received from upstream"))
+	})
+
+	AfterEach(func() {
+		tc.Stop()
+		if CurrentGinkgoTestDescription().Failed {
+			infra.DumpErrorData()
+		}
+		infra.Stop()
+	})
+
+	Describe("with the file-reporter writing endpoint status to '/tmp/endpoint-status'", func() {
+		buildStatCmdInFelix := func(felix *infrastructure.Felix, filename string) func() error {
+			return func() error {
+				return felix.ExecMayFail("stat", filename)
+			}
+		}
+
+		It("should receive DataplaneInSync message from the dataplane", func() {
+			tc.Felixes[0].TriggerDelayedStart()
+			Eventually(dataplaneInSyncReceivedC, "10s").Should(BeClosed(), "receipt of DataplaneInSync message not seen in logs")
+		})
+
+		It("should create endpoint-status files in a directory named endpoint-status with the specified directory prefix", func() {
+			tc.Felixes[0].TriggerDelayedStart()
+			var filenames [2]string
+			var statCmds [2]func() error
+			for i := range dummyWorkloads {
+				dummyWorkloads[i] = workload.New(tc.Felixes[0], fmt.Sprintf("workload-endpoint-status-tests-%d", i), "default", fmt.Sprintf("10.65.0.%d", 10+i), "8080", "tcp")
+				err := dummyWorkloads[i].Start()
+				Expect(err).NotTo(HaveOccurred())
+				dummyWorkloads[i].ConfigureInInfra(infra)
+
+				key, err := names.V3WorkloadEndpointToWorkloadEndpointKey(dummyWorkloads[i].WorkloadEndpoint)
+				Expect(err).NotTo(HaveOccurred())
+				filenames[i] = names.WorkloadEndpointKeyToStatusFilename(key)
+				statCmds[i] = buildStatCmdInFelix(tc.Felixes[0], filepath.Join("/tmp/endpoint-status", filenames[i]))
+			}
+
+			Expect(filenames[0]).NotTo(Equal(filenames[1]), "duplicated filenames found")
+
+			for i := range dummyWorkloads {
+				Eventually(statCmds[i], "10s").Should(BeNil(), "status file was not programmed")
+			}
+
+			for i, w := range dummyWorkloads {
+				w.RemoveFromInfra(infra)
+				Eventually(statCmds[i], "10s").Should(HaveOccurred(), "status file was not deleted following workload removal")
+			}
+		})
+
+		It("should cleanup stale files after a restart", func() {
+			By("creating a file whose name we know Felix will not match to a workload")
+			name := filepath.Join("/tmp/endpoint-status", "sdfsdf")
+			tc.Felixes[0].Exec("mkdir", "/tmp/endpoint-status")
+			tc.Felixes[0].Exec("touch", name)
+
+			By("Waiting for Felix's status file reporter to come in-sync")
+			tc.Felixes[0].TriggerDelayedStart()
+			Eventually(dataplaneInSyncReceivedC, "10s").Should(BeClosed(), "receipt of DataplaneInSync message not seen in logs")
+
+			By("checking if the stale file has been cleaned up")
+			fileExists := func() bool {
+				logrus.WithField("file", name).Info("Checking existence of expected file in endpoint-status dir")
+				_, err := tc.Felixes[0].ExecOutput("stat", name)
+				return err == nil
+			}
+			Eventually(fileExists).Should(BeFalse(), "Stale file was not cleaned up by Felix")
+		})
+
+		It("should re-use pre-existing, valid files after a restart", func() {
+			By("creating a workload before Felix starts")
+			wl := workload.New(tc.Felixes[0], "workload-endpoint-status-tests-0", "default", "10.65.0.10", "8080", "tcp")
+			wl.ConfigureInInfra(infra)
+
+			By("determining the filename Felix will look for")
+			wKey, err := names.V3WorkloadEndpointToWorkloadEndpointKey(wl.WorkloadEndpoint)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(wKey).NotTo(BeNil(), "failed to create a workload endpoint key from a v3 workload endpoint")
+
+			filename := names.WorkloadEndpointKeyToStatusFilename(wKey)
+			Expect(filename).NotTo(HaveLen(0), "failed to create a status filename from a workload endpoint key")
+
+			By("creating a file with the determined name before Felix starts")
+			expectedFilename := filepath.Join("/tmp/endpoint-status", filename)
+			tc.Felixes[0].Exec("mkdir", "/tmp/endpoint-status")
+			tc.Felixes[0].Exec("touch", expectedFilename)
+			// This stat call returns the time since epoch when the file was last accessed.
+			lastAccessed, err := tc.Felixes[0].ExecOutput("stat", "--format='%y'", expectedFilename)
+			Expect(err).NotTo(HaveOccurred(), "stat call failed while trying to create a file")
+
+			By("waiting for Felix's status file reporter to become in-sync")
+			tc.Felixes[0].TriggerDelayedStart()
+			Eventually(dataplaneInSyncReceivedC, "10s").Should(BeClosed(), "receipt of DataplaneInSync message not seen in logs")
+
+			By("checking if the pre-existing file's access time changed")
+			checkLastAccessed := func() string {
+				lastAccessedPostStartup, _ := tc.Felixes[0].ExecOutput("stat", "--format='%y'", expectedFilename)
+				return lastAccessedPostStartup
+			}
+			Consistently(checkLastAccessed, "3s").Should(BeEquivalentTo(lastAccessed), "felix modified/deleted a file it didn't need to, or the test and Felix expected differing filenames")
+		})
+	})
+
+})
---
+++ felix/statusrep/status_file_reporter.go
@@ -0,0 +1,348 @@
+// Copyright (c) 2024 Tigera, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package statusrep
+
+import (
+	"context"
+	"errors"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"time"
+
+	"k8s.io/apimachinery/pkg/util/wait"
+
+	"github.com/projectcalico/calico/felix/deltatracker"
+	"github.com/projectcalico/calico/felix/proto"
+	"github.com/projectcalico/calico/libcalico-go/lib/names"
+
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	dirStatus = "endpoint-status"
+)
+
+// EndpointStatusFileReporter writes a file to the FS
+// any time it sees an Endpoint go up in the dataplane.
+//
+//   - Currently only writes to a directory "status", creating
+//     an entry for each workload, when each workload's
+//     policy is programmed for the first time.
+type EndpointStatusFileReporter struct {
+	endpointUpdatesC        <-chan interface{}
+	endpointStatusDirPrefix string
+
+	// DeltaTracker for the policy subdirectory
+	statusDirDeltaTracker *deltatracker.SetDeltaTracker[string]
+	inSyncWithUpstream    bool
+
+	// Wraps and manages a real or mock wait.Backoff.
+	bom backoffManager
+	// Interval between maintainence re-applies when dataplane is healthy.
+	reapplyInterval time.Duration
+
+	hostname string
+}
+
+// Backoff wraps a timer-based-retry type which can be stepped.
+type Backoff interface {
+	Step() time.Duration
+}
+
+// backoffManager wraps and manages a real or mock wait.Backoff.
+type backoffManager struct {
+	Backoff
+	newBackoffFunc func() Backoff
+}
+
+// newBackoffManager creates a backoffManager which uses the
+// passed func to create backoffs.
+func newBackoffManager(newBackoffFunc func() Backoff) backoffManager {
+	return backoffManager{
+		Backoff:        newBackoffFunc(),
+		newBackoffFunc: newBackoffFunc,
+	}
+}
+
+// Reset the manager's backoff to its original state.
+func (bom *backoffManager) reset() {
+	bom.Backoff = bom.newBackoffFunc()
+}
+
+// FileReporterOption allows modification of a new EndpointStatusFileReporter.
+type FileReporterOption func(*EndpointStatusFileReporter)
+
+// NewEndpointStatusFileReporter creates a new EndpointStatusFileReporter.
+func NewEndpointStatusFileReporter(
+	endpointUpdatesC <-chan interface{},
+	statusDirPath string,
+	opts ...FileReporterOption,
+) *EndpointStatusFileReporter {
+
+	sr := &EndpointStatusFileReporter{
+		endpointUpdatesC:        endpointUpdatesC,
+		endpointStatusDirPrefix: statusDirPath,
+		statusDirDeltaTracker:   deltatracker.NewSetDeltaTracker[string](),
+		bom:                     newBackoffManager(newDefaultBackoff),
+		hostname:                "",
+		reapplyInterval:         10 * time.Second,
+	}
+
+	for _, o := range opts {
+		o(sr)
+	}
+
+	return sr
+}
+
+// WithNewBackoffFunc returns a FileReporterOption which alters the backoff
+// used by the reporter's backoff manager.
+func WithNewBackoffFunc(newBackoffFunc func() Backoff) FileReporterOption {
+	return func(fr *EndpointStatusFileReporter) {
+		fr.bom = newBackoffManager(newBackoffFunc)
+	}
+}
+
+// WithHostname instructs the reporter to use the give hostname
+// when creating endpoint structures.
+func WithHostname(hostname string) FileReporterOption {
+	return func(fr *EndpointStatusFileReporter) {
+		fr.hostname = hostname
+	}
+}
+
+// SyncForever blocks until ctx is cancelled.
+// Continuously pulls status-updates from updates C,
+// and reconciles the filesystem with internal state.
+func (fr *EndpointStatusFileReporter) SyncForever(ctx context.Context) {
+	// State flags influencing the loop.
+	// Each flag triggers one behaviour within the loop.
+	// Once the behaviour succeeds, the flag is switched off.
+	// If the behaviour fails, the flag is left on, and a retry is queued.
+	var retryTimerResetNeeded, resyncWithKernelNeeded bool
+
+	// Timer channels are stored separately from the timer, and nilled-out when not needed.
+	var retry, scheduledReapply *time.Timer
+	var retryC, scheduledReapplyC <-chan time.Time
+	logrus.Debug("Endpoint status file reporter running.")
+	for {
+		select {
+		case <-ctx.Done():
+			logrus.Debug("Context cancelled, cleaning up and stopping endpoint status file reporter...")
+			fr.drainTimer(retry)
+			fr.drainTimer(scheduledReapply)
+			return
+
+		case e, ok := <-fr.endpointUpdatesC:
+			if !ok {
+				logrus.Panic("Input channel closed unexpectedly")
+			}
+
+			switch e.(type) {
+			case *proto.DataplaneInSync:
+				logrus.Debug("DataplaneInSync received from upstream.")
+				fr.inSyncWithUpstream = true
+				resyncWithKernelNeeded = true
+
+			default:
+				logrus.WithField("endpoint", e).Debug("Handling endpoint update")
+				fr.handleEndpointUpdate(e)
+			}
+
+		case <-retryC:
+		case <-scheduledReapplyC:
+			resyncWithKernelNeeded = true
+		}
+
+		if resyncWithKernelNeeded {
+			err := fr.resyncDataplaneWithKernel()
+			if err != nil {
+				logrus.WithError(err).Warn("Encountered an error while resyncing state with kernel. Queueing retry...")
+				retryTimerResetNeeded = true
+			} else {
+				resyncWithKernelNeeded = false
+			}
+		}
+
+		if fr.inSyncWithUpstream {
+			// There may be no updates to apply here,
+			// but that case will be close to a no-op.
+			err := fr.reconcileStatusFiles()
+			if err != nil {
+				logrus.WithError(err).Warn("Encountered one or more errors while reconciling endpoint status dir. Queueing retry...")
+				retryTimerResetNeeded = true
+				resyncWithKernelNeeded = true
+			}
+		}
+
+		// Timer leak-protection; stop timers check if we need to drain timer channels.
+		fr.drainTimer(retry)
+		fr.drainTimer(scheduledReapply)
+
+		// Retry should be stopped in all cases by now, and retryC drained.
+		// It's safe now to reset / nil-out resources.
+		if retryTimerResetNeeded {
+			retry = fr.resetStoppedTimerOrInit(retry, fr.bom.Step())
+			retryC = retry.C
+			retryTimerResetNeeded = false
+		} else {
+			// No need to nil-out our local timer channel var, as it's guaranteed to be drained anyway.
+			fr.bom.reset()
+		}
+
+		// If we're in-sync and healthy (no retry queued), queue a periodic resync.
+		// Useful in-case a 3rd-party is interfering with the dataplane underneath us.
+		if fr.inSyncWithUpstream && retryC == nil {
+			scheduledReapply = fr.resetStoppedTimerOrInit(scheduledReapply, fr.reapplyInterval)
+			scheduledReapplyC = scheduledReapply.C
+		}
+	}
+}
+
+func (fr *EndpointStatusFileReporter) drainTimer(t *time.Timer) {
+	if t == nil || t.Stop() {
+		return
+	}
+	select {
+	// Case where timer was not already drained.
+	case <-t.C:
+		// Case where timer was already drained.
+	default:
+	}
+}
+
+// Passed timer must be guaranteed stopped and drained if not-nil.
+func (fr *EndpointStatusFileReporter) resetStoppedTimerOrInit(t *time.Timer, d time.Duration) *time.Timer {
+	if t == nil {
+		return time.NewTimer(d)
+	}
+	_ = t.Reset(d)
+	return t
+}
+
+// A sub-call of SyncForever, not intended to be called outside the main loop.
+// Updates delta tracker state to match the received update.
+// Logs and discards errors generated from converting endpoint updates to endpoint keys.
+func (fr *EndpointStatusFileReporter) handleEndpointUpdate(e interface{}) {
+	switch m := e.(type) {
+	case *proto.WorkloadEndpointStatusUpdate:
+		if m.Id == nil {
+			logrus.WithField("update", m).Warn("Couldn't handle nil WorkloadEndpointStatusUpdate")
+			return
+		}
+		key := names.WorkloadEndpointIDToWorkloadEndpointKey(m.Id, fr.hostname)
+		fn := names.WorkloadEndpointKeyToStatusFilename(key)
+		fr.statusDirDeltaTracker.Desired().Add(fn)
+	case *proto.WorkloadEndpointStatusRemove:
+		if m.Id == nil {
+			logrus.WithField("update", m).Warn("Couldn't handle nil WorkloadEndpointStatusRemove")
+			return
+		}
+		key := names.WorkloadEndpointIDToWorkloadEndpointKey(m.Id, fr.hostname)
+		fn := names.WorkloadEndpointKeyToStatusFilename(key)
+		fr.statusDirDeltaTracker.Desired().Delete(fn)
+	default:
+		logrus.WithField("update", e).Debug("Skipping undesired endpoint update")
+	}
+}
+
+// Overwrites our user-space representation of the kernel with a fresh snapshot.
+func (fr *EndpointStatusFileReporter) resyncDataplaneWithKernel() error {
+	// Load any pre-existing committed dataplane entries.
+	entries, err := ensureStatusDir(fr.endpointStatusDirPrefix)
+	if err != nil {
+		return err
+	}
+
+	return fr.statusDirDeltaTracker.Dataplane().ReplaceFromIter(func(f func(k string)) error {
+		for _, entry := range entries {
+			f(entry.Name())
+		}
+		return nil
+	})
+}
+
+// A sub-call of SyncForever. Not intended to be called outside the main loop.
+// Applies pending updates and deletes pending deletions.
+func (fr *EndpointStatusFileReporter) reconcileStatusFiles() error {
+	var lastError error
+	fr.statusDirDeltaTracker.PendingUpdates().Iter(func(name string) deltatracker.IterAction {
+		err := fr.writeStatusFile(name)
+		if err != nil {
+			if !errors.Is(err, fs.ErrExist) {
+				lastError = err
+				return deltatracker.IterActionNoOp
+			}
+		}
+		return deltatracker.IterActionUpdateDataplane
+	})
+
+	fr.statusDirDeltaTracker.PendingDeletions().Iter(func(name string) deltatracker.IterAction {
+		err := fr.deleteStatusFile(name)
+		if err != nil {
+			// Carry on as normal (with a warning) if the file is somehow already deleted.
+			if !errors.Is(err, fs.ErrNotExist) {
+				lastError = err
+				return deltatracker.IterActionNoOp
+			}
+			logrus.WithField("file", name).Warn("Ignoring error; attempted to delete file which does not exist")
+		}
+		return deltatracker.IterActionUpdateDataplane
+	})
+
+	return lastError
+}
+
+func (fr *EndpointStatusFileReporter) writeStatusFile(name string) error {
+	// Write file to dir.
+	logrus.WithField("filename", name).Debug("Writing endpoint-status file to status-dir")
+	filename := filepath.Join(fr.endpointStatusDirPrefix, dirStatus, name)
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	return f.Close()
+}
+
+func (fr *EndpointStatusFileReporter) deleteStatusFile(name string) error {
+	filename := filepath.Join(fr.endpointStatusDirPrefix, dirStatus, name)
+	return os.Remove(filename)
+}
+
+// ensureStatusDir ensures there is a directory named "endpoint-status", within
+// the parent dir specified by prefix. Attempts to create the dir if it doesn't exist.
+// Returns all entries within the dir if any exist.
+func ensureStatusDir(prefix string) (entries []fs.DirEntry, err error) {
+	filename := filepath.Join(prefix, dirStatus)
+
+	entries, err = os.ReadDir(filename)
+	if err != nil && errors.Is(err, fs.ErrNotExist) {
+		// Discard ErrNotExist and return the result of attempting to create it.
+		return entries, os.Mkdir(filename, fs.FileMode(0644))
+	}
+
+	return entries, err
+}
+
+func newDefaultBackoff() Backoff {
+	return &wait.Backoff{
+		Duration: 50 * time.Millisecond,
+		Factor:   10,
+		Jitter:   0,
+		Steps:    3,
+		Cap:      5 * time.Second,
+	}
+}
