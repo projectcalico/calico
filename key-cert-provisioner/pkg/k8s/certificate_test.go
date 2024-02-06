8f0a766f97cdbf14464ab9933b4548d764cb3df9
rene-dekker <rene@tigera.io>
2024-02-06 15:31:46 -0800

    Refactor Makefile after moving to monorepo

--- key-cert-provisioner/pkg/k8s/certificate_test.go
+++ key-cert-provisioner/pkg/k8s/certificate_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 Tigera, Inc. All rights reserved.
+// Copyright (c) 2024 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
 	"github.com/projectcalico/calico/key-cert-provisioner/pkg/cfg"
 	"github.com/projectcalico/calico/key-cert-provisioner/pkg/k8s"
 	"github.com/projectcalico/calico/key-cert-provisioner/pkg/tls"
