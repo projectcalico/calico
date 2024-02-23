0628528fbaf9288b814d4702e40e3d340351e0fa
Daniel Fox <dan.fox@tigera.io>
2024-02-23 10:49:34 -0800

    Remove (never-seen) progress & handle HTTP errors

--- hack/postrelease/tests/utilities.py
+++ hack/postrelease/tests/utilities.py
@@ -5,7 +5,6 @@ from functools import cache
 
 import pytest
 import requests
-import rich.progress
 import yaml
 
 import variables
@@ -15,31 +14,15 @@ HELM_CHART = "https://projectcalico.docs.tigera.io/charts/index.yaml"
 def skip_if_master(reason):
     return pytest.mark.skipif(variables.RELEASE_STREAM == "master", reason=reason)
 
-class ResponseMissingContentDispositionError(Exception):
+class FailedDownloadError(Exception):
     pass
-
-def get_filename_from_url(url):
-    url_path = urllib.parse.urlparse(url).path
-    filename = pathlib.Path(url_path).name
-    return filename
-
-
-def get_content_disposition(response):
-    filename = response.headers["Content-Disposition"].replace(
-        "attachment; filename=", ""
-    )
-    return filename
-
 
 def download_url_to_file(url, destination):
     response = requests.get(url, stream=True)
-    length = int(response.headers["Content-Length"])
-    filename = get_content_disposition(response)
-    with rich.progress.wrap_file(
-        response.raw, total=length, description=filename
-    ) as infile:
-        with open(destination, "wb") as outfile:
-            shutil.copyfileobj(infile, outfile)
+    if response.status_code != 200:
+        raise FailedDownloadError(f"HTTP error {response.status_code}")
+    with open(destination, "wb") as outfile:
+        shutil.copyfileobj(response.raw, outfile)
 
 
 def download_url_to_file_noprogress(url, destination):
