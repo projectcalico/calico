580245b59f1bfafdcbf97e89bc073e36774039da
JinXinWang <opencmit2@126.com>
2024-03-29 09:11:56 +0800

    Add the command to determine whether the system has netstat
    Signed-off-by: JinXinWang <opencmit2@126.com>

--- calicoctl/calicoctl/commands/node/diags.go
+++ calicoctl/calicoctl/commands/node/diags.go
@@ -118,50 +118,30 @@ func runDiags(logDir string) error {
 	}
 	diagsTmpDir := filepath.Join(tmpDir, "diagnostics")
 
-	netstatCmd := diagCmd{
-		{"Dumping netstat", "netstat -a -n", "netstat"},
-	}
-
-	ssCmd := diagCmd{
-		{"Dumping ss", "ss -a -n", "ss"},
+	netstatCmd := diagCmd{  
+		info:     "Dumping netstat",  
+		cmd:      "netstat -a -n",  
+		filename: "netstat",  
+	}  
+	  
+	ssCmd := diagCmd{  
+		info:     "Dumping ss",  
+		cmd:      "ss -a -n",  
+		filename: "ss",  
 	}
 	
-	if _, err := exec.LookPath("netstat"); err == nil {
+	// sometimes socket information is not collected as netstat tool
+	// is obsolete and removed in Ubuntu and other distros. so when
+	// "netstat -a -n " fails, we should use "ss -a -n" instead of it
+	if _, err := exec.LookPath(netstatCmd.filename); err == nil {
 		cmds = append(cmds, netstatCmd)
-	}else {
+	} else {
 		cmds = append(cmds, ssCmd)
 	}
 
 	for _, v := range cmds {
 		_ = writeDiags(v, diagsTmpDir)
 	}
-
-	// for _, v := range cmds {
-	// 	if v.cmd != "netstat -a -n" {
-	// 		_ = writeDiags(v, diagsTmpDir)
-	// 		continue
-	// 	}
-
-	// 	if _, err := exec.LookPath("netstat"); err == nil {
-	// 		_ = writeDiags(v, diagsTmpDir)
-	// 	} else {
-	// 		// sometimes socket information is not collected as netstat tool
-	// 		// is obsolete and removed in Ubuntu and other distros. so when
-	// 		// "netstat -a -n " fails, we should use "ss -a -n" instead of it
-	// 		parts := []string{"ss", "-a", "-n"}
-	// 		content, err := exec.Command(parts[0], parts[1], parts[2]).CombinedOutput()
-	// 		if err != nil {
-	// 			fmt.Printf("Failed to run command: %s\nError: %s\n", strings.Join(parts, " "), string(content))
-	// 		} else {
-	// 			fmt.Println("Dumping ss")
-	// 		}
-
-	// 		fp := filepath.Join(diagsTmpDir, parts[0])
-	// 		if err := os.WriteFile(fp, content, 0666); err != nil {
-	// 			log.Errorf("Error writing diags to file: %s\n", err)
-	// 		}
-	// 	}
-	// }
 
 	tmpLogDir := filepath.Join(diagsTmpDir, "logs")
 
