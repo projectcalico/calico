ba7c5deffd79405de1ea1319783f15e612f65a29
Daniel Fox <dan.fox@tigera.io>
2024-05-16 10:27:54 -0700

    Update postrelease tests for v3.28 (#8814)

---
+++ hack/postrelease/tests/conftest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import pytest
+import variables
+
+
+@pytest.fixture(scope="session", autouse=True)
+def log_global_env_facts(record_testsuite_property):
+    record_testsuite_property("Release Stream", variables.RELEASE_STREAM)
+    record_testsuite_property("Release Version", variables.RELEASE_VERSION)
+    record_testsuite_property("Flannel Version", variables.FLANNEL_VERSION)
+    record_testsuite_property("Operator Version", variables.OPERATOR_VERSION)
