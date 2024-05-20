6d4c09789fae78f6e0e535050708e76e11da09d1
Casey Davenport <davenport.cas@gmail.com>
2024-05-20 14:20:30 -0700

    Fix static checks

--- felix/nftables/table_test.go
+++ felix/nftables/table_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2022 Tigera, Inc. All rights reserved.
+// Copyright (c) 2017-2024 Tigera, Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
