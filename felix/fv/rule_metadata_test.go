83f6b6b45d2dd142568afa1e79088670a2ae470b
Casey Davenport <davenport.cas@gmail.com>
2024-05-24 16:11:39 -0700

    Updates to FV tests

--- felix/fv/rule_metadata_test.go
+++ felix/fv/rule_metadata_test.go
@@ -33,7 +33,6 @@ import (
 )
 
 var _ = Describe("Rule Metadata tests", func() {
-
 	var (
 		tc     infrastructure.TopologyContainers
 		client client.Interface
@@ -62,7 +61,6 @@ var _ = Describe("Rule Metadata tests", func() {
 	})
 
 	Context("With a GlobalNetworkPolicy with rule metadata", func() {
-
 		var gnp *api.GlobalNetworkPolicy
 
 		BeforeEach(func() {
@@ -77,13 +75,16 @@ var _ = Describe("Rule Metadata tests", func() {
 			Expect(err).NotTo(HaveOccurred())
 		})
 
-		It("should add comments to iptables", func() {
-			Eventually(getIPTables(tc.Felixes[0].Name)).Should(ContainSubstring("-m comment --comment \"fvtest=gnp\""))
+		It("should add comments to the dataplane", func() {
+			if NFTMode() {
+				Eventually(getNFTables(tc.Felixes[0])).Should(ContainSubstring("fvtest=gnp"))
+			} else {
+				Eventually(getIPTables(tc.Felixes[0].Name)).Should(ContainSubstring("-m comment --comment \"fvtest=gnp\""))
+			}
 		})
 	})
 
 	Context("With a Profile with rule metadata", func() {
-
 		var p *api.Profile
 
 		BeforeEach(func() {
@@ -97,13 +98,16 @@ var _ = Describe("Rule Metadata tests", func() {
 			Expect(err).NotTo(HaveOccurred())
 		})
 
-		It("should add comments to iptables", func() {
-			Eventually(getIPTables(tc.Felixes[0].Name)).Should(ContainSubstring("-m comment --comment \"fvtest=profile\""))
+		It("should add comments to the dataplane", func() {
+			if NFTMode() {
+				Eventually(getNFTables(tc.Felixes[0])).Should(ContainSubstring("fvtest=profile"))
+			} else {
+				Eventually(getIPTables(tc.Felixes[0].Name)).Should(ContainSubstring("-m comment --comment \"fvtest=profile\""))
+			}
 		})
 	})
 
 	Context("With a NetworkPolicy with rule metadata", func() {
-
 		var np *api.NetworkPolicy
 
 		BeforeEach(func() {
@@ -120,13 +124,15 @@ var _ = Describe("Rule Metadata tests", func() {
 		})
 
 		It("should add comments to iptables", func() {
-			Eventually(getIPTables(tc.Felixes[0].Name)).Should(ContainSubstring("-m comment --comment \"fvtest=networkpolicy\""))
+			if NFTMode() {
+				Eventually(getNFTables(tc.Felixes[0])).Should(ContainSubstring("fvtest=networkpolicy"))
+			} else {
+				Eventually(getIPTables(tc.Felixes[0].Name)).Should(ContainSubstring("-m comment --comment \"fvtest=networkpolicy\""))
+			}
 		})
-
 	})
 
 	Context("With a Profile with rule metadata including newlines and unicode", func() {
-
 		var p *api.Profile
 
 		BeforeEach(func() {
@@ -145,7 +151,8 @@ var _ = Describe("Rule Metadata tests", func() {
 				Action: api.Allow,
 				Metadata: &api.RuleMetadata{Annotations: map[string]string{
 					"hometown": "Småland",
-					"random":   string(rv)}},
+					"random":   string(rv),
+				}},
 			}}
 			_, err = client.Profiles().Create(utils.Ctx, p, utils.NoOptions)
 			Expect(err).NotTo(HaveOccurred())
@@ -153,9 +160,15 @@ var _ = Describe("Rule Metadata tests", func() {
 
 		It("should add comments to iptables", func() {
 			// Felix replaces anything other than "safe" shell characters with _
-			Eventually(getIPTables(tc.Felixes[0].Name)).Should(ContainSubstring("-m comment --comment \"foo=hello_world\""))
-			Eventually(getIPTables(tc.Felixes[0].Name)).Should(ContainSubstring("-m comment --comment \"hometown=Sm_land\""))
-			Eventually(getIPTables(tc.Felixes[0].Name)).Should(ContainSubstring("-m comment --comment \"random="))
+			if NFTMode() {
+				Eventually(getNFTables(tc.Felixes[0])).Should(ContainSubstring("foo=hello_world"))
+				Eventually(getNFTables(tc.Felixes[0])).Should(ContainSubstring("hometown=Sm_land"))
+				Eventually(getNFTables(tc.Felixes[0])).Should(ContainSubstring("random="))
+			} else {
+				Eventually(getIPTables(tc.Felixes[0].Name)).Should(ContainSubstring("-m comment --comment \"foo=hello_world\""))
+				Eventually(getIPTables(tc.Felixes[0].Name)).Should(ContainSubstring("-m comment --comment \"hometown=Sm_land\""))
+				Eventually(getIPTables(tc.Felixes[0].Name)).Should(ContainSubstring("-m comment --comment \"random="))
+			}
 		})
 
 		// This test case verifies that "interesting" annotations like newlines and unicode don't break
@@ -175,6 +188,14 @@ func getIPTables(name string) func() string {
 	return func() string {
 		cmd := utils.Command("docker", "exec", name, "iptables-save", "-c")
 		out, err := cmd.Output()
+		Expect(err).ToNot(HaveOccurred())
+		return string(out)
+	}
+}
+
+func getNFTables(f *infrastructure.Felix) func() string {
+	return func() string {
+		out, err := f.ExecOutput("nft", "list", "table", "calico")
 		Expect(err).ToNot(HaveOccurred())
 		return string(out)
 	}
