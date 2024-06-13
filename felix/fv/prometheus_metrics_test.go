cbe3a7b35727912ba0aca6e1fe120950d0eeade2
Alex O'Regan <alex.oregan@tigera.io>
2024-06-13 11:50:43 +0100

    bump metrics tests timeouts

--- felix/fv/prometheus_metrics_test.go
+++ felix/fv/prometheus_metrics_test.go
@@ -76,7 +76,7 @@ var _ = infrastructure.DatastoreDescribe("_BPF-SAFE_ Prometheus metrics tests", 
 
 		It("should not serve debug paths on the prometheus port", func() {
 			// Wait for server to come up.
-			Eventually(tc.Felixes[0].PromMetric("felix_resync_state").Int).Should(Equal(3 /*in-sync*/))
+			Eventually(tc.Felixes[0].PromMetric("felix_resync_state").Int, "5s").Should(Equal(3 /*in-sync*/))
 
 			metricsServer := fmt.Sprintf("http://%s:%d/", tc.Felixes[0].IP, metrics.Port)
 			debugServer := fmt.Sprintf("http://%s:%d/", tc.Felixes[0].IP, 6061)
