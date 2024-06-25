03fbef438c0dd5ee8b84c6681a58c295907e5537
tuti <tuti@tigera.io>
2024-06-24 22:45:19 -0700

    additional cleanup

--- build/main.go
+++ build/main.go
@@ -4,17 +4,15 @@ import (
 	"github.com/projectcalico/fixham/pkg/api"
 )
 
-// Calico is a struct that represents projectcalico
+// ProjectCalicoBuilder is a struct that represents projectcalico
 //
 // This will be used to define the tasks for the projectcalico
 // such as cutting new branch, hashrelease and release
-type Calico struct {
-	api.Component
+type ProjectCalicoBuilder struct {
+	api.Builder
 }
 
 func main() {
-	c := &Calico{
-		Component: *api.NewComponent("calico", "github.com/projectcalico/calico"),
-	}
+	c := api.NewBuilder("projectcalico", "github.com/projectcalico/calico")
 	c.Register()
 }
