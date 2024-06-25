26a358cdb01d03fb3ec10b239a0854ac29cc9e86
tuti <tuti@tigera.io>
2024-06-24 22:45:19 -0700

    additional cleanup

--- fixham/pkg/config/config.go
+++ fixham/pkg/config/config.go
@@ -2,13 +2,16 @@ package config
 
 type Config struct {
 	packageName    string
-	GoBuildVersion string `envconfig:"GO_BUILD_VERSION" default:"v0.91"`
+	GoBuildImage   string `envconfig:"GO_BUILD_IMAGE" default:"calico/go-build"`
+	GoBuildVersion string `envconfig:"GO_BUILD_VER" default:"v0.91"`
 	GitUseSSH      bool   `envconfig:"GIT_USE_SSH" default:"false"`
 	LintArgs       string `envconfig:"LINT_ARGS" default:"--max-issues-per-linter 0 --max-same-issues 0 --timeout 8m"`
 }
 
-func (c *Config) SetPackageName(packageName string) {
-	c.packageName = packageName
+func NewConfig(packageName string) *Config {
+	return &Config{
+		packageName: packageName,
+	}
 }
 
 func (c *Config) PackageName() string {
