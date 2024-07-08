53da624145e8a4807c5da14a32bec010e805dcea
Casey Davenport <davenport.cas@gmail.com>
2024-07-08 11:48:54 -0700

    Fix UT post merge

--- libcalico-go/lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
+++ libcalico-go/lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
@@ -45,7 +45,7 @@ const (
 )
 
 const (
-	numBaseFelixConfigs = 139
+	numBaseFelixConfigs = 140
 )
 
 var _ = Describe("Test the generic configuration update processor and the concrete implementations", func() {
