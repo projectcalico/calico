4e7ff712452988104bcf02f77baa3597484b17a6
Alex O'Regan <alex.oregan@tigera.io>
2024-07-10 13:59:57 +0100

    increase various FV timeouts causing CI failures [PRE-BPF-IPTABLES/CONNTRACK BUG WORKAROUND]

--- felix/fv/connectivity/conncheck.go
+++ felix/fv/connectivity/conncheck.go
@@ -1064,7 +1064,7 @@ func (pc *PersistentConnection) Start() error {
 	}
 	Eventually(func() error {
 		return pc.Runtime.ExecMayFail("stat", loopFile)
-	}, 5*time.Second, time.Second).Should(
+	}, 65*time.Second, time.Second).Should(
 		HaveOccurred(),
 		"Failed to wait for test-connection to be ready, the loop file did not disappear",
 	)
