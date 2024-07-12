2aaa11d669c5b97655b3474895dc341a031e4bfe
Casey Davenport <caseydavenport@users.noreply.github.com>
2024-07-12 12:13:01 -0700

    [nftables] Limit concurrent set reads (#8996)

--- felix/nftables/ipsets.go
+++ felix/nftables/ipsets.go
@@ -410,12 +410,23 @@ func (s *IPSets) tryResync() error {
 	setsChan := make(chan setData)
 	defer close(setsChan)
 
-	// Start a goroutine to list the elements of each set.
+	// Start a goroutine to list the elements of each set. Limit concurrent set reads to
+	// avoid spawning too many goroutines if there are a large number of sets.
+	routineLimit := make(chan struct{}, 100)
+	defer close(routineLimit)
 	for _, setName := range sets {
+		// Wait for room in the limiting channel.
+		routineLimit <- struct{}{}
+
+		// Start a goroutine to read this set.
 		go func(name string) {
+			// Make sure to indicate that we're done by removing ourselves from the limiter channel.
+			defer func() { <-routineLimit }()
+
 			elems, err := s.nft.ListElements(ctx, "set", name)
 			if err != nil {
 				setsChan <- setData{setName: name, err: err}
+				return
 			}
 			strElems := []string{}
 			for _, e := range elems {
@@ -444,7 +455,7 @@ func (s *IPSets) tryResync() error {
 		logCxt := s.logCxt.WithField("setName", setName)
 		if setData.err != nil {
 			logCxt.WithError(err).Error("Failed to list set elements.")
-			return err
+			return setData.err
 		}
 
 		metadata, ok := s.setNameToAllMetadata[setName]
