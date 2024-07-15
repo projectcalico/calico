ee4c6bb360542df3c6f61736910b4c599c34179c
Daniel Fox <dan.fox@tigera.io>
2024-07-15 15:45:50 -0700

    Initial commit of some postrelease tests

---
+++ hack/postrelease/golang/pkg/container/container.go
@@ -0,0 +1,38 @@
+package container
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+)
+
+type Image struct {
+	Name string
+	Tag  string
+}
+
+func (i Image) FullPath() string {
+	return fmt.Sprintf("%s:%s", i.Name, i.Tag)
+}
+
+func (i Image) NameWithTag() string {
+	return fmt.Sprintf("%s:%s", i.Name, i.Tag)
+}
+
+func (i Image) GetManifest() string {
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+	containers, err := cli.ContainerList(context.Background(), container.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, ctr := range containers {
+		fmt.Printf("%s %s\n", ctr.ID, ctr.Image)
+	}
+	return "food"
+}
