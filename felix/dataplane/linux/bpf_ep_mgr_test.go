41788346248878072afb21ac874567981e090aae
sridhar <sridhar@tigera.io>
2024-07-23 12:44:09 -0700

    Add v4 and v6 routes for UDP service when in dual stack mode.

--- felix/dataplane/linux/bpf_ep_mgr_test.go
+++ felix/dataplane/linux/bpf_ep_mgr_test.go
@@ -1066,21 +1066,25 @@ var _ = Describe("BPF Endpoint Manager", func() {
 	})
 
 	Describe("bpfnatip", func() {
+		JustBeforeEach(func() {
+			newBpfEpMgr(true)
+		})
 		It("should program the routes reflecting service state", func() {
 			bpfEpMgr.OnUpdate(&proto.ServiceUpdate{
-				Name:      "service",
-				Namespace: "test",
-				ClusterIp: "1.2.3.4",
+				Name:       "service",
+				Namespace:  "test",
+				ClusterIps: []string{"1.2.3.4", "1::2"},
 			})
 			err := bpfEpMgr.CompleteDeferredWork()
 			Expect(err).NotTo(HaveOccurred())
-			Expect(dp.routes).To(HaveLen(1))
+			Expect(dp.routes).To(HaveLen(2))
 			Expect(dp.routes).To(HaveKey(ip.MustParseCIDROrIP("1.2.3.4")))
+			Expect(dp.routes).To(HaveKey(ip.MustParseCIDROrIP("1::2")))
 
 			bpfEpMgr.OnUpdate(&proto.ServiceUpdate{
 				Name:           "service",
 				Namespace:      "test",
-				ClusterIp:      "1.2.3.4",
+				ClusterIps:     []string{"1.2.3.4"},
 				LoadbalancerIp: "5.6.7.8",
 			})
 			err = bpfEpMgr.CompleteDeferredWork()
@@ -1092,7 +1096,7 @@ var _ = Describe("BPF Endpoint Manager", func() {
 			bpfEpMgr.OnUpdate(&proto.ServiceUpdate{
 				Name:           "service",
 				Namespace:      "test",
-				ClusterIp:      "1.2.3.4",
+				ClusterIps:     []string{"1.2.3.4"},
 				LoadbalancerIp: "5.6.7.8",
 				ExternalIps:    []string{"1.0.0.1", "1.0.0.2"},
 			})
@@ -1103,9 +1107,9 @@ var _ = Describe("BPF Endpoint Manager", func() {
 			Expect(dp.routes).To(HaveKey(ip.MustParseCIDROrIP("5.6.7.8")))
 
 			bpfEpMgr.OnUpdate(&proto.ServiceUpdate{
-				Name:      "service",
-				Namespace: "test",
-				ClusterIp: "1.2.3.4",
+				Name:       "service",
+				Namespace:  "test",
+				ClusterIps: []string{"1.2.3.4"},
 			})
 			err = bpfEpMgr.CompleteDeferredWork()
 			Expect(err).NotTo(HaveOccurred())
@@ -1123,7 +1127,7 @@ var _ = Describe("BPF Endpoint Manager", func() {
 			bpfEpMgr.OnUpdate(&proto.ServiceUpdate{
 				Name:           "service",
 				Namespace:      "test",
-				ClusterIp:      "1.2.3.4",
+				ClusterIps:     []string{"1.2.3.4"},
 				LoadbalancerIp: "5.6.7.8",
 			})
 			err = bpfEpMgr.CompleteDeferredWork()
