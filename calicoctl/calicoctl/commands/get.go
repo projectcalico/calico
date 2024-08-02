2c8a78a9c54e306262c1c9d4f340164638dc1254
Casey Davenport <caseydavenport@users.noreply.github.com>
2024-08-02 13:44:01 -0700

    [release-v3.28] Auto pick #9054: Build resource lists from registered resources (#9095)

--- calicoctl/calicoctl/commands/get.go
+++ calicoctl/calicoctl/commands/get.go
@@ -15,11 +15,11 @@
 package commands
 
 import (
-	"github.com/docopt/docopt-go"
-
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/docopt/docopt-go"
 
 	log "github.com/sirupsen/logrus"
 
@@ -74,21 +74,7 @@ Description:
 
   Valid resource types are:
 
-    * bgpConfiguration
-    * bgpPeer
-    * felixConfiguration
-    * globalNetworkPolicy
-    * globalNetworkSet
-    * hostEndpoint
-    * ipPool
-    * ipReservation
-    * kubeControllersConfiguration
-    * networkPolicy
-    * networkSet
-    * node
-    * profile
-    * workloadEndpoint
-
+<RESOURCE_LIST>
   The resource type is case-insensitive and may be pluralized.
 
   Attempting to get resources that do not exist will simply return no results.
@@ -126,6 +112,9 @@ Description:
 	// Replace all instances of BINARY_NAME with the name of the binary.
 	name, _ := util.NameAndDescription()
 	doc = strings.ReplaceAll(doc, "<BINARY_NAME>", name)
+
+	// Replace <RESOURCE_LIST> with the list of resource types.
+	doc = strings.Replace(doc, "<RESOURCE_LIST>", util.Resources(), 1)
 
 	// -a option Backward compatibility
 	for k, v := range args {
