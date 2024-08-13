d757ad58495cc89aeeb17e3118a8bd0e12775a5d
Shaun Crampton <shaun@tigera.io>
2024-08-13 11:59:50 +0100

    Stop calling logrus.SetNoLock
    It causes a race in Felix's set-up due to starting background
    goroutines (which log)  before we finish configuring logging.

--- felix/logutils/logutils.go
+++ felix/logutils/logutils.go
@@ -140,11 +140,6 @@ func ConfigureLogging(configParams *config.Config) {
 	// hook above to fan out logs to multiple destinations.
 	log.SetOutput(&logutils.NullWriter{})
 
-	// Since we push our logs onto a second thread via a channel, we can disable the
-	// Logger's built-in mutex completely.
-	// FIXME: disabled due to causing a race in felix's set-up.
-	//log.StandardLogger().SetNoLock()
-
 	// Do any deferred error logging.
 	if fileDirErr != nil {
 		log.WithError(fileDirErr).WithField("file", configParams.LogFilePath).
