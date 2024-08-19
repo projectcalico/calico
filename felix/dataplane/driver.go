ba0e76670c4000a27ffa81d55e1b05bd096e79f2
Casey Davenport <davenport.cas@gmail.com>
2024-08-19 13:40:57 -0700

    Add methods for easier value determination

--- felix/dataplane/driver.go
+++ felix/dataplane/driver.go
@@ -83,7 +83,7 @@ func StartDataplaneDriver(configParams *config.Config,
 			log.Panic("Starting dataplane with nil callback func.")
 		}
 
-		allowedMarkBits := configParams.IptablesMarkMask
+		allowedMarkBits := configParams.MarkMask()
 		if configParams.BPFEnabled {
 			// In BPF mode, the BPF programs use mark bits that are not configurable.  Make sure that those
 			// bits are covered by our allowed mask.
@@ -92,7 +92,7 @@ func StartDataplaneDriver(configParams *config.Config,
 					"Name":            "felix-iptables",
 					"MarkMask":        allowedMarkBits,
 					"RequiredBPFBits": tcdefs.MarksMask,
-				}).Panic("IptablesMarkMask doesn't cover bits that are used (unconditionally) by eBPF mode.")
+				}).Panic("IptablesMarkMask/NftablesMarkMask doesn't cover bits that are used (unconditionally) by eBPF mode.")
 			}
 			allowedMarkBits ^= allowedMarkBits & tcdefs.MarksMask
 			log.WithField("updatedBits", allowedMarkBits).Info(
@@ -199,14 +199,6 @@ func StartDataplaneDriver(configParams *config.Config,
 			felixNodeZone = felixNode.Labels[coreV1.LabelTopologyZone]
 		}
 
-		// Configure some fields depending on dataplane mode.
-		tableRefreshInterval := configParams.IptablesRefreshInterval
-
-		nftablesEnabled := configParams.NFTablesMode == "Enabled"
-		if nftablesEnabled {
-			tableRefreshInterval = configParams.NFTablesRefreshInterval
-		}
-
 		dpConfig := intdataplane.Config{
 			Hostname:           felixHostname,
 			NodeZone:           felixNodeZone,
@@ -217,7 +209,7 @@ func StartDataplaneDriver(configParams *config.Config,
 				NetlinkTimeout:    configParams.NetlinkTimeoutSecs,
 			},
 			RulesConfig: rules.Config{
-				NFTables:              nftablesEnabled,
+				NFTables:              configParams.NFTablesMode == "Enabled",
 				WorkloadIfacePrefixes: configParams.InterfacePrefixes(),
 
 				IPSetConfigV4: ipsets.NewIPVersionConfig(
@@ -273,9 +265,9 @@ func StartDataplaneDriver(configParams *config.Config,
 
 				IptablesLogPrefix:         configParams.LogPrefix,
 				EndpointToHostAction:      configParams.DefaultEndpointToHostAction,
-				IptablesFilterAllowAction: configParams.IptablesFilterAllowAction,
-				IptablesMangleAllowAction: configParams.IptablesMangleAllowAction,
-				IptablesFilterDenyAction:  configParams.IptablesFilterDenyAction,
+				IptablesFilterAllowAction: configParams.FilterAllowAction(),
+				IptablesMangleAllowAction: configParams.MangleAllowAction(),
+				IptablesFilterDenyAction:  configParams.FilterDenyAction(),
 
 				FailsafeInboundHostPorts:  configParams.FailsafeInboundHostPorts,
 				FailsafeOutboundHostPorts: configParams.FailsafeOutboundHostPorts,
@@ -312,7 +304,7 @@ func StartDataplaneDriver(configParams *config.Config,
 			VXLANMTUV6:                     configParams.VXLANMTUV6,
 			VXLANPort:                      configParams.VXLANPort,
 			IptablesBackend:                configParams.IptablesBackend,
-			TableRefreshInterval:           configParams.IptablesRefreshInterval,
+			TableRefreshInterval:           configParams.TableRefreshInterval(),
 			RouteSyncDisabled:              configParams.RouteSyncDisabled,
 			RouteRefreshInterval:           configParams.RouteRefreshInterval,
 			DeviceRouteSourceAddress:       configParams.DeviceRouteSourceAddress,
