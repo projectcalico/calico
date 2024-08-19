1d86efbb8e5e5005ff941952754e3e04f87d310d
Casey Davenport <davenport.cas@gmail.com>
2024-08-19 11:43:03 -0700

    Use individual fields

--- libcalico-go/lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
+++ libcalico-go/lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
@@ -45,7 +45,7 @@ const (
 )
 
 const (
-	numBaseFelixConfigs = 141
+	numBaseFelixConfigs = 145
 )
 
 var _ = Describe("Test the generic configuration update processor and the concrete implementations", func() {
