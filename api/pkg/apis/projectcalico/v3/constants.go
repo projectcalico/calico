8ff0abcb9275ad3c583e976e648cbaf89526c3cf
Mazdak Nasab <mazdak.nasab@gmail.com>
2024-08-20 17:36:37 -0700

    remove unused lable

--- api/pkg/apis/projectcalico/v3/constants.go
+++ api/pkg/apis/projectcalico/v3/constants.go
@@ -28,10 +28,6 @@ const (
 	// AllNames is used for List or Watch queries to wildcard the name.
 	AllNames = ""
 
-	// Label used to denote the Name. This is added to network sets by Calico and may be used for
-	// label matches by Policy selectors.
-	LabelName = "projectcalico.org/name"
-
 	// Label used to denote the Namespace.  This is added to workload endpoints and network sets by Calico
 	// and may be used for label matches by Policy selectors.
 	LabelNamespace = "projectcalico.org/namespace"
