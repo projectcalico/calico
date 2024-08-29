1e036b8e674486095145d5ac2725f0dc1dd0bd24
Mazdak Nasab <mazdak.nasab@gmail.com>
2024-08-29 11:11:12 -0700

    fix typo

--- libcalico-go/lib/resources/tier.go
+++ libcalico-go/lib/resources/tier.go
@@ -20,8 +20,8 @@ import (
 
 func DefaultTierFields(res *apiv3.Tier) {
 	// nil order was allowed before, and it was used for the default tier.
-	// For the implementation of BaselineAdminNetworkPolicy, we needed to add a tier
-	// after the default one. As such, the default tier order changed to 100,000 from ni.
+	// For the implementation of BaselineAdminNetworkPolicy, we need to add a tier
+	// before the default one. As such, the default tier order is changed to 100,000 from nil.
 	// To keep the behavior in sync with user defined tiers with nil order, nil order is
 	// treated similar to the value of 100,000.
 	if res.Spec.Order == nil {
