c3814f9fa6f207c1225db2c763a79e10f0a77d30
Mazdak Nasab <mazdak.nasab@gmail.com>
2024-09-03 09:41:27 -0700

    Initial commit

--- libcalico-go/lib/backend/k8s/conversion/constants.go
+++ libcalico-go/lib/backend/k8s/conversion/constants.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021 Tigera, Inc. All rights reserved.
+// Copyright (c) 2017-2024 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ const (
 	NamespaceLabelPrefix            = "pcns."
 	NamespaceProfileNamePrefix      = "kns."
 	K8sNetworkPolicyNamePrefix      = "knp.default."
+	K8sAdminNetworkPolicyNamePrefix = "kanp.anp."
 	ServiceAccountLabelPrefix       = "pcsa."
 	ServiceAccountProfileNamePrefix = "ksa."
 
