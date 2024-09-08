9d675a0c41feb4c4a628301f833de59a5ddde87b
Mazdak Nasab <mazdak.nasab@gmail.com>
2024-09-07 17:29:04 -0700

    fix formating

--- libcalico-go/lib/backend/k8s/conversion/adminnetworkpolicy_test.go
+++ libcalico-go/lib/backend/k8s/conversion/adminnetworkpolicy_test.go
@@ -22,13 +22,14 @@ import (
 
 	apiv3 "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
 	"github.com/projectcalico/api/pkg/lib/numorstring"
-	"github.com/projectcalico/calico/libcalico-go/lib/backend/model"
-	cerrors "github.com/projectcalico/calico/libcalico-go/lib/errors"
-	"github.com/projectcalico/calico/libcalico-go/lib/names"
 	kapiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	adminpolicy "sigs.k8s.io/network-policy-api/apis/v1alpha1"
+
+	"github.com/projectcalico/calico/libcalico-go/lib/backend/model"
+	cerrors "github.com/projectcalico/calico/libcalico-go/lib/errors"
+	"github.com/projectcalico/calico/libcalico-go/lib/names"
 )
 
 var _ = Describe("Test AdminNetworkPolicy conversion", func() {
