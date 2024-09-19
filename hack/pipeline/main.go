4db9c211c8f82f47635b4be5bc21f01ff7d36935
tuti <tuti@tigera.io>
2024-09-19 11:19:14 -0700

    update based on feedback

--- hack/pipeline/main.go
+++ hack/pipeline/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"net/http"
 	"os"
@@ -12,6 +13,20 @@ import (
 
 	"github.com/sirupsen/logrus"
 )
+
+var (
+	dir   string
+	org   string
+	token string
+	debug bool
+)
+
+func init() {
+	flag.StringVar(&dir, "dir", ".", "directory to search for YAML files")
+	flag.StringVar(&org, "organization", os.Getenv("SEMAPHORE_ORGANIZATION"), "Semaphore organization")
+	flag.StringVar(&token, "token", "", "Semaphore API token")
+	flag.BoolVar(&debug, "debug", false, "enable debug logging")
+}
 
 func getSemaphoreDirs(dir string) ([]string, error) {
 	var dirs []string
@@ -27,13 +42,15 @@ func getSemaphoreDirs(dir string) ([]string, error) {
 	return dirs, err
 }
 
-func getYAMLFiles(dir string) ([]string, error) {
+func getPipelineYAMLFiles(dir string) ([]string, error) {
 	var files []string
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		// Skip the YAML .semaphore/semaphore.yml.d directory
+		// as it contains building blocks which are not full pipeline definitions
+		// The resulting pipeline will be validated as part of semaphore.yml and semaphore-scheduled-builds.yml
 		if info.IsDir() && strings.HasSuffix(path, "semaphore.yml.d") {
 			return filepath.SkipDir
 		}
@@ -45,7 +62,7 @@ func getYAMLFiles(dir string) ([]string, error) {
 	return files, err
 }
 
-func validateYAML(file string) error {
+func validateYAML(file, org, token string) error {
 	logrus.WithField("file", file).Info("validating YAML")
 	content, err := os.ReadFile(file)
 	if err != nil {
@@ -60,13 +77,13 @@ func validateYAML(file string) error {
 		logrus.WithError(err).Error("failed to marshal payload for yaml validation")
 		return err
 	}
-	req, err := http.NewRequest(http.MethodPost, "https://tigera.semaphoreci.com/api/v1alpha/yaml", bytes.NewBuffer(data))
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("https://%s.semaphoreci.com/api/v1alpha/yaml", org), bytes.NewBuffer(data))
 	if err != nil {
 		logrus.WithError(err).Error("failed to create request for yaml validation")
 		return err
 	}
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("SEMAPHORE_API_TOKEN")))
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		logrus.WithError(err).Error("failed to make request for yaml validation")
@@ -81,17 +98,38 @@ func validateYAML(file string) error {
 		logrus.WithError(err).Error("failed to decode response for yaml validation")
 		return err
 	}
-	logrus.Info(result["message"].(string))
+	logrus.Debug(result["message"].(string))
 	return nil
 }
 
 func main() {
-	cmd := exec.Command("git", "rev-parse", "--show-toplevel")
-	out, err := cmd.Output()
-	if err != nil {
-		logrus.WithError(err).Fatal("failed to get repo root dir")
+	flag.Parse()
+	if debug {
+		logrus.SetLevel(logrus.DebugLevel)
 	}
-	dir := strings.TrimSpace(string(out))
+	if org == "" {
+		logrus.Fatal("Semaphore organization is required, set the SEMAPHORE_ORGANIZATION environment variable or use the -organization flag")
+	}
+	if token == "" {
+		if os.Getenv("SEMAPHORE_API_TOKEN") == "" {
+			logrus.Fatal("Semaphore API token is required, set the SEMAPHORE_API_TOKEN environment variable or use the -token flag")
+		} else {
+			token = os.Getenv("SEMAPHORE_API_TOKEN")
+		}
+	}
+	if dir == "" {
+		logrus.Warn("no directory specified, using git repository root")
+		cmd := exec.Command("git", "rev-parse", "--show-toplevel")
+		out, err := cmd.Output()
+		if err != nil {
+			logrus.WithError(err).Fatal("failed to get repo root dir")
+		}
+		dir = strings.TrimSpace(string(out))
+	}
+	dir, err := filepath.Abs(dir)
+	if err != nil {
+		logrus.WithField("dir", dir).WithError(err).Fatal("failed to get absolute path to directory")
+	}
 	semaphoreDirs, err := getSemaphoreDirs(dir)
 	logrus.WithField("semaphoreDirs", semaphoreDirs).Debug("found semaphore directories")
 	if err != nil {
@@ -99,7 +137,7 @@ func main() {
 	}
 	var yamlFiles []string
 	for _, semaphoreDir := range semaphoreDirs {
-		files, err := getYAMLFiles(semaphoreDir)
+		files, err := getPipelineYAMLFiles(semaphoreDir)
 		if err != nil {
 			logrus.WithError(err).Errorf("failed to get YAML files in %s", semaphoreDir)
 			continue
@@ -110,10 +148,10 @@ func main() {
 		logrus.Info("no YAML files found")
 		return
 	}
-	logrus.WithField("yamlFiles", yamlFiles).Debug("found YAML files")
+	logrus.Infof("found %d YAML files", len(yamlFiles))
 	var failedFiles []string
 	for _, file := range yamlFiles {
-		err = validateYAML(file)
+		err = validateYAML(file, org, token)
 		if err != nil {
 			logrus.WithError(err).Error("invalid YAML definition")
 			failedFiles = append(failedFiles, file)
