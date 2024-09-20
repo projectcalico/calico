b99ee14cd44ca5a27ca172c7abafbfacec1789d0
sridhar <sridhar@tigera.io>
2024-09-20 11:31:46 -0700

    Dump ipset IDs in bpf policy dump

--- felix/bpf/polprog/pol_prog_builder.go
+++ felix/bpf/polprog/pol_prog_builder.go
@@ -527,6 +527,10 @@ func (p *Builder) writePolicyRules(policy Policy, actionLabels map[string]string
 	for ruleIdx, rule := range policy.Rules {
 		log.Debugf("Start of rule %d", ruleIdx)
 		p.b.AddCommentF("Start of rule %s", rule)
+		ipsets := p.printIPSetIDs(rule)
+		if ipsets != "" {
+			p.b.AddCommentF("IPSets %s", p.printIPSetIDs(rule))
+		}
 		p.b.AddCommentF("Rule MatchID: %d", rule.MatchID)
 		action := strings.ToLower(rule.Action)
 		if action == "log" {
@@ -891,6 +895,40 @@ func (p *Builder) writeCIDRSMatch(negate bool, leg matchLeg, cidrs []string) {
 		// Label the next match so we can skip to it on success.
 		p.b.LabelNextInsn(onMatchLabel)
 	}
+}
+
+func (p *Builder) printIPSetIDs(r Rule) string {
+	str := ""
+	concatIDs := func(ipSets []string) string {
+		idString := ""
+		for idx, ipSetID := range ipSets {
+			id := p.ipSetIDProvider.GetNoAlloc(ipSetID)
+			if id != 0 {
+				idString = idString + fmt.Sprintf("0x%x", id)
+				if idx < len(r.SrcIpSetIds)-1 {
+					idString = idString + ","
+				}
+			}
+		}
+		return idString
+	}
+	srcIDString := concatIDs(r.SrcIpSetIds)
+	if srcIDString != "" {
+		str = str + fmt.Sprintf("src_ip_set_ids:<%s> ", srcIDString)
+	}
+	notSrcIDString := concatIDs(r.NotSrcIpSetIds)
+	if notSrcIDString != "" {
+		str = str + fmt.Sprintf("not_src_ip_set_ids:<%s> ", notSrcIDString)
+	}
+	dstIDString := concatIDs(r.DstIpSetIds)
+	if dstIDString != "" {
+		str = str + fmt.Sprintf("dst_ip_set_ids:<%s> ", dstIDString)
+	}
+	notDstIDString := concatIDs(r.NotDstIpSetIds)
+	if notDstIDString != "" {
+		str = str + fmt.Sprintf("not_dst_ip_set_ids:<%s> ", notDstIDString)
+	}
+	return str
 }
 
 func (p *Builder) writeIPSetMatch(negate bool, leg matchLeg, ipSets []string) {
