e918683e4d5c327ac50763278e3b264c32b15839
tscogin <tscogin@bloomberg.net>
2024-09-26 10:56:21 -0400

    Allow changes to annotations
    Signed-off-by: tscogin <tscogin@bloomberg.net>

--- networking-calico/networking_calico/plugins/ml2/drivers/calico/test/test_plugin_etcd.py
+++ networking-calico/networking_calico/plugins/ml2/drivers/calico/test/test_plugin_etcd.py
@@ -121,6 +121,9 @@ class _TestEtcdBase(lib.Lib, unittest.TestCase):
                 if 'labels' in self.recent_writes[key]['metadata']:
                     existing_v3_metadata['labels'] = \
                         self.recent_writes[key]['metadata']['labels']
+                elif 'annotations' in self.recent_writes[key]['metadata']:
+                    existing_v3_metadata['annotations'] = \
+                        self.recent_writes[key]['metadata']['annotations']
                 self.assertEqual(existing_v3_metadata,
                                  self.recent_writes[key]['metadata'])
             # Now delete not-easily-predictable metadata fields from the data
