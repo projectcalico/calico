5ede16120ce7887f7a4e5a706c8648462e973deb
Alex O'Regan <alex.oregan@tigera.io>
2024-10-01 11:03:51 +0100

    Use go-test-compatible Gomega, and go-test table, rather than Ginkgo.

--- pod2daemon/csidriver/driver/driver_test.go
+++ pod2daemon/csidriver/driver/driver_test.go
@@ -6,75 +6,106 @@ import (
 	"fmt"
 	"io/fs"
 	"os"
+	"testing"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 )
 
-type podInfoTestSetup func(filename, contents string)
+func testRetrievePodInfoFromFile(g *WithT, setup podInfoTestSetup, volumeID, credsJSON string, checkErr validateReturnError) {
+	err := setup(volumeID, credsJSON)
+	g.Expect(err).NotTo(HaveOccurred(), "Test setup failed")
 
-var _ = DescribeTable("Test func: retrievePodInfoFromFile", testRetrievePodInfoFromFile,
-	Entry("Test valid JSON",
-		createRealTempJSONFile,
-		"volume0",
-		`{
+	nodeServiceConfig := ConfigurationOptions{
+		NodeAgentCredentialsHomeDir: "/tmp",
+	}
+	nodeService := &nodeService{&nodeServiceConfig}
+	_, err = nodeService.retrievePodInfoFromFile(volumeID)
+	g.Expect(checkErr(err)).NotTo(HaveOccurred(), "Error check failed")
+}
+
+var testTableRetrievePodInfoFromFile = []struct {
+	description string
+	volumeID    string
+	credsJSON   string
+	setupTest   podInfoTestSetup
+	validate    validateReturnError
+}{
+	{
+		description: "Test valid JSON",
+		volumeID:    "volume0",
+		credsJSON: `{
 			"uid": "abc-def-123-456",
 			"workload": "test-workload",
 			"namespace": "test-ns",
 			"serviceAccount": "test-sa"
 		}`,
-		func(err error) error {
-			return err
-		},
-	),
-	Entry("Test invalid JSON",
-		createRealTempJSONFile,
-		"volumeBadJSONFIle",
-		`{
+		setupTest: createRealTempJSONFile,
+		validate:  expectNoError,
+	},
+	{
+		description: "Test invalid JSON",
+		volumeID:    "volumeBadJSONFIle",
+		credsJSON: `{
 			sfljksflkjsdf
 		}`,
-		func(err error) error {
+		setupTest: createRealTempJSONFile,
+		validate: func(err error) error {
+			// Expect to receive a json.SyntaxError.
 			se := &json.SyntaxError{}
 			if err == nil || !errors.As(err, &se) {
 				return fmt.Errorf("Expected json.SyntaxError, but got: %w", err)
 			}
 			return nil
 		},
-	),
-	Entry("Test missing creds file",
-		dontCreateCredsFile,
-		"volumeMissingFilename", ``,
-		func(err error) error {
+	},
+	{
+		description: "Test missing creds file",
+		volumeID:    "volumeMissingFilename",
+		credsJSON:   ``,
+		setupTest:   dontCreateCredsFile,
+		validate: func(err error) error {
 			if err == nil || !errors.Is(err, fs.ErrNotExist) {
 				return fmt.Errorf("Expected fs.ErrNotExist, but got: %w", err)
 			}
 			return nil
 		},
-	),
-)
-
-func testRetrievePodInfoFromFile(setup podInfoTestSetup, volumeID, credsJSON string, checkErr func(error) error) {
-	setup(volumeID, credsJSON)
-
-	nodeServiceConfig := ConfigurationOptions{
-		NodeAgentCredentialsHomeDir: "/tmp",
-	}
-	nodeService := &nodeService{&nodeServiceConfig}
-	_, err := nodeService.retrievePodInfoFromFile(volumeID)
-	Expect(checkErr(err)).NotTo(HaveOccurred(), "Error check failed")
+	},
 }
 
-func createRealTempJSONFile(name, contents string) {
+func TestRetrievePodInfoFromFile(t *testing.T) {
+	for _, tt := range testTableRetrievePodInfoFromFile {
+		t.Run(tt.description, func(t *testing.T) {
+			g := NewWithT(t)
+			testRetrievePodInfoFromFile(g, tt.setupTest, tt.volumeID, tt.credsJSON, tt.validate)
+		})
+	}
+}
+
+// Create (or dont) the podInfo test file at start of test.
+type podInfoTestSetup func(filename, contents string) error
+
+// Validate the returned-error from the function under test.
+// If the correct error is received, return nil, otherwise return the error, optionally wrapped.
+type validateReturnError func(err error) error
+
+func expectNoError(err error) error {
+	return err
+}
+
+func createRealTempJSONFile(name, contents string) error {
 	tmpFile, err := os.Create("/tmp/" + name + ".json")
-	Expect(err).NotTo(HaveOccurred(), "Test setup failed - couldn't create a temp file")
+	if err != nil {
+		return fmt.Errorf("Couldn't create temp JSON file: %w", err)
+	}
 	defer tmpFile.Close()
 
-	b, err := tmpFile.Write([]byte(contents))
-	Expect(err).NotTo(HaveOccurred(), "Test setup failed - couldn't write to pod-info file")
+	_, err = tmpFile.Write([]byte(contents))
+	if err != nil {
+		return fmt.Errorf("Couldn't write to temp JSON file: %w", err)
+	}
 
-	By(fmt.Sprintf("Writing %d bytes to %s", b, tmpFile.Name()))
+	return nil
 }
 
 // For testing file-not-exists error handling.
-func dontCreateCredsFile(_, _ string) {}
+func dontCreateCredsFile(_, _ string) error { return nil }
