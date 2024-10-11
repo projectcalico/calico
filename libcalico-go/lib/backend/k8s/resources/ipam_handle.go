4172691f354d5f3dabe13760b58680fb8175e974
Jiawei Huang <jiawei@tigera.io>
2024-10-11 09:28:52 -0700

    Don't capitalize error strings

--- libcalico-go/lib/backend/k8s/resources/ipam_handle.go
+++ libcalico-go/lib/backend/k8s/resources/ipam_handle.go
@@ -190,7 +190,7 @@ func (c *ipamHandleClient) Get(ctx context.Context, key model.Key, revision stri
 		if _, err := c.DeleteKVP(ctx, v1kvp); err != nil {
 			return nil, err
 		}
-		return nil, cerrors.ErrorResourceDoesNotExist{Err: errors.New("Resource was deleted"), Identifier: key}
+		return nil, cerrors.ErrorResourceDoesNotExist{Err: errors.New("resource was deleted"), Identifier: key}
 	}
 
 	return v1kvp, nil
