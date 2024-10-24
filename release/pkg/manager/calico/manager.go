f46485f0479fd77a2887e7ab757f29f318986f16
tuti <tuti@tigera.io>
2024-10-24 15:00:05 -0700

    fix errors and update git usage

--- release/pkg/manager/calico/manager.go
+++ release/pkg/manager/calico/manager.go
@@ -327,7 +327,7 @@ func (r *CalicoManager) PreReleaseValidate(ver string) error {
 	out, err := r.git("describe", "--exact-match", "--tags", "HEAD")
 	if err == nil {
 		// On a current tag.
-		return fmt.Errorf("Already on a tag (%s), refusing to create release", out)
+		return fmt.Errorf("already on a tag (%s), refusing to create release", out)
 	}
 
 	// Check that the repository is not a shallow clone. We need correct history.
@@ -336,7 +336,7 @@ func (r *CalicoManager) PreReleaseValidate(ver string) error {
 		return fmt.Errorf("rev-parse failed: %s", err)
 	}
 	if strings.TrimSpace(out) == "true" {
-		return fmt.Errorf("Attempt to release from a shallow clone is not possible")
+		return fmt.Errorf("attempt to release from a shallow clone is not possible")
 	}
 
 	// Assert that manifests are using the correct version.
@@ -360,7 +360,7 @@ func (r *CalicoManager) PreReleaseValidate(ver string) error {
 func (r *CalicoManager) DeleteTag(ver string) error {
 	_, err := r.git("tag", "-d", ver)
 	if err != nil {
-		return fmt.Errorf("Failed to delete tag: %s", err)
+		return fmt.Errorf("failed to delete tag: %s", err)
 	}
 	return nil
 }
@@ -370,7 +370,7 @@ func (r *CalicoManager) TagRelease(ver string) error {
 	logrus.WithFields(logrus.Fields{"branch": branch, "version": ver}).Infof("Creating Calico release from branch")
 	_, err := r.git("tag", ver)
 	if err != nil {
-		return fmt.Errorf("Failed to tag release: %s", err)
+		return fmt.Errorf("failed to tag release: %s", err)
 	}
 	return nil
 }
@@ -424,31 +424,25 @@ func (r *CalicoManager) buildOCPBundle() error {
 }
 
 func (r *CalicoManager) PublishRelease() error {
-	// Determine the currently checked-out tag.
-	ver, err := r.git("describe", "--exact-match", "--tags", "HEAD")
-	if err != nil {
-		return fmt.Errorf("failed to get tag for checked-out commit, is there one? %s", err)
-	}
-
 	// Check that the environment has the necessary prereqs.
-	if err = r.publishPrereqs(); err != nil {
+	if err := r.publishPrereqs(); err != nil {
 		return err
 	}
 
 	// Publish container images.
-	if err = r.publishContainerImages(ver); err != nil {
+	if err := r.publishContainerImages(r.calicoVersion); err != nil {
 		return fmt.Errorf("failed to publish container images: %s", err)
 	}
 
 	if r.publishTag {
 		// If all else is successful, push the git tag.
-		if _, err = r.git("push", r.remote, ver); err != nil {
+		if _, err := r.git("push", r.remote, r.calicoVersion); err != nil {
 			return fmt.Errorf("failed to push git tag: %s", err)
 		}
 	}
 
 	// Publish the release to github.
-	if err = r.publishGithubRelease(ver); err != nil {
+	if err := r.publishGithubRelease(r.calicoVersion); err != nil {
 		return fmt.Errorf("failed to publish github release: %s", err)
 	}
 
@@ -460,14 +454,14 @@ func (r *CalicoManager) releasePrereqs() error {
 	// Check that we're not on the master branch. We never cut releases from master.
 	branch := r.determineBranch()
 	if branch == "master" {
-		return fmt.Errorf("Cannot cut release from branch: %s", branch)
+		return fmt.Errorf("cannot cut release from branch: %s", branch)
 	}
 
 	// Make sure we have a github token - needed for publishing to GH.
 	// Strictly only needed for publishing, but we check during release anyway so
 	// that we don't get all the way through the build to find out we're missing it!
 	if token := os.Getenv("GITHUB_TOKEN"); token == "" {
-		return fmt.Errorf("No GITHUB_TOKEN present in environment")
+		return fmt.Errorf("no GITHUB_TOKEN present in environment")
 	}
 
 	// TODO: Make sure the environment isn't dirty.
@@ -600,7 +594,7 @@ func (r *CalicoManager) buildReleaseTar(ver string, targetDir string) error {
 	releaseBase := filepath.Join(r.repoRoot, "release", "_output", fmt.Sprintf("release-%s", ver))
 	err := os.MkdirAll(releaseBase+"/images", os.ModePerm)
 	if err != nil {
-		return fmt.Errorf("Failed to create images dir: %s", err)
+		return fmt.Errorf("failed to create images dir: %s", err)
 	}
 	imgDir := filepath.Join(releaseBase, "images")
 	registry := r.imageRegistries[0]
@@ -624,7 +618,7 @@ func (r *CalicoManager) buildReleaseTar(ver string, targetDir string) error {
 	binDir := filepath.Join(releaseBase, "bin")
 	err = os.MkdirAll(binDir, os.ModePerm)
 	if err != nil {
-		return fmt.Errorf("Failed to create images dir: %s", err)
+		return fmt.Errorf("failed to create images dir: %s", err)
 	}
 
 	binaries := map[string]string{
@@ -691,7 +685,7 @@ func (r *CalicoManager) buildContainerImages(ver string) error {
 		out, err := r.makeInDirectoryWithOutput(filepath.Join(r.repoRoot, dir), "release-build", env...)
 		if err != nil {
 			logrus.Error(out)
-			return fmt.Errorf("Failed to build %s: %s", dir, err)
+			return fmt.Errorf("failed to build %s: %s", dir, err)
 		}
 		logrus.Info(out)
 	}
@@ -700,7 +694,7 @@ func (r *CalicoManager) buildContainerImages(ver string) error {
 		out, err := r.makeInDirectoryWithOutput(filepath.Join(r.repoRoot, dir), "image-windows", env...)
 		if err != nil {
 			logrus.Error(out)
-			return fmt.Errorf("Failed to build %s: %s", dir, err)
+			return fmt.Errorf("failed to build %s: %s", dir, err)
 		}
 		logrus.Info(out)
 	}
@@ -801,7 +795,7 @@ func (r *CalicoManager) publishContainerImages(ver string) error {
 					continue
 				}
 				logrus.Error(out)
-				return fmt.Errorf("Failed to publish %s: %s", dir, err)
+				return fmt.Errorf("failed to publish %s: %s", dir, err)
 			}
 
 			// Success - move on to the next directory.
@@ -820,7 +814,7 @@ func (r *CalicoManager) publishContainerImages(ver string) error {
 					continue
 				}
 				logrus.Error(out)
-				return fmt.Errorf("Failed to publish %s: %s", dir, err)
+				return fmt.Errorf("failed to publish %s: %s", dir, err)
 			}
 
 			// Success - move on to the next directory.
@@ -869,7 +863,7 @@ func (r *CalicoManager) assertManifestVersions(ver string) error {
 				continue
 			}
 			if !strings.HasSuffix(i, ver) {
-				return fmt.Errorf("Incorrect image version (expected %s) in manifest %s: %s", ver, m, i)
+				return fmt.Errorf("incorrect image version (expected %s) in manifest %s: %s", ver, m, i)
 			}
 		}
 	}
