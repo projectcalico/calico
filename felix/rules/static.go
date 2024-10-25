7b7b7d68941d59d659ad19e353d166628ad6806b
Shaun Crampton <shaun@tigera.io>
2024-10-25 10:10:17 +0100

    Fix that untracked policy mark clearance.
    Was using SetMark(0), which ends up using a mask of 0, so it
    is a no-op.

--- felix/rules/static.go
+++ felix/rules/static.go
@@ -1262,7 +1262,7 @@ func (r *DefaultRuleRenderer) StaticBPFModeRawChains(ipVersion uint8,
 		Rules: []generictables.Rule{
 			// At this point we know bypass mark is set, which means that the packet has
 			// been explicitly allowed by untracked ingress policy (XDP).  We should
-			// clear the mark so as not to affect any FROM_HOST processing.  (There
+			// clear the mark so as not to affect any FROM_HOST processing.  There
 			// shouldn't be any FROM_HOST processing, because untracked policy is only
 			// intended for traffic to/from the host.  But if the traffic is in fact
 			// forwarded and goes to or through another endpoint, it's better to enforce
@@ -1271,7 +1271,7 @@ func (r *DefaultRuleRenderer) StaticBPFModeRawChains(ipVersion uint8,
 			// logic because no one else's iptables should have had a chance to execute
 			// yet.
 			{
-				Action: r.SetMark(0),
+				Action: r.SetMaskedMark(0, tcdefs.MarksMask),
 			},
 			// Now ensure that the packet is not tracked.
 			{
