a9e86442ffcca7d8f6bc3cb7c746d810c1242da9
tuti <tuti@tigera.io>
2024-11-05 14:10:08 -0800

    fix publishing images for hashrelease
    - allow changing registry in pinnedversion
    - allowing skipping validation for publish
    - move hashrelease validation to calico manager
    - update .gitignore to ignore publishing identifier files
    - fix key-cert-provisioner publishing

--- release/pkg/tasks/hashrelease.go
+++ release/pkg/tasks/hashrelease.go
@@ -18,7 +18,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/sirupsen/logrus"
 
@@ -26,113 +25,9 @@ import (
 	"github.com/projectcalico/calico/release/internal/hashreleaseserver"
 	"github.com/projectcalico/calico/release/internal/imagescanner"
 	"github.com/projectcalico/calico/release/internal/pinnedversion"
-	"github.com/projectcalico/calico/release/internal/registry"
 	"github.com/projectcalico/calico/release/internal/slack"
 	"github.com/projectcalico/calico/release/internal/utils"
-	"github.com/projectcalico/calico/release/internal/version"
 )
-
-type imageExistsResult struct {
-	name   string
-	image  string
-	exists bool
-	err    error
-}
-
-func imgExists(name string, component registry.Component, ch chan imageExistsResult) {
-	r := imageExistsResult{
-		name:  name,
-		image: component.String(),
-	}
-	r.exists, r.err = registry.ImageExists(component.ImageRef())
-	ch <- r
-}
-
-// HashreleaseValidate validates the images in the hashrelease.
-// These images are checked to ensure they exist in the registry
-// as they should have been pushed in the standard build process.
-func HashreleaseValidate(cfg *config.Config, skipISS bool) error {
-	tmpDir := cfg.TmpFolderPath()
-	pinned, err := pinnedversion.RetrievePinnedVersion(tmpDir)
-	if err != nil {
-		logrus.WithError(err).Error("Failed to get release name")
-		return err
-	}
-	productBranch, err := utils.GitBranch(cfg.RepoRootDir)
-	if err != nil {
-		logrus.WithError(err).Errorf("Failed to get %s branch name", utils.ProductName)
-		return err
-	}
-	productVersion := pinned.Title
-	productStream := version.DeterminePublishStream(productBranch, productVersion)
-	images, err := pinnedversion.RetrieveComponentsToValidate(tmpDir)
-	if err != nil {
-		logrus.WithError(err).Error("Failed to get pinned version")
-	}
-	results := make(map[string]imageExistsResult, len(images))
-
-	ch := make(chan imageExistsResult)
-	for name, component := range images {
-		go imgExists(name, component, ch)
-	}
-	for range images {
-		res := <-ch
-		results[res.name] = res
-	}
-	failedImages := []registry.Component{}
-	failedImageNames := []string{}
-	for name, r := range results {
-		logrus.WithFields(logrus.Fields{
-			"image":  r.image,
-			"exists": r.exists,
-		}).Info("Validating image")
-		if r.err != nil || !r.exists {
-			logrus.WithError(r.err).WithField("image", name).Error("Error checking image")
-			failedImageNames = append(failedImageNames, name)
-			failedImages = append(failedImages, images[name])
-		} else {
-			logrus.WithField("image", name).Info("Image exists")
-		}
-	}
-	failedCount := len(failedImageNames)
-	if failedCount > 0 {
-		// We only care to send failure messages if we are in CI
-		if cfg.CI.IsCI {
-			slackMsg := slack.Message{
-				Config: cfg.SlackConfig,
-				Data: slack.MessageData{
-					ReleaseName:     pinned.ReleaseName,
-					Product:         utils.DisplayProductName(),
-					Stream:          productStream,
-					Version:         productVersion,
-					OperatorVersion: pinned.TigeraOperator.Version,
-					CIURL:           cfg.CI.URL(),
-					FailedImages:    failedImages,
-				},
-			}
-			if err := slackMsg.SendFailure(logrus.IsLevelEnabled(logrus.DebugLevel)); err != nil {
-				logrus.WithError(err).Error("Failed to send slack message")
-			}
-		}
-		logrus.WithField("images", strings.Join(failedImageNames, ", ")).
-			Errorf("Failed to validate %d images, see above for details", failedCount)
-		return fmt.Errorf("failed to validate %d images", failedCount)
-	}
-	if !skipISS {
-		logrus.Info("Sending images to ISS")
-		imageList := []string{}
-		for _, component := range images {
-			imageList = append(imageList, component.String())
-		}
-		imageScanner := imagescanner.New(cfg.ImageScannerConfig)
-		err := imageScanner.Scan(imageList, productStream, false, cfg.OutputDir)
-		if err != nil {
-			// Error is logged and ignored as this is not considered a fatal error
-			logrus.WithError(err).Error("Failed to scan images")
-		}
-	}
-	return nil
-}
 
 // HashreleasePublished checks if the hashrelease has already been published.
 // If it has, the process is halted.
@@ -152,7 +47,7 @@ func HashreleasePublished(cfg *config.Config, hash string) (bool, error) {
 
 // HashreleaseSlackMessage sends a slack message to notify that a hashrelease has been published.
 func HashreleaseSlackMessage(cfg *config.Config, hashrel *hashreleaseserver.Hashrelease) error {
-	scanResultURL := imagescanner.RetrieveResultURL(cfg.OutputDir)
+	scanResultURL := imagescanner.RetrieveResultURL(cfg.TmpFolderPath())
 	if scanResultURL == "" {
 		logrus.Warn("No image scan result URL found")
 	}
