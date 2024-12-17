2413c5675cb491889d5f42c4091cb6e9d44dbe5c
Tomas Hruby <tomas@tigera.io>
2024-12-16 16:44:46 -0800

    [BPF] make conntrack timeouts configurable
    BPFConntrackTimers overides the default values for the specified conntrack timer if
    set. It is a struct of values, where each value can be either a duration or `auto` to
    pick the value from a Linux conntrack timeout.
    
    Possible values for the keys are: CreationGracePeriod, TCPPreEstablished,
    TCPEstablished, TCPFinsSeen, TCPResetSeen, UDPLastSeen, GenericIPLastSeen,
    ICMPLastSeen.
    
    Unset or incorrect values are replaced by the default values with a warning log for
    incorrect values.
    
    Current auto mappings:
    
    TCPPreEstablished: nf_conntrack_tcp_timeout_syn_sent
    TCPEstablished:    nf_conntrack_tcp_timeout_established
    TCPFinsSeen:       nf_conntrack_tcp_timeout_time_wait
    GenericIPLastSeen: nf_conntrack_generic_timeout
    ICMPLastSeen:      nf_conntrack_icmp_timeout
    
    If there is no mapping, 'auto' is replaced by the default value.
    
    [Default:
         CreationGracePeriod: 10s
         TCPPreEstablished:   20s
         TCPEstablished:      1h
         TCPFinsSeen:         auto (30s is default)
         TCPResetSeen:        40s
         UDPLastSeen:         60s
         GenericIPLastSeen:   10m
         ICMPLastSeen:        5s
    ]

--- libcalico-go/lib/backend/syncersv1/updateprocessors/felixconfigprocessor.go
+++ libcalico-go/lib/backend/syncersv1/updateprocessors/felixconfigprocessor.go
@@ -41,6 +41,7 @@ func NewFelixConfigUpdateProcessor() watchersyncer.SyncerUpdateProcessor {
 			"RouteTableRange":           routeTableRangeToString,
 			"RouteTableRanges":          routeTableRangeListToString,
 			"HealthTimeoutOverrides":    healthTimeoutOverridesToString,
+			"BPFConntrackTimeouts":      bpfConntrackTimeoutsToString,
 		},
 	)
 }
@@ -98,4 +99,39 @@ func healthTimeoutOverridesToString(value interface{}) interface{} {
 		parts = append(parts, hto.Name+"="+hto.Timeout.Duration.String())
 	}
 	return strings.Join(parts, ",")
+}
+
+func structToKeyValueString(input interface{}) (string, error) {
+	// Get the type and value of the input struct
+	v := reflect.ValueOf(input)
+	t := reflect.TypeOf(input)
+
+	// Ensure the input is a struct
+	if t.Kind() != reflect.Struct {
+		return "", fmt.Errorf("input must be a struct")
+	}
+
+	// Build the key=value pairs
+	var parts []string
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		value := v.Field(i)
+
+		// Check if the field is exportable and is a string
+		if field.PkgPath == "" && value.Kind() == reflect.String {
+			s := value.String()
+			if s == "" {
+				continue
+			}
+
+			parts = append(parts, fmt.Sprintf("%s=%s", field.Name, s))
+		}
+	}
+
+	return strings.Join(parts, ","), nil
+}
+
+func bpfConntrackTimeoutsToString(value interface{}) interface{} {
+	res, _ := structToKeyValueString(value)
+	return res
 }
