90bc11e0cd11ad59dd29b157bf2fced2acd33bf5
Brian McMahon <brianmcmahon135@gmail.com>
2025-02-04 15:59:34 -0800

    Add copyright, remove unused encode / decode functions

--- lib/httpapimachinery/pkg/encoding/encoding.go
+++ lib/httpapimachinery/pkg/encoding/encoding.go
@@ -1,37 +1,44 @@
-// Copyright (c) 2024 Tigera, Inc. All rights reserved.
+// Copyright (c) 2025 Tigera, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package encoding
 
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/google/uuid"
 	"net/http"
-	"net/url"
 	"reflect"
 	"strings"
-	"unicode"
 
-	"github.com/google/uuid"
-
-	// TODO I don't think we want to import these utilities and instead leave it up to the user of the library
-	// TODO what muxer / logger they use. We'll need to provide some sort of interfacing for this though.
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/go-playground/form"
 	"github.com/go-playground/validator/v10"
+
+	"github.com/projectcalico/calico/lib/httpapimachinery/pkg/header"
 )
 
 const (
-	// TODO Probably want header constants in some other package or file and just have this package or file deal with
-	// TODO encoding / decoding (import the header values if needed).
-	HeaderContentType          = "Content-Type"
-	ContentTypeApplicationJson = "application/json"
-	ContentTypeTextCSV         = "text/csv"
+	tagURLPath  = "urlPath"
+	tagURLQuery = "urlQuery"
+	tagHeader   = "header"
+	tagJSON     = "json"
 )
 
 var (
-	//schemaDecoder *schema.Decoder
 	validate *validator.Validate
 
 	urlPathDecoder  *form.Decoder
@@ -51,18 +58,6 @@ var (
 			translated := fmt.Sprintf("Missing required field (%s).", fieldError.Field())
 			return translated
 		},
-		"min": func(fieldError validator.FieldError) string {
-			var translated string
-			switch fieldError.Type().Kind() {
-			case reflect.Slice, reflect.Array, reflect.Map:
-				translated = fmt.Sprintf("Number of elements is less than %s (%s).", fieldError.Param(), fieldError.Field())
-			case reflect.String:
-				translated = fmt.Sprintf("Length is less than %s (%s).", fieldError.Param(), fieldError.Field())
-			default:
-				translated = fmt.Sprintf("Value is less than %s (%s).", fieldError.Param(), fieldError.Field())
-			}
-			return translated
-		},
 	}
 )
 
@@ -74,7 +69,7 @@ func init() {
 	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
 		name := fld.Name
 
-		for _, tagName := range []string{"json", "urlPath", "urlQuery"} {
+		for _, tagName := range []string{tagJSON, tagURLPath, tagURLQuery, tagHeader} {
 			if _, ok := fld.Tag.Lookup(tagName); ok {
 				name = strings.SplitN(fld.Tag.Get(tagName), ",", 2)[0]
 				if name == "-" {
@@ -87,23 +82,11 @@ func init() {
 		return name
 	})
 
-	// TODO Probably just want to remove this from the default validation.
-	RegisterValidation("sha256digest", validateSHA256Digest, false, func(fieldError validator.FieldError) string {
-		translated := fmt.Sprintf("field \"%s\" is not a sha256 digest (must start with sha256:)", fieldError.Field())
-		return translated
-	})
-
-	// TODO Should we remove this from the default validation?
-	RegisterValidation("nouppercase", validateNoUpperCase, false, func(fieldError validator.FieldError) string {
-		translated := fmt.Sprintf("Cannot contain upper case letter (%s).", fieldError.Value())
-		return translated
-	})
-
 	urlPathDecoder = form.NewDecoder()
 	urlPathEncoder = form.NewEncoder()
-	// TODO obviously should be using constants or a variable for these tag names.
-	urlPathDecoder.SetTagName("urlPath")
-	urlPathEncoder.SetTagName("urlPath")
+
+	urlPathDecoder.SetTagName(tagURLPath)
+	urlPathEncoder.SetTagName(tagURLPath)
 
 	// ModeExplicit ensures that we don't try to parse structs that don't have the tag.
 	urlPathDecoder.SetMode(form.ModeExplicit)
@@ -111,18 +94,18 @@ func init() {
 
 	urlQueryDecoder = form.NewDecoder()
 	urlQueryEncoder = form.NewEncoder()
-	urlQueryDecoder.SetTagName("urlQuery")
-	urlQueryEncoder.SetTagName("urlQuery")
+	urlQueryDecoder.SetTagName(tagURLQuery)
+	urlQueryEncoder.SetTagName(tagURLQuery)
 	urlQueryDecoder.SetMode(form.ModeExplicit)
 	urlQueryEncoder.SetMode(form.ModeExplicit)
 
 	RegisterCustomDecodeTypeFunc(decodeUUID, uuid.UUID{})
-	RegisterCustomEncodeTypeFunc(encodeUUI, uuid.UUID{})
+	RegisterCustomEncodeTypeFunc(encodeUUID, uuid.UUID{})
 
 	headerDecoder = form.NewDecoder()
 	headerEncoder = form.NewEncoder()
-	headerDecoder.SetTagName("header")
-	headerEncoder.SetTagName("header")
+	headerDecoder.SetTagName(tagHeader)
+	headerEncoder.SetTagName(tagHeader)
 	headerDecoder.SetMode(form.ModeExplicit)
 	headerEncoder.SetMode(form.ModeExplicit)
 }
@@ -137,19 +120,7 @@ func RegisterCustomEncodeTypeFunc(fn form.EncodeCustomTypeFunc, types ...interfa
 	urlQueryEncoder.RegisterCustomTypeFunc(fn, types...)
 }
 
-// RegisterValidation wraps the validate.RegisterValidation function so that a messageFunc can also be registered with
-// the validation function.
-func RegisterValidation(tag string, validatorFunc validator.Func, callValidationEvenIfNull bool, messageFunc func(fieldError validator.FieldError) string) {
-	err := validate.RegisterValidation(tag, validatorFunc, callValidationEvenIfNull)
-	if err != nil {
-		panic(err)
-	}
-
-	validationMessageFunctions[tag] = messageFunc
-}
-
-// TODO not sure if this should be kept here, possible it could be made better? It's used to get an nicer error message
-// TODO to respond with when validation fails.
+// TODO Actually use this an make it better.
 func TranslateValidationErrors(errors validator.ValidationErrors) []string {
 	translations := make([]string, 0)
 	for _, fieldError := range errors {
@@ -163,7 +134,17 @@ func TranslateValidationErrors(errors validator.ValidationErrors) []string {
 	return translations
 }
 
-// TODO Probably want to keep this as a default.
+// RegisterValidation wraps the validate.RegisterValidation function so that a messageFunc can also be registered with
+// the validation function.
+func RegisterValidation(tag string, validatorFunc validator.Func, callValidationEvenIfNull bool, messageFunc func(fieldError validator.FieldError) string) {
+	err := validate.RegisterValidation(tag, validatorFunc, callValidationEvenIfNull)
+	if err != nil {
+		panic(err)
+	}
+
+	validationMessageFunctions[tag] = messageFunc
+}
+
 // decodeUUID is a form.Decoder decoding function that converts a string into a UUID.
 func decodeUUID(vals []string) (interface{}, error) {
 	if len(vals) >= 1 {
@@ -173,57 +154,13 @@ func decodeUUID(vals []string) (interface{}, error) {
 }
 
 // decodeUUID is a form.Decoder decoding function that converts a string into a UUID.
-func encodeUUI(obj interface{}) ([]string, error) {
+func encodeUUID(obj interface{}) ([]string, error) {
 	uid, ok := obj.(uuid.UUID)
 	if !ok {
 		return nil, fmt.Errorf("object is not a uuid.UUID")
 	}
 
 	return []string{uid.String()}, nil
-}
-
-// validateNoUpperCase is a validator.Validate validator function that checks that the string field isn't uppercase.
-func validateNoUpperCase(fl validator.FieldLevel) bool {
-	v := fl.Field()
-	var stringValue string
-	if v.Kind() == reflect.Ptr {
-		if v.IsNil() {
-			return true
-		}
-		stringValue = *(v.Interface().(*string))
-	} else {
-		stringValue = v.Interface().(string)
-	}
-	for _, r := range stringValue {
-		if unicode.IsLetter(r) && unicode.IsUpper(r) {
-			return false
-		}
-	}
-	return true
-}
-
-// validateSHA256Digest is a validator.Validate validator function that checks that the string field is in a sha256
-// digest format.
-func validateSHA256Digest(fl validator.FieldLevel) bool {
-	v := fl.Field()
-	var digest string
-	if v.Kind() == reflect.Ptr {
-		if v.IsNil() {
-			return true
-		}
-		digest = *(v.Interface().(*string))
-	} else {
-		digest = v.Interface().(string)
-	}
-
-	// It's possible that the digest is in the format of "registry.com/repo@sha256:digest" so we need to remove the
-	// registry and repo path if it exists and validate the rest.
-	repoPathAndDigest := strings.SplitN(digest, "@", 2)
-	if len(repoPathAndDigest) >= 2 {
-		digest = repoPathAndDigest[1]
-	}
-
-	return strings.HasPrefix(digest, "sha256:")
 }
 
 // DecodeAndValidateReqParams decodes the request in the specific RequestParam type, and validates the fields based on
@@ -233,8 +170,8 @@ func DecodeAndValidateReqParams[RequestParam any](req *http.Request) (*RequestPa
 	reqParams := new(RequestParam)
 
 	// Don't assume the body is json (or even available) if the json header content type isn't set.
-	content := strings.ToLower(strings.TrimSpace(req.Header.Get(HeaderContentType)))
-	if content == ContentTypeApplicationJson {
+	content := strings.ToLower(strings.TrimSpace(req.Header.Get(header.ContentType)))
+	if content == header.ApplicationJSON {
 		// TODO Consider a different pattern than decoding the entire body at once, it might be more prudent to return
 		// TODO some sort of decoding structure so caller isn't forced to load everything in memory at once while reading
 		// TODO the request. It may be able to process the request items and discard them from memory as it reads them,
@@ -254,57 +191,6 @@ func DecodeAndValidateReqParams[RequestParam any](req *http.Request) (*RequestPa
 	}
 
 	return reqParams, nil
-}
-
-// TODO this all need to be evaluated based on the current use cases. Image assurance uses them for various applications,
-// TODO such as encoding requests in the bast API client. I suspect there's some better patterns here.
-
-func EncodeToPathParameters(obj interface{}) (map[string]string, error) {
-	pathParams := map[string]string{}
-	values, err := urlPathEncoder.Encode(obj)
-	if err != nil {
-		return nil, err
-	}
-
-	for key, value := range values {
-		var val string
-		if len(value) > 0 {
-			val = value[0]
-		}
-		pathParams[key] = val
-	}
-
-	return pathParams, nil
-}
-
-func EncodeToPathParameterPairList(obj interface{}) ([]string, error) {
-	var list []string
-	values, err := urlPathEncoder.Encode(obj)
-	if err != nil {
-		return nil, err
-	}
-
-	for key, value := range values {
-		var val string
-		if len(value) > 0 {
-			val = value[0]
-		}
-		list = append(list, key, val)
-	}
-
-	return list, nil
-}
-
-func EncodeToQueryParameters(obj interface{}) (url.Values, error) {
-	return urlQueryEncoder.Encode(obj)
-}
-
-func EncodeToHeaders(obj interface{}) (url.Values, error) {
-	return headerEncoder.Encode(obj)
-}
-
-func DecodeURLQueryParameters(v interface{}, values url.Values) error {
-	return urlQueryDecoder.Decode(v, values)
 }
 
 func DecodeAndValidateURLParameters[T any](obj *T, header map[string][]string, path map[string]string, query map[string][]string) error {
