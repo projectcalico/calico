f3f940cf58d46a92b4ab49a0afe8500268745064
Mazdak Nasab <mazdak.nasab@gmail.com>
2025-02-11 11:11:48 -0800

    update copyrights

--- app-policy/checker/server_test.go
+++ app-policy/checker/server_test.go
@@ -1,12 +1,34 @@
-// Copyright (c) 2018-2022 Tigera, Inc. All rights reserved.
+// Copyright (c) 2018-2025 Tigera, Inc. All rights reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package checker
 
-// . "github.com/onsi/gomega"
+import (
+	"context"
+	"testing"
 
-// . "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 
-/*func TestCheckNoStore(t *testing.T) {
+	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	authz "github.com/envoyproxy/go-control-plane/envoy/service/auth/v3"
+	"github.com/projectcalico/calico/app-policy/policystore"
+	"github.com/projectcalico/calico/felix/proto"
+	"github.com/projectcalico/calico/felix/types"
+	"google.golang.org/genproto/googleapis/rpc/status"
+)
+
+func TestCheckNoStore(t *testing.T) {
 	RegisterTestingT(t)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -54,9 +76,9 @@ func TestCheckStore(t *testing.T) {
 		return rsp
 	}
 	Eventually(chk).Should(Equal(&authz.CheckResponse{Status: &status.Status{Code: OK}}))
-}*/
+}
 
-/*func TestCheckStoreNoHTTP(t *testing.T) {
+func TestCheckStoreNoHTTP(t *testing.T) {
 	RegisterTestingT(t)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -89,9 +111,9 @@ func TestCheckStore(t *testing.T) {
 		return rsp
 	}
 	Eventually(chk).Should(Equal(&authz.CheckResponse{Status: &status.Status{Code: OK}}))
-}*/
+}
 
-/*func TestCheckStoreHTTPAllowed(t *testing.T) {
+func TestCheckStoreHTTPAllowed(t *testing.T) {
 	RegisterTestingT(t)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -160,9 +182,9 @@ func TestCheckStore(t *testing.T) {
 		return rsp
 	}
 	Eventually(chk).Should(Equal(&authz.CheckResponse{Status: &status.Status{Code: OK}}))
-}*/
+}
 
-/*func TestCheckStoreHTTPDenied(t *testing.T) {
+func TestCheckStoreHTTPDenied(t *testing.T) {
 	RegisterTestingT(t)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -230,4 +252,4 @@ func TestCheckStore(t *testing.T) {
 		return rsp
 	}
 	Eventually(chk).Should(Equal(&authz.CheckResponse{Status: &status.Status{Code: PERMISSION_DENIED}}))
-}*/
+}
