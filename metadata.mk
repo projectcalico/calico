20bfb7eda8aa947169a94a798aac5656d64e9b2d
Jiawei Huang <jiawei@tigera.io>
2025-02-18 15:56:50 -0800

    Pin calico/base to release tags
    This change pins calico/base version to a particular tag instead of `latest`.
    It resolves issues during a (hash) release when non-amd64 components
    accidentally pull in amd64 bases.

--- metadata.mk
+++ metadata.mk
@@ -2,8 +2,10 @@
 # This file contains Makefile configuration parameters and metadata for this branch.
 #################################################################################################
 
-# The version of github.com/projectcalico/go-build to use.
+# The version of calico/go-build and calico/base to use.
 GO_BUILD_VER=1.24.0-llvm18.1.8-k8s1.31.5-2
+CALICO_BASE_VER=ubi8-1739912267
+
 # Env var to ACK Ginkgo deprecation warnings, may need updating with go-build.
 ACK_GINKGO=ACK_GINKGO_DEPRECATIONS=1.16.5
 
