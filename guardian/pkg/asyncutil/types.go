0cd524eb8c52c3ffb64d0bd1f8688d173853b416
Brian McMahon <brianmcmahon135@gmail.com>
2025-02-26 14:46:04 -0800

    add more docuementation

--- guardian/pkg/asyncutil/types.go
+++ guardian/pkg/asyncutil/types.go
@@ -1,10 +1,28 @@
+// Copyright (c) 2025 Tigera, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package asyncutil
 
+// Command represents a command to run asynchronously. It allows for providing paramters to be passed to whatever is
+// executing the command and has a channel to wait for the result on.
 type Command[P any, R any] struct {
 	params     P
 	resultChan chan Result[R]
 }
 
+// NewCommand creates a new command of the given type with the given parameter. It also returns the result channel that
+// the result will be written to when the command is executed.
 func NewCommand[P any, R any](params P) (Command[P, R], chan Result[R]) {
 	resultChan := make(chan Result[R], 1)
 	return Command[P, R]{params: params, resultChan: resultChan}, resultChan
@@ -38,6 +56,7 @@ func (c Command[C, R]) ReturnError(err error) {
 	}
 }
 
+// Signaler is an interface used for waiting for and sending simple signals.
 type Signaler interface {
 	Send()
 	Receive() <-chan struct{}
@@ -65,6 +84,7 @@ func NewSignaler() Signaler {
 	return &signaler{ch: make(chan struct{}, 1)}
 }
 
+// AsyncErrorBuffer is an error buffer that can be used in multiple routines.
 type AsyncErrorBuffer interface {
 	Write(err error)
 	Receive() <-chan error
