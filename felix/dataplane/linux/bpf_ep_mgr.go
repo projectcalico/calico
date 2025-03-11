d5c1bff98a178c49de5fa718c519bdbef1f4f7b9
Tomas Hruby <tomas@tigera.io>
2025-03-11 15:14:33 -0700

    [BPF] compile distinct programs for VXLAN
    We need to know that we are forwarding from and L2 overlay.

--- felix/dataplane/linux/bpf_ep_mgr.go
+++ felix/dataplane/linux/bpf_ep_mgr.go
@@ -2854,6 +2854,9 @@ func (m *bpfEndpointManager) getEndpointType(ifaceName string) tcdefs.EndpointTy
 	m.ifacesLock.Unlock()
 	switch ifaceType {
 	case IfaceTypeData, IfaceTypeVXLAN, IfaceTypeBond, IfaceTypeBondSlave:
+		if ifaceName == "vxlan.calico" || ifaceName == "vxlan-v6.calico" {
+			return tcdefs.EpTypeVXLAN
+		}
 		if ifaceName == "lo" {
 			return tcdefs.EpTypeLO
 		}
