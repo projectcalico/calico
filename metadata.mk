6a28fa19acd010c484df1a91cd342011c9535bdc
skoryk-oleksandr <skorichok88@gmail.com>
2025-03-12 16:43:26 -0700

    CORE-11149: Updating k8s, golang.org/x/crypto versions

--- metadata.mk
+++ metadata.mk
@@ -8,7 +8,7 @@ GO_BUILD_VER=v0.94
 ACK_GINKGO=ACK_GINKGO_DEPRECATIONS=1.16.5
 
 # Version of Kubernetes to use for tests, bitnami/kubectl, and kubectl binary release.
-K8S_VERSION=v1.30.10
+K8S_VERSION=v1.30.7
 
 # Version of various tools used in the build and tests.
 COREDNS_VERSION=1.5.2
