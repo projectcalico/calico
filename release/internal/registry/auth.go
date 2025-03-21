003901699da0b40c9ee776dbde66260f5fe6d8f0
Daniel Fox <dan.fox@tigera.io>
2025-03-21 10:14:12 -0700

    [release tool] auth and image validation fixups [master]

--- release/internal/registry/auth.go
+++ release/internal/registry/auth.go
@@ -24,17 +24,28 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/avast/retry-go/v4"
 	"github.com/docker/docker/api/types/registry"
 	"github.com/sirupsen/logrus"
 )
+
+var defaultDockerConfigDir = filepath.Join(os.Getenv("HOME"), ".config/docker")
 
 type DockerConfig struct {
 	Auths map[string]registry.AuthConfig `json:"auths"`
 }
 
+func dockerConfigDir() string {
+	configDir := os.Getenv("DOCKER_CONFIG")
+	if configDir == "" {
+		configDir = defaultDockerConfigDir
+	}
+	return configDir
+}
+
 // readDockerConfig reads the docker config file.
 func readDockerConfig() (DockerConfig, error) {
-	dockerConfigPath := filepath.Join(os.Getenv("HOME"), ".docker", "config.json")
+	dockerConfigPath := filepath.Join(dockerConfigDir(), "config.json")
 	file, err := os.Open(dockerConfigPath)
 	if err != nil {
 		logrus.WithError(err).Error("failed to open docker config file")
@@ -89,7 +100,16 @@ func getBearerTokenWithDefaultAuth(registry Registry, scope string) (string, err
 	if err != nil {
 		return "", fmt.Errorf("failed to get auth from docker config: %w", err)
 	}
-	return getBearerTokenWithAuth(fmt.Sprintf("%s:%s", auth.Username, auth.Password), registry, scope)
+	// We use retry.DoWithData to re-attempt getting bearer tokens.
+	// We set attempts to 5, the delay between attempts to 1000ms, and we set a custom
+	// retry function. In this case, we only retry if we got a "too many requests" error
+	// from the server, but we could add in connection timeouts, etc.
+	token, err := retry.DoWithData(func() (string, error) {
+		return getBearerTokenWithAuth(fmt.Sprintf("%s:%s", auth.Username, auth.Password), registry, scope)
+	}, retry.Attempts(5), retry.Delay(1000), retry.RetryIf(func(err error) bool {
+		return err.Error() == "too many requests"
+	}))
+	return token, err
 }
 
 // getBearerTokenWithAuth retrieves a bearer token to use for the image with given authentication.
@@ -109,12 +129,23 @@ func getBearerTokenWithAuth(auth string, registry Registry, scope string) (strin
 		return "", err
 	}
 	defer res.Body.Close()
-	if res.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("failed to get bearer token: %s", res.Status)
+
+	// If we got HTTP 200, decode the response and return the token
+	if res.StatusCode == http.StatusOK {
+		resp := map[string]interface{}{}
+		if err := json.NewDecoder(res.Body).Decode(&resp); err != nil {
+			return "", err
+		}
+		return resp["token"].(string), nil
 	}
-	resp := map[string]interface{}{}
-	if err := json.NewDecoder(res.Body).Decode(&resp); err != nil {
-		return "", err
+
+	// If we got HTTP 429, log that we got throttled and then return the error
+	if res.StatusCode == http.StatusTooManyRequests {
+		logrus.WithFields(logrus.Fields{
+			"TokenURL": tokenURL,
+			"Scope":    scope,
+		}).Error("server is throttling our requests, backing off")
+		return "", fmt.Errorf("too many requests")
 	}
-	return resp["token"].(string), nil
+	return "", fmt.Errorf("failed to get bearer token: %s", res.Status)
 }
