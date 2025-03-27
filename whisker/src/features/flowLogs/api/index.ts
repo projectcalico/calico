36f3e87a6c66f8b4ab3918c8eff82b4d41aed5ce
Ronan Conneely <ronan.conneely@tigera.io>
2025-03-27 14:21:12 +0000

    fix lint issue

--- whisker/src/features/flowLogs/api/index.ts
+++ whisker/src/features/flowLogs/api/index.ts
@@ -97,9 +97,11 @@ export const useFlowLogsStream = (
     // First flow start time is needed for accurate filtering
     React.useEffect(() => {
         if (initialStreamStartTime.current === null && data.length > 0) {
-            initialStreamStartTime.current = data
-                .sort((a, b) => b.start_time.getTime() - a.start_time.getTime())
-                [data.length - 1].start_time.getTime();
+            const sorted = data.sort(
+                (a, b) => b.start_time.getTime() - a.start_time.getTime(),
+            );
+            initialStreamStartTime.current =
+                sorted[data.length - 1].start_time.getTime();
         }
     }, [data.length]);
 
