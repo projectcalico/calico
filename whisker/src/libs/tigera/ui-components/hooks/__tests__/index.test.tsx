d67d064833dc2cb15907f935451ceb0555776fb1
Ronan Conneely <ronan.conneely@tigera.io>
2025-03-28 16:17:54 +0000

    cherry pick creatable omni-filter | store re-ordered columns

--- whisker/src/libs/tigera/ui-components/hooks/__tests__/index.test.tsx
+++ whisker/src/libs/tigera/ui-components/hooks/__tests__/index.test.tsx
@@ -1,5 +1,6 @@
-import { render } from '@/test-utils/helper';
-import { useDidUpdate } from '..';
+import { render, screen } from '@/test-utils/helper';
+import { useDidUpdate, useLocalStorage } from '..';
+import React from 'react';
 
 describe('useDidUpdate', () => {
     const TestUseDidUpdateHook = ({
@@ -29,5 +30,56 @@ describe('useDidUpdate', () => {
         );
 
         expect(mockUpdateCallback).toHaveBeenCalledTimes(1);
+    });
+});
+
+describe('useLocalStorage', () => {
+    const TestUseLocalStorageHook = ({ key, defaultResult }: any) => {
+        const result = useLocalStorage(key, defaultResult);
+        return <div>result: {result}</div>;
+    };
+
+    const TestUseLocalStorageSetOnMountHook = ({
+        key,
+        defaultResult,
+        setValue,
+    }: any) => {
+        const [result, setResult] = useLocalStorage(key, defaultResult);
+        React.useEffect(() => {
+            setResult(setValue);
+        }, [setValue]);
+        return <div>result: {result}</div>;
+    };
+
+    it('executes useLocalStorage to read invalid data structure', () => {
+        render(
+            <TestUseLocalStorageHook
+                key={'test'}
+                defaultResult={'result: mockDefaultResult'}
+            />,
+        );
+
+        expect(screen.getByText(/result: /)).toBeInTheDocument();
+        expect(
+            screen.getByText(/result: mockDefaultResult/),
+        ).toBeInTheDocument();
+    });
+
+    it('executes useLocalStorage to set invalid data structure', () => {
+        const cyclicalObj = { a: undefined };
+        cyclicalObj.a = { b: cyclicalObj } as any;
+
+        render(
+            <TestUseLocalStorageSetOnMountHook
+                key={'test'}
+                defaultResult={'result: mockDefaultResult'}
+                setValue={cyclicalObj}
+            />,
+        );
+
+        expect(screen.getByText(/result: /)).toBeInTheDocument();
+        expect(
+            screen.getByText(/result: mockDefaultResult/),
+        ).toBeInTheDocument();
     });
 });
