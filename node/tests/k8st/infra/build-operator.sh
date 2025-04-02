b512d2601e55a51497ab0c1212f6378a65d9ad48
Casey Davenport <davenport.cas@gmail.com>
2025-04-02 14:19:17 -0700

    Update versions.yml

--- node/tests/k8st/infra/build-operator.sh
+++ node/tests/k8st/infra/build-operator.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# This script produces a custom operator build that is used 
+# This script produces a custom operator build that is used
 # in the ST and e2e tests in this repo.
 
 # Clone the repository if needed.
-git clone --depth=1 https://github.com/caseydavenport/operator -b casey-add-pull-policy
+git clone --depth=1 https://github.com/tigera/operator -b ${BRANCH:-master}
 
 # Modify the versions that are in-use to match our locally built images.
-pushd operator 
+pushd operator
 make build/_output/bin/gen-versions
 build/_output/bin/gen-versions -os-versions=../calico_versions.yml > pkg/components/calico.go
 
