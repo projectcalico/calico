2fe15d3625d16e0dc9ff44ed7ddfc391371208ff
jlgonzalez <jlgonzalez@sarenet.es>
2025-04-04 08:58:48 +0200

    feat: add default support for MTU auto-detection in Debian 12 enX interfaces

--- felix/config/config_params.go
+++ felix/config/config_params.go
@@ -466,7 +466,7 @@ type Config struct {
 	GoMaxProcs int `config:"int(-1);-1"`
 
 	// Configures MTU auto-detection.
-	MTUIfacePattern *regexp.Regexp `config:"regexp;^((en|wl|ww|sl|ib)[Pcopsvx].*|(eth|wlan|wwan).*)"`
+	MTUIfacePattern *regexp.Regexp `config:"regexp;^((en|wl|ww|sl|ib)[PcopsvxX].*|(eth|wlan|wwan).*)"`
 
 	// Encapsulation information calculated from IP Pools and FelixConfiguration (VXLANEnabled and IpInIpEnabled)
 	Encapsulation Encapsulation
