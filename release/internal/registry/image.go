d50e13995da7e15f5333de7a2382f3a59d68ecac
Daniel Fox <dan.fox@tigera.io>
2025-04-11 11:30:47 -0700

    Add clarification to ImageExists() error

--- release/internal/registry/image.go
+++ release/internal/registry/image.go
@@ -107,7 +107,7 @@ func ImageExists(img ImageRef) (bool, error) {
 		return true, nil
 	} else if resp.StatusCode == http.StatusNotFound {
 		body, _ := io.ReadAll(resp.Body)
-		logrus.WithField("image", img.String()).Error("Failed to get manifest")
+		logrus.WithField("image", img.String()).Error("Failed to get manifest: HTTP 404")
 		return false, fmt.Errorf("unable to find image: %s", body)
 	}
 	return false, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
