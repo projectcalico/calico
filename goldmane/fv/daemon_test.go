782e18063c7a0c3841840e6444914d5ad96baf88
Casey Davenport <caseydavenport@users.noreply.github.com>
2025-04-11 14:39:58 -0700

    Refactor stream implementation in Goldmane (#10182)
    The stream implementation had a few bottlenecks that were showing up in larger tests.
    
    1. Registering and closing streams went via the main goroutine, which meant they would be blocked by other operations - including stream backfilling.
    2. If a stream was closed by a client during backfill, there was no way to tell the main loop to cancel the backfill operation.
    3. Stream writes were usually fast, but if enough back pressure was applied on the way out it could freeze the main log aggregator loop.
    4. Unpacking every Flow for each stream was using a lot of memory. 
    
    To resolve / mitigate these:
    
    - Stream registration and unregistration now happens on a separate goroutine, allowing for streams to terminate during other operations. 
    - Stream termination cancels a per-stream Context, which can be used to terminate operations writing to the stream.
    - Add a buffered channel between the main aggreagtor loop and the stream manager. 
    - Increase the channel buffering per-stream based on the request. 100 was not nearly enough for very large clusters.
    - Pass references to the data all the way to the point that we convert to protobuf format, allowing us to re-use the proto.FlowUpdate structure for each event, substantially reducing the memory impact of a Stream.
    
    This is really just a short-term mitigation. A larger re-work of this is probably in order. Namely, a couple of problems still exist:
    
    - For really large clusters, or Streams that ask for huge amounts of data (e.g., O(1M) flows), backfilling the stream can still delay the main loop causing dropped flows. This is just a function of iterating a large number of flows.
    - Rollover() currently iterates the entire bucket to send flows to the stream. This can cause the rollover() event to impact flow ingestion as well.
    
    To fix these, we probably need a slightly different concurrency model that allows for streams to execute both backfill and rollover logic asynchronously from the main ingestion loop. This will take some care to ensure we're handling data access correctly.

--- goldmane/fv/daemon_test.go
+++ goldmane/fv/daemon_test.go
@@ -211,6 +211,17 @@ func TestFlows(t *testing.T) {
 	// Sincse we only emit after 2 seconds with an emitter aggregation window of 2 seconds, we
 	// should see at least one flow emitted after 4 seconds. We'll wait for 10 seconds to be sure.
 	Eventually(emitted.Count, 10*time.Second, 1*time.Second).Should(BeNumerically(">", 0))
+
+	// We should be able to see flows emitted in the stream as well.
+	s, err := cli.Stream(ctx, &proto.FlowStreamRequest{StartTimeGte: -300})
+	require.NoError(t, err)
+
+	for range 10 {
+		// We should receive a flow.
+		f, err := s.Recv()
+		require.NoError(t, err)
+		require.NotNil(t, f)
+	}
 }
 
 // TestHints tests that we can successfully retrieve hints from generated flows.
