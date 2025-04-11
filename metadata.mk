ebad07d1ff4f782a86aa905f89e955116dda5b40
Jiawei Huang <jiawei@tigera.io>
2025-04-11 16:05:46 -0700

    Update calico/base pin to fix s390x loader symlink
    This change updates calico/base image for s390x to fix a dangling
    symlink issue that prevents programs from loading. See changes
    in [1].
    
    [1] https://github.com/projectcalico/go-build/pull/672

--- metadata.mk
+++ metadata.mk
@@ -4,9 +4,9 @@
 
 # The version of calico/go-build and calico/base to use.
 GO_BUILD_VER=1.24.2-llvm18.1.8-k8s1.32.3
-CALICO_BASE_VER=ubi8-1743740882
+CALICO_BASE_VER=ubi8-1744398299
 # TODO Remove once CALICO_BASE is updated to UBI9
-CALICO_BASE_UBI9_VER=ubi9-1743740882
+CALICO_BASE_UBI9_VER=ubi9-1744398299
 
 # Env var to ACK Ginkgo deprecation warnings, may need updating with go-build.
 ACK_GINKGO=ACK_GINKGO_DEPRECATIONS=1.16.5
