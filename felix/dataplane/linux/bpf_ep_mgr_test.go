0e37b6650d44510344af0035a22751831a57dcde
sridhar <sridhar@tigera.io>
2025-04-12 20:51:18 -0700

    configure arp and rp_filter for bpf nat devices only when both bpfin.cali and bpfout.cali are oper up.

--- felix/dataplane/linux/bpf_ep_mgr_test.go
+++ felix/dataplane/linux/bpf_ep_mgr_test.go
@@ -63,13 +63,14 @@ import (
 )
 
 type mockDataplane struct {
-	mutex       sync.Mutex
-	lastProgID  int
-	progs       map[string]int
-	numAttaches map[string]int
-	policy      map[string]polprog.Rules
-	routes      map[ip.CIDR]struct{}
-	netlinkShim netlinkshim.Interface
+	mutex                sync.Mutex
+	lastProgID           int
+	progs                map[string]int
+	numAttaches          map[string]int
+	policy               map[string]polprog.Rules
+	routes               map[ip.CIDR]struct{}
+	netlinkShim          netlinkshim.Interface
+	natDevicesConfigured bool
 
 	ensureStartedFn    func()
 	ensureQdiscFn      func(string) (bool, error)
@@ -107,6 +108,11 @@ func (m *mockDataplane) interfaceByIndex(ifindex int) (*net.Interface, error) {
 }
 
 func (m *mockDataplane) ensureBPFDevices() error {
+	return nil
+}
+
+func (m *mockDataplane) configureBPFDevices() error {
+	m.natDevicesConfigured = true
 	return nil
 }
 
@@ -660,6 +666,13 @@ var _ = Describe("BPF Endpoint Manager", func() {
 			genWLUpdate("cali12345")()
 			genIfaceUpdate("cali12345", ifacemonitor.StateUp, 15)()
 			genHEPUpdate(allInterfaces, hostEpNorm)()
+		})
+
+		It("should configure NAT devices only when both bpfin and bpfout are oper up", func() {
+			genIfaceUpdate("bpfin.cali", ifacemonitor.StateUp, 1000)()
+			Expect(dp.natDevicesConfigured).To(BeFalse())
+			genIfaceUpdate("bpfout.cali", ifacemonitor.StateUp, 1000)()
+			Expect(dp.natDevicesConfigured).To(BeTrue())
 		})
 
 		It("does not have host-* policy on the workload interface", func() {
