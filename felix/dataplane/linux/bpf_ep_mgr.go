8a7c81d21952284a9d8f52e318e3fc8a2f210a8c
Mazdak Nasab <mazdak.nasab@gmail.com>
2025-04-15 15:10:52 -0700

    Fix removing staged policies from dataplane  (#10221)

--- felix/dataplane/linux/bpf_ep_mgr.go
+++ felix/dataplane/linux/bpf_ep_mgr.go
@@ -3008,7 +3008,7 @@ func (m *bpfEndpointManager) extractTiers(tiers []*proto.TierInfo, direction Pol
 
 			for i, polName := range directionalPols {
 				if model.PolicyIsStaged(polName) {
-					logrus.Debugf("SKipping staged policy %v", polName)
+					logrus.Debugf("Skipping staged policy %v", polName)
 					continue
 				}
 				stagedOnly = false
