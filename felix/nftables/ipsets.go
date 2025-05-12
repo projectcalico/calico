26c3c1bbff985bcfc69e1fd97a546e64362f4042
Shaun Crampton <shaun@tigera.io>
2025-05-12 13:36:27 +0100

    Upstream IP sets dataplane API changes.
    Enterprise needs an extra callback to tell it when particular
    IPs have been updated.  Upstream that, just to reduce OS<->Enterprise
    code diffs.

--- felix/nftables/ipsets.go
+++ felix/nftables/ipsets.go
@@ -325,7 +325,7 @@ func (s *IPSets) GetDesiredMembers(setID string) (set.Set[string], error) {
 
 // ApplyUpdates applies the updates to the dataplane.  Returns a set of programmed IPs in the IPSets included by the
 // ipsetFilter.
-func (s *IPSets) ApplyUpdates() {
+func (s *IPSets) ApplyUpdates(filter func(setName string) bool) (programmedIPs set.Set[string]) {
 	success := false
 	retryDelay := 1 * time.Millisecond
 	backOff := func() {
@@ -333,6 +333,7 @@ func (s *IPSets) ApplyUpdates() {
 		retryDelay *= 2
 	}
 
+	programmedIPs = set.New[string]()
 	for attempt := 0; attempt < 10; attempt++ {
 		if attempt > 0 {
 			s.logCxt.Info("Retrying after an nftables set update failure...")
@@ -351,7 +352,7 @@ func (s *IPSets) ApplyUpdates() {
 			s.resyncRequired = false
 		}
 
-		if err := s.tryUpdates(); err != nil {
+		if err := s.tryUpdates(filter, programmedIPs); err != nil {
 			// Update failures may mean that our iptables updates fail.  We need to do an immediate resync.
 			s.logCxt.WithError(err).Warning("Failed to update IP sets. Marking dataplane for resync.")
 			s.resyncRequired = true
@@ -365,6 +366,7 @@ func (s *IPSets) ApplyUpdates() {
 	if !success {
 		s.logCxt.Panic("Failed to update IP sets after multiple retries.")
 	}
+	return
 }
 
 // tryResync attempts to bring our state into sync with the dataplane.  It scans the contents of the
@@ -580,7 +582,7 @@ func (s *IPSets) NFTablesSet(name string) *knftables.Set {
 }
 
 // tryUpdates attempts to apply any pending updates to the dataplane.
-func (s *IPSets) tryUpdates() error {
+func (s *IPSets) tryUpdates(ipsetFilter func(ipSetName string) bool, programmedIPs set.Set[string]) error {
 	var dirtyIPSets []string
 
 	s.ipSetsWithDirtyMembers.Iter(func(setName string) error {
@@ -637,6 +639,10 @@ func (s *IPSets) tryUpdates() error {
 		members.Desired().Iter(func(member SetMember) {
 			if members.Dataplane().Contains(member) {
 				return
+			}
+			if ipsetFilter != nil && ipsetFilter(setName) {
+				// We want to include the IPs from this set.
+				programmedIPs.Add(member.String())
 			}
 			tx.Add(&knftables.Element{
 				Set: setName,
