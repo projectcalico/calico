4324bc1f89c25ea59a9339aa38f919c0ca8beab5
Brian McMahon <brianmcmahon135@gmail.com>
2025-05-14 10:40:36 -0700

    more changes

--- lib/httpmachinery/go.mod
+++ lib/httpmachinery/go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/net v0.39.0 // indirect
--- lib/std/log/types/log.go
+++ lib/std/log/types/log.go
@@ -106,29 +106,39 @@ type Logger interface {
 }
 
 type Entry interface {
+	Logger() Logger
 	Bytes() ([]byte, error)
 	Debug(args ...interface{})
 	Debugf(format string, args ...interface{})
+	Debugln(args ...interface{})
 	Error(args ...interface{})
 	Errorf(format string, args ...interface{})
+	Errorln(args ...interface{})
 	Fatal(args ...interface{})
 	Fatalf(format string, args ...interface{})
 	Info(args ...interface{})
 	Infof(format string, args ...interface{})
+	Infoln(args ...interface{})
 	Log(level Level, args ...interface{})
 	Logf(level Level, format string, args ...interface{})
 	Panic(args ...interface{})
 	Panicf(format string, args ...interface{})
 	Print(args ...interface{})
+	Printf(format string, args ...interface{})
+	Println(args ...interface{})
 	String() (string, error)
 	Trace(args ...interface{})
 	Warn(args ...interface{})
 	Warnf(format string, args ...interface{})
+	Warnln(args ...interface{})
 	Warning(args ...interface{})
 	Warningf(format string, args ...interface{})
+	Warningln(args ...interface{})
 	WithContext(ctx context.Context) Entry
 	WithError(err error) Entry
 	WithField(key string, value interface{}) Entry
 	WithFields(fields Fields) Entry
 	WithTime(t time.Time) Entry
+	Fields() Fields
+	GetLevel() Level
 }
