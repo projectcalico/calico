28296aaef873aec1a42474127ff9059e3347355c
Brian McMahon <brianmcmahon135@gmail.com>
2025-05-15 13:37:05 -0700

    copyright updates

--- apiserver/cmd/apiserver/server/server.go
+++ apiserver/cmd/apiserver/server/server.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021-2024 Tigera, Inc. All rights reserved.
+// Copyright (c) 2021-2025 Tigera, Inc. All rights reserved.
 
 /*
 Copyright 2016 The Kubernetes Authors.
--- apiserver/pkg/registry/projectcalico/authorizer/authorizer.go
+++ apiserver/pkg/registry/projectcalico/authorizer/authorizer.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 Tigera, Inc. All rights reserved.
+// Copyright (c) 2024-2025 Tigera, Inc. All rights reserved.
 
 package authorizer
 
