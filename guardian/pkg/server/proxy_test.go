fdaecaa3123015566ce3fab2b2100a3741ed6366
William Ong <william@tigera.io>
2025-05-20 09:31:28 -0700

    Restructured tests with t.Run and table tests

--- guardian/pkg/server/proxy_test.go
+++ guardian/pkg/server/proxy_test.go
@@ -16,198 +16,158 @@ import (
 	"github.com/projectcalico/calico/guardian/test/utils"
 )
 
-func TestProxyWithHTTP(t *testing.T) {
+func TestProxy(t *testing.T) {
 	RegisterTestingT(t)
+	t.Run("TestProxyWithHTTP", func(t *testing.T) {
+		t.Parallel()
 
-	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		Expect(r.URL.Path).To(Equal("/test"))
-		w.WriteHeader(http.StatusOK)
-		_, err := w.Write([]byte("Hello"))
-		Expect(err).NotTo(HaveOccurred())
+		mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			Expect(r.URL.Path).To(Equal("/test"))
+			w.WriteHeader(http.StatusOK)
+			_, err := w.Write([]byte("Hello"))
+			Expect(err).NotTo(HaveOccurred())
 
-	}))
-	defer mockServer.Close()
+		}))
+		defer mockServer.Close()
 
-	targetURL, err := url.Parse(mockServer.URL)
-	Expect(err).NotTo(HaveOccurred())
-
-	targets := []server.Target{
-		{
-			Path: "/test",
-			Dest: targetURL,
-		},
-	}
-
-	proxy, err := server.NewProxy(targets)
-	Expect(proxy).NotTo(BeNil())
-	Expect(err).NotTo(HaveOccurred())
-
-	t.Run("ServeHTTP proxies requests to the correct target", func(t *testing.T) {
-		req := httptest.NewRequest(http.MethodGet, "/test", nil)
-		rec := httptest.NewRecorder()
-
-		proxy.ServeHTTP(rec, req)
-
-		Expect(rec.Code).To(Equal(http.StatusOK))
-		Expect(rec.Body.String()).To(Equal("Hello"))
-	})
-
-	t.Run("ServeHTTP returns 404 for unmatched path", func(t *testing.T) {
-		req := httptest.NewRequest(http.MethodGet, "/notfound", nil)
-		rec := httptest.NewRecorder()
-
-		proxy.ServeHTTP(rec, req)
-
-		Expect(rec.Code).To(Equal(http.StatusNotFound))
-	})
-}
-
-func TestProxyWithHTTPS(t *testing.T) {
-	RegisterTestingT(t)
-
-	mockServer := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		_, err := w.Write([]byte("Hello from HTTPS"))
-		Expect(err).NotTo(HaveOccurred())
-	}))
-
-	tmpDir := os.TempDir()
-
-	serverCrt, serverKey := utils.CreateKeyCertPair(tmpDir)
-	defer serverCrt.Close()
-	defer serverKey.Close()
-
-	cert, err := tls.LoadX509KeyPair(serverCrt.Name(), serverKey.Name())
-	if err != nil {
-		t.Fatalf("Failed to load server certificate and key: %v", err)
-	}
-
-	mockServer.TLS = &tls.Config{Certificates: []tls.Certificate{cert}}
-	mockServer.StartTLS()
-	defer mockServer.Close()
-
-	t.Run("ServeHTTP proxies requests to the correct HTTPS target", func(t *testing.T) {
 		targetURL, err := url.Parse(mockServer.URL)
 		Expect(err).NotTo(HaveOccurred())
 
 		targets := []server.Target{
 			{
-				Path:   "/test",
-				Dest:   targetURL,
-				CAFile: serverCrt.Name(),
+				Path: "/test",
+				Dest: targetURL,
 			},
 		}
 
 		proxy, err := server.NewProxy(targets)
-		Expect(err).NotTo(HaveOccurred())
 		Expect(proxy).NotTo(BeNil())
-
-		req := httptest.NewRequest(http.MethodGet, "/test", nil)
-		rec := httptest.NewRecorder()
-
-		proxy.ServeHTTP(rec, req)
-
-		Expect(http.StatusOK).To(Equal(rec.Code))
-		Expect(rec.Body.String()).To(Equal("Hello from HTTPS"))
-	})
-}
-
-func TestProxyWithPathRegexp(t *testing.T) {
-	RegisterTestingT(t)
-
-	mockServer := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		_, err := w.Write([]byte(fmt.Sprintf("Received expected URL %s", r.URL.Path)))
 		Expect(err).NotTo(HaveOccurred())
-	}))
-	defer mockServer.Close()
 
-	tmpDir := os.TempDir()
-	serverCrt, serverKey := utils.CreateKeyCertPair(tmpDir)
-	defer serverCrt.Close()
-	defer serverKey.Close()
-
-	cert, err := tls.LoadX509KeyPair(serverCrt.Name(), serverKey.Name())
-	Expect(err).NotTo(HaveOccurred())
-
-	mockServer.TLS = &tls.Config{Certificates: []tls.Certificate{cert}}
-	mockServer.StartTLS()
-	defer mockServer.Close()
-
-	regex, err := regexp.Compile("^/test$")
-	Expect(err).NotTo(HaveOccurred())
-
-	targetURL, err := url.Parse(mockServer.URL)
-	Expect(err).NotTo(HaveOccurred())
-
-	t.Run("Rejects request if PathRegexp does not match", func(t *testing.T) {
-		targets := []server.Target{
+		tt := []struct {
+			description     string
+			expectedCode    int
+			expectedMessage string
+			requestPath     string
+		}{
 			{
-				Path:       "/invalid-path",
-				PathRegexp: regex,
-				Dest:       targetURL,
-				CAFile:     serverCrt.Name(),
+				description:     "ServeHTTP proxies requests to the correct target",
+				expectedCode:    http.StatusOK,
+				expectedMessage: "Hello",
+				requestPath:     "/test",
 			},
-		}
-
-		proxy, err := server.NewProxy(targets)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(proxy).NotTo(BeNil())
-
-		req := httptest.NewRequest(http.MethodGet, "/invalid-path", nil)
-		rec := httptest.NewRecorder()
-
-		proxy.ServeHTTP(rec, req)
-
-		Expect(rec.Code).To(Equal(http.StatusNotFound))
-	})
-
-	t.Run("Accepts request if PathRegexp does match", func(t *testing.T) {
-		targets := []server.Target{
 			{
-				Path:       "/test",
-				PathRegexp: regex,
-				Dest:       targetURL,
-				CAFile:     serverCrt.Name(),
+				description:     "ServeHTTP returns 404 for unmatched path",
+				expectedCode:    http.StatusNotFound,
+				expectedMessage: "404 page not found\n",
+				requestPath:     "/notfound",
 			},
 		}
+		for _, tc := range tt {
+			t.Run(tc.description, func(t *testing.T) {
+				rec := httptest.NewRecorder()
+				req := httptest.NewRequest(http.MethodGet, tc.requestPath, nil)
 
-		proxy, err := server.NewProxy(targets)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(proxy).NotTo(BeNil())
+				proxy.ServeHTTP(rec, req)
 
-		req := httptest.NewRequest(http.MethodGet, "/test", nil)
-		rec := httptest.NewRecorder()
-
-		proxy.ServeHTTP(rec, req)
-
-		Expect(rec.Code).To(Equal(http.StatusOK))
-		Expect(rec.Body.String()).To(Equal("Received expected URL /test"))
+				Expect(rec.Code).To(Equal(tc.expectedCode))
+				Expect(rec.Body.String()).To(Equal(tc.expectedMessage))
+			})
+		}
 	})
 
-	t.Run("Replaces URL path using PathReplace", func(t *testing.T) {
-		targets := []server.Target{
+	t.Run("Test Proxy With HTTPS", func(t *testing.T) {
+		t.Parallel()
+
+		mockServer := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			_, err := w.Write([]byte(fmt.Sprintf("Hello from HTTPS, with expected URL %s", r.URL.Path)))
+			Expect(err).NotTo(HaveOccurred())
+		}))
+
+		tmpDir := os.TempDir()
+
+		serverCrt, serverKey := utils.CreateKeyCertPair(tmpDir)
+		defer serverCrt.Close()
+		defer serverKey.Close()
+
+		cert, err := tls.LoadX509KeyPair(serverCrt.Name(), serverKey.Name())
+		if err != nil {
+			t.Fatalf("Failed to load server certificate and key: %v", err)
+		}
+
+		mockServer.TLS = &tls.Config{Certificates: []tls.Certificate{cert}}
+		mockServer.StartTLS()
+		defer mockServer.Close()
+		targetURL, err := url.Parse(mockServer.URL)
+		Expect(err).NotTo(HaveOccurred())
+
+		regex, err := regexp.Compile("^/test$")
+		Expect(err).NotTo(HaveOccurred())
+		tt := []struct {
+			description     string
+			target          server.Target
+			expectedMessage string
+			expectedCode    int
+		}{
 			{
-				Path:        "/test",
-				PathRegexp:  regex,
-				PathReplace: []byte(`/new-path`),
-				Dest:        targetURL,
-				CAFile:      serverCrt.Name(),
+				description: "ServeHTTP proxies requests to the correct HTTPS target",
+				target: server.Target{
+					Path:   "/test",
+					Dest:   targetURL,
+					CAFile: serverCrt.Name()},
+				expectedCode:    http.StatusOK,
+				expectedMessage: "Hello from HTTPS, with expected URL /test",
+			},
+			{
+				description: "Regexp - Rejects request if PathRegexp does not match",
+				target: server.Target{
+					Path:       "/invalid-path",
+					PathRegexp: regex,
+					Dest:       targetURL,
+					CAFile:     serverCrt.Name()},
+				expectedCode:    http.StatusNotFound,
+				expectedMessage: "Not found\n",
+			},
+			{
+				description: "Regexp - Accepts request if PathRegexp does match",
+				target: server.Target{
+					Path:       "/test",
+					PathRegexp: regex,
+					Dest:       targetURL,
+					CAFile:     serverCrt.Name(),
+				},
+				expectedCode:    http.StatusOK,
+				expectedMessage: "Hello from HTTPS, with expected URL /test",
+			},
+			{
+				description: "Regexp - Replaces URL path using PathReplace",
+				target: server.Target{
+					Path:        "/test",
+					PathRegexp:  regex,
+					PathReplace: []byte(`/new-path`),
+					Dest:        targetURL,
+					CAFile:      serverCrt.Name(),
+				},
+				expectedCode:    http.StatusOK,
+				expectedMessage: "Hello from HTTPS, with expected URL /new-path",
 			},
 		}
 
-		proxy, err := server.NewProxy(targets)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(proxy).NotTo(BeNil())
+		for _, tc := range tt {
+			t.Run(tc.description, func(t *testing.T) {
+				proxy, err := server.NewProxy([]server.Target{tc.target})
+				Expect(err).NotTo(HaveOccurred())
+				Expect(proxy).NotTo(BeNil())
 
-		req := httptest.NewRequest(http.MethodGet, "/test", nil)
-		rec := httptest.NewRecorder()
+				req := httptest.NewRequest(http.MethodGet, tc.target.Path, nil)
+				rec := httptest.NewRecorder()
 
-		proxy.ServeHTTP(rec, req)
+				proxy.ServeHTTP(rec, req)
 
-		Expect(rec.Code).To(Equal(http.StatusOK))
-		Expect(rec.Body.String()).To(Equal("Received expected URL /new-path"))
-
+				Expect(rec.Code).To(Equal(tc.expectedCode))
+				Expect(rec.Body.String()).To(Equal(tc.expectedMessage))
+			})
+		}
 	})
-
 }
