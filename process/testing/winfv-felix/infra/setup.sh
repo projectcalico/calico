561365f43f2f14fa737bdfaec0ae945544cc2274
song jiang <song@tigera.io>
2025-05-20 13:36:30 +0000

    minor comments

--- process/testing/winfv-felix/infra/setup.sh
+++ process/testing/winfv-felix/infra/setup.sh
@@ -18,7 +18,7 @@ set -o nounset
 set -o pipefail
 
 CURRENT_DIR=$(dirname "$0")
-kubectl create ns demo --kubeconfig $1 || true
+kubectl create ns demo --kubeconfig $1
 
 kubectl apply -f $CURRENT_DIR/porter.yaml --kubeconfig $1
 kubectl apply -f $CURRENT_DIR/nginx.yaml --kubeconfig $1
