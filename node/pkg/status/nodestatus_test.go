9a3dd197b4e1e983a5f369e5c1cbcaf3862b0e07
theboringstuff <39027092+theboringstuff@users.noreply.github.com>
2025-06-12 19:13:12 +0500

    catch-up on conflict

--- node/pkg/status/nodestatus_test.go
+++ node/pkg/status/nodestatus_test.go
@@ -17,6 +17,7 @@ package status_test
 import (
 	"context"
 	"errors"
+	"os"
 	"time"
 
 	. "github.com/onsi/ginkgo"
@@ -27,6 +28,8 @@ import (
 
 	"github.com/projectcalico/calico/libcalico-go/lib/apiconfig"
 	"github.com/projectcalico/calico/libcalico-go/lib/backend"
+	"github.com/projectcalico/calico/libcalico-go/lib/backend/api"
+	"github.com/projectcalico/calico/libcalico-go/lib/backend/model"
 	"github.com/projectcalico/calico/libcalico-go/lib/backend/syncersv1/nodestatussyncer"
 	client "github.com/projectcalico/calico/libcalico-go/lib/clientv3"
 	"github.com/projectcalico/calico/libcalico-go/lib/options"
@@ -48,6 +51,9 @@ var _ = Describe("Node status FV tests", func() {
 	Expect(err).NotTo(HaveOccurred())
 	cfg.Spec = apiconfig.CalicoAPIConfigSpec{
 		DatastoreType: apiconfig.Kubernetes,
+		KubeConfig: apiconfig.KubeConfig{
+			Kubeconfig: os.Getenv("KUBECONFIG"),
+		},
 	}
 
 	c, err := client.New(*cfg)
@@ -320,6 +326,46 @@ var _ = Describe("Node status FV tests", func() {
 			Expect(new.Status.BGP).To(Equal(emptyBGP))
 			Expect(new.Status.Routes).To(Equal(emptyRoutes))
 		})
+	})
+
+	Specify("Reporter should handle conflict in case of syncer missing update", func() {
+		// Create reporter without syncer, because we are going to simulate syncer missing update
+		mock := newMockBird(v4Status, v6Status, v4Peer, v6Peer, v4Route, v6Route)
+		r := status.NewNodeStatusReporter(nodeName, cfg, c, getPopulators(mock))
+		go r.Run()
+		defer r.Stop()
+
+		// Create a node status request with update period of 5 seconds, notify reporter
+		be.Clean()
+		createCalicoNodeStatus(c, nodeName, name, 5)
+		r.OnUpdates([]api.Update{
+			{KVPair: model.KVPair{Key: model.ResourceKey{Name: name}, Value: getCurrentStatus()}},
+		})
+		r.OnStatusUpdated(api.InSync)
+
+		// Wait for first immediate status update
+		var err error
+		var firstStatus *apiv3.CalicoNodeStatus
+		Eventually(func() metav1.Time {
+			firstStatus, err = c.CalicoNodeStatus().Get(context.Background(), name, options.GetOptions{})
+			Expect(err).NotTo(HaveOccurred())
+			return firstStatus.Status.LastUpdated
+		}, 2*time.Second, 500*time.Millisecond).Should(Not(BeZero()))
+
+		// Update object, but do not notify reporter (simulate syncer missing update)
+		newPeriod := uint32(1)
+		secondStatus := firstStatus.DeepCopy()
+		secondStatus.Spec.UpdatePeriodSeconds = &newPeriod
+		_, err = c.CalicoNodeStatus().Update(context.Background(), secondStatus, options.SetOptions{})
+		Expect(err).To(Not(HaveOccurred()))
+
+		// Update actual state and sleep to wait reporter to encounter and resolve conflict
+		mock.setLastBootTime(BootTimeSecond)
+		time.Sleep(10 * time.Second)
+
+		// Make sure status was updated after all, i.e. conflict was resolved by reporter on its own
+		latest := getCurrentStatus()
+		Expect(latest.Status.LastUpdated).NotTo(Equal(firstStatus.Status.LastUpdated))
 	})
 })
 
