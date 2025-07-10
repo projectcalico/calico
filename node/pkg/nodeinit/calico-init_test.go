b13be459fc93cffd11f041bdf5961446d9c421f1
Lucas Sampaio <lucas@tigera.io>
2025-07-10 12:18:02 -0700

    Changing UT from vanilla `go test` to ginkgo due to failures when running gcc.

--- node/pkg/nodeinit/calico-init_test.go
+++ node/pkg/nodeinit/calico-init_test.go
@@ -16,42 +16,30 @@ package nodeinit
 
 import (
 	"net"
-	"testing"
 
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 )
 
-func TestIPPortParsing(t *testing.T) {
-	RegisterTestingT(t)
-	testCases := []struct {
-		addr            string
-		expectedIPPorts []IPPort
-		errorExpected   bool
-	}{
-		{"", nil, true},
-		{
-			"1.1.1.1:443",
-			[]IPPort{IPPort{net.IPv4(1, 1, 1, 1), 443, true}},
-			false,
+var _ = DescribeTable("Parse comma-separated IP:port addresses",
+	func(addr string, expectedIPPorts []IPPort, errorExpected bool) {
+		ipPorts, err := parseCommaSeparatedIPPorts(addr)
+		Expect(err != nil).To(Equal(errorExpected))
+		Expect(ipPorts).To(Equal(expectedIPPorts))
+	},
+	Entry("Parsing empty string", "", nil, true),
+	Entry("Parsing a single IPv4:Port string", "1.1.1.1:443", []IPPort{{net.ParseIP("1.1.1.1"), 443, true}}, false),
+	Entry("Parsing a single IPv6:Port string", "[2001:db8::1]:6443", []IPPort{{net.ParseIP("2001:db8::1"), 6443, false}}, false),
+	Entry("Parsing multiple IP:Port strings",
+		"192.168.0.1:6443,[2001:db8::1]:6443",
+		[]IPPort{
+			{net.ParseIP("192.168.0.1"), 6443, true},
+			{net.ParseIP("2001:db8::1"), 6443, false},
 		},
-		{
-			"192.168.0.1:6443,[2001:db8::1]:6443",
-			[]IPPort{
-				IPPort{net.ParseIP("192.168.0.1"), 6443, true},
-				IPPort{net.ParseIP("2001:db8::1"), 6443, false},
-			},
-			false,
-		},
-		{"1.1.1.1:port", nil, true},
-		{"1.1.1:80", nil, true},
-		{"made-up-addr", nil, true},
-		{"[2001:db8::1]:443,", nil, true},
-		{",[2001:db8::1]:443", nil, true},
-	}
-
-	for _, testCase := range testCases {
-		ipPorts, err := parseCommaSeparatedIPPorts(testCase.addr)
-		Expect(err != nil).To(Equal(testCase.errorExpected))
-		Expect(ipPorts).To(Equal(testCase.expectedIPPorts))
-	}
-}
+		false),
+	Entry("Parsing invalid port", "1.1.1.1:port", nil, true),
+	Entry("Parsing invalid IP", "1.1.1:80", nil, true),
+	Entry("Parsing invalid address", "made-up-addr", nil, true),
+	Entry("Parsing invalid string - ending with comma", "[2001:db8::1]:443,", nil, true),
+	Entry("Parsing invalid string - starting with comma", ",[2001:db8::1]:443", nil, true),
+)
