ca1ec3e2c9d62319766bf7e4ea784bf3efe2a13a
Mazdak Nasab <mazdak.nasab@gmail.com>
2025-07-15 16:55:54 -0700

    calc

--- felix/calc/rule_scanner.go
+++ felix/calc/rule_scanner.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 	"strings"
 
+	v3 "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
 	"github.com/projectcalico/api/pkg/lib/numorstring"
 	log "github.com/sirupsen/logrus"
 
@@ -183,6 +184,12 @@ func (rs *RuleScanner) OnPolicyActive(key model.PolicyKey, policy *model.Policy)
 func (rs *RuleScanner) OnPolicyInactive(key model.PolicyKey) {
 	rs.updateRules(key, nil, nil, false, false, "", "")
 	rs.RulesUpdateCallbacks.OnPolicyInactive(key)
+}
+
+func (rs *RuleScanner) OnQoSPolicyActive(key string, policy *v3.QoSPolicy) {
+}
+
+func (rs *RuleScanner) OnQoSPolicyInactive(key string) {
 }
 
 func (rs *RuleScanner) updateRules(
