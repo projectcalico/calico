921ca84d3c00e4399de52bed7ecaab1e4a132cf8
Shaun Crampton <shaun@tigera.io>
2025-07-21 10:44:16 +0100

    Squash "bad CIDR" log when parsing headless service. (#10702)
    Headless services have an explicit "None" value in their
    ClusterIPs instead of an empty list.

--- felix/dataplane/linux/bpf_ep_mgr.go
+++ felix/dataplane/linux/bpf_ep_mgr.go
@@ -48,6 +48,7 @@ import (
 	"github.com/vishvananda/netlink"
 	"golang.org/x/sync/semaphore"
 	"golang.org/x/sys/unix"
+	k8sv1 "k8s.io/api/core/v1"
 
 	apiv3 "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
 
@@ -4063,6 +4064,10 @@ func (m *bpfEndpointManager) onServiceUpdate(update *proto.ServiceUpdate) {
 
 	ips := make([]ip.CIDR, 0, len(ipstr))
 	for _, i := range ipstr {
+		if i == k8sv1.ClusterIPNone {
+			// Headless services have an explicit "None" value for ClusterIPs.
+			continue
+		}
 		cidr, err := ip.ParseCIDROrIP(i)
 		if err != nil {
 			log.WithFields(log.Fields{"service": key, "ip": i}).Warn("Not a valid CIDR.")
