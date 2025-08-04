4df333232a07bee059eb1690252553bec55b00eb
Casey Davenport <davenport.cas@gmail.com>
2025-08-04 15:56:50 -0700

    Don't use global policy

--- e2e/pkg/tests/policy/policy.go
+++ e2e/pkg/tests/policy/policy.go
@@ -334,26 +334,26 @@ var _ = describe.CalicoDescribe(
 			policyName := "deny-egress"
 			o := 500.0
 
-			// Create a GNP that selects the namespace under test and denies egress explicitly
-			// to other workloads within the namespace.
-			// Note, this policy will actually deny all egress anyway because there are no "Allow" rules.
-			// Be careful - a mistake here could break egress for the entire cluster.
-			policy := v3.NewGlobalNetworkPolicy()
-			policy.Name = policyName
-			policy.Spec.Selector = fmt.Sprintf("projectcalico.org/namespace == '%s'", nsName)
-			policy.Spec.Order = &o
-			policy.Spec.Egress = []v3.Rule{
+			// Create an egress policy that explicitly denies all egress traffic from the namespace.
+			defaultDeny := v3.NewNetworkPolicy()
+			defaultDeny.Name = policyName
+			defaultDeny.Namespace = nsName
+			defaultDeny.Spec.Selector = "all()"
+			defaultDeny.Spec.Order = &o
+			defaultDeny.Spec.Egress = []v3.Rule{
 				{
 					Action: v3.Deny,
-					Destination: v3.EntityRule{
-						Selector: fmt.Sprintf("projectcalico.org/namespace == '%s'", nsName),
-					},
+				},
+				{
+					// This rule shouldn't be hit.
+					Action: v3.Allow,
 				},
 			}
-			err := cli.Create(context.Background(), policy)
+
+			err := cli.Create(context.Background(), defaultDeny)
 			Expect(err).NotTo(HaveOccurred())
 			defer func() {
-				Expect(cli.Delete(context.Background(), policy)).NotTo(HaveOccurred())
+				Expect(cli.Delete(context.Background(), defaultDeny)).NotTo(HaveOccurred())
 			}()
 
 			By("checking client not able to contact the server since deny egress rule created.")
