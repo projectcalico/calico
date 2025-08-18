4b7d157954cb03aea37394634010a6b1234bac5f
Nell Jerram <nell@tigera.io>
2025-08-18 17:29:33 +0100

    Make OpenStack resync interval configurable and disable-able
    In principle the ongoing resync isn't needed at all, so let's make it possible to switch it off, and
    do our DevStack (FV) testing like that, to confirm the principle.
    
    On the other hand, if it is wanted, let's make the period configurable.  For now we continue to
    default to the same as before, i.e. 60s.
    
    Plus a small variable naming improvement.

--- networking-calico/devstack/bootstrap.sh
+++ networking-calico/devstack/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Copyright 2015 Metaswitch Networks
 # All Rights Reserved.
+# Copyright (c) 2025 Tigera, Inc. All rights reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -161,6 +162,10 @@ SCENARIO_IMAGE_TYPE=ignore
 GIT_BASE=https://github.com
 
 LIBVIRT_TYPE=qemu
+
+# Disable ongoing resync.  In principle this isn't needed; disable it in order to build evidence to
+# confirm that.
+CALICO_RESYNC_INTERVAL_SECS=0
 
 EOF
 
