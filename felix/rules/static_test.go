50f07eb719a4195a0271145f024cb59119e88652
Mazdak Nasab <mazdak.nasab@gmail.com>
2025-08-22 12:18:20 -0700

    Rename QoS policy manager to DSCP manager to avoid confusion (#10869)

--- felix/rules/static_test.go
+++ felix/rules/static_test.go
@@ -51,7 +51,7 @@ var _ = Describe("Static", func() {
 						SourceIPSet(allPoolSetName).
 						NotDestIPSet(allPoolSetName).
 						NotDestIPSet(allHostsSetName),
-					Action:  JumpAction{Target: ChainQoSPolicy},
+					Action:  JumpAction{Target: ChainEgressDSCP},
 					Comment: []string{"set dscp for workloads traffic leaving cluster."},
 				},
 				{
@@ -59,7 +59,7 @@ var _ = Describe("Static", func() {
 						SourceIPSet(allHostsSetName).
 						NotDestIPSet(allPoolSetName).
 						NotDestIPSet(allHostsSetName),
-					Action:  JumpAction{Target: ChainQoSPolicy},
+					Action:  JumpAction{Target: ChainEgressDSCP},
 					Comment: []string{"set dscp for host endpoints traffic leaving cluster."},
 				},
 				// Accept already accepted.
