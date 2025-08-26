984ab6e5fd3edc80d55029e8e65117b31d323fe4
Lucas Secchin <lucas@tigera.io>
2025-08-26 09:49:27 -0700

    [CORE-11756] New felixConfig CgroupV2Path (#10877)
    * New felixConfig CgroupV2Path
    
    * Fix numBaseFelixConfigs

--- felix/config/config_params.go
+++ felix/config/config_params.go
@@ -224,6 +224,9 @@ type Config struct {
 	BPFExportBufferSizeMB              int               `config:"int;1;non-zero"`
 	BPFProfiling                       string            `config:"oneof(Disabled,Enabled);Disabled;non-zero"`
 
+	// CgroupV2Path is not used by Felix, but its init container
+	CgroupV2Path string `config:"string;;"`
+
 	// DebugBPFCgroupV2 controls the cgroup v2 path that we apply the connect-time load balancer to.  Most distros
 	// are configured for cgroup v1, which prevents all but the root cgroup v2 from working so this is only useful
 	// for development right now.
