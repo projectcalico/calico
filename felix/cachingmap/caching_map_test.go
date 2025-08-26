65f06cc015f4bedb5ed6c8b90ff985e32ef740c9
Tomas Hruby <tomas@tigera.io>
2025-08-25 18:42:22 -0700

    Add failures tests for batching caching map

--- felix/cachingmap/caching_map_test.go
+++ felix/cachingmap/caching_map_test.go
@@ -367,6 +367,126 @@ func testCachingMap_Resync(t *testing.T, batched bool) {
 	Expect(mockMap.OpCount()).To(Equal(preApplyOpCount))
 }
 
+// TestCachingMap_BatchUpdateFailures tests that when some updates fail and some succeed
+// during ApplyUpdatesOnly, the successful ones are applied and errors are returned.
+func TestCachingMap_BatchUpdateFailures(t *testing.T) {
+	mockMap, cm := setupCachingMapTest(t, false) // Test with non-batched for key-specific failures
+	mockMap.Contents = map[string]string{
+		"existing": "value",
+	}
+
+	// Set up some pending updates
+	cm.Desired().Set("key1", "value1") // This should succeed
+	cm.Desired().Set("key2", "value2") // This will fail
+	cm.Desired().Set("key3", "value3") // This should succeed
+	cm.Desired().Set("key4", "value4") // This will fail
+
+	// Configure specific failures
+	mockMap.UpdateFailures["key2"] = fmt.Errorf("update key2 failed")
+	mockMap.UpdateFailures["key4"] = fmt.Errorf("update key4 failed")
+
+	err := cm.ApplyUpdatesOnly()
+
+	// Should get an error slice with 2 errors
+	Expect(err).To(HaveOccurred())
+	errSlice, ok := err.(ErrSlice)
+	Expect(ok).To(BeTrue(), "Expected ErrSlice type")
+	Expect(len(errSlice)).To(Equal(2), "Expected 2 errors")
+
+	// Check that successful operations were applied
+	Expect(mockMap.Contents).To(Equal(map[string]string{
+		"existing": "value",
+		"key1":     "value1", // succeeded
+		"key3":     "value3", // succeeded
+		// key2 and key4 should not be present due to failures
+	}))
+
+	// All update calls should have been attempted
+	Expect(mockMap.UpdateCount).To(Equal(4))
+	Expect(mockMap.LoadCount).To(Equal(1))
+}
+
+// TestCachingMap_BatchDeleteFailures tests that when some deletions fail and some succeed
+// during ApplyDeletionsOnly, the successful ones are applied and errors are returned.
+func TestCachingMap_BatchDeleteFailures(t *testing.T) {
+	mockMap, cm := setupCachingMapTest(t, false) // Test with non-batched for key-specific failures
+	mockMap.Contents = map[string]string{
+		"key1": "value1",
+		"key2": "value2",
+		"key3": "value3",
+		"key4": "value4",
+		"keep": "keepvalue",
+	}
+
+	// Set up desired state that will cause deletions
+	cm.Desired().Set("keep", "keepvalue") // This key should remain
+
+	// Configure specific delete failures
+	mockMap.DeleteFailures["key2"] = fmt.Errorf("delete key2 failed")
+	mockMap.DeleteFailures["key4"] = fmt.Errorf("delete key4 failed")
+
+	err := cm.ApplyDeletionsOnly()
+
+	// Should get an error slice with 2 errors
+	Expect(err).To(HaveOccurred())
+	errSlice, ok := err.(ErrSlice)
+	Expect(ok).To(BeTrue(), "Expected ErrSlice type")
+	Expect(len(errSlice)).To(Equal(2), "Expected 2 errors")
+
+	// Check that successful deletions were applied
+	Expect(mockMap.Contents).To(Equal(map[string]string{
+		"key2": "value2",    // failed to delete
+		"key4": "value4",    // failed to delete
+		"keep": "keepvalue", // kept as desired
+	}))
+
+	// Deletion attempts should have been made
+	Expect(mockMap.DeleteCount).To(Equal(4)) // key1, key2, key3, key4 deletion attempts
+	Expect(mockMap.LoadCount).To(Equal(1))
+}
+
+// TestCachingMap_BatchAllChangesFailures tests that when both updates and deletions
+// have partial failures during ApplyAllChanges, successful operations are applied
+// and all errors are returned.
+func TestCachingMap_BatchAllChangesFailures(t *testing.T) {
+	mockMap, cm := setupCachingMapTest(t, false) // Test with non-batched for key-specific failures
+	mockMap.Contents = map[string]string{
+		"delete1": "value1",
+		"delete2": "value2", // Will fail to delete
+		"update1": "oldval", // Will be updated
+	}
+
+	// Set up desired state
+	cm.Desired().Set("update1", "newval")  // Update existing (should succeed)
+	cm.Desired().Set("update2", "newval2") // Add new (will fail)
+	cm.Desired().Set("update3", "newval3") // Add new (should succeed)
+
+	// Configure failures
+	mockMap.UpdateFailures["update2"] = fmt.Errorf("update2 failed")
+	mockMap.DeleteFailures["delete2"] = fmt.Errorf("delete2 failed")
+
+	err := cm.ApplyAllChanges()
+
+	// Should get errors from both deletions and updates
+	Expect(err).To(HaveOccurred())
+	errSlice, ok := err.(ErrSlice)
+	Expect(ok).To(BeTrue(), "Expected ErrSlice type")
+	Expect(len(errSlice)).To(Equal(2), "Expected 2 errors (1 delete + 1 update)")
+
+	// Check final state - successful operations should be applied
+	Expect(mockMap.Contents).To(Equal(map[string]string{
+		"delete2": "value2",  // failed to delete
+		"update1": "newval",  // successfully updated
+		"update3": "newval3", // successfully added
+		// update2 not present due to failure
+	}))
+
+	// Verify operation counts
+	Expect(mockMap.DeleteCount).To(Equal(2)) // delete1, delete2 attempts
+	Expect(mockMap.UpdateCount).To(Equal(3)) // update1, update2, update3 attempts
+	Expect(mockMap.LoadCount).To(Equal(1))
+}
+
 func setupCachingMapTest(t *testing.T, batched bool) (*Map, *CachingMap[string, string]) {
 	RegisterTestingT(t)
 
@@ -401,6 +521,10 @@ type Map struct {
 	LoadErr   error
 	UpdateErr error
 	DeleteErr error
+
+	// Maps to specify which specific keys should fail
+	UpdateFailures map[string]error
+	DeleteFailures map[string]error
 }
 
 var errNotExists = fmt.Errorf("does not exist")
@@ -412,6 +536,14 @@ func (m *Map) ErrIsNotExists(err error) bool {
 func (m *Map) Update(k, v string) error {
 	log.Debugf("Update(\"%s\", \"%s\")", k, v)
 	m.UpdateCount++
+
+	// Check for key-specific failure first
+	if m.UpdateFailures != nil {
+		if err, exists := m.UpdateFailures[k]; exists {
+			return err
+		}
+	}
+
 	if m.UpdateErr != nil {
 		return m.UpdateErr
 	}
@@ -435,6 +567,14 @@ func (m *Map) Get(k string) (string, error) {
 func (m *Map) Delete(k string) error {
 	log.Debugf("Delete(\"%s\")", k)
 	m.DeleteCount++
+
+	// Check for key-specific failure first
+	if m.DeleteFailures != nil {
+		if err, exists := m.DeleteFailures[k]; exists {
+			return err
+		}
+	}
+
 	if m.DeleteErr != nil {
 		return m.DeleteErr
 	}
@@ -458,7 +598,9 @@ func (m *Map) OpCount() int {
 
 func newMockMap() *Map {
 	m := &Map{
-		Contents: map[string]string{},
+		Contents:       map[string]string{},
+		UpdateFailures: make(map[string]error),
+		DeleteFailures: make(map[string]error),
 	}
 	return m
 }
