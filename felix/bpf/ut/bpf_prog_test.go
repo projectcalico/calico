7ecb56114f19412e9f85f78bd36786661f446594
Alex O'Regan <alex.oregan@tigera.io>
2025-08-26 13:51:23 +0100

    adapt some nat_test runs to program consistenthash LUTs, flag services as consistenthash, and check packets are forwarded correctly

--- felix/bpf/ut/bpf_prog_test.go
+++ felix/bpf/ut/bpf_prog_test.go
@@ -1389,7 +1389,6 @@ func dumpConsistentHashMap(chMap maps.Map) {
 	m, err := nat.LoadConsistentHashMap(chMap)
 	Expect(err).NotTo(HaveOccurred())
 	for k, v := range m {
-
 		fmt.Printf("%s: %s\n", k, v)
 	}
 }
