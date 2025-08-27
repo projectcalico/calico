4b4e06f41d9ff452b5473f51a08d580c374c4ed0
Casey Davenport <davenport.cas@gmail.com>
2025-08-27 11:32:49 -0700

    Clean up MTU

--- e2e/pkg/tests/networking/mtu.go
+++ e2e/pkg/tests/networking/mtu.go
@@ -12,12 +12,10 @@
 package networking
 
 import (
-	"context"
 	"fmt"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	v3 "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
 	"github.com/projectcalico/calico/e2e/pkg/describe"
 	"github.com/projectcalico/calico/e2e/pkg/utils"
 	"github.com/projectcalico/calico/e2e/pkg/utils/client"
@@ -67,13 +65,6 @@ var _ = describe.CalicoDescribe(
 			out, err := conncheck.ExecInPod(clientPod.Pod(), "sh", "-c", "ip link show eth0")
 			Expect(err).NotTo(HaveOccurred())
 
-			// // Get the MTU of the underlying network.
-			// underlayMTU, err := underlayMTU()
-			// Expect(err).NotTo(HaveOccurred())
-			//
-			// // Get expected overhead.
-			// overhead, err := encapOverhead(cli)
-			// Expect(err).NotTo(HaveOccurred())
 			expectedMTU := utils.ExpectedPodMTU(f)
 			Expect(expectedMTU).NotTo(BeNil(), "Expected MTU should not be nil")
 
@@ -81,61 +72,3 @@ var _ = describe.CalicoDescribe(
 			Expect(out).To(ContainSubstring(fmt.Sprintf("mtu %d", *expectedMTU)))
 		})
 	})
-
-// encapOverhead returns the expected overhead based on enabled encapsulations.
-// TODO: This should, strictly speaking, also check daemonset env vars and config files to determine enabled encap.
-//
-//	However, we don't currently use those methods to enable this in felix in any of our manifests.
-func encapOverhead(cli ctrlclient.Client) (int, error) {
-	// Get FelixConfig to look at Wireguard config and IPIP/VXLAN override configs
-	felixConfig := v3.NewFelixConfiguration()
-	err := cli.Get(context.TODO(), ctrlclient.ObjectKey{Name: "default"}, felixConfig)
-	if err != nil {
-		return 0, err
-	}
-
-	// Get IPIP, IPv4 VXLAN and IPv6 VXLAN encapsulation from the existing IP pools
-	poolIPIP, poolVXLAN, poolVXLANV6, err := utils.GetEncapsulationFromPools(cli)
-	if err != nil {
-		return 0, err
-	}
-
-	// The pod MTU can only be the same for IPv4 and IPv6, thus it must be the
-	// smallest value possible according to the enabled encapsulation. Hence,
-	// return the largest overhead.
-	vxlanV6 := felixConfig.Spec.VXLANEnabled
-	if vxlanV6 == nil {
-		vxlanV6 = &poolVXLANV6
-	}
-	if vxlanV6 != nil && *vxlanV6 {
-		// IPv6 VXLAN is enabled, overhead is 70 bytes.
-		return 70, nil
-	}
-
-	wg := felixConfig.Spec.WireguardEnabled
-	if wg != nil && *wg {
-		// IPv4 Wireguard is enabled, overhead is 60 bytes.
-		return 60, nil
-	}
-
-	vxlan := felixConfig.Spec.VXLANEnabled
-	if vxlan == nil {
-		vxlan = &poolVXLAN
-	}
-	if vxlan != nil && *vxlan {
-		// IPv4 VXLAN is enabled, overhead is 50 bytes.
-		return 50, nil
-	}
-
-	ipip := felixConfig.Spec.IPIPEnabled
-	if ipip == nil {
-		ipip = &poolIPIP
-	}
-	if ipip != nil && *ipip {
-		// IPv4 IPIP is enabled, overhead is 20 bytes.
-		return 20, nil
-	}
-
-	// No encap is enabled, overhead is 0.
-	return 0, nil
-}
