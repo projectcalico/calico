c901cdff02e571f16d9be43cec62eb4b7e504736
Shaun Crampton <shaun@tigera.io>
2025-09-01 14:02:27 +0100

    Don't assume ordering of deletion events.

--- libcalico-go/lib/clientv3/bgpfilter_e2e_test.go
+++ libcalico-go/lib/clientv3/bgpfilter_e2e_test.go
@@ -473,7 +473,7 @@ var _ = testutils.E2eDatastoreDescribe("BGPFilter tests", testutils.DatastoreAll
 
 					By("Cleaning the datastore and expecting deletion events for each configured resource (tests prefix deletes results in individual events for each key)")
 					be.Clean()
-					testWatcher4.ExpectEvents(apiv3.KindBGPFilter, []watch.Event{
+					testWatcher4.ExpectEventsAnyOrder(apiv3.KindBGPFilter, []watch.Event{
 						{
 							Type:     watch.Deleted,
 							Previous: outRes1,
--- libcalico-go/lib/clientv3/felixconfig_e2e_test.go
+++ libcalico-go/lib/clientv3/felixconfig_e2e_test.go
@@ -460,7 +460,7 @@ var _ = testutils.E2eDatastoreDescribe("FelixConfiguration tests", testutils.Dat
 
 			By("Cleaning the datastore and expecting deletion events for each configured resource (tests prefix deletes results in individual events for each key)")
 			be.Clean()
-			testWatcher4.ExpectEvents(apiv3.KindFelixConfiguration, []watch.Event{
+			testWatcher4.ExpectEventsAnyOrder(apiv3.KindFelixConfiguration, []watch.Event{
 				{
 					Type:     watch.Deleted,
 					Previous: outRes1,
