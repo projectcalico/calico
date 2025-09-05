b8fc869c17a5ae8f7f73496443456ed0a8da98dd
Tomas Hruby <tomas@tigera.io>
2025-09-05 13:45:27 -0700

    enable kube-proxy healthz server
    Up until now we were only running the service health, but not the proxy
    health. The service listeners were started within the healthz server,
    but the main LIstener for the proxy needs to kick off Run().
    
    refs https://github.com/projectcalico/calico/issues/10748

--- felix/bpf/proxy/proxy.go
+++ felix/bpf/proxy/proxy.go
@@ -111,9 +111,11 @@ type proxy struct {
 	svcHealthServer healthcheck.ServiceHealthServer
 	healthzServer   healthcheckIntf
 
-	stopCh   chan struct{}
-	stopWg   sync.WaitGroup
-	stopOnce sync.Once
+	stopCh      chan struct{}
+	stopWg      sync.WaitGroup
+	stopOnce    sync.Once
+	cancelCtx   context.Context
+	cancelCtxFn context.CancelFunc
 }
 
 type healthcheckIntf interface {
@@ -152,6 +154,8 @@ func New(k8s kubernetes.Interface, dp DPSyncer, hostname string, opts ...Option)
 		stopCh:        make(chan struct{}),
 		healthzServer: new(alwaysHealthy),
 	}
+
+	p.cancelCtx, p.cancelCtxFn = context.WithCancel(context.Background())
 
 	for _, o := range opts {
 		if err := o(p); err != nil {
@@ -207,6 +211,21 @@ func New(k8s kubernetes.Interface, dp DPSyncer, hostname string, opts ...Option)
 	p.startRoutine(func() { informerFactory.Start(p.stopCh) })
 	p.startRoutine(func() { svcConfig.Run(p.stopCh) })
 
+	// We cannot wait for the healthz server as we cannot stop it.
+	go func() {
+		for {
+			err := p.healthzServer.Run(p.cancelCtx)
+			if err != nil {
+				log.WithError(err).Error("Healthz server failed, restarting")
+			}
+			select {
+			case <-p.cancelCtx.Done():
+				return
+			default:
+			}
+		}
+	}()
+
 	return p, nil
 }
 
@@ -226,6 +245,7 @@ func (p *proxy) setIpFamily(ipFamily int) {
 func (p *proxy) Stop() {
 	p.stopOnce.Do(func() {
 		log.Info("Proxy stopping")
+		p.cancelCtxFn()
 		// Pass empty update to close all the health checks.
 		_ = p.svcHealthServer.SyncServices(map[types.NamespacedName]uint16{})
 		_ = p.svcHealthServer.SyncEndpoints(map[types.NamespacedName]int{})
