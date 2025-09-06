741c2d72e9d7a14c2e97ecabc99f30ab7c70b074
Jiawei Huang <jiawei@tigera.io>
2025-09-05 21:04:00 -0700

    Fix cni-plugin static checks

--- cni-plugin/tests/calico_cni_test.go
+++ cni-plugin/tests/calico_cni_test.go
@@ -895,7 +895,7 @@ var _ = Describe("CalicoCni", func() {
 			Expect(err).ToNot(HaveOccurred())
 
 			go func() {
-				defer stdin.Close()
+				defer func() { _ = stdin.Close() }()
 				_, _ = io.WriteString(stdin, netconf)
 			}()
 
@@ -926,7 +926,7 @@ var _ = Describe("CalicoCni", func() {
 			Expect(err).ToNot(HaveOccurred())
 
 			go func() {
-				defer stdin.Close()
+				defer func() { _ = stdin.Close() }()
 				_, _ = io.WriteString(stdin, netconf)
 			}()
 
