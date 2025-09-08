aa84ecf6b9b08260a4154b4676be8de9890c0030
Kishen Viswanathan <kishen.viswanathan@ibm.com>
2025-09-08 12:01:16 +0530

    Update birdcl path to /bin/birdcl

--- node/pkg/health/health.go
+++ node/pkg/health/health.go
@@ -89,7 +89,7 @@ func RunOutput(bird, bird6, felixReady, felixLive, birdLive, bird6Live bool, thr
 			}
 
 			// Check that BIRD is actually responding to commands.
-			out, err := exec.Command("/usr/bin/birdcl", "-s", "/var/run/calico/bird.ctl", "show", "status").Output()
+			out, err := exec.Command("/bin/birdcl", "-s", "/var/run/calico/bird.ctl", "show", "status").Output()
 			if err != nil {
 				return fmt.Errorf("calico/node is not ready: bird is not live: %+v", err)
 			}
@@ -108,7 +108,7 @@ func RunOutput(bird, bird6, felixReady, felixLive, birdLive, bird6Live bool, thr
 			}
 
 			// Check that BIRD is actually responding to commands.
-			out, err := exec.Command("/usr/bin/birdcl", "-s", "/var/run/calico/bird6.ctl", "show", "status").Output()
+			out, err := exec.Command("/bin/birdcl", "-s", "/var/run/calico/bird6.ctl", "show", "status").Output()
 			if err != nil {
 				return fmt.Errorf("calico/node is not ready: bird6 is not live: %+v", err)
 			}
