6e5241536b4cb7c203cf8048f11d54d8f2af7ab7
sridhar <sridhar@tigera.io>
2025-09-16 11:28:29 -0700

    Address review comments

--- felix/dataplane/linux/bpf_ep_mgr.go
+++ felix/dataplane/linux/bpf_ep_mgr.go
@@ -1090,9 +1090,7 @@ func cleanupTcxPins(iface string) {
 				Hook:  attachHook,
 			},
 		}
-		if err := ap.DetachTcxProgram(); err != nil {
-			log.WithError(err).Warnf("Failed to detach old tcx program from now gone device '%s' hook %s", iface, attachHook)
-		}
+		os.Remove(ap.ProgPinPath())
 	}
 }
 
