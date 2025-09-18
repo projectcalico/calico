3d80291543fe1f9fa4ad0d4d413800fb94d7809a
MichalFupso <michal@tigera.io>
2025-09-18 10:02:57 -0700

    Add check to see if projectcalico/v3 api is available (#11040) (#11042)
    * Add check to see if projectcalico/v3 api is available

--- e2e/pkg/utils/client/client.go
+++ e2e/pkg/utils/client/client.go
@@ -15,12 +15,11 @@
 package client
 
 import (
-	"context"
-
 	v3 "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
 	"github.com/sirupsen/logrus"
 	operatorv1 "github.com/tigera/operator/api/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -33,17 +32,26 @@ func New(cfg *rest.Config) (client.Client, error) {
 		return nil, err
 	}
 
-	// Check to see if an APIServer is available.
-	l := &operatorv1.APIServerList{}
-	err = c.List(context.TODO(), l)
-	if err == nil && len(l.Items) > 0 {
-		logrus.Infof("Using API server client for projectcalico.org/v3 API")
-		return c, nil
-	} else {
-		logrus.WithError(err).Infof("Falling back to calicoctl exec client for projectcalico.org/v3 API")
+	discoveryClient, err := discovery.NewDiscoveryClientForConfig(cfg)
+	if err != nil {
+		return nil, err
 	}
 
-	// No API server available, fall back to calicoctl exec client.
+	// Checks to see if the projectcalico.org/v3 API is available.
+	available, err := calicoV3APIAvailable(discoveryClient)
+	if err != nil {
+		return nil, err
+	}
+
+	if available {
+		// API is available, we can return the calicoclient
+		logrus.Infof("Using API server client for projectcalico.org/v3 API")
+		return c, nil
+	}
+
+	// If the projectcalico.org/v3 apigroup is not found,
+	// then we can assume that the API server is not present and default to calicoctl.
+	logrus.Infof("projectcalico.org/v3 API not available, falling back to calicoctl exec client")
 	return NewCalicoctlExecClient(c)
 }
 
@@ -81,4 +89,21 @@ func newScheme() (*runtime.Scheme, error) {
 		return nil, err
 	}
 	return scheme, nil
+}
+
+func calicoV3APIAvailable(discoveryClient discovery.DiscoveryInterface) (bool, error) {
+	groups, err := discoveryClient.ServerGroups()
+	if err != nil {
+		return false, err
+	}
+	for _, group := range groups.Groups {
+		if group.Name == "projectcalico.org" {
+			for _, version := range group.Versions {
+				if version.Version == "v3" {
+					return true, nil
+				}
+			}
+		}
+	}
+	return false, nil
 }
