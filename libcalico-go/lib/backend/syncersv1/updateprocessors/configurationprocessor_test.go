f1f2e865dcb19228a1cd2980b56aa7d78941969f
Tomas Hruby <tomas@tigera.io>
2025-09-19 11:39:44 -0700

    update numBaseFelixConfigs

--- libcalico-go/lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
+++ libcalico-go/lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
@@ -43,7 +43,7 @@ const (
 )
 
 const (
-	numBaseFelixConfigs = 167
+	numBaseFelixConfigs = 168
 )
 
 var _ = Describe("Test the generic configuration update processor and the concrete implementations", func() {
