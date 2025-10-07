5aa9da443f05aaacffd19e83f6bcd94ed14eea52
Tomas Hruby <tomas@tigera.io>
2025-10-06 17:05:40 -0700

    place the CALI_DEBUG strings in .rodata.cali_debug
    This avoid mixing the global variables and the CALI_DEBUG strings and
    braking initial configuration.

--- felix/bpf/ut/bpf_prog_test.go
+++ felix/bpf/ut/bpf_prog_test.go
@@ -763,7 +763,10 @@ func objLoad(fname, bpfFsDir, ipFamily string, topts testOpts, polProg, hasHostC
 
 	for m, err := obj.FirstMap(); m != nil && err == nil; m, err = m.NextMap() {
 		if m.IsMapInternal() {
-			if strings.Contains(m.Name(), ".rodata") {
+			if ipFamily != "preamble" {
+				continue
+			}
+			if !strings.HasSuffix(m.Name(), ".rodata") {
 				continue
 			}
 			if forXDP {
@@ -931,6 +934,9 @@ func objUTLoad(fname, bpfFsDir, ipFamily string, topts testOpts, polProg, hasHos
 
 	for m, err := obj.FirstMap(); m != nil && err == nil; m, err = m.NextMap() {
 		if m.IsMapInternal() {
+			if !strings.HasSuffix(m.Name(), ".rodata") {
+				continue
+			}
 			globals := libbpf.TcGlobalData{
 				Tmtu:       natTunnelMTU,
 				VxlanPort:  testVxlanPort,
