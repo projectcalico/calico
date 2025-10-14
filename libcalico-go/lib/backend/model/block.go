73745d41acec70f511dbfdb4617fe9158436d956
Shaun Crampton <shaun@tigera.io>
2025-10-14 15:20:57 +0100

    Apply suggestion from @caseydavenport
    Co-authored-by: Casey Davenport <caseydavenport@users.noreply.github.com>

--- libcalico-go/lib/backend/model/block.go
+++ libcalico-go/lib/backend/model/block.go
@@ -115,6 +115,7 @@ type AllocationBlock struct {
 	// "host:<hostname>". If not set, this block is not affine to a host.
 	Affinity *string `json:"affinity"`
 
+
 	// Time the affinity was claimed; may be zero for old blocks that predate
 	// this field.
 	AffinityClaimTime *metav1.Time `json:"affinity_claim_time,omitempty"`
