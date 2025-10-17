6e08956b0ea71a53ea9157a55aa008a2e5a78f03
Casey Davenport <davenport.cas@gmail.com>
2025-10-17 14:51:50 -0700

    Use proper type for NftablesMode constants

--- api/pkg/openapi/generated.openapi.go
+++ api/pkg/openapi/generated.openapi.go
@@ -3125,10 +3125,10 @@ func schema_pkg_apis_projectcalico_v3_FelixConfigurationSpec(ref common.Referenc
 					},
 					"nftablesMode": {
 						SchemaProps: spec.SchemaProps{
-							Description: "NFTablesMode configures nftables support in Felix. [Default: Disabled]",
+							Description: "NFTablesMode configures nftables support in Felix. [Default: Disabled]\n\nPossible enum values:\n - `\"Disabled\"`\n - `\"Enabled\"`",
 							Type:        []string{"string"},
 							Format:      "",
-							Enum:        []interface{}{},
+							Enum:        []interface{}{"Disabled", "Enabled"},
 						},
 					},
 					"nftablesRefreshInterval": {
