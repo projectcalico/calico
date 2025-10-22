9d0e730f80ad10da652cc72954d13a5503ace5ec
Ronan Conneely <139970314+ronanc-tigera@users.noreply.github.com>
2025-10-22 15:47:23 +0100

    add shadcn and tabs component (#6)

---
+++ whisker/src/context/VariantProvider/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const VariantContext = React.createContext<string>('default');
+
+export const useVariant = <T extends string = string>(): T =>
+    React.useContext(VariantContext) as T;
+
+type VariantProviderProps = {
+    children: React.ReactNode;
+    variant: string;
+};
+
+const VariantProvider: React.FC<VariantProviderProps> = ({
+    children,
+    variant = 'default',
+}) => {
+    return (
+        <VariantContext.Provider value={variant}>
+            {children}
+        </VariantContext.Provider>
+    );
+};
+
+export default VariantProvider;
