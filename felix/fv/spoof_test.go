487ab78974270f165887dc2e00407a0c126f4929
Mazdak Nasab <mazdak.nasab@gmail.com>
2025-10-24 12:10:46 -0700

    fix formatiing

--- felix/fv/spoof_test.go
+++ felix/fv/spoof_test.go
@@ -21,12 +21,12 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	api "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
-	client "github.com/projectcalico/calico/libcalico-go/lib/clientv3"
 
 	"github.com/projectcalico/calico/felix/fv/connectivity"
 	"github.com/projectcalico/calico/felix/fv/containers"
 	"github.com/projectcalico/calico/felix/fv/infrastructure"
 	"github.com/projectcalico/calico/felix/fv/workload"
+	client "github.com/projectcalico/calico/libcalico-go/lib/clientv3"
 )
 
 var _ = Describe("Spoof tests", func() {
