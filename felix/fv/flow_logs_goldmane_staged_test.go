1da2629de06e4ea75ad7f661cc3b81b1dad13d88
Mazdak Nasab <mazdak.nasab@gmail.com>
2025-10-28 17:07:46 -0700

    cleanup

--- felix/fv/flow_logs_goldmane_staged_test.go
+++ felix/fv/flow_logs_goldmane_staged_test.go
@@ -1663,7 +1663,7 @@ var _ = infrastructure.DatastoreDescribe("_BPF-SAFE_ goldmane flow log with stag
 		time.Sleep(5 * time.Second)
 	}
 
-	It("pepper get expected flow logs with pending policies", func() {
+	It("get expected flow logs with pending policies", func() {
 		// Describe the connectivity that we now expect.
 		// For ep1_1 -> ep2_1 we use the service cluster IP to test service info in the flow log
 		cc = &connectivity.Checker{}
