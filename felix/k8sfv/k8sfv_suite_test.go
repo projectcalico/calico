93885e18fb3ce4933500d72654de0e31fed6a37e
Shaun Crampton <shaun@tigera.io>
2025-11-12 15:56:56 +0000

    Tweak report name for typha/non-typha k8sfv.

--- felix/k8sfv/k8sfv_suite_test.go
+++ felix/k8sfv/k8sfv_suite_test.go
@@ -44,10 +44,12 @@ func init() {
 func TestK8sFV(t *testing.T) {
 	RegisterFailHandler(Fail)
 	// The run-test script runs this file from k8sfv/output.
-	junitReporter := reporters.NewJUnitReporter("../../report/felix_k8sfv_suite.xml")
-	withTypha := ""
+	nameTypha := ""
+	fileTypha := ""
 	if os.Getenv("USE_TYPHA") == "true" {
-		withTypha = " (typha)"
+		nameTypha = " (typha)"
+		fileTypha = "typha_"
 	}
-	RunSpecsWithDefaultAndCustomReporters(t, "k8sfv tests"+withTypha, []Reporter{junitReporter})
+	junitReporter := reporters.NewJUnitReporter("../../report/felix_k8sfv_" + fileTypha + "suite.xml")
+	RunSpecsWithDefaultAndCustomReporters(t, "k8sfv tests"+nameTypha, []Reporter{junitReporter})
 }
