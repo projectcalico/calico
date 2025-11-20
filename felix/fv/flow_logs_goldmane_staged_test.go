702cd0d63aacf7420eb75d6e3acae1dc29c28de5
Casey Davenport <davenport.cas@gmail.com>
2025-11-20 12:57:03 -0800

    Fix BPF FV policy name matching

--- felix/fv/flow_logs_goldmane_staged_test.go
+++ felix/fv/flow_logs_goldmane_staged_test.go
@@ -1417,10 +1417,10 @@ var _ = infrastructure.DatastoreDescribe("_BPF-SAFE_ goldmane flow log with stag
 			Consistently(getRuleFunc(tc.Felixes[0], "staged"), "5s", "1s").Should(HaveOccurred())
 			Consistently(getRuleFunc(tc.Felixes[1], "staged"), "5s", "1s").Should(HaveOccurred())
 		} else {
-			bpfWaitForPolicy(tc.Felixes[0], ep1_1.InterfaceName, "egress", "NetworkPolicy/default/tier1.np1-1")
-			bpfWaitForPolicy(tc.Felixes[0], ep1_1.InterfaceName, "ingress", "NetworkPolicy/default/tier1.np1-1")
-			bpfWaitForPolicy(tc.Felixes[1], ep2_1.InterfaceName, "egress", "NetworkPolicy/default/tier1.np1-1")
-			bpfWaitForPolicy(tc.Felixes[1], ep2_1.InterfaceName, "ingress", "NetworkPolicy/default/tier1.np1-1")
+			bpfWaitForPolicy(tc.Felixes[0], ep1_1.InterfaceName, "egress", "tier1.np1-1")
+			bpfWaitForPolicy(tc.Felixes[0], ep1_1.InterfaceName, "ingress", "tier1.np1-1")
+			bpfWaitForPolicy(tc.Felixes[1], ep2_1.InterfaceName, "egress", "tier1.np1-1")
+			bpfWaitForPolicy(tc.Felixes[1], ep2_1.InterfaceName, "ingress", "tier1.np1-1")
 			// When policies are programmed, make sure no staged policy is programmed. Staged policies must be skipped.
 			Consistently(bpfDumpPolicy(tc.Felixes[0], ep1_1.InterfaceName, "ingress"), "5s", "1s").ShouldNot(ContainSubstring("staged"))
 			Consistently(bpfDumpPolicy(tc.Felixes[0], ep1_1.InterfaceName, "egress"), "5s", "1s").ShouldNot(ContainSubstring("staged"))
