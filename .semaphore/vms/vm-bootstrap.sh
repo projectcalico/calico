ed3eb5d0001858ef470f9e2911f2d1751772a98a
Shaun Crampton <shaun@tigera.io>
2025-11-21 13:44:06 +0000

    Pin docker versions.  Rename create-vm to configure-vm

--- .semaphore/vms/vm-bootstrap.sh
+++ .semaphore/vms/vm-bootstrap.sh
@@ -10,17 +10,31 @@ install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 
+ubuntu_codename=$(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+
 # Add the repository to Apt sources:
 tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
-Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Suites: $ubuntu_codename
 Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
+# Pin the docker version; the node tests use an older docker client so we can't
+# let it float.
+if [ "$ubuntu_codename" = "jammy" ]; then
+  docker_version="=5:20.10.14~3-0~ubuntu-jammy"
+elif [ "$ubuntu_codename" = "noble" ]; then
+  docker_version="=5:27.5.1-1~ubuntu.24.04~noble"
+elif [ "$ubuntu_codename" = "plucky" ]; then
+  docker_version="=5:28.3.3-1~ubuntu.25.04~plucky"
+else
+  docker_version=""
+fi
+
 apt-get update -y
-apt-get install -y --no-install-recommends git docker-ce docker-ce-cli docker-buildx-plugin containerd.io make iproute2 wireguard
+apt-get install -y --no-install-recommends git docker-ce"${docker_version}" docker-ce-cli"${docker_version}" docker-buildx-plugin containerd.io make iproute2 wireguard
 usermod -a -G docker ubuntu
 modprobe ipip
 if [ -s /etc/docker/daemon.json ] ; then
