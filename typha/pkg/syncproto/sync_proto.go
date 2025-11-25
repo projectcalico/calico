bf08da2ce9bb5b5ca01daa88b2d8368ddea3804a
Casey Davenport <davenport.cas@gmail.com>
2025-11-25 10:50:24 -0800

    Fix comment

--- typha/pkg/syncproto/sync_proto.go
+++ typha/pkg/syncproto/sync_proto.go
@@ -258,7 +258,7 @@ type MsgClientHello struct {
 	SupportsDecoderRestart         bool
 	SupportedCompressionAlgorithms []CompressionAlgorithm
 
-	// SupportsNodeResourceUpdates provides to the server whether this client supports modern PolicyKey
+	// SupportsModernPolicyKeys tells the server whether this client supports modern PolicyKey
 	// syntax, i.e., using Kind/Namespace/Name instead of Tier/Name. If the client does not set this field,
 	// Typha will reject the connection attempt and wait for the client to be upgraded.
 	SupportsModernPolicyKeys bool
