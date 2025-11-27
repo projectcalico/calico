aca91c92bb8142cfd118a95033edee9d77224e4c
song jiang <song@tigera.io>
2025-11-27 14:11:11 +0000

    Upate

--- cni-plugin/win_tests/calico_cni_k8s_windows_test.go
+++ cni-plugin/win_tests/calico_cni_k8s_windows_test.go
@@ -392,6 +392,9 @@ var _ = Describe("Kubernetes CNI tests", func() {
 				Expect(hostEP.IPAddress.String()).Should(Equal("10.254.112.2"))
 				Expect(strings.EqualFold(hostEP.VirtualNetwork, hnsNetwork.Id)).To(BeTrue())
 				Expect(hostEP.VirtualNetworkName).Should(Equal(hnsNetwork.Name))
+
+				log.Infof("Creating container")
+				containerID, result, contVeth, contAddresses, contRoutes, err := testutils.CreateContainer(netconf, name, testutils.HnsNoneNs, "", nsName)
 				Expect(err).ShouldNot(HaveOccurred())
 				defer func() {
 					log.Infof("Container Delete  call")
