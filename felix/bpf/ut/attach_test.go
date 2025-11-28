19ba1675944fa51ce899cc83733030621c19948e
sridhar <sridhar@tigera.io>
2025-11-28 09:23:22 -0800

    Fix tests

--- felix/bpf/ut/attach_test.go
+++ felix/bpf/ut/attach_test.go
@@ -601,7 +601,7 @@ func runAttachTest(t *testing.T, ipv6Enabled bool) {
 
 		// And they still have the same data so that existing preamble programs
 		// and policies can still point to the right stuff.
-		oldProgsIngParams := hook.IngressProgramsMapParameters
+		oldProgsIngParams := hook.FromHostProgramsMapParameters
 		oldProgsIngParams.PinDir = tmp
 		oldProgs := maps.NewPinnedMap(oldProgsIngParams)
 		err = oldProgs.Open()
@@ -615,7 +615,7 @@ func runAttachTest(t *testing.T, ipv6Enabled bool) {
 		}
 		Expect(pm).To(HaveLen(programsCount))
 
-		oldPoliciesParams := jump.IngressMapParameters
+		oldPoliciesParams := jump.FromHostMapParameters
 		oldPoliciesParams.PinDir = tmp
 		oldPolicies := maps.NewPinnedMap(oldPoliciesParams)
 		err = oldPolicies.Open()
