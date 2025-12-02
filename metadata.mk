9871e76a231b645fa506fa76897908c3242c7463
marvin-tigera <marvin@projectcalico.io>
2025-12-02 20:38:32 +0000

    Automatic Pin Updates

--- metadata.mk
+++ metadata.mk
@@ -3,8 +3,8 @@
 #################################################################################################
 
 # The version of calico/go-build and calico/base to use.
-GO_BUILD_VER=1.25.4-llvm18.1.8-k8s1.34.1
-CALICO_BASE_VER=ubi9-1762560525
+GO_BUILD_VER=1.25.5-llvm18.1.8-k8s1.34.2
+CALICO_BASE_VER=ubi9-1764706938
 
 # Env var to ACK Ginkgo deprecation warnings, may need updating with go-build.
 ACK_GINKGO=ACK_GINKGO_DEPRECATIONS=1.16.5
