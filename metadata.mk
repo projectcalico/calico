4b23df2ecfce54c68697fbf3605587c94de8af1a
Jiawei Huang <jiawei@tigera.io>
2025-12-05 18:36:49 -0800

    Update golang to v1.24.11 and update calico/base pin

--- metadata.mk
+++ metadata.mk
@@ -3,8 +3,8 @@
 #################################################################################################
 
 # The version of calico/go-build and calico/base to use.
-GO_BUILD_VER=1.24.9-llvm18.1.8-k8s1.33.5
-CALICO_BASE_VER=ubi9-1762560525
+GO_BUILD_VER=1.24.11-llvm18.1.8-k8s1.33.6
+CALICO_BASE_VER=ubi9-1764972441
 
 # Env var to ACK Ginkgo deprecation warnings, may need updating with go-build.
 ACK_GINKGO=ACK_GINKGO_DEPRECATIONS=1.16.5
