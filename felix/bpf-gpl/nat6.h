42a282d2dfa4a532056258467125003d11c2f28e
Tomas Hruby <tomas@tigera.io>
2026-01-05 15:10:46 -0800

    update copyright

--- felix/bpf-gpl/nat6.h
+++ felix/bpf-gpl/nat6.h
@@ -1,5 +1,5 @@
 // Project Calico BPF dataplane programs.
-// Copyright (c) 2020-2023 Tigera, Inc. All rights reserved.
+// Copyright (c) 2020-2026 Tigera, Inc. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
 
 #ifndef __CALI_NAT6_H__
