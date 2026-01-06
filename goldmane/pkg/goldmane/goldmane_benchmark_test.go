84ff875db852c3b1abaecaf2f16801bde928bb7b
skoryk-oleksandr <skorichok88@gmail.com>
2026-01-06 14:21:33 -0800

    CORE-11900: reuse utils function

--- goldmane/pkg/goldmane/goldmane_benchmark_test.go
+++ goldmane/pkg/goldmane/goldmane_benchmark_test.go
@@ -15,7 +15,7 @@
 package goldmane_test
 
 import (
-	"fmt"
+	"github.com/projectcalico/calico/goldmane/pkg/testutils"
 	"math/rand/v2"
 	"os"
 	"runtime"
@@ -26,7 +26,6 @@ import (
 
 	"github.com/projectcalico/calico/goldmane/pkg/goldmane"
 	"github.com/projectcalico/calico/goldmane/pkg/types"
-	"github.com/projectcalico/calico/goldmane/proto"
 	"github.com/projectcalico/calico/lib/std/time"
 )
 
@@ -49,7 +48,7 @@ func init() {
 		flowStartTime := now.Add(time.Duration(randomNumber) * time.Millisecond)
 		flows = append(flows,
 			types.ProtoToFlow(
-				newRandomFlow(flowStartTime.Unix()),
+				testutils.NewRandomFlow(flowStartTime.Unix()),
 			),
 		)
 	}
@@ -178,130 +177,4 @@ func BenchmarkGoldmaneReceiveParallel(b *testing.B) {
 	if heapAllocMB > maxHeapAllocMBParallel {
 		b.Fatalf("HeapAllocMB too high: got %.2f MB, max %.2f MB", heapAllocMB, maxHeapAllocMBParallel)
 	}
-}
-
-func newRandomFlow(start int64) *proto.Flow {
-	labelCount := 10
-	srcNames := map[int]string{
-		0: "client-aggr-1",
-		1: "client-aggr-2",
-		2: "client-aggr-3",
-		3: "client-aggr-4",
-	}
-	dstNames := map[int]string{
-		0: "server-aggr-1",
-		1: "server-aggr-2",
-		2: "server-aggr-3",
-		3: "server-aggr-4",
-	}
-	actions := map[int]proto.Action{
-		0: proto.Action_Allow,
-		1: proto.Action_Deny,
-	}
-	reporters := map[int]proto.Reporter{
-		0: proto.Reporter_Src,
-		1: proto.Reporter_Dst,
-	}
-	services := map[int]string{
-		0: "frontend-service",
-		1: "backend-service",
-		2: "db-service",
-	}
-	namespaces := map[int]string{
-		0: "test-ns",
-		1: "test-ns-2",
-		2: "test-ns-3",
-		3: "test-ns-4",
-		4: "test-ns-5",
-	}
-	tiers := map[int]string{
-		0: "tier-1",
-		1: "tier-2",
-		2: "tier-3",
-		3: "default",
-	}
-	policies := map[int]string{
-		0: "policy-1",
-		1: "policy-2",
-		2: "policy-3",
-		3: "policy-4",
-		4: "policy-5",
-	}
-	indices := map[int]int64{
-		0: 0,
-		1: 1,
-		2: 2,
-		3: 3,
-	}
-
-	dstNs := randomFromMap(namespaces)
-	srcNs := randomFromMap(namespaces)
-	action := randomFromMap(actions)
-	reporter := randomFromMap(reporters)
-	polNs := dstNs
-	if reporter == proto.Reporter_Src {
-		polNs = srcNs
-	}
-	f := &proto.Flow{
-		Key: &proto.FlowKey{
-			SourceName:           randomFromMap(srcNames),
-			SourceNamespace:      srcNs,
-			DestName:             randomFromMap(dstNames),
-			DestNamespace:        dstNs,
-			Proto:                "tcp",
-			Action:               action,
-			Reporter:             reporter,
-			DestServiceName:      randomFromMap(services),
-			DestServicePort:      80,
-			DestServiceNamespace: dstNs,
-			Policies: &proto.PolicyTrace{
-				EnforcedPolicies: []*proto.PolicyHit{
-					{
-						Kind:        proto.PolicyKind_CalicoNetworkPolicy,
-						Tier:        randomFromMap(tiers),
-						Name:        randomFromMap(policies),
-						Namespace:   polNs,
-						Action:      action,
-						PolicyIndex: randomFromMap(indices),
-						RuleIndex:   0,
-					},
-					{
-						Kind:        proto.PolicyKind_CalicoNetworkPolicy,
-						Tier:        "default",
-						Name:        "default-allow",
-						Namespace:   "default",
-						Action:      proto.Action_Allow,
-						PolicyIndex: 1,
-						RuleIndex:   1,
-					},
-				},
-			},
-		},
-		StartTime:               start,
-		EndTime:                 start + 1,
-		BytesIn:                 100,
-		BytesOut:                200,
-		PacketsIn:               10,
-		PacketsOut:              20,
-		NumConnectionsStarted:   1,
-		NumConnectionsLive:      2,
-		NumConnectionsCompleted: 3,
-	}
-
-	f.SourceLabels = make([]string, labelCount)
-	f.DestLabels = make([]string, labelCount)
-	for i := 0; i < labelCount; i++ {
-		f.SourceLabels[i] = fmt.Sprintf("srcLabel%d=value", i)
-		f.DestLabels[i] = fmt.Sprintf("dstLabel%d=value", i)
-	}
-
-	// For now, just copy the enforced policies to the pending policies. This is
-	// equivalent to there being no staged policies in the trace.
-	f.Key.Policies.PendingPolicies = f.Key.Policies.EnforcedPolicies
-	return f
-}
-
-func randomFromMap[E comparable](m map[int]E) E {
-	// Generate a random number within the size of the map.
-	return m[rand.IntN(len(m))]
 }
