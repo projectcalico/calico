5404e99ebe8fdf93d77cf821d4e27c19e50d3a05
Pedro Coutinho <pedro@tigera.io>
2026-01-19 15:59:58 -0800

    [CORE-12132] Move windows e2es to azr-aso provisioner
    change semaphore machine to f1
    
    add dummy needrestart script when needed
    
    add installer: operator configuration
    
    add new RUN_LOCAL_TESTS option to e2e tests and use them on the windows pipeline
    
    use azr-aso provisioner for iptables run
    
    skip linux only e2es and temporarily disable bpf and nft
    
    add env var to fix porter pods
    
    fix conncheck TCP command for Windows

--- e2e/pkg/tests/policy/policy.go
+++ e2e/pkg/tests/policy/policy.go
@@ -41,6 +41,7 @@ var _ = describe.CalicoDescribe(
 	describe.WithTeam(describe.Core),
 	describe.WithFeature("NetworkPolicy"),
 	describe.WithCategory(describe.Policy),
+	describe.WithWindows(),
 	"Calico NetworkPolicy",
 	func() {
 		// Define variables common across all tests.
@@ -93,7 +94,7 @@ var _ = describe.CalicoDescribe(
 		// - Creating a default-deny policy applied to both namespaces using a GlobalNetworkPolicy.
 		// - Isolating both namespaces with ingress and egress policies
 		// - Asserting only same namespace connectivity exists.
-		framework.ConformanceIt("should correctly isolate namespaces [RunsOnWindows]", func() {
+		framework.ConformanceIt("should correctly isolate namespaces", func() {
 			nsA := f.Namespace
 
 			By("Creating a second namespace B")
