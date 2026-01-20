87b7bfd9c9f1ce1e8710c1d265463b77dfe52616
Shaun Crampton <shaun@tigera.io>
2026-01-20 11:38:43 +0000

    Felix docs: add heuristic for YAML port ranges.

--- felix/config/metadata.go
+++ felix/config/metadata.go
@@ -217,6 +217,11 @@ func CombinedFieldInfo() ([]*FieldInfo, error) {
 			// String schema tends to have the ranges, which are missing from the YAML.
 			pm.YAMLSchema = pm.StringSchema
 		}
+		if pm.GoType == "*numorstring.Port" {
+			// The Port type has its own string encoding.
+			pm.YAMLSchema = "Port range: either a single number in [0,65535] or a range of numbers `n:m`"
+		}
+
 		pm.StringSchemaHTML = convertSchemaToHTML(pm.StringSchema)
 		pm.YAMLSchemaHTML = convertSchemaToHTML(pm.YAMLSchema)
 		pm.DescriptionHTML = convertDescriptionToHTML(pm.Description)
