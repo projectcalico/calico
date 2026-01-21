6fdcb109af92bd994c9b730f440199253555ecab
Mazdak Nasab <mazdak.nasab@gmail.com>
2026-01-21 13:00:39 -0800

    fix

--- goldmane/pkg/client/mocks/FlowsClient.go
+++ goldmane/pkg/client/mocks/FlowsClient.go
@@ -7,8 +7,9 @@ package mocks
 import (
 	"context"
 
-	"github.com/projectcalico/calico/goldmane/proto"
 	mock "github.com/stretchr/testify/mock"
+
+	"github.com/projectcalico/calico/goldmane/proto"
 )
 
 // NewFlowsClient creates a new instance of FlowsClient. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
