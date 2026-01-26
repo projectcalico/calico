5faca368193a2a24de97fae047a48eb92042a300
Casey Davenport <davenport.cas@gmail.com>
2026-01-26 09:54:55 -0800

    Move to nftables pkg

--- felix/dataplane/linux/int_dataplane.go
+++ felix/dataplane/linux/int_dataplane.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2025 Tigera, Inc. All rights reserved.
+// Copyright (c) 2020-2026 Tigera, Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -36,7 +36,6 @@ import (
 	"golang.org/x/sys/unix"
 	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 	"k8s.io/client-go/kubernetes"
-	"sigs.k8s.io/knftables"
 
 	"github.com/projectcalico/calico/felix/bpf"
 	"github.com/projectcalico/calico/felix/bpf/bpfmap"
@@ -281,7 +280,7 @@ type Config struct {
 	KubernetesProvider felixconfig.Provider
 
 	// For testing purposes - allows unit tests to mock out the creation of the nftables dataplane.
-	NewNftablesDataplane func(knftables.Family, string, ...knftables.Option) (knftables.Interface, error)
+	NewNftablesDataplane nftables.NewNftablesDataplaneFn
 }
 
 type UpdateBatchResolver interface {
@@ -443,7 +442,7 @@ func NewIntDataplaneDriver(config Config) *InternalDataplane {
 	log.WithField("config", config).Info("Creating internal dataplane driver.")
 
 	// Decide whether to use nftables or iptables based on configuration and kube-proxy mode.
-	detectKubeProxyNftablesMode := kubeProxyNftablesFn(config)
+	detectKubeProxyNftablesMode := nftables.KubeProxyNftablesEnabledFn(config.NewNftablesDataplane)
 	kubeProxyNftablesEnabled, err := detectKubeProxyNftablesMode()
 	if err != nil {
 		log.WithError(err).Panic("Unable to detect kube-proxy nftables mode, shutting down")
@@ -1816,72 +1815,6 @@ func (d *InternalDataplane) monitorKubeProxyNftablesMode() {
 			}).Info("kube-proxy nftables mode changed. Restart felix.")
 			d.config.ConfigChangedRestartCallback()
 		}
-	}
-}
-
-// kubeProxyNftablesFn returns a function that can be used to check if kube-proxy is running
-// in nftables mode. It does this by checking for the presence of the nftables chains that
-// kube-proxy creates when running in nftables mode.
-func kubeProxyNftablesFn(config Config) func() (bool, error) {
-	newDataplane := knftables.New
-	if config.NewNftablesDataplane != nil {
-		newDataplane = config.NewNftablesDataplane
-	}
-
-	// Create v4 and v6 nftables interfaces. If either succeeds, we can use it to check for
-	// the presence of the kube-proxy nftables table.
-	nft, err := newDataplane(knftables.IPv4Family, "kube-proxy")
-	if err != nil {
-		// Don't return an error here - some systems may not have nftables support. We handle
-		// this case in the returned function.
-		log.WithError(err).Warn("Failed to create nftables interface to check kube-proxy mode.")
-	}
-	nftv6, err := newDataplane(knftables.IPv6Family, "kube-proxy")
-	if err != nil {
-		// Don't return an error here - some systems may not have nftables support. We handle
-		// this case in the returned function.
-		log.WithError(err).Warn("Failed to create IPv6 nftables interface to check kube-proxy mode.")
-	}
-
-	// Common function to check for the presence of the kube-proxy nftables table for a given family.
-	checkTable := func(nft knftables.Interface) (bool, error) {
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-
-		objs, err := nft.List(ctx, "chains")
-		if err != nil {
-			if knftables.IsNotFound(err) {
-				// Table not found, kube-proxy is not in nftables mode.
-				return false, nil
-			}
-			log.WithError(err).Warn("Failed to list nftables to check kube-proxy mode.")
-			return false, err
-		}
-
-		// If there are any chains, kube-proxy is in nftables mode.
-		return len(objs) > 0, nil
-	}
-
-	// Return a function that checks both IPv4 and IPv6 tables, depending on which are available.
-	return func() (bool, error) {
-		if nft != nil {
-			ipv4Enabled, err := checkTable(nft)
-			if err != nil {
-				// Failed to check IPv4 table.
-				return false, err
-			} else if ipv4Enabled {
-				// Kube-proxy is in nftables mode.
-				return true, nil
-			}
-			// kube-proxy not in nftables mode for IPv4, maybe check IPv6.
-		}
-		if nftv6 != nil {
-			// Check IPv6 table.
-			return checkTable(nftv6)
-		}
-
-		// Neither IPv4 nor IPv6 nftables interfaces are available, kube-proxy must not be in nftables mode.
-		return false, nil
 	}
 }
 
