b6007e662df2f25dd853b751235671e9cd4b4ec7
Pedro Coutinho <pedro@tigera.io>
2026-02-03 12:52:57 -0800

    [windows] ASO: add support for nftables and BPF dataplanes (on linux nodes)
    Add support for the nftables and BPF dataplanes on linux nodes
    to the ASO test infra.
    
    Remove docker installation as only containerd
    is necessary.
    
    Use a config yaml for kubeadm init instead of CLI flags.

--- process/testing/aso/export-env.sh
+++ process/testing/aso/export-env.sh
@@ -42,6 +42,9 @@ export SSH_KEY_FILE="${ASO_DIR}/.sshkey"
 export GCR_IO_PULL_SECRET="${GCR_IO_PULL_SECRET:="${HOME}/secrets/docker_cfg.json"}"
 export TSEE_TEST_LICENSE="${TSEE_TEST_LICENSE:="${HOME}/secrets/license.yaml"}"
 
+export CALICO_LINUX_DATAPLANE="${CALICO_LINUX_DATAPLANE:="Iptables"}" # 'Iptables', 'Nftables' or 'BPF'
+export KUBE_PROXY_MODE="${KUBE_PROXY_MODE:="iptables"}" # 'iptables' or 'nftables', use 'nftables' with BPF
+
 export PRODUCT="calico"
 export RELEASE_STREAM="master"
 export HASH_RELEASE="true"
