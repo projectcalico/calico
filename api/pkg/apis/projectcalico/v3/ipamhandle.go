21bb924031d538f0bc19eebd20fcbe756bbc03d9
Casey Davenport <davenport.cas@gmail.com>
2026-02-09 14:51:21 -0800

    Add print columns for CRDs in kubectl output

--- api/pkg/apis/projectcalico/v3/ipamhandle.go
+++ api/pkg/apis/projectcalico/v3/ipamhandle.go
@@ -18,6 +18,7 @@ import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:printcolumn:name="ID",type=string,JSONPath=".spec.handleID",description="The handle ID"
 
 // +k8s:openapi-gen=true
 // +kubebuilder:resource:scope=Cluster
