5c983be0dae304c0b3b159b21d9fc9e1b9a58ba6
Alex O'Regan <alex.oregan@tigera.io>
2026-02-11 15:24:04 +0000

    throttle hostmetadata_cache updates

--- felix/dataplane/linux/hostmetadata_cache.go
+++ felix/dataplane/linux/hostmetadata_cache.go
@@ -3,52 +3,56 @@ package intdataplane
 import (
 	"maps"
 	"sync"
+	"time"
 
 	"github.com/projectcalico/calico/felix/proto"
+	"github.com/sirupsen/logrus"
 )
 
 type HostMetadataCache struct {
-	updates map[string]*proto.HostMetadataV4V6Update
-	inSync  bool
+	updates     map[string]*proto.HostMetadataV4V6Update
+	updatesLock sync.Mutex
+	inSync      bool
 
 	onHostUpdateCB func(map[string]*proto.HostMetadataV4V6Update)
 	cbLock         sync.Mutex
+
+	queue chan signal
 }
 
+type signal struct{}
+
 func NewHostMetadataCache() *HostMetadataCache {
-	return &HostMetadataCache{
+	c := &HostMetadataCache{
 		updates: make(map[string]*proto.HostMetadataV4V6Update),
+		queue:   make(chan signal, 1),
 	}
+
+	go c.loopFlushingUpdates()
+	return c
 }
 
 func (c *HostMetadataCache) CompleteDeferredWork() error {
 	c.inSync = true
-	c.sendAllUpdates()
+	logrus.Debug("Now in sync")
+	c.requestUpdate()
 	return nil
 }
 
 func (c *HostMetadataCache) OnUpdate(u any) {
 	switch upd := u.(type) {
 	case *proto.HostMetadataV4V6Update:
+		logrus.WithField("update", upd).Debug("Received HostMetadataV4V6Update message")
 		c.onHostMetadataV4V6Update(upd)
 	case *proto.HostMetadataV4V6Remove:
+		logrus.WithField("update", upd).Debug("Received HostMetadataV4V6Remove message")
 		c.onHostMetadataV4V6Remove(upd)
 	default:
 		return
 	}
 
 	if c.inSync {
-		c.sendAllUpdates()
-	}
-}
-
-func (c *HostMetadataCache) sendAllUpdates() {
-	c.cbLock.Lock()
-	defer c.cbLock.Unlock()
-	if c.onHostUpdateCB != nil {
-		upds := make(map[string]*proto.HostMetadataV4V6Update)
-		maps.Copy(upds, c.updates)
-		c.onHostUpdateCB(upds)
+		c.requestUpdate()
 	}
 }
 
@@ -58,6 +62,43 @@ func (c *HostMetadataCache) onHostMetadataV4V6Update(u *proto.HostMetadataV4V6Up
 
 func (c *HostMetadataCache) onHostMetadataV4V6Remove(u *proto.HostMetadataV4V6Remove) {
 	delete(c.updates, u.Hostname)
+}
+
+func (c *HostMetadataCache) requestUpdate() {
+	select {
+	case c.queue <- signal{}:
+	default:
+	}
+}
+
+func (c *HostMetadataCache) loopFlushingUpdates() {
+	var timer *time.Timer
+	for {
+		<-c.queue
+		c.sendAllUpdates()
+
+		if timer == nil {
+			timer = time.NewTimer(time.Second)
+		} else {
+			_ = timer.Reset(time.Second)
+		}
+		<-timer.C
+	}
+}
+
+func (c *HostMetadataCache) sendAllUpdates() error {
+	c.cbLock.Lock()
+	defer c.cbLock.Unlock()
+	c.updatesLock.Lock()
+	defer c.updatesLock.Lock()
+
+	if c.onHostUpdateCB != nil {
+		upds := make(map[string]*proto.HostMetadataV4V6Update)
+		maps.Copy(upds, c.updates)
+		c.onHostUpdateCB(upds)
+	}
+
+	return nil
 }
 
 func (c *HostMetadataCache) SetOnHostUpdateCB(cb func(map[string]*proto.HostMetadataV4V6Update)) {
