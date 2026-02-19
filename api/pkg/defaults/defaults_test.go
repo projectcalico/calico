dff52caa162b4ab24e0f7137a5dd21b390fe39e8
Casey Davenport <davenport.cas@gmail.com>
2026-02-19 14:53:00 -0800

    Make generation

--- api/pkg/defaults/defaults_test.go
+++ api/pkg/defaults/defaults_test.go
@@ -16,11 +16,9 @@ package defaults_test
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-
 	v3 "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	"github.com/projectcalico/api/pkg/defaults"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe("Default", func() {
