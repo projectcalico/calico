b9d19b34829a3aa86d1764cb5346b394e5737807
Casey Davenport <davenport.cas@gmail.com>
2026-02-20 17:31:16 -0800

    Fix apiserver impl

--- apiserver/pkg/storage/calico/storage_interface.go
+++ apiserver/pkg/storage/calico/storage_interface.go
@@ -59,6 +59,8 @@ func NewStorage(opts Options) (registry.DryRunnableStorage, factory.DestroyFunc)
 		return NewFelixConfigurationStorage(opts)
 	case "projectcalico.org/kubecontrollersconfigurations":
 		return NewKubeControllersConfigurationStorage(opts)
+	case "projectcalico.org/kubecontrollersconfigurations/status":
+		return NewKubeControllersConfigurationStatusStorage(opts)
 	case "projectcalico.org/clusterinformations":
 		return NewClusterInformationStorage(opts)
 	case "projectcalico.org/caliconodestatuses":
@@ -72,3 +74,4 @@ func NewStorage(opts Options) (registry.DryRunnableStorage, factory.DestroyFunc)
 		return registry.DryRunnableStorage{}, nil
 	}
 }
+
