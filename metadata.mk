e4cf645035ea6ebe0806c96abb36dd1b4a1437cd
Jiawei Huang <jiawei@tigera.io>
2026-02-27 17:31:19 -0800

    Upgrade to Kubernetes v1.35 release train

--- metadata.mk
+++ metadata.mk
@@ -9,7 +9,7 @@ RUST_BUILD_VER=1.93.1
 CALICO_BASE_VER=ubi9-1773420533
 
 # Version of Kubernetes to use for tests, rancher/kubectl, and kubectl binary release.
-K8S_VERSION=v1.34.3
+K8S_VERSION=v1.35.2
 
 # Version of various tools used in the build and tests.
 COREDNS_VERSION=1.5.2
@@ -19,7 +19,7 @@ GHR_VERSION=v0.17.0
 GITHUB_CLI_VERSION=2.76.2
 GOTESTSUM_VERSION=v1.12.3
 HELM_VERSION=v3.11.3
-KINDEST_NODE_VERSION=v1.34.3
+KINDEST_NODE_VERSION=v1.35.1
 KIND_VERSION=v0.29.0
 
 # Configuration for Semaphore/Github integration.  This needs to be set
