1d9135d943d4c6bd1de03856b02a1bbb003882c2
Casey Davenport <davenport.cas@gmail.com>
2026-03-23 08:03:29 -0700

    Add oneshot tests for TTL security, BGP filters, and other scenarios
    Add 22 new oneshot test scenarios extracted from bash test functions.
    Each test's inline YAML resources have been extracted into input.yaml
    files under mock_data/calicoctl/.
    
    New test files:
    - ttl_security_test.go: 3 scenarios (explicit_node, peer_selector, global)
    - bgpfilter_test.go: 12 scenarios (single/multi filter, match operators,
      match source, match interface, filter names, import only, node mesh,
      v6 only)
    - extra_oneshot_test.go: 7 scenarios (ignored interfaces, reachable-by,
      felix cluster routing, next hop mode, reverse peering)
    
    All tests pass against both KDD and etcd backends.

---
+++ confd/tests/bgpfilter_test.go
@@ -0,0 +1,48 @@
+// Copyright (c) 2026 Tigera, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tests
+
+import "testing"
+
+func TestBGPFilterTemplates(t *testing.T) {
+	tests := []struct {
+		name      string
+		inputYAML string
+		goldenDir string
+	}{
+		{"single_filter/global_peer", "bgpfilter/single_filter/global_peer/input.yaml", "bgpfilter/single_filter/global_peer"},
+		{"single_filter/explicit_peer", "bgpfilter/single_filter/explicit_peer/input.yaml", "bgpfilter/single_filter/explicit_peer"},
+		{"multi_filter/global_peer", "bgpfilter/multi_filter/global_peer/input.yaml", "bgpfilter/multi_filter/global_peer"},
+		{"multi_filter/explicit_peer", "bgpfilter/multi_filter/explicit_peer/input.yaml", "bgpfilter/multi_filter/explicit_peer"},
+		{"node_mesh", "bgpfilter/node_mesh/input.yaml", "bgpfilter/node_mesh"},
+		{"match_operators", "bgpfilter/match_operators/input.yaml", "bgpfilter/match_operators"},
+		{"match_source", "bgpfilter/match_source/input.yaml", "bgpfilter/match_source"},
+		{"match_interface", "bgpfilter/match_interface/input.yaml", "bgpfilter/match_interface"},
+		{"filter_names", "bgpfilter/filter_names/input.yaml", "bgpfilter/filter_names"},
+		{"import_only/explicit_peer", "bgpfilter/import_only/explicit_peer/input.yaml", "bgpfilter/import_only/explicit_peer"},
+		{"import_only/global_peer", "bgpfilter/import_only/global_peer/input.yaml", "bgpfilter/import_only/global_peer"},
+		{"v6_only/global_peer", "bgpfilter/v6_only/global_peer/input.yaml", "bgpfilter/v6_only/global_peer"},
+	}
+
+	for _, be := range activeBackends {
+		t.Run(be.name, func(t *testing.T) {
+			for _, tc := range tests {
+				t.Run(tc.name, func(t *testing.T) {
+					runConfdTest(t, be, tc.inputYAML, tc.goldenDir)
+				})
+			}
+		})
+	}
+}
