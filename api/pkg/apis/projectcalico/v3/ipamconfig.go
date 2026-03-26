d7e67fc75c6c75e817e4472e6c848a34ab65e31b
Casey Davenport <davenport.cas@gmail.com>
2026-03-26 10:08:15 -0700

    Fix streaming_list_type_json_tags openapi-gen warnings
    Add omitempty to the ListMeta JSON tag on all 23 v3 list types to
    match the upstream Kubernetes convention for streaming list support.
    This resolves the openapi-gen API rule violation warnings emitted
    during `make -C api gen-files`.

--- api/pkg/apis/projectcalico/v3/ipamconfig.go
+++ api/pkg/apis/projectcalico/v3/ipamconfig.go
@@ -30,7 +30,7 @@ const (
 // IPAMConfigurationList contains a list of IPAMConfiguration resources.
 type IPAMConfigurationList struct {
 	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata" protobuf:"bytes,1,opt,name=metadata"`
+	metav1.ListMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
 
 	Items []IPAMConfiguration `json:"items" protobuf:"bytes,2,rep,name=items"`
 }
--- api/pkg/apis/projectcalico/v3/ippool.go
+++ api/pkg/apis/projectcalico/v3/ippool.go
@@ -30,7 +30,7 @@ const (
 // IPPoolList contains a list of IPPool resources.
 type IPPoolList struct {
 	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata" protobuf:"bytes,1,opt,name=metadata"`
+	metav1.ListMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
 
 	Items []IPPool `json:"items" protobuf:"bytes,2,rep,name=items"`
 }
