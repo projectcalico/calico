bedd0cecaf220032e352ee3c7fe48423d09c0f34
marvin-tigera <marvin-tigera@users.noreply.github.com>
2026-04-08 19:26:41 +0000

    Update dependency-updates

--- metadata.mk
+++ metadata.mk
@@ -3,14 +3,14 @@
 #################################################################################################
 
 # The project Go version
-GO_VERSION=1.25.8
+GO_VERSION=1.25.9
 # Version of Kubernetes to use for dependencies, tests, registry.k8s.io/kubectl, and kubectl binary release.
 K8S_VERSION=v1.33.10
 # The version of LLVM to use for go-build and calico/base images.
 LLVM_VERSION=18.1.8
 # The version of calico/go-build and calico/base to use.
 GO_BUILD_VER=$(GO_VERSION)-llvm$(LLVM_VERSION)-k8s$(K8S_VERSION:v%=%)
-CALICO_BASE_VER=ubi9-1757635857
+CALICO_BASE_VER=ubi9-1775601218
 
 # Env var to ACK Ginkgo deprecation warnings, may need updating with go-build.
 ACK_GINKGO=ACK_GINKGO_DEPRECATIONS=1.16.5
