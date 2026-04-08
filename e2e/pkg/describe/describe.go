19100b69fb16db81edf67b1d3dd9a60731cbcd4b
Casey Davenport <davenport.cas@gmail.com>
2026-04-07 21:34:13 -0700

    Add Feature:Goldmane label to staged policy e2e tests
    The staged policy tests depend on Whisker (which requires Goldmane) to
    read flow logs. Add the Goldmane feature label so these tests can be
    skipped on clusters where Goldmane is not installed.

--- e2e/pkg/describe/describe.go
+++ e2e/pkg/describe/describe.go
@@ -75,6 +75,7 @@ var features = map[string]bool{
 	"QoS":             true,
 	"Datapath":        true,
 	"Istio":           true,
+	"Goldmane":        true,
 }
 
 // RequiresNoEncap marks tests that require unencapsulated traffic to function.
