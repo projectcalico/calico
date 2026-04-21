d210221ea0e911ddc583d9f74b88855493319d37
marvin-tigera <marvin-tigera@users.noreply.github.com>
2026-04-21 01:49:35 +0000

    Update dependency-updates

--- metadata.mk
+++ metadata.mk
@@ -4,13 +4,13 @@
 # The project Go version
 GO_VERSION=1.25.9
 # Version of Kubernetes to use for dependencies, tests, registry.k8s.io/kubectl, and kubectl binary release.
-K8S_VERSION=v1.35.3
+K8S_VERSION=v1.35.4
 # The version of LLVM to use for go-build and calico/base images.
 LLVM_VERSION=18.1.8
 # The version of calico/go-build and calico/base to use.
 GO_BUILD_VER=$(GO_VERSION)-llvm$(LLVM_VERSION)-k8s$(K8S_VERSION:v%=%)
 RUST_BUILD_VER=1.93.1
-CALICO_BASE_VER=ubi9-1771532994
+CALICO_BASE_VER=ubi9-1776708455
 
 # Version of various tools used in the build and tests.
 COREDNS_VERSION=1.5.2
