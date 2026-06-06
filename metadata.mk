5ac2bd38c40d3494b6b92a3450576985a710dedb
marvin-tigera <marvin-tigera@users.noreply.github.com>
2026-06-06 01:03:08 +0000

    Update dependency-updates

--- metadata.mk
+++ metadata.mk
@@ -2,7 +2,7 @@
 # This file contains Makefile configuration parameters and metadata for this branch.
 #################################################################################################
 # The project Go version
-GO_VERSION=1.26.3
+GO_VERSION=1.26.4
 # Version of Kubernetes to use for dependencies, tests, registry.k8s.io/kubectl, and kubectl binary release.
 K8S_VERSION=v1.36.1
 # The version of LLVM to use for go-build and calico/base images.
@@ -11,7 +11,7 @@ LLVM_VERSION=21.1.8
 GO_BUILD_VER=$(GO_VERSION)-llvm$(LLVM_VERSION)-k8s$(K8S_VERSION:v%=%)
 RUST_BUILD_VER=1.94.1
 
-CALICO_BASE_VER=ubi9-1779935431
+CALICO_BASE_VER=ubi9-1780507355
 
 # Version of various tools used in the build and tests.
 COREDNS_VERSION=1.5.2
