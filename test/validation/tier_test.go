deb97137c59083d2533d2d868be91c6fdc868049
Mazdak Nasab <mazdak.nasab@gmail.com>
2026-07-21 10:36:45 -0700

    update

--- test/validation/tier_test.go
+++ test/validation/tier_test.go
@@ -20,7 +20,7 @@ import (
 	"testing"
 	"time"
 
-	v3 "github.com/tigera/api/pkg/apis/projectcalico/v3"
+	v3 "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/ptr"
