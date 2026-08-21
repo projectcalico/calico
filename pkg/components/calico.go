146f906d645443385a3b9b699ed5eb979a2b0d49
Daniel Fox <dan.fox@tigera.io>
2026-08-20 17:21:13 -0700

    [release-v1.40]  Auto Release Update for v1.40.15 (#5235)

--- pkg/components/calico.go
+++ pkg/components/calico.go
@@ -20,154 +20,154 @@ package components
 import "github.com/tigera/operator/version"
 
 var (
-	CalicoRelease string = "v3.31.6"
+	CalicoRelease string = "v3.31.7"
 
 	ComponentCalicoCNI = Component{
-		Version:  "v3.31.6",
+		Version:  "v3.31.7",
 		Image:    "calico/cni",
 		Registry: "",
 	}
 
 	ComponentCalicoCNIFIPS = Component{
-		Version:  "v3.31.6-fips",
+		Version:  "v3.31.7-fips",
 		Image:    "calico/cni",
 		Registry: "",
 	}
 
 	ComponentCalicoCNIWindows = Component{
-		Version:  "v3.31.6",
+		Version:  "v3.31.7",
 		Image:    "calico/cni-windows",
 		Registry: "",
 	}
 
 	ComponentCalicoCSRInitContainer = Component{
-		Version:  "v3.31.6",
+		Version:  "v3.31.7",
 		Image:    "calico/key-cert-provisioner",
 		Registry: "",
 	}
 
 	ComponentCalicoKubeControllers = Component{
-		Version:  "v3.31.6",
+		Version:  "v3.31.7",
 		Image:    "calico/kube-controllers",
 		Registry: "",
 	}
 
 	ComponentCalicoKubeControllersFIPS = Component{
-		Version:  "v3.31.6-fips",
+		Version:  "v3.31.7-fips",
 		Image:    "calico/kube-controllers",
 		Registry: "",
 	}
 
 	ComponentCalicoNode = Component{
-		Version:  "v3.31.6",
+		Version:  "v3.31.7",
 		Image:    "calico/node",
 		Registry: "",
 	}
 
 	ComponentCalicoNodeFIPS = Component{
-		Version:  "v3.31.6-fips",
+		Version:  "v3.31.7-fips",
 		Image:    "calico/node",
 		Registry: "",
 	}
 
 	ComponentCalicoNodeWindows = Component{
-		Version:  "v3.31.6",
+		Version:  "v3.31.7",
 		Image:    "calico/node-windows",
 		Registry: "",
 	}
 
 	ComponentCalicoTypha = Component{
-		Version:  "v3.31.6",
+		Version:  "v3.31.7",
 		Image:    "calico/typha",
 		Registry: "",
 	}
 
 	ComponentCalicoTyphaFIPS = Component{
-		Version:  "v3.31.6-fips",
+		Version:  "v3.31.7-fips",
 		Image:    "calico/typha",
 		Registry: "",
 	}
 
 	ComponentCalicoFlexVolume = Component{
-		Version:  "v3.31.6",
+		Version:  "v3.31.7",
 		Image:    "calico/pod2daemon-flexvol",
 		Registry: "",
 	}
 
 	ComponentCalicoAPIServer = Component{
-		Version:  "v3.31.6",
+		Version:  "v3.31.7",
 		Image:    "calico/apiserver",
 		Registry: "",
 	}
 
 	ComponentCalicoAPIServerFIPS = Component{
-		Version:  "v3.31.6-fips",
+		Version:  "v3.31.7-fips",
 		Image:    "calico/apiserver",
 		Registry: "",
 	}
 
 	ComponentCalicoCSI = Component{
-		Version:  "v3.31.6",
+		Version:  "v3.31.7",
 		Image:    "calico/csi",
 		Registry: "",
 	}
 
 	ComponentCalicoCSIFIPS = Component{
-		Version:  "v3.31.6-fips",
+		Version:  "v3.31.7-fips",
 		Image:    "calico/csi",
 		Registry: "",
 	}
 
 	ComponentCalicoCSIRegistrar = Component{
-		Version:  "v3.31.6",
+		Version:  "v3.31.7",
 		Image:    "calico/node-driver-registrar",
 		Registry: "",
 	}
 
 	ComponentCalicoCSIRegistrarFIPS = Component{
-		Version:  "v3.31.6-fips",
+		Version:  "v3.31.7-fips",
 		Image:    "calico/node-driver-registrar",
 		Registry: "",
 	}
 
 	ComponentCalicoGoldmane = Component{
-		Version:  "v3.31.6",
+		Version:  "v3.31.7",
 		Image:    "calico/goldmane",
 		Registry: "",
 	}
 
 	ComponentCalicoWhisker = Component{
-		Version:  "v3.31.6",
+		Version:  "v3.31.7",
 		Image:    "calico/whisker",
 		Registry: "",
 	}
 
 	ComponentCalicoWhiskerBackend = Component{
-		Version:  "v3.31.6",
+		Version:  "v3.31.7",
 		Image:    "calico/whisker-backend",
 		Registry: "",
 	}
 
 	ComponentCalicoEnvoyGateway = Component{
-		Version:  "v3.31.6",
+		Version:  "v3.31.7",
 		Image:    "calico/envoy-gateway",
 		Registry: "",
 	}
 
 	ComponentCalicoEnvoyProxy = Component{
-		Version:  "v3.31.6",
+		Version:  "v3.31.7",
 		Image:    "calico/envoy-proxy",
 		Registry: "",
 	}
 
 	ComponentCalicoEnvoyRatelimit = Component{
-		Version:  "v3.31.6",
+		Version:  "v3.31.7",
 		Image:    "calico/envoy-ratelimit",
 		Registry: "",
 	}
 
 	ComponentCalicoGuardian = Component{
-		Version:  "v3.31.6",
+		Version:  "v3.31.7",
 		Image:    "calico/guardian",
 		Registry: "",
 	}
